4b5913bdf9bcc01f68e17e0f421145d2a9242e7b
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-04 14:07:34 +0100

    Add commanded

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Tronto.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:commanded, "~> 1.3"}
     ]
   end
 
