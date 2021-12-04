39594af037621ba47d18f8a5c544a5ceee421225
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-04 15:30:16 +0100

    Add docker-compose

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :tronto, Tronto.Repo,
   password: "postgres",
   database: "tronto_dev",
   hostname: "localhost",
+  port: 5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
