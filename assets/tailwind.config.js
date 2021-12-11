f2fe33051fa32f3cc5498354e299a3adb338bbf1
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-11 13:19:10 +0100

    Add branding colors (#14)

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   purge: ['./js/**/*.js', '../lib/*_web/**/*.*ex'],
   theme: {
+    extend: {
+      colors: {
+        'pine-green': '#0C322C',
+        'jungle-green': '#30BA78',
+        'midnight-blue': '#192072',
+        'waterhole-blue': '#2453FF',
+        'mint': '#90EBCD',
+        'persimmon': '#FE7C3F',
+        'fog': '#EFEFEF',
+      },
+    },  
     fontFamily: {
       sans: ['Lato'],
     },
