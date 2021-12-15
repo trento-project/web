d0b6e1a1205c5c1846a9cef237ad732dd9a47833
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-15 09:28:57 +0100

    Heartbeat part I (#18)
    * Add quantum dependency
    
    * Add Tronto Scheduler
    
    * Add heartbeat context and schema
    
    * Start and supervise scheduler
    
    * Wire-up and config tronto scheduler and heartbeat
    
    * Silence ecto logs in dev
    
    * Add mock to deps
    
    * Add Faker to the mix
    
    * Add heartbeats tests
    
    * Add specs, fix formatting
    
    * Add credo config
    
    * Add local without parens formatting for assert_called macro

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,9 @@
 [
   import_deps: [:ecto, :phoenix],
+  locals_without_parens: [
+    # mock
+    assert_called: :*
+  ],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
 ]
---
+++ priv/repo/migrations/20211212192656_create_heartbeats.exs
@@ -0,0 +1,10 @@
+defmodule Tronto.Repo.Migrations.CreateHeartbeats do
+  use Ecto.Migration
+
+  def change do
+    create table(:heartbeats, primary_key: false) do
+      add :agent_id, :string, primary_key: true
+      add :timestamp, :utc_datetime_usec
+    end
+  end
+end
