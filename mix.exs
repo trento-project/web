0632f1efe20286f3ef89040f5480e67d142755aa
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-26 14:01:58 +0100

    Fix formatting

--- mix.exs
+++ mix.exs
@@ -30,7 +30,9 @@ defmodule Tronto.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support", "deps/commanded/test/support/aggregate_case.ex"]
+  defp elixirc_paths(:test),
+    do: ["lib", "test/support", "deps/commanded/test/support/aggregate_case.ex"]
+
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
