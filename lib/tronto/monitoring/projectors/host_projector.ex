178612e89adb07a8c9e96c73085f0398abaa120c
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-28 16:13:03 +0100

    Add and wire-up host projector

---
+++ lib/tronto/monitoring/projectors/host_projector.ex
@@ -0,0 +1,34 @@
+defmodule Tronto.Monitoring.HostProjector do
+  @moduledoc """
+  Host projector
+  """
+
+  use Commanded.Projections.Ecto,
+    application: Tronto.Commanded,
+    repo: Tronto.Repo,
+    name: "host_projector"
+
+  alias Tronto.Monitoring.Domain.Events.HostRegistered
+  alias Tronto.Monitoring.HostReadModel
+
+  project(
+    %HostRegistered{
+      id_host: id,
+      hostname: hostname,
+      ip_addresses: ip_addresses,
+      agent_version: agent_version
+    },
+    fn multi ->
+      changeset =
+        %HostReadModel{}
+        |> HostReadModel.changeset(%{
+          id: id,
+          hostname: hostname,
+          ip_addresses: ip_addresses,
+          agent_version: agent_version
+        })
+
+        Ecto.Multi.insert(multi, :host, changeset)
+    end
+  )
+end
---
+++ lib/tronto/monitoring/projectors/projectors_supervisor.ex
@@ -0,0 +1,18 @@
+defmodule Tronto.Monitoring.ProjectorsSupervisor do
+  @moduledoc false
+
+  use Supervisor
+
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_init_arg) do
+    children = [
+      Tronto.Monitoring.HostProjector
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
