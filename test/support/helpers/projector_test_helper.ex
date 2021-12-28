3dd1cee6394b6f54b48779130a0590b79020d648
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-28 16:13:59 +0100

    Add projectors test helper

---
+++ test/support/helpers/projector_test_helper.ex
@@ -0,0 +1,28 @@
+defmodule Tronto.ProjectorTestHelper do
+  @moduledoc """
+  This module contains helper functions for testing projectors
+  """
+
+  def project(projector, event, projection_name) do
+    :ok =
+      projector.handle(event, %{
+        event_number: next_event_number(projector, projection_name),
+        handler_name: projection_name
+      })
+  end
+
+  defp next_event_number(projector, projection_name), do: last_seen_event_number(projector, projection_name) + 1
+
+  defp last_seen_event_number(projector, projection_name) do
+    projector
+    |> Module.concat(ProjectionVersion)
+    |> Tronto.Repo.get(projection_name)
+    |> case do
+      nil ->
+        0
+
+      projection_version ->
+        Map.get(projection_version, :last_seen_event_number)
+    end
+  end
+end
