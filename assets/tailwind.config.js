d49348e7456c8be5d6eb8de752aabfb990650c80
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-28 11:10:30 +0100

    Fix tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,8 +1,3 @@
-const Color = require('color');
-const alpha = (clr, val) => Color(clr).alpha(val).rgb().string();
-const lighten = (clr, val) => Color(clr).lighten(val).rgb().string();
-const darken = (clr, val) => Color(clr).darken(val).rgb().string();
-
 module.exports = {
   purge: ['./js/**/*.js', '../lib/*_web/**/*.*ex'],
   theme: {
