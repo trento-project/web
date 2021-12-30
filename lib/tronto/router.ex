e9b24834518361beef0253dd68968095e61f7471
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-30 14:43:43 +0100

    Add validation middleware

--- lib/tronto/router.ex
+++ lib/tronto/router.ex
@@ -1,12 +1,16 @@
 defmodule Tronto.Router do
   use Commanded.Commands.Router
 
+  alias Tronto.Support.Middleware.Validate
+
   alias Tronto.Monitoring.Domain.Host
 
   alias Tronto.Monitoring.Domain.Commands.{
     RegisterHost
   }
 
-  identify(Host, by: :id_host)
-  dispatch(RegisterHost, to: Host)
+  middleware Validate
+
+  identify Host, by: :id_host
+  dispatch RegisterHost, to: Host
 end
