29d8792f9d0aa1b1f77322b3cd4583aa097689d9
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-01-02 12:13:04 +0100

    Add RegisterHost command test (#42)

---
+++ test/tronto/monitoring/domain/host/commands/register_host_test.exs
@@ -0,0 +1,19 @@
+defmodule Tronto.Support.Middleware.RegisterHostTest do
+  use ExUnit.Case
+
+  alias Tronto.Monitoring.Domain.Commands.RegisterHost
+
+  @moduletag :unit
+
+  test "should not validate if id_host is not a valid uuid" do
+    command =
+      RegisterHost.new!(%{
+        id_host: Faker.StarWars.character(),
+        hostname: Faker.String.naughty(),
+        ip_addresses: [],
+        agent_version: Faker.App.version()
+      })
+
+    assert not Vex.valid?(command)
+  end
+end
