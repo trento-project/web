fc8443077d6b11dec8a35ccf5aa63692edd244ec
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-01-04 15:01:37 +0100

    Add hosts seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -17,3 +17,18 @@
   confirm_password: "secret1234"
 })
 |> Tronto.Repo.insert!()
+
+agent_version = Faker.App.semver()
+Enum.each(
+  0..5,
+  fn _ ->
+    %{
+      id_host: Faker.UUID.v4(),
+      hostname: Faker.StarWars.character() |> Macro.underscore() |> String.replace(" ", ""),
+      ip_addresses: [Faker.Internet.ip_v4_address()],
+      agent_version: agent_version,
+    }
+    |> Tronto.Monitoring.Domain.Commands.RegisterHost.new!()
+    |> Tronto.Commanded.dispatch()
+  end
+)
