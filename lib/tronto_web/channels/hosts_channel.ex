c0c3b00fbca87e5ee07ffa9fe83d7c9e5be7aa0a
Alessio Biancalana <dottorblaster@gmail.com>
2022-01-06 12:57:44 +0100

    Fix dialyzer for real lol

--- lib/tronto_web/channels/hosts_channel.ex
+++ lib/tronto_web/channels/hosts_channel.ex
@@ -6,22 +6,11 @@ defmodule TrontoWeb.HostsChannel do
   use TrontoWeb, :channel
 
   @impl true
-  def join("hosts:notifications", payload, socket) do
-    case authorized?(payload) do
-      true ->
-        {:ok, socket}
-
-        # _ ->
-        # {:error, %{reason: "unauthorized"}}
-    end
-  end
+  def join("hosts:notifications", payload, socket), do: {:ok, socket}
 
   @impl true
   def handle_in("host_registered", payload, socket) do
     broadcast!(socket, "host_registered", payload)
     {:noreply, socket}
   end
-
-  # TODO: Add authorization logic here as required.
-  defp authorized?(_), do: true
 end
