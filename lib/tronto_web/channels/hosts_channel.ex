81c8c1fa3a679b2748dbd31779271fb2420fa0c9
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-01-10 11:05:10 +0100

    Remove unused channel in function clause

--- lib/tronto_web/channels/hosts_channel.ex
+++ lib/tronto_web/channels/hosts_channel.ex
@@ -7,10 +7,4 @@ defmodule TrontoWeb.HostsChannel do
 
   @impl true
   def join("hosts:notifications", _payload, socket), do: {:ok, socket}
-
-  @impl true
-  def handle_in("host_registered", payload, socket) do
-    broadcast!(socket, "host_registered", payload)
-    {:noreply, socket}
-  end
 end
