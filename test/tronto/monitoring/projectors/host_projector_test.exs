1d793dc59396ba299f4dddde6a93ed602bd4ed95
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-01-26 12:53:57 +0000

    Add HostDetailsUpdated host_projector tests (#84)

--- test/tronto/monitoring/projectors/host_projector_test.exs
+++ test/tronto/monitoring/projectors/host_projector_test.exs
@@ -10,6 +10,7 @@ defmodule Tronto.Monitoring.HostProjectorTest do
   alias Tronto.Monitoring.Domain.Events.{
     HeartbeatFailed,
     HeartbeatSucceded,
+    HostDetailsUpdated,
     HostRegistered
   }
 
@@ -37,6 +38,31 @@ defmodule Tronto.Monitoring.HostProjectorTest do
     assert event.heartbeat == host_projection.heartbeat
   end
 
+  test "should update an existing host when HostDetailsUpdated event is received" do
+    Repo.insert!(%HostReadModel{
+      id: id_host = Faker.UUID.v4(),
+      hostname: Faker.StarWars.character(),
+      ip_addresses: [Faker.Internet.ip_v4_address()],
+      agent_version: Faker.StarWars.planet(),
+      heartbeat: :unknown
+    })
+
+    event = %HostDetailsUpdated{
+      id_host: id_host,
+      hostname: hostname = Faker.StarWars.character(),
+      ip_addresses: [Faker.Internet.ip_v4_address()],
+      agent_version: Faker.StarWars.planet()
+    }
+
+    ProjectorTestHelper.project(HostProjector, event, "host_projector")
+    host_projection = Repo.get!(HostReadModel, event.id_host)
+
+    assert event.id_host == host_projection.id
+    assert event.hostname == host_projection.hostname
+    assert event.ip_addresses == host_projection.ip_addresses
+    assert event.agent_version == host_projection.agent_version
+  end
+
   test "should update the heartbeat field to passing status when HeartbeatSucceded is received" do
     Repo.insert!(%HostReadModel{
       id: id_host = Faker.UUID.v4(),
@@ -56,7 +82,7 @@ defmodule Tronto.Monitoring.HostProjectorTest do
     assert :passing == host_projection.heartbeat
   end
 
-  test "should update the heartbeat field to critical status when HeartbeatSucceded is received" do
+  test "should update the heartbeat field to critical status when HeartbeatFailed is received" do
     Repo.insert!(%HostReadModel{
       id: id_host = Faker.UUID.v4(),
       hostname: Faker.StarWars.character(),
