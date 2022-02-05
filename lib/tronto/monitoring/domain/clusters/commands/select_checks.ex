3cdb6094bf7ad3fde4705d703a47af19a8e996ac
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-02-05 16:03:30 +0100

    Add checks related command and events

---
+++ lib/tronto/monitoring/domain/clusters/commands/select_checks.ex
@@ -0,0 +1,19 @@
+defmodule Tronto.Monitoring.Domain.Commands.SelectChecks do
+  @moduledoc """
+    Select the checks to be executed in the cluster.
+  """
+
+  use TypedStruct
+  use Domo
+
+  typedstruct do
+    @typedoc "SelecteChecks command"
+
+    field :cluster_id, String.t(), enforce: true
+    field :checks, [String.t()], enforce: true
+  end
+
+  use Vex.Struct
+
+  validates :cluster_id, uuid: true
+end
