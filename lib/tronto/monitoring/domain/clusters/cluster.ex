e00ebe61adcbfd9803f1bf14b53231eff5a2cfeb
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-02-05 16:04:13 +0100

    Update cluster aggregate to handle checks business logic

--- lib/tronto/monitoring/domain/clusters/cluster.ex
+++ lib/tronto/monitoring/domain/clusters/cluster.ex
@@ -1,11 +1,22 @@
 defmodule Tronto.Monitoring.Domain.Cluster do
   @moduledoc false
 
-  alias Tronto.Monitoring.Domain.Cluster
+  alias Tronto.Monitoring.Domain.{
+    Cluster,
+    CheckResult
+  }
 
-  alias Tronto.Monitoring.Domain.Commands.RegisterCluster
+  alias Tronto.Monitoring.Domain.Commands.{
+    RegisterCluster,
+    RequestChecksExecution,
+    SelectChecks,
+    StoreChecksResults
+  }
 
   alias Tronto.Monitoring.Domain.Events.{
+    ChecksExecutionRequested,
+    ChecksResultsStored,
+    ChecksSelected,
     ClusterDetailsUpdated,
     ClusterRegistered,
     HostAddedToCluster
@@ -16,7 +27,9 @@ defmodule Tronto.Monitoring.Domain.Cluster do
     :name,
     :type,
     :sid,
-    hosts: []
+    hosts: [],
+    selected_checks: [],
+    hosts_checks_results: %{}
   ]
 
   @type t :: %__MODULE__{
@@ -24,7 +37,9 @@ defmodule Tronto.Monitoring.Domain.Cluster do
           name: [String.t()],
           type: :hana_scale_up | :hana_scale_out | :unknown,
           sid: String.t(),
-          hosts: [String.t()]
+          hosts: [String.t()],
+          selected_checks: [String.t()],
+          hosts_checks_results: %{String.t() => [CheckResult.t()]}
         }
 
   # New cluster registered
@@ -92,6 +107,65 @@ defmodule Tronto.Monitoring.Domain.Cluster do
     ] ++ maybe_emit_host_added_to_cluster_event(cluster, host_id)
   end
 
+  # Cluster does not exist
+  def execute(%Cluster{cluster_id: nil}, _), do: {:error, :cluster_does_not_exist}
+
+  # Checks selected
+  def execute(
+        %Cluster{
+          cluster_id: cluster_id,
+          hosts: hosts
+        },
+        %SelectChecks{
+          checks: selected_checks
+        }
+      ) do
+    [
+      %ChecksSelected{
+        cluster_id: cluster_id,
+        checks: selected_checks
+      },
+      %ChecksExecutionRequested{
+        cluster_id: cluster_id,
+        hosts: hosts,
+        checks: selected_checks
+      }
+    ]
+  end
+
+  # Request checks execution
+  def execute(
+        %Cluster{
+          cluster_id: cluster_id,
+          hosts: hosts,
+          selected_checks: selected_checks
+        },
+        %RequestChecksExecution{cluster_id: cluster_id}
+      ) do
+    %ChecksExecutionRequested{
+      cluster_id: cluster_id,
+      hosts: hosts,
+      checks: selected_checks
+    }
+  end
+
+  # Store checks results
+  def execute(
+        %Cluster{
+          cluster_id: cluster_id
+        },
+        %StoreChecksResults{
+          host_id: host_id,
+          checks_results: checks_results
+        }
+      ) do
+    %ChecksResultsStored{
+      cluster_id: cluster_id,
+      host_id: host_id,
+      checks_results: checks_results
+    }
+  end
+
   def apply(
         %Cluster{} = cluster,
         %ClusterRegistered{
@@ -135,6 +209,49 @@ defmodule Tronto.Monitoring.Domain.Cluster do
     %Cluster{
       cluster
       | hosts: [host_id | hosts]
+    }
+  end
+
+  def apply(
+        %Cluster{} = cluster,
+        %ChecksSelected{
+          checks: selected_checks
+        }
+      ) do
+    %Cluster{
+      cluster
+      | selected_checks: selected_checks
+    }
+  end
+
+  def apply(
+        %Cluster{selected_checks: selected_checks, hosts: hosts} = cluster,
+        %ChecksExecutionRequested{}
+      ) do
+    hosts_checks_results =
+      Enum.reduce(hosts, %{}, fn host, acc ->
+        Map.put(
+          acc,
+          host,
+          Enum.map(selected_checks, fn check_id ->
+            %CheckResult{check_id: check_id, result: :running}
+          end)
+        )
+      end)
+
+    %Cluster{
+      cluster
+      | hosts_checks_results: hosts_checks_results
+    }
+  end
+
+  def apply(
+        %Cluster{hosts_checks_results: hosts_checks_results} = cluster,
+        %ChecksResultsStored{host_id: host_id, checks_results: checks_results}
+      ) do
+    %Cluster{
+      cluster
+      | hosts_checks_results: Map.put(hosts_checks_results, host_id, checks_results)
     }
   end
 
