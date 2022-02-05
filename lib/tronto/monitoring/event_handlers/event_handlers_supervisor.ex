4efa041f69b856dc72a90c9f59b08f3d65453a59
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-02-05 16:06:51 +0100

    Add checks execution event handler

---
+++ lib/tronto/monitoring/event_handlers/event_handlers_supervisor.ex
@@ -0,0 +1,18 @@
+defmodule Tronto.Monitoring.EventHandlersSupervisor do
+  @moduledoc false
+
+  use Supervisor
+
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_init_arg) do
+    children = [
+      Tronto.Monitoring.ChecksEventsHandler
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
