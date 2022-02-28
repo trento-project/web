f9bf4b134b8ccfbf681e35e482e89f716976b831
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-02-28 14:27:35 +0100

    Add prod release (#131)

--- config/runtime.exs
+++ config/runtime.exs
@@ -18,7 +18,18 @@ if config_env() == :prod do
     # ssl: true,
     # socket_options: [:inet6],
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    pool_size: String.to_integer(System.get_env("DATABASE_POOL_SIZE") || "10")
+
+  evenstore_url =
+    System.get_env("EVENTSTORE_URL") ||
+      raise """
+      environment variable EVENTSTORE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  config :tronto, Tronto.EventStore,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("EVENTSTORE_POOL_SIZE") || "10")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
