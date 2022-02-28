2a39dca9c92b251a12ebf9ad76ee175e2bc3fa9e
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-02-28 14:33:36 +0100

    Use factory in host projector test

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -3,7 +3,10 @@ defmodule Tronto.Factory do
   A simple Factory helper module to be used within tests to generate test data
   """
 
+  alias Tronto.Repo
+
   alias Tronto.Monitoring.Domain.Events.HostRegistered
+  alias Tronto.Monitoring.HostReadModel
 
   def host_registered_event(attrs \\ []) do
     %HostRegistered{
@@ -13,5 +16,19 @@ defmodule Tronto.Factory do
       agent_version: Keyword.get(attrs, :agent_version, Faker.App.semver()),
       heartbeat: :unknown
     }
+  end
+
+  def host_projection(attrs \\ []) do
+    host_projection = %HostReadModel{
+      id: Keyword.get(attrs, :id, Faker.UUID.v4()),
+      hostname: Faker.StarWars.character(),
+      ip_addresses: [Faker.Internet.ip_v4_address()],
+      agent_version: Faker.StarWars.planet(),
+      heartbeat: :unknown
+    }
+
+    Repo.insert!(host_projection)
+
+    host_projection
   end
 end
