82bff6a6fee56a280e63126c75dbb757a137cdef
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-03-05 14:13:53 +0100

    Add SAP System and Database projectors

---
+++ lib/tronto/monitoring/projectors/sap_system_projector.ex
@@ -0,0 +1,64 @@
+defmodule Tronto.Monitoring.SapSystemProjector do
+  @moduledoc """
+  SAP System projector
+  """
+
+  use Commanded.Projections.Ecto,
+    application: Tronto.Commanded,
+    repo: Tronto.Repo,
+    name: "sap_system_projector"
+
+  alias Tronto.Monitoring.Domain.Events.{
+    ApplicationInstanceRegistered,
+    SapSystemRegistered
+  }
+
+  alias Tronto.Monitoring.{
+    ApplicationInstanceReadModel,
+    SapSystemReadModel
+  }
+
+  project(
+    %SapSystemRegistered{
+      sap_system_id: sap_system_id,
+      sid: sid,
+      tenant: tenant,
+      db_host: db_host
+    },
+    fn multi ->
+      changeset =
+        %SapSystemReadModel{}
+        |> SapSystemReadModel.changeset(%{
+          id: sap_system_id,
+          sid: sid,
+          tenant: tenant,
+          db_host: db_host
+        })
+
+      Ecto.Multi.insert(multi, :sap_system, changeset)
+    end
+  )
+
+  project(
+    %ApplicationInstanceRegistered{
+      sap_system_id: sap_system_id,
+      sid: sid,
+      instance_number: instance_number,
+      features: features,
+      host_id: host_id
+    },
+    fn multi ->
+      changeset =
+        %ApplicationInstanceReadModel{}
+        |> ApplicationInstanceReadModel.changeset(%{
+          sap_system_id: sap_system_id,
+          sid: sid,
+          instance_number: instance_number,
+          features: features,
+          host_id: host_id
+        })
+
+      Ecto.Multi.insert(multi, :application_instance, changeset)
+    end
+  )
+end
