fe205507d10469d4b5cd65221a0f763155f9cbe2
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-03-08 14:55:35 +0000

    Allow dropdowns to expand beyond table div

--- assets/js/components/Table/Table.jsx
+++ assets/js/components/Table/Table.jsx
@@ -28,8 +28,8 @@ const Table = ({ config, data = [] }) => {
   return (
     <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
       <div className="py-8">
-        <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-          <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+        <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4">
+          <div className="inline-block min-w-full shadow rounded-lg">
             <table className="min-w-full leading-normal">
               <thead>
                 <tr>
