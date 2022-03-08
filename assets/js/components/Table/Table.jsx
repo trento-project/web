7ae052c12ff34e62220637596e7cb6569d52d488
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-03-08 15:19:22 +0000

    Make table headers look a bit better

--- assets/js/components/Table/Table.jsx
+++ assets/js/components/Table/Table.jsx
@@ -26,10 +26,10 @@ const Table = ({ config, data = [] }) => {
   const { columns } = config;
 
   return (
-    <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
+    <div className="container mx-auto px-4 sm:px-8 max-w-4xl">
       <div className="py-8">
         <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4">
-          <div className="inline-block min-w-full shadow rounded-lg">
+          <div className="inline-block min-w-fit shadow rounded-lg">
             <table className="min-w-full leading-normal">
               <thead>
                 <tr>
@@ -37,7 +37,7 @@ const Table = ({ config, data = [] }) => {
                     <th
                       key={title}
                       scope="col"
-                      className="px-5 py-3 bg-white border-b border-gray-200 text-gray-800 text-left text-sm uppercase font-normal"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
                       {title}
                     </th>
