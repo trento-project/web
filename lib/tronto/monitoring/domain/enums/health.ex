9e08d909111d8be1901c2b87a28ba2f3a86ab552
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-03-09 08:46:01 +0100

    Add health enum type

---
+++ lib/tronto/monitoring/domain/enums/health.ex
@@ -0,0 +1,7 @@
+defmodule Tronto.Monitoring.Domain.Health do
+  @moduledoc """
+  Type that represents the possible health values in the system.
+  """
+
+  @type t :: :passing | :warning | :critical | :unknown
+end
