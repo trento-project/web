0f3fb3764dc615602b20ff60230f7e6054ec4b39
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-03-09 12:41:59 +0100

    Add preload

--- lib/tronto/monitoring/monitoring.ex
+++ lib/tronto/monitoring/monitoring.ex
@@ -87,15 +87,15 @@ defmodule Tronto.Monitoring do
   def get_all_sap_systems do
     SapSystemReadModel
     |> Repo.all()
-    |> Repo.preload(application_instances: :host)
-    |> Repo.preload(database_instances: :host)
+    |> Repo.preload(application_instances: [host: :cluster])
+    |> Repo.preload(database_instances: [host: :cluster])
   end
 
   @spec get_all_databases :: [map]
   def get_all_databases do
     DatabaseReadModel
     |> Repo.all()
-    |> Repo.preload(database_instances: :host)
+    |> Repo.preload(database_instances: [host: :cluster])
   end
 
   @spec build_check_results([String.t()]) :: {:ok, [CheckResult.t()]} | {:error, any}
