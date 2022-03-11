258f31b43d22d71cecabf53f89aa6d417eb2fa71
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-03-11 14:27:14 +0100

    CHORE: remove seeds for clusters and hosts, let's use photofinish with a real cluster scenario ;)

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -21,34 +21,3 @@ FunWithFlags.enable(:convert_agent_ids)
   confirm_password: "secret1234"
 })
 |> Tronto.Repo.insert!()
-
-agent_version = Faker.App.semver()
-
-Enum.each(
-  0..5,
-  fn index ->
-    %{
-      host_id: generate_sequential_uuid(index),
-      hostname: Faker.StarWars.character() |> Macro.underscore() |> String.replace(" ", ""),
-      ip_addresses: [Faker.Internet.ip_v4_address()],
-      agent_version: agent_version
-    }
-    |> Tronto.Monitoring.Domain.Commands.RegisterHost.new!()
-    |> Tronto.Commanded.dispatch()
-  end
-)
-
-Enum.each(
-  6..11,
-  fn index ->
-    %{
-      cluster_id: generate_sequential_uuid(index),
-      host_id: generate_sequential_uuid(index - 6),
-      name: Faker.StarWars.character() |> Macro.underscore() |> String.replace(" ", ""),
-      sid: "PRD",
-      type: :hana_scale_up
-    }
-    |> Tronto.Monitoring.Domain.Commands.RegisterCluster.new!()
-    |> Tronto.Commanded.dispatch()
-  end
-)
