d75ae0ae4962c08c1c3ee36cf3e6cb8ed1f0d8dc
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-03-11 16:39:06 +0100

    Rename RegisterCluster command to RegisterClusterHost

--- lib/tronto/monitoring/domain/clusters/commands/register_cluster_host.ex
+++ lib/tronto/monitoring/domain/clusters/commands/register_cluster_host.ex
@@ -1,19 +1,20 @@
-defmodule Tronto.Monitoring.Domain.Commands.RegisterCluster do
+defmodule Tronto.Monitoring.Domain.Commands.RegisterClusterHost do
   @moduledoc """
-  Register a cluster to the monitoring system.
+  Register a cluster node to the monitoring system.
   """
 
   use TypedStruct
   use Domo
 
   typedstruct do
-    @typedoc "RegisterCluster command"
+    @typedoc "RegisterClusterHost command"
 
     field :cluster_id, String.t(), enforce: true
     field :host_id, String.t(), enforce: true
     field :name, String.t(), enforce: true
     field :type, :hana_scale_up | :hana_scale_out | :unknown, enforce: true
     field :sid, String.t() | nil, enforce: true
+    field :designated_controller, boolean, enforce: true
   end
 
   use Vex.Struct
--- test/tronto/monitoring/domain/cluster/commands/register_cluster_test.exs
+++ test/tronto/monitoring/domain/cluster/commands/register_cluster_test.exs
@@ -1,18 +1,19 @@
-defmodule Tronto.Monitoring.Domain.Commands.RegisterClusterTest do
+defmodule Tronto.Monitoring.Domain.Commands.RegisterClusterHostTest do
   use ExUnit.Case
 
-  alias Tronto.Monitoring.Domain.Commands.RegisterCluster
+  alias Tronto.Monitoring.Domain.Commands.RegisterClusterHost
 
   @moduletag :unit
 
   test "should not validate if cluster_id is not a valid uuid" do
     command =
-      RegisterCluster.new!(%{
+      RegisterClusterHost.new!(%{
         cluster_id: Faker.String.naughty(),
         host_id: Faker.UUID.v4(),
         name: Faker.StarWars.character(),
         sid: Faker.Lorem.word(),
-        type: :unknown
+        type: :unknown,
+        designated_controller: false
       })
 
     assert not Vex.valid?(command)
@@ -20,12 +21,13 @@ defmodule Tronto.Monitoring.Domain.Commands.RegisterClusterTest do
 
   test "should not validate if host_id is not a valid uuid" do
     command =
-      RegisterCluster.new!(%{
+      RegisterClusterHost.new!(%{
         cluster_id: Faker.UUID.v4(),
         host_id: Faker.String.naughty(),
         name: Faker.StarWars.character(),
         sid: Faker.Lorem.word(),
-        type: :unknown
+        type: :unknown,
+        designated_controller: false
       })
 
     assert not Vex.valid?(command)
