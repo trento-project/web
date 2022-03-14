1e480749ea563b8ed7a2a5f9733684705769db18
nelsonkopliku <nelson.kopliku@suse.com>
2022-03-14 22:59:55 +0100

    Adds a HealthIcon component and refactor ClusterList to use it

---
+++ assets/js/components/Health/HealthIcon.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Spinner from '../Spinner';
+
+import { EOS_LENS_FILLED } from 'eos-icons-react';
+
+const HealthIcon = ({ health = undefined }) => {
+  switch (health) {
+    case 'passing':
+      return <EOS_LENS_FILLED className="fill-jungle-green-500" />;
+    case 'warning':
+      return <EOS_LENS_FILLED className="fill-yellow-500" />;
+    case 'critical':
+      return <EOS_LENS_FILLED className="fill-red-500" />;
+    case 'pending':
+      return <Spinner />;
+    default:
+      return <EOS_LENS_FILLED className="fill-gray-500" />;
+  }
+};
+
+export default HealthIcon;
