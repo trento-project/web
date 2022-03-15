0a353b158a94a5908d21a91b7de5b75dd68eff02
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-03-15 14:52:40 +0100

    Store discovery events

--- lib/tronto/monitoring/discovery/discovery.ex
+++ lib/tronto/monitoring/discovery/discovery.ex
@@ -5,8 +5,13 @@ defmodule Tronto.Monitoring.Discovery do
 
   require Logger
 
+  alias Tronto.Repo
+
+  import Ecto.Query
+
   alias Tronto.Monitoring.Discovery.{
     ClusterPolicy,
+    DiscoveryEvent,
     HostPolicy,
     SapSystemPolicy
   }
@@ -17,8 +22,8 @@ defmodule Tronto.Monitoring.Discovery do
   def handle(event) do
     # TODO: Add a cast/validation step here
     # credo:disable-for-next-line
-    with {:ok, commands} <- do_handle(event) do
-      # TODO: Add the discovery event append log here
+    with {:ok, commands} <- do_handle(event),
+         {:ok, _} <- store_discovery_event(event) do
       dispatch(commands)
     else
       # TODO improve error handling, bubbling up validation / command dispatch errors
@@ -28,19 +33,46 @@ defmodule Tronto.Monitoring.Discovery do
     end
   end
 
-  def do_handle(%{"discovery_type" => "host_discovery"} = event),
+  @spec get_current_discovery_events :: [DiscoveryEvent.t()]
+  def get_current_discovery_events do
+    subquery =
+      from d in DiscoveryEvent,
+        select: max(d.id),
+        group_by: [d.agent_id, d.discovery_type]
+
+    query =
+      from d in DiscoveryEvent,
+        where: d.id in subquery(subquery)
+
+    Repo.all(query)
+  end
+
+  @spec store_discovery_event(map) :: {:ok, DiscoveryEvent.t()} | {:error, any}
+  defp store_discovery_event(%{
+         "agent_id" => agent_id,
+         "discovery_type" => discovery_type,
+         "payload" => payload
+       }) do
+    Repo.insert(%DiscoveryEvent{
+      agent_id: agent_id,
+      discovery_type: discovery_type,
+      payload: payload
+    })
+  end
+
+  defp do_handle(%{"discovery_type" => "host_discovery"} = event),
     do: HostPolicy.handle(event)
 
-  def do_handle(%{"discovery_type" => "cloud_discovery"} = event),
+  defp do_handle(%{"discovery_type" => "cloud_discovery"} = event),
     do: HostPolicy.handle(event)
 
-  def do_handle(%{"discovery_type" => "subscription_discovery"} = event),
+  defp do_handle(%{"discovery_type" => "subscription_discovery"} = event),
     do: HostPolicy.handle(event)
 
-  def do_handle(%{"discovery_type" => "ha_cluster_discovery"} = event),
+  defp do_handle(%{"discovery_type" => "ha_cluster_discovery"} = event),
     do: ClusterPolicy.handle(event)
 
-  def do_handle(%{"discovery_type" => "sap_system_discovery"} = event),
+  defp do_handle(%{"discovery_type" => "sap_system_discovery"} = event),
     do: SapSystemPolicy.handle(event)
 
   @spec dispatch(command | [command]) :: :ok | {:error, any}
