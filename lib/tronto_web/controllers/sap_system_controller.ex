23bd0b671ecc40b29532990495540f8f7a6e5619
nelsonkopliku <nelson.kopliku@suse.com>
2022-03-16 08:08:13 +0100

    Transform sap systems and databases o a map before sending a json response

--- lib/tronto_web/controllers/sap_system_controller.ex
+++ lib/tronto_web/controllers/sap_system_controller.ex
@@ -2,15 +2,22 @@ defmodule TrontoWeb.SapSystemController do
   use TrontoWeb, :controller
 
   alias Tronto.Monitoring
+  alias Tronto.Support.StructHelper
 
   def list(conn, _) do
-    sap_systems = Monitoring.get_all_sap_systems()
+    sap_systems =
+      Monitoring.get_all_sap_systems()
+      # TODO: fix me with DTOs
+      |> StructHelper.to_map()
 
     json(conn, sap_systems)
   end
 
   def list_databases(conn, _) do
-    databases = Monitoring.get_all_databases()
+    databases =
+      Monitoring.get_all_databases()
+      # TODO: fix me with DTOs
+      |> StructHelper.to_map()
 
     json(conn, databases)
   end
