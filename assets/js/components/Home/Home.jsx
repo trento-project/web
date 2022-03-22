a6e4234843b67289643b62088aca3ac2fe83a864
Alessio Biancalana <alessio.biancalana@suse.com>
2022-03-22 18:06:14 +0100

    Use table component for livefeed

--- assets/js/components/Home/Home.jsx
+++ assets/js/components/Home/Home.jsx
@@ -3,6 +3,24 @@ import { useSelector } from 'react-redux';
 
 import day from 'dayjs';
 
+import Table from '@components/Table';
+
+const liveFeedConfig = {
+  columns: [
+    {
+      title: 'Time',
+      key: 'time',
+      render: (time) => (
+        <div className="content-center">
+          {day(time).format('YYYY-MM-DD HH:mm:ss')}
+        </div>
+      ),
+    },
+    { title: 'Source', key: 'source' },
+    { title: 'Message', key: 'message' },
+  ],
+};
+
 const Home = () => {
   const liveFeed = useSelector((state) => state.liveFeed.entries);
   const hosts = useSelector((state) => state.hostsList.hosts);
@@ -31,53 +49,7 @@ const Home = () => {
         </div>
       </div>
       <div className="flex flex-col mt-16">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Time
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Source
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Message
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {liveFeed.map(({ time, source, message }) => (
-                    <tr key={time} className="animate-fade">
-                      <td className="px-6 py-4 whitespace-nowrap text-xs">
-                        <div className="content-center">
-                          {day(time).format('YYYY-MM-DD HH:mm:ss')}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm">
-                        {source}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm">
-                        {message}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <Table config={liveFeedConfig} data={liveFeed} />
       </div>
     </div>
   );
