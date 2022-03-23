1227359d9c4e6d8302cff5ea10f8a69e8acf7392
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-03-23 13:32:31 +0000

    Add tags test in host, cluster, sap_systems ctrls

---
+++ test/trento_web/controllers/host_controller_test.exs
@@ -0,0 +1,44 @@
+defmodule TrentoWeb.HostControllerTest do
+  use TrentoWeb.ConnCase, async: true
+
+  import Trento.Factory
+
+  alias Trento.Tag
+
+  describe "tags" do
+    test "add a tag to a host", %{conn: conn} do
+      conn =
+        post(conn, Routes.host_path(conn, :create_tag, Faker.UUID.v4()), %{
+          "value" => Faker.Beer.style()
+        })
+
+      assert 201 == conn.status
+    end
+
+    test "remove a tag from a host", %{conn: conn} do
+      %Tag{
+        id: _id,
+        value: value,
+        resource_id: resource_id,
+        resource_type: _resource_type
+      } = tag()
+
+      conn = delete(conn, Routes.host_path(conn, :delete_tag, resource_id, value))
+
+      assert 204 == conn.status
+    end
+
+    test "remove a non existing tag from a host", %{conn: conn} do
+      %Tag{
+        id: _id,
+        value: _value,
+        resource_id: resource_id,
+        resource_type: _resource_type
+      } = tag()
+
+      conn = delete(conn, Routes.host_path(conn, :delete_tag, resource_id, "non-existing-tag"))
+
+      assert 404 == conn.status
+    end
+  end
+end
---
+++ test/trento_web/controllers/sap_system_controller_test.exs
@@ -0,0 +1,45 @@
+defmodule TrentoWeb.SapSystemControllerTest do
+  use TrentoWeb.ConnCase, async: true
+
+  import Trento.Factory
+
+  alias Trento.Tag
+
+  describe "tags" do
+    test "add a tag to a sap system", %{conn: conn} do
+      conn =
+        post(conn, Routes.sap_system_path(conn, :create_tag, Faker.UUID.v4()), %{
+          "value" => Faker.Beer.style()
+        })
+
+      assert 201 == conn.status
+    end
+
+    test "remove a tag from a sap system", %{conn: conn} do
+      %Tag{
+        id: _id,
+        value: value,
+        resource_id: resource_id,
+        resource_type: _resource_type
+      } = tag()
+
+      conn = delete(conn, Routes.sap_system_path(conn, :delete_tag, resource_id, value))
+
+      assert 204 == conn.status
+    end
+
+    test "remove a non existing tag from a sap system", %{conn: conn} do
+      %Tag{
+        id: _id,
+        value: _value,
+        resource_id: resource_id,
+        resource_type: _resource_type
+      } = tag()
+
+      conn =
+        delete(conn, Routes.sap_system_path(conn, :delete_tag, resource_id, "non-existing-tag"))
+
+      assert 404 == conn.status
+    end
+  end
+end
