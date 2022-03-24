63d4e25ef6025f973dc97ea3ebf80e36f09064c8
arbulu89 <xarbulu@suse.com>
2022-03-24 14:08:25 +0100

    Add collapsable element to the checks catalog

--- assets/js/components/ChecksCatalog/ChecksCatalog.jsx
+++ assets/js/components/ChecksCatalog/ChecksCatalog.jsx
@@ -2,38 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 
-import { Switch } from '@headlessui/react';
+import { Switch, Disclosure, Transition } from '@headlessui/react';
+import { ChevronRightIcon } from '@heroicons/react/solid'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
-const toggle = (list, element) =>
-  list.includes(element)
-    ? list.filter((string) => string !== element)
-    : [...list, element];
 
 const ChecksCatalog = () => {
-  const { clusterID } = useParams();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
   const catalog = useSelector((state) => state.catalog.catalog);
-  const clusters = useSelector((state) => state.clustersList.clusters);
-  const cluster = clusters.find((cluster) => cluster.id === clusterID);
-
-  const [selectedChecks, setSelectedChecks] = useState(
-    cluster ? cluster.selected_checks : []
-  );
-
-  const isSelected = (check_id) =>
-    selectedChecks ? selectedChecks.includes(check_id) : false;
-
-  useEffect(() => {
-    if (cluster) {
-      setSelectedChecks(cluster.selected_checks ? cluster.selected_checks : []);
-    }
-  }, [cluster]);
 
   return (
     <div>
@@ -50,66 +27,51 @@ const ChecksCatalog = () => {
           <ul role="list" className="divide-y divide-gray-200">
             {checks.map((check) => (
               <li key={check.id}>
-                <a href="#" className="block hover:bg-gray-50">
-                  <div className="px-4 py-4 sm:px-6">
-                    <div className="flex items-center">
-                      <p className="text-sm font-medium">{check.name}</p>
-                      <p className="ml-2 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        {check.id}
-                      </p>
-                    </div>
-                    <div className="mt-2 sm:flex sm:justify-between">
-                      <div className="sm:flex">
-                        <p className="flex items-center text-sm text-gray-500">
-                          {check.description}
+                <Disclosure>
+                  <Disclosure.Button as="div" className="flex justify-between w-full cursor-pointer hover:bg-gray-100">
+                    <div className="px-4 py-4 sm:px-6">
+                      <div className="flex items-center">
+                        <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                          {check.id}
                         </p>
+                        {check.premium > 0 &&
+                          <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                            Premium
+                          </p>
+                        }
                       </div>
-                      <Switch.Group as="div" className="flex items-center">
-                        <Switch
-                          checked={isSelected(check.id)}
-                          className={classNames(
-                            isSelected(check.id)
-                              ? 'bg-jungle-green-500'
-                              : 'bg-gray-200',
-                            'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer focus:outline-none transition-colors ease-in-out duration-200'
-                          )}
-                          onChange={() => {
-                            setSelectedChecks(toggle(selectedChecks, check.id));
-                          }}
-                        >
-                          <span
-                            aria-hidden="true"
-                            className={classNames(
-                              isSelected(check.id)
-                                ? 'translate-x-5'
-                                : 'translate-x-0',
-                              'inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
-                            )}
-                          />
-                        </Switch>
-                      </Switch.Group>
+                      <div className="mt-2 sm:flex sm:justify-between">
+                        <div className="sm:flex">
+                          <p className="flex items-center text-sm">
+                            <ReactMarkdown remarkPlugins={[remarkGfm]} children={check.description} />
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </a>
+
+                  </Disclosure.Button>
+                  <Transition
+                    enter="transition duration-100 ease-out"
+                    enterFrom="transform opacity-0"
+                    enterTo="transform opacity-100"
+                    leave="transition duration-100 ease-out"
+                    leaveFrom="transform opacity-100"
+                    leaveTo="transform opacity-0"
+                  >
+                    <Disclosure.Panel className="border-none">
+                      <div className="px-8 py-4 sm:px-8">
+                        <div className="px-4 py-4 sm:px-4 bg-slate-100 rounded">
+                          <ReactMarkdown remarkPlugins={[remarkGfm]} children={check.remediation} />
+                        </div>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </Disclosure>
               </li>
             ))}
           </ul>
         </div>
       ))}
-      <div className="place-items-end">
-        <button
-          className="bg-jungle-green-500 hover:opacity-75 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            dispatch({
-              type: 'CHECKS_SELECTED',
-              payload: { checks: selectedChecks, clusterID: clusterID },
-            });
-            navigate(`/clusters/${clusterID}/checks/results`);
-          }}
-        >
-          Save
-        </button>
-      </div>
     </div>
   );
 };
