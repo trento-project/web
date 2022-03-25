1c09ab45e5c9d4894e602a22db4bd7b29240712f
arbulu89 <xarbulu@suse.com>
2022-03-25 08:54:45 +0100

    Add cloud provider selection in the catalog

--- assets/js/components/ChecksCatalog/ChecksCatalog.jsx
+++ assets/js/components/ChecksCatalog/ChecksCatalog.jsx
@@ -1,77 +1,88 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import { Switch, Disclosure, Transition } from '@headlessui/react';
-import { ChevronRightIcon } from '@heroicons/react/solid'
+import { Disclosure, Transition } from '@headlessui/react';
 
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
+import ProviderSelection from './ProviderSelection';
 
 const ChecksCatalog = () => {
+
   const catalog = useSelector((state) => state.catalog.catalog);
+  const providers = catalog.map(provider => provider.provider)
+  const [selected, setSelected] = useState(providers[0])
+
+  useEffect(() => {
+     setSelected(providers[0])
+  },[providers[0]])
 
   return (
     <div>
-      {catalog.map(({ group, checks }) => (
-        <div
-          key={group.id}
-          className="bg-white shadow overflow-hidden sm:rounded-md mb-8"
-        >
-          <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-            <h3 className="text-lg leading-6 font-medium text-gray-900">
-              {group}
-            </h3>
-          </div>
-          <ul role="list" className="divide-y divide-gray-200">
-            {checks.map((check) => (
-              <li key={check.id}>
-                <Disclosure>
-                  <Disclosure.Button as="div" className="flex justify-between w-full cursor-pointer hover:bg-gray-100">
-                    <div className="px-4 py-4 sm:px-6">
-                      <div className="flex items-center">
-                        <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                          {check.id}
-                        </p>
-                        {check.premium > 0 &&
-                          <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                            Premium
-                          </p>
-                        }
-                      </div>
-                      <div className="mt-2 sm:flex sm:justify-between">
-                        <div className="sm:flex">
-                          <p className="flex items-center text-sm">
-                            <ReactMarkdown remarkPlugins={[remarkGfm]} children={check.description} />
-                          </p>
+      <ProviderSelection providers={providers} selected={selected} setSelected={setSelected}/>
+      {catalog.filter(provider => provider.provider == selected)
+        .map(({provider, groups}) =>
+          groups.map(({group, checks}) =>
+            <div
+              key={group.id}
+              className="bg-white shadow overflow-hidden sm:rounded-md mb-8"
+            >
+              <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  {group}
+                </h3>
+              </div>
+              <ul role="list" className="divide-y divide-gray-200">
+                {checks.map((check) => (
+                  <li key={check.id}>
+                    <Disclosure>
+                      <Disclosure.Button as="div" className="flex justify-between w-full cursor-pointer hover:bg-gray-100">
+                        <div className="px-4 py-4 sm:px-6">
+                          <div className="flex items-center">
+                            <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                              {check.id}
+                            </p>
+                            {check.premium > 0 &&
+                              <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                Premium
+                              </p>
+                            }
+                          </div>
+                          <div className="mt-2 sm:flex sm:justify-between">
+                            <div className="sm:flex">
+                              <p className="flex items-center text-sm">
+                                <ReactMarkdown remarkPlugins={[remarkGfm]} children={check.description} />
+                              </p>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div>
 
-                  </Disclosure.Button>
-                  <Transition
-                    enter="transition duration-100 ease-out"
-                    enterFrom="transform opacity-0"
-                    enterTo="transform opacity-100"
-                    leave="transition duration-100 ease-out"
-                    leaveFrom="transform opacity-100"
-                    leaveTo="transform opacity-0"
-                  >
-                    <Disclosure.Panel className="border-none">
-                      <div className="px-8 py-4 sm:px-8">
-                        <div className="px-4 py-4 sm:px-4 bg-slate-100 rounded">
-                          <ReactMarkdown remarkPlugins={[remarkGfm]} children={check.remediation} />
-                        </div>
-                      </div>
-                    </Disclosure.Panel>
-                  </Transition>
-                </Disclosure>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+                      </Disclosure.Button>
+                      <Transition
+                        enter="transition duration-100 ease-out"
+                        enterFrom="transform opacity-0"
+                        enterTo="transform opacity-100"
+                        leave="transition duration-100 ease-out"
+                        leaveFrom="transform opacity-100"
+                        leaveTo="transform opacity-0"
+                      >
+                        <Disclosure.Panel className="border-none">
+                          <div className="px-8 py-4 sm:px-8">
+                            <div className="px-4 py-4 sm:px-4 bg-slate-100 rounded">
+                              <ReactMarkdown remarkPlugins={[remarkGfm]} children={check.remediation} />
+                            </div>
+                          </div>
+                        </Disclosure.Panel>
+                      </Transition>
+                    </Disclosure>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )
+        )
+      }
     </div>
   );
 };
---
+++ assets/js/components/ChecksCatalog/ProviderSelection.jsx
@@ -0,0 +1,64 @@
+import React, { Fragment, useState } from 'react';
+
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+
+const ProviderSelection = ({providers, selected, setSelected}) => {
+
+  return (
+    <div className="w-72 pb-4">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+            <span className="block truncate">{selected}</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <SelectorIcon
+                className="w-5 h-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {providers.map((provider, providerIdx) => (
+                <Listbox.Option
+                  key={providerIdx}
+                  className={({ active }) =>
+                    `cursor-default select-none relative py-2 pl-10 pr-4 ${
+                      active ? 'text-green-900 bg-green-100' : 'text-gray-900'
+                    }`
+                  }
+                  value={provider}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {provider}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
+                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+};
+
+export default ProviderSelection;
