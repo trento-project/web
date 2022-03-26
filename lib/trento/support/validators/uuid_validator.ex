7c4c0ea112cd7a967ae612a6d02fad95bf7d61c8
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-03-26 15:22:51 +0100

    Remove validation middleware and clean-up commands tests

--- lib/trento/support/validators/uuid_validator.ex
+++
@@ -1,9 +0,0 @@
-defmodule Trento.Support.UUIDValidator do
-  @moduledoc """
-  Ensure a value is a valid UUID string.
-  """
-
-  use Vex.Validator
-
-  def validate(value, _options), do: Vex.Validators.Uuid.validate(value, format: :default)
-end
