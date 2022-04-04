36b671bf60644b0e5c5f2491c969aa23c09180ba
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-04-04 18:42:53 +0200

    Forward grafana url to the FE

--- lib/trento_web/controllers/page_controller.ex
+++ lib/trento_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule TrentoWeb.PageController do
   use TrentoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    grafana_public_url = Application.fetch_env!(:trento, :grafana)[:public_url]
+
+    render(conn, "index.html", grafana_public_url: grafana_public_url)
   end
 end
