6441cd869bf9a055d1b1219e5446f1513b54d947
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-04-07 19:53:43 +0200

    Refactor filter to use hook in the hook folder

--- assets/js/components/Table/Filter.jsx
+++ assets/js/components/Table/Filter.jsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import { Transition } from '@headlessui/react';
 
 import { toggle, hasOne } from '@lib/lists';
+import useOnClickOutside from '@hooks/useOnClickOutside';
 
 const getLabel = (value, placeholder) =>
   value.length === 0 ? placeholder : value.join(', ');
@@ -112,26 +113,5 @@ const Filter = ({ options, title, value, onChange }) => {
     </div>
   );
 };
-
-function useOnClickOutside(ref, handler) {
-  useEffect(() => {
-    const listener = (event) => {
-      // Do nothing if clicking ref's element or descendent elements
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-
-      handler(event);
-    };
-
-    document.addEventListener('mousedown', listener);
-    document.addEventListener('touchstart', listener);
-
-    return () => {
-      document.removeEventListener('mousedown', listener);
-      document.removeEventListener('touchstart', listener);
-    };
-  }, [ref, handler]);
-}
 
 export default Filter;
