f8ebfec3e3652c61c9817eeac593aa85934161d6
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-04-08 10:04:32 +0100

    Add basic e2e tests for the clusters overview

---
+++ test/e2e/cypress/integration/clusters_overview.js
@@ -0,0 +1,37 @@
+import {
+    allClusterNames,
+    allClusterIds,
+    clusterIdByName,
+  } from '../fixtures/clusters-overview/available_clusters';
+  
+  context('Clusters Overview', () => {
+    const availableClusters = allClusterNames();
+    const availableClustersId = allClusterIds();
+    before(() => {
+      cy.loadScenario('healthy-27-node-SAP-cluster');
+      cy.login();
+      cy.visit('/clusters');
+    });
+  
+    describe('Registered Clusters should be available in the overview', () => {
+      it('should show all of the registered clusters', () => {
+        cy.get('.tn-clustername')
+          .its('length')
+          .should('eq', availableClusters.length);
+      });
+      it('should have 1 pages', () => {
+        cy.get(".tn-page-item").its("length").should("eq", 1);
+      });
+      describe('Discovered clusternames are the expected ones', () => {
+        availableClusters.forEach((clusterName) => {
+          it(`should have a cluster named ${clusterName}`, () => {
+            cy.get('.tn-clustername').each(($link) => {
+              const displayedClusterName = $link.text().trim();
+              expect(availableClusters).to.include(displayedClusterName);
+            });
+          });
+        });
+      });
+    });
+  });
+  
