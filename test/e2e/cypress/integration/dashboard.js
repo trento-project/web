f709454d6d529bbfa556ea7db90dac456ebbd8ea
nelsonkopliku <nelson.kopliku@suse.com>
2022-04-12 19:14:05 +0200

    Updated e2e tests

--- test/e2e/cypress/integration/dashboard.js
+++ test/e2e/cypress/integration/dashboard.js
@@ -13,6 +13,7 @@ describe("Dashboard page", () => {
   describe("The current state should be available in a summary", () => {
     it("should display 2 Passing clusters and 1 critical", () => {
       cy.get(".bg-green-200 > .rounded > .flex > .font-semibold").should("contain", "2");
+      cy.get(".bg-yellow-200 > .rounded > .flex > .font-semibold").should("contain", "0");
       cy.get(".bg-red-200 > .rounded > .flex > .font-semibold").should("contain", "1");
     });
   });
