fec57134bce3f52d5255421fc7f3359bb796cfa5
arbulu89 <xarbulu@suse.com>
2022-04-12 17:21:33 +0200

    Rename to hosts_checks_executions

--- lib/trento/application/read_models/host_checks_executions_read_model.ex
+++ lib/trento/application/read_models/host_checks_executions_read_model.ex
@@ -11,7 +11,7 @@ defmodule Trento.HostChecksExecutionsReadModel do
 
   @derive {Jason.Encoder, except: [:__meta__, :__struct__]}
   @primary_key false
-  schema "hosts_checks_results" do
+  schema "hosts_checks_executions" do
     field :cluster_id, Ecto.UUID, primary_key: true
     field :host_id, Ecto.UUID, primary_key: true
     field :reachable, :boolean
@@ -19,7 +19,7 @@ defmodule Trento.HostChecksExecutionsReadModel do
   end
 
   @spec changeset(t() | Ecto.Changeset.t(), map) :: Ecto.Changeset.t()
-  def changeset(host_checks_results, attrs) do
-    cast(host_checks_results, attrs, __MODULE__.__schema__(:fields))
+  def changeset(host_checks_executions, attrs) do
+    cast(host_checks_executions, attrs, __MODULE__.__schema__(:fields))
   end
 end
--- priv/repo/migrations/20220405142819_create_host_checks_executions.exs
+++ priv/repo/migrations/20220405142819_create_host_checks_executions.exs
@@ -1,8 +1,8 @@
-defmodule Trento.Repo.Migrations.CreateHostChecksResults do
+defmodule Trento.Repo.Migrations.CreateHostChecksExecutions do
   use Ecto.Migration
 
   def change do
-    create table(:hosts_checks_results, primary_key: false) do
+    create table(:hosts_checks_executions, primary_key: false) do
       add :cluster_id, :uuid, primary_key: true
       add :host_id, :uuid, primary_key: true
       add :reachable, :boolean
