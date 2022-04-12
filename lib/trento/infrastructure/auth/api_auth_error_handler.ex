37629d38ab3450595d039ee6136cdeb48b407172
nelsonkopliku <nelson.kopliku@suse.com>
2022-04-12 17:01:59 +0200

    Adds a Plug to support basic API key authentication

---
+++ lib/trento/infrastructure/auth/api_auth_error_handler.ex
@@ -0,0 +1,18 @@
+defmodule Trento.Infrastructure.Auth.AuthenticatedAPIErrorHandler do
+  @moduledoc """
+  Used to handle authentication error in APIs
+
+  Can be attached to
+  `Pow.Plug.RequireAuthenticated`, `Trento.Infrastructure.Auth.AuthenticateAPIKeyPlug`
+  and any other auth plug supporting an :error_handler
+  """
+  use TrentoWeb, :controller
+  alias Plug.Conn
+
+  @spec call(Conn.t(), :not_authenticated) :: Conn.t()
+  def call(conn, :not_authenticated) do
+    conn
+    |> put_status(:unauthorized)
+    |> json(%{error: "Unauthorized"})
+  end
+end
