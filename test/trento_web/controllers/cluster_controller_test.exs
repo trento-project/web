aa5592f28af82a462ccdeeeb5cd92c25d651f8dd
nelsonkopliku <nelson.kopliku@suse.com>
2022-04-21 08:44:25 +0200

    Adds connection settings management capabilities to the Clusters controller

--- test/trento_web/controllers/cluster_controller_test.exs
+++ test/trento_web/controllers/cluster_controller_test.exs
@@ -32,4 +32,85 @@ defmodule TrentoWeb.ClusterControllerTest do
              ] = json_response(conn, 200)
     end
   end
+
+  describe "Connection Settings Management for the Hosts of a Cluster" do
+    setup do
+      cluster_id = Faker.UUID.v4()
+      cluster_projection(id: cluster_id)
+
+      %{
+        cluster_id: cluster_id,
+        hosts: [
+          host_projection(hostname: "A-01", cluster_id: cluster_id),
+          host_projection(hostname: "B-01", cluster_id: cluster_id)
+        ]
+      }
+    end
+
+    test "should retrieve connection settings for a given cluster", %{
+      conn: conn,
+      cluster_id: cluster_id,
+      hosts: [
+        %{id: a_host_id, cluster_id: cluster_id},
+        %{id: another_host_id, cluster_id: cluster_id}
+      ]
+    } do
+      resp =
+        conn
+        |> get(Routes.cluster_path(conn, :get_connection_settings, cluster_id))
+        |> json_response(200)
+
+      assert [
+               %{
+                 "host_id" => ^a_host_id,
+                 "user" => nil
+               },
+               %{
+                 "host_id" => ^another_host_id,
+                 "user" => nil
+               }
+             ] = resp
+    end
+
+    test "should apply desired connection settings for the hosts of a given cluster", %{
+      conn: conn,
+      cluster_id: cluster_id,
+      hosts: [
+        %{id: a_host_id, cluster_id: cluster_id},
+        %{id: another_host_id, cluster_id: cluster_id}
+      ]
+    } do
+      connection_user = "cloudadmin"
+
+      resp =
+        conn
+        |> post(
+          Routes.cluster_path(conn, :save_connection_settings, cluster_id),
+          %{
+            "settings" => [
+              %{
+                "host_id" => a_host_id,
+                "user" => connection_user
+              },
+              %{
+                "host_id" => another_host_id,
+                "user" => connection_user
+              }
+            ]
+          }
+        )
+        |> json_response(200)
+
+      assert [
+               %{
+                 "host_id" => ^a_host_id,
+                 "user" => ^connection_user
+               },
+               %{
+                 "host_id" => ^another_host_id,
+                 "user" => ^connection_user
+               }
+             ] = resp
+    end
+  end
 end
