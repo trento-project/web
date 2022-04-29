057a6c4e193a7c0ebc4a90d425f9a483748821b8
Alessio Biancalana <alessio.biancalana@suse.com>
2022-04-29 14:51:03 +0200

    Add CHANGELOG, bump version in mix.exs (#477)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Trento.MixProject do
   def project do
     [
       app: :trento,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
