1581f96b7e01c941c9191c3ebeca13bb4db43b84
Stefano Torresi <stefano@torresi.io>
2022-05-03 19:01:46 +0200

    add @moduledoc

--- lib/trento/support/config.ex
+++ lib/trento/support/config.ex
@@ -1,6 +1,10 @@
 import Config
 
 defmodule Trento.Config do
+  @moduledoc """
+  Configuration helper functions
+  """
+
   def db_name(name, env \\ config_env()) do
     if env == :prod do
       name
