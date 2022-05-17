fd898f243bb1569e0b64fd0106b1fba2e55f1c7d
arbulu89 <xarbulu@suse.com>
2022-05-17 16:51:36 +0200

    Add HANA details page e2e tests

---
+++ test/e2e/cypress/fixtures/hana-database-details/selected_database.js
@@ -0,0 +1,48 @@
+export const selectedDatabase = {
+  Id: 'f534a4ad-cef7-5234-b196-e67082ffb50c',
+  Sid: 'HDD',
+  Type: 'HANA Database',
+  Hosts: [
+    {
+      Hostname: 'vmhdbdev02',
+      Instance: '10',
+      Features: 'HDB|HDB_WORKER',
+      HttpPort: '51013',
+      HttpsPort: '51014',
+      StartPriority: '0.3',
+      Status: 'SAPControl-GREEN',
+      StatusBadge: 'GREEN',
+    },
+    {
+      Hostname: 'vmhdbdev01',
+      Instance: '10',
+      Features: 'HDB|HDB_WORKER',
+      HttpPort: '51013',
+      HttpsPort: '51014',
+      StartPriority: '0.3',
+      Status: 'SAPControl-GREEN',
+      StatusBadge: 'GREEN',
+    },
+  ],
+};
+
+export const attachedHosts = [
+  {
+    Name: 'vmhdbdev02',
+    AgentId: '0a055c90-4cb6-54ce-ac9c-ae3fedaf40d4',
+    Addresses: ['10.100.1.12'],
+    Provider: 'azure',
+    Cluster: 'hana_cluster',
+    ClusterId: '7965f822-0254-5858-abca-f6e8b4c27714',
+    Version: '0.7.1+git.dev42.1640084952.33229fc',
+  },
+  {
+    Name: 'vmhdbdev01',
+    AgentId: '13e8c25c-3180-5a9a-95c8-51ec38e50cfc',
+    Addresses: ['10.100.1.11', '10.100.1.13'],
+    Provider: 'azure',
+    Cluster: 'hana_cluster',
+    ClusterId: '7965f822-0254-5858-abca-f6e8b4c27714',
+    Version: '0.7.1+git.dev42.1640084952.33229fc',
+  },
+];
