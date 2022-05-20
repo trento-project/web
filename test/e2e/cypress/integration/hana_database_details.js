acae22a0ec88f57bbdc46a02e5b8de7627bf7836
Xabier Arbulu Insausti <xarbulu@suse.com>
2022-05-20 13:28:21 +0200

    Speed up CI process (#556)
    * Store build dependencies in cache in ci
    
    * Disable recording in cypress
    
    * Load known landscape data at the beginning of e2e tests
    
    * Login just once during the e2e tests

--- test/e2e/cypress/integration/hana_database_details.js
+++ test/e2e/cypress/integration/hana_database_details.js
@@ -6,22 +6,9 @@ import {
 } from '../fixtures/hana-database-details/selected_database';
 
 context('HANA database details', () => {
-  const sessionCookie = '_trento_key';
-
   before(() => {
-    cy.loadScenario('healthy-27-node-SAP-cluster');
-    cy.login();
-
     cy.visit(`/databases/${selectedDatabase.Id}`);
     cy.url().should('include', `/databases/${selectedDatabase.Id}`);
-  });
-
-  after(() => {
-    cy.clearCookie(sessionCookie);
-  });
-
-  beforeEach(() => {
-    cy.Cookies.preserveOnce(sessionCookie);
   });
 
   describe('HANA database details page is available', () => {
@@ -48,6 +35,11 @@ context('HANA database details', () => {
     before(() => {
       cy.visit(`/databases/${selectedDatabase.Id}`);
       cy.url().should('include', `/databases/${selectedDatabase.Id}`);
+    });
+
+    after(() => {
+      // Restore instance health
+      cy.loadScenario('hana-database-detail-GREEN');
     });
 
     selectedDatabase.Hosts.forEach((instance, index) => {
