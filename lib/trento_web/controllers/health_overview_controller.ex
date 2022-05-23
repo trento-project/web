e9cc0e11d281577287eccc7cfb19059c9ff2ea97
nelsonkopliku <nelson.kopliku@suse.com>
2022-05-23 15:06:32 +0200

    Add SapSystems Health overview OpenApi spec

--- lib/trento_web/controllers/health_overview_controller.ex
+++ lib/trento_web/controllers/health_overview_controller.ex
@@ -1,8 +1,24 @@
 defmodule TrentoWeb.HealthOverviewController do
   use TrentoWeb, :controller
 
-  alias Trento.SapSystems.HealthSummaryService
+  alias TrentoWeb.OpenApi.Schema
 
+  alias Trento.SapSystems.HealthSummaryService
+  use OpenApiSpex.ControllerSpecs
+
+  operation(:overview,
+    summary: "Health overview of the disovered SAP Systems",
+    tags: ["Landscape"],
+    description:
+      "Provide an aggregated overview of the health of the discovered SAP Systems (and their components) on the target infrastructure",
+    responses: [
+      ok:
+        {"An overview of the health of the disovered SAP Systems and their components",
+         "application/json", Schema.SAPSystem.HealthOverview}
+    ]
+  )
+
+  @spec overview(Plug.Conn.t(), any) :: Plug.Conn.t()
   def overview(conn, _) do
     summary = HealthSummaryService.get_health_summary()
 
