afb67f8a949608fff3a510e6b2403dfa34f9e5af
nelsonkopliku <nelson.kopliku@suse.com>
2022-05-23 16:16:23 +0200

    Add Accept Eula openapi spec

--- lib/trento_web/controllers/settings_controller.ex
+++ lib/trento_web/controllers/settings_controller.ex
@@ -3,6 +3,13 @@ defmodule TrentoWeb.SettingsController do
 
   alias Trento.Installation
 
+  alias TrentoWeb.OpenApi.Schema
+
+  use OpenApiSpex.ControllerSpecs
+
+  operation :settings, false
+
+  @spec settings(Plug.Conn.t(), any) :: Plug.Conn.t()
   def settings(conn, _) do
     conn
     |> json(%{
@@ -11,6 +18,17 @@ defmodule TrentoWeb.SettingsController do
     })
   end
 
+  operation :accept_eula,
+    summary: "Accept Eula",
+    tags: ["Platform"],
+    description: "Accepting EULA allows the end user to use the platform",
+    responses: [
+      ok:
+        {"EULA acceptance has been correctly registered and the user may continue using the platform",
+         "application/json", Schema.Common.EmptyResponse}
+    ]
+
+  @spec accept_eula(Plug.Conn.t(), any) :: Plug.Conn.t()
   def accept_eula(conn, _) do
     :ok = Installation.accept_eula()
 
