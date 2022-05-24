d95270405179e12a350e1fcff41b67af8848e999
Xabier Arbulu Insausti <xarbulu@suse.com>
2022-05-24 10:21:49 +0200

    About page e2e (#545)
    * Improve about page e2e tests
    
    * Add mix version task and test version in about page

---
+++ lib/trento/support/mix/tasks/version.ex
@@ -0,0 +1,10 @@
+defmodule Mix.Tasks.Version do
+  @moduledoc "The hello mix task: `mix help hello`"
+
+  use Mix.Task
+
+  @shortdoc "Print application version"
+  def run(_args) do
+    IO.puts(Mix.Project.config()[:version])
+  end
+end
