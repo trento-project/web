0f9a73a183895918a73b36382be5993ce06e276b
Alessio Biancalana <alessio.biancalana@suse.com>
2022-05-26 16:38:28 +0200

    Add the possibility to decode lists of items with deftype (#601)

--- test/trento/support/type_test.exs
+++ test/trento/support/type_test.exs
@@ -33,4 +33,56 @@ defmodule Trento.TypeTest do
                name: "a"
              })
   end
+
+  test "should validate a list of maps" do
+    assert {:error, [%{embedded: ["can't be blank"]}, %{embedded: ["can't be blank"]}]} ==
+             TestData.from_list([
+               %{id: Faker.UUID.v4(), name: "carbonara"},
+               %{id: Faker.UUID.v4(), name: "amatriciana"}
+             ])
+
+    assert {:error, [%{embedded: ["can't be blank"]}, %{embedded: ["can't be blank"]}]} ==
+             TestData.from_list([
+               %{id: Faker.UUID.v4(), name: "carbonara"},
+               %{id: Faker.UUID.v4(), name: "amatriciana"},
+               %{
+                 id: Faker.UUID.v4(),
+                 name: "cacio_pepe",
+                 embedded: %{id: Faker.UUID.v4(), name: "yay"}
+               }
+             ])
+
+    {
+      :ok,
+      [
+        %TestData{
+          embedded: %EmbeddedTestData{id: _id1, name: "yay"},
+          id: _id2,
+          name: "cacio_pepe"
+        },
+        %TestData{
+          embedded: %EmbeddedTestData{
+            id: _id3,
+            name: "wow"
+          },
+          id: _id4,
+          name: "lasagne"
+        }
+      ] = decoded_list
+    } =
+      TestData.from_list([
+        %{
+          id: Faker.UUID.v4(),
+          name: "cacio_pepe",
+          embedded: %{id: Faker.UUID.v4(), name: "yay"}
+        },
+        %{
+          id: Faker.UUID.v4(),
+          name: "lasagne",
+          embedded: %{id: Faker.UUID.v4(), name: "wow"}
+        }
+      ])
+
+    assert 2 == length(decoded_list)
+  end
 end
