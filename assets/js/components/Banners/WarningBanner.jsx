54873aee1523b8cca1cb8ad329038f885f2a62f1
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2022-09-26 13:18:11 +0200

    Changed Warning border to 8px. Included correcting signing details.

--- assets/js/components/Banners/WarningBanner.jsx
+++ assets/js/components/Banners/WarningBanner.jsx
@@ -4,7 +4,7 @@ import { EOS_WARNING_OUTLINED } from 'eos-icons-react';
 
 const WarningBanner = ({ children }) => {
   return (
-    <div className="bg-yellow-50 rounded mt-2 mb-2 p-3 border-2 border-yellow-500">
+    <div className="bg-yellow-50 rounded-lg mt-2 mb-2 p-3 border-2 border-yellow-500">
       <div className="flex flex-wrap items-center justify-between">
         <div className="flex w-0 flex-1 items-center">
           <EOS_WARNING_OUTLINED className="h-6 w-6 fill-yellow-500" />
