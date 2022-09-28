f8df42d4fbfa108a679b26fd9c902ab952fdb30c
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2022-09-28 16:39:23 +0200

    Added margin to the bottom of each result/host container.

--- assets/js/components/ClusterDetails/ChecksResults.jsx
+++ assets/js/components/ClusterDetails/ChecksResults.jsx
@@ -126,7 +126,7 @@ export const ChecksResults = () => {
 
     pageContent = lastExecution.map(
       ({ _cluster_id, host_id, reachable, msg }, idx) => (
-        <div key={idx} className="flex flex-col">
+        <div key={idx} className={`flex flex-col ${idx != (cluster.hosts_executions.length - 1) ? "mb-8" : ""}`}>
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
               <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
