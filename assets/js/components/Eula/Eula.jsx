dfbafa670cc43635c96e03dc59977a5955544c63
arbulu89 <xarbulu@suse.com>
2022-09-28 15:46:19 +0200

    Add community eula component

--- assets/js/components/Eula/Eula.jsx
+++ assets/js/components/Eula/Eula.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Premium from '@components/Eula/Premium';
+import Community from '@components/Eula/Community';
 
 const Eula = () => {
   const eulaVisible = useSelector((state) => state.settings.eulaVisible);
+  const isPremium = true;
   const dispatch = useDispatch();
 
-  return (
-    <Premium visible={eulaVisible} dispatch={dispatch} />
-  );
+  if (isPremium) {
+    return <Premium visible={eulaVisible} dispatch={dispatch} />;
+  } else {
+    return <Community visible={eulaVisible} dispatch={dispatch} />;
+  }
 };
 
 export default Eula;
