79aaa3b1ef0984a1899c011bbe3dc59e7a7bea23
Jamie Rodriguez <jamie.rodriguez@suse.com>
2022-10-11 15:42:29 +0200

    Reduce size of `ProviderDetails` box when provider doesn't have info

--- assets/js/components/HostDetails/KvmDetails.jsx
+++ assets/js/components/HostDetails/KvmDetails.jsx
@@ -6,7 +6,6 @@ const KvmDetails = ({ provider }) => {
   return (
     <div className="mt-4 bg-white shadow rounded-lg py-4 px-8">
       <ListView
-        className="grid-rows-2"
         orientation="vertical"
         rows={2}
         data={[
--- assets/js/components/HostDetails/NutanixDetails.jsx
+++ assets/js/components/HostDetails/NutanixDetails.jsx
@@ -6,7 +6,6 @@ const NutanixDetails = ({ provider }) => {
   return (
     <div className="mt-4 bg-white shadow rounded-lg py-4 px-8">
       <ListView
-        className="grid-rows-2"
         orientation="vertical"
         rows={2}
         data={[
