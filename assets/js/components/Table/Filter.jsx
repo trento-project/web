95289203b3c7e5f3e0306b7f221c8b9b67333867
Jurgen <jurgen.goldschmidt@suse.com>
2022-10-20 14:58:02 +0200

    Added Clear button to Filter component when one or more values are selected. (#915)
    * Added Clear button to Filter when one or more values are selected.
    
    * Changed onChange event based on review recommedation

--- assets/js/components/Table/Filter.jsx
+++ assets/js/components/Table/Filter.jsx
@@ -4,6 +4,7 @@ import { Transition } from '@headlessui/react';
 
 import { toggle, hasOne } from '@lib/lists';
 import useOnClickOutside from '@hooks/useOnClickOutside';
+import { EOS_CLOSE } from 'eos-icons-react';
 
 const getLabel = (value, placeholder) =>
   value.length === 0 ? placeholder : value.join(', ');
@@ -21,6 +22,18 @@ const Filter = ({ options, title, value, onChange }) => {
   return (
     <div className="w-64 w-72 top-16 mr-4" ref={ref}>
       <div className="mt-1 relative">
+        {value != '' && (
+          <button
+            className="block absolute z-20 right-0 h-full pr-2 flex items-center"
+            onClick={() => onChange([])}
+          >
+            <EOS_CLOSE
+              size="20"
+              className="text-gray-400"
+              color="currentColor"
+            />
+          </button>
+        )}
         <button
           type="button"
           onClick={() => setOpen(!open)}
@@ -36,19 +49,21 @@ const Filter = ({ options, title, value, onChange }) => {
             </span>
           </span>
           <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-            <svg
-              className="h-5 w-5 text-gray-400"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            {value == '' && (
+              <svg
+                className="h-5 w-5 text-gray-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            )}
           </span>
         </button>
         <Transition
