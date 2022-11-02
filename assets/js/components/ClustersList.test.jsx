3b8a731b310397905b1e177b9e2723e147b1b9f3
Carmine Di Monaco <carmine.dimonaco@suse.com>
2022-11-02 14:20:31 +0100

    Add ClustersList test with jest

---
+++ assets/js/components/ClustersList.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import 'intersection-observer';
+import '@testing-library/jest-dom';
+
+import ClustersList from './ClustersList';
+import { renderWithRouter, withState } from '../lib/test-utils';
+
+describe('ClustersList component', () => {
+  describe('query string filtering behavior', () => {
+    it('should put the filters values in the query string when filters are selected', () => {
+      const [StatefulClustersList] = withState(<ClustersList />);
+      renderWithRouter(StatefulClustersList);
+
+      ['Health', 'Name', 'SID', 'Type', 'Tags'].forEach((filter) => {
+        fireEvent.click(screen.getByTestId(`filter-${filter}`));
+
+        fireEvent.click(
+          screen
+            .getByTestId(`filter-${filter}-options`)
+            .querySelector('li > div > span').firstChild
+        );
+      });
+
+      expect(window.location.search).toEqual(
+        '?health=unknown&name=drbd_cluster&sid=HDD&type=unknown&tags=tag1'
+      );
+    });
+  });
+});
