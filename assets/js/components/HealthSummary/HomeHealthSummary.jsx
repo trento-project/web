d199a6835fa0aadb4102ee57f917aa7af62a18a9
Carmine Di Monaco <carmine.dimonaco@suse.com>
2022-11-04 12:33:22 +0100

    Fix relative imports in HealthSummary js module

--- assets/js/components/HealthSummary/HomeHealthSummary.jsx
+++ assets/js/components/HealthSummary/HomeHealthSummary.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Table from '@components/Table';
-import HealthIcon from '../Health/HealthIcon';
+import HealthIcon from '@components/Health/HealthIcon';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import HealthSummary from '@components/HealthSummary';
