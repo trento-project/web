67c2da1ba2ddc2116342554732c8a376146ce56e
Carmine Di Monaco <carmine.dimonaco@suse.com>
2022-11-07 14:08:32 +0100

    Add hover style to health icon (#959)

--- assets/js/components/Health/HealthIcon.jsx
+++ assets/js/components/Health/HealthIcon.jsx
@@ -9,25 +9,35 @@ import {
 } from 'eos-icons-react';
 
 import Spinner from '@components/Spinner';
+import classNames from 'classnames';
 
 const HealthIcon = ({ health = undefined, centered = false }) => {
   switch (health) {
     case 'passing':
       return (
         <EOS_CHECK_CIRCLE_OUTLINED
-          className={computedIconCssClass('fill-jungle-green-500', centered)}
+          className={classNames(
+            'hover:opacity-75',
+            computedIconCssClass('fill-jungle-green-500', centered)
+          )}
         />
       );
     case 'warning':
       return (
         <EOS_WARNING_OUTLINED
-          className={computedIconCssClass('fill-yellow-500', centered)}
+          className={classNames(
+            'hover:opacity-75',
+            computedIconCssClass('fill-yellow-500', centered)
+          )}
         />
       );
     case 'critical':
       return (
         <EOS_ERROR_OUTLINED
-          className={computedIconCssClass('fill-red-500', centered)}
+          className={classNames(
+            'hover:opacity-75',
+            computedIconCssClass('fill-red-500', centered)
+          )}
         />
       );
     case 'pending':
@@ -35,7 +45,10 @@ const HealthIcon = ({ health = undefined, centered = false }) => {
     default:
       return (
         <EOS_LENS_FILLED
-          className={computedIconCssClass('fill-gray-500', centered)}
+          className={classNames(
+            'hover:opacity-75',
+            computedIconCssClass('fill-gray-500', centered)
+          )}
         />
       );
   }
