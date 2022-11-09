b631a15890c394debf1f781242dd73be45fd25b3
arbulu89 <xarbulu@suse.com>
2022-11-09 10:33:19 +0100

    Set checks group as children in CatalogContainer

--- assets/js/components/ChecksCatalog/ChecksCatalogNew.jsx
+++ assets/js/components/ChecksCatalog/ChecksCatalogNew.jsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
+import { groupBy } from '@lib/lists';
+
 import CatalogContainer from './CatalogContainer';
+import CheckItem from './CheckItem';
 
 const wandaURL = process.env.WANDA_URL;
 
@@ -33,9 +36,37 @@ export const ChecksCatalogNew = () => {
   return (
     <CatalogContainer
       onRefresh={() => getCatalog()}
-      catalogData={catalogData}
+      catalogLength={catalogData.length}
       catalogError={catalogError}
       loading={loading}
-    />
+    >
+      <div>
+        {Object.entries(groupBy(catalogData, 'group')).map(
+          ([group, checks], idx) => (
+            <div
+              key={idx}
+              className="check-group bg-white shadow overflow-hidden sm:rounded-md mb-8"
+            >
+              <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  {group}
+                </h3>
+              </div>
+              <ul role="list" className="divide-y divide-gray-200">
+                {checks.map((check) => (
+                  <CheckItem
+                    key={check.id}
+                    checkID={check.id}
+                    premium={check.premium}
+                    description={check.description}
+                    remediation={check.remediation}
+                  />
+                ))}
+              </ul>
+            </div>
+          )
+        )}
+      </div>
+    </CatalogContainer>
   );
 };
