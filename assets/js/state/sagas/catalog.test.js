35eb5fee2e501da0abaa9488fcd554ced0a5f853
arbulu89 <xarbulu@suse.com>
2022-11-11 14:10:06 +0100

    Add catalog saga tests

---
+++ assets/js/state/sagas/catalog.test.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { catalogCheckFactory } from '@lib/test-utils/factories';
+import { recordSaga } from '@lib/test-utils';
+
+import { updateCatalog } from './catalog';
+
+import {
+  setCatalogLoading,
+  setCatalogData,
+  setCatalogError,
+} from '../catalog_new';
+
+const wandaURL = process.env.WANDA_URL;
+const url = `${wandaURL}/api/checks/catalog`;
+const axiosMock = new MockAdapter(axios);
+
+describe('Catalog saga', () => {
+  beforeEach(() => {
+    axiosMock.reset();
+    jest.spyOn(console, 'error').mockImplementation(() => null);
+  });
+
+  afterEach(() => {
+    /* eslint-disable no-console */
+    console.error.mockRestore();
+  });
+
+  it('should update catalog', async () => {
+    const catalog = catalogCheckFactory.buildList(5);
+
+    axiosMock.onGet(url).reply(200, {
+      items: catalog,
+    });
+
+    const dispatched = await recordSaga(updateCatalog, {});
+
+    expect(dispatched).toContainEqual(setCatalogLoading());
+    expect(dispatched).toContainEqual(setCatalogData({ data: catalog }));
+  });
+
+  it('should update catalog with error', async () => {
+    axiosMock.onGet(url).networkError();
+
+    const dispatched = await recordSaga(updateCatalog, {});
+
+    expect(dispatched).toContainEqual(setCatalogLoading());
+    expect(dispatched).toContainEqual(
+      setCatalogError({ error: 'Network Error' })
+    );
+  });
+});
