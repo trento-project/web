e19fb509bea45dfe5303789bfbe3b0535607b899
arbulu89 <xarbulu@suse.com>
2022-11-11 14:09:52 +0100

    Add catalog reducer tests

---
+++ assets/js/state/catalog_new.test.js
@@ -0,0 +1,62 @@
+import catalogNewReducer, {
+  setCatalogLoading,
+  setCatalogData,
+  setCatalogError,
+} from './catalog_new';
+
+describe('Catalog reducer', () => {
+  it('should set catalog on loading state', () => {
+    const initialState = {
+      loading: false,
+    };
+
+    const action = setCatalogLoading();
+
+    const expectedState = {
+      loading: true,
+    };
+
+    expect(catalogNewReducer(initialState, action)).toEqual(expectedState);
+  });
+
+  it('should set catalog data', () => {
+    const initialState = {
+      loading: true,
+      data: [],
+    };
+
+    const action = setCatalogData({ data: [1, 2, 3] });
+
+    const expectedState = {
+      loading: false,
+      data: [1, 2, 3],
+      error: null,
+    };
+
+    const actual = catalogNewReducer(initialState, action);
+
+    expect(actual).toEqual(expectedState);
+  });
+
+  it('should set catalog error', () => {
+    const initialState = {
+      loading: true,
+      data: [],
+      error: null,
+    };
+
+    const error = 'some-error';
+
+    const action = setCatalogError({ data: [], error: error });
+
+    const expectedState = {
+      loading: false,
+      data: [],
+      error: error,
+    };
+
+    const actual = catalogNewReducer(initialState, action);
+
+    expect(actual).toEqual(expectedState);
+  });
+});
