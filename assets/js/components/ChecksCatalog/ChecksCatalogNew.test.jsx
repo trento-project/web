26fafecf7e016564b6e9ecd7a7d3910a051b8393
arbulu89 <xarbulu@suse.com>
2022-11-11 14:10:23 +0100

    Fix catalog view tests

--- assets/js/components/ChecksCatalog/ChecksCatalogNew.test.jsx
+++ assets/js/components/ChecksCatalog/ChecksCatalogNew.test.jsx
@@ -3,22 +3,13 @@ import React from 'react';
 import { screen, within, act, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
 import { faker } from '@faker-js/faker';
-import { renderWithRouter } from '@lib/test-utils';
+import { withState, renderWithRouter } from '@lib/test-utils';
 import { catalogCheckFactory } from '@lib/test-utils/factories';
 
 import { ChecksCatalogNew } from './ChecksCatalogNew';
 
-const wandaURL = process.env.WANDA_URL;
-const axiosMock = new MockAdapter(axios);
-
 describe('ChecksCatalog ChecksCatalogNew component', () => {
-  beforeEach(() => {
-    axiosMock.reset();
-  });
-
   it('should render the checks catalog with fetched data', async () => {
     const groupName1 = faker.animal.cat();
     const groupName2 = faker.animal.cat();
@@ -26,11 +17,12 @@ describe('ChecksCatalog ChecksCatalogNew component', () => {
     const group2 = catalogCheckFactory.buildList(5, { group: groupName2 });
     const catalog = group1.concat(group2);
 
-    axiosMock.onGet(`${wandaURL}/api/checks/catalog`).reply(200, {
-      items: catalog,
-    });
+    const initialState = {
+      catalog_new: { loading: false, data: catalog, error: null },
+    };
+    const [statefulCatalog] = withState(<ChecksCatalogNew />, initialState);
 
-    await act(async () => renderWithRouter(<ChecksCatalogNew />));
+    await act(async () => renderWithRouter(statefulCatalog));
 
     const groups = await waitFor(() => screen.getAllByRole('list'));
     expect(groups.length).toBe(2);
