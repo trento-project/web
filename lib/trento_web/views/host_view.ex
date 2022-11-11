9fe0a61ad921829cdca0e39d6b49e4262d3d2ed8
Carmine Di Monaco <carmine.dimonaco@suse.com>
2022-11-11 09:47:33 +0100

    HostProject uses phoenix view to render broadcasted messages: registered_host host_details_updated

--- lib/trento_web/views/host_view.ex
+++ lib/trento_web/views/host_view.ex
@@ -6,4 +6,23 @@ defmodule TrentoWeb.HostView do
   end
 
   def render("host.json", %{host: host}), do: host
+
+  def render("host_details_updated.json", %{host: host}) do
+    host
+    |> Map.from_struct()
+    |> Map.delete(:__meta__)
+    |> Map.delete(:sles_subscriptions)
+    |> Map.delete(:tags)
+    |> Map.delete(:cluster_id)
+    |> Map.delete(:heartbeat)
+    |> Map.delete(:provider)
+  end
+
+  def render("host_registered.json", %{host: host}) do
+    host
+    |> Map.from_struct()
+    |> Map.delete(:__meta__)
+    |> Map.delete(:sles_subscriptions)
+    |> Map.delete(:tags)
+  end
 end
