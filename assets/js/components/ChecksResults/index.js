4615e22553add603f21b466556bfebbb0954960d
Alessio Biancalana <alessio.biancalana@suse.com>
2022-11-14 17:42:47 +0100

    Export some legacy components to be reused in ExecutionResults

--- assets/js/components/ChecksResults/index.js
+++ assets/js/components/ChecksResults/index.js
@@ -1,3 +1,8 @@
 import ChecksResults from './ChecksResults';
+import ResultsContainer from './ResultsContainer';
+import HostResultsWrapper from './HostResultsWrapper';
+import CheckResult from './CheckResult';
+
+export { ResultsContainer, HostResultsWrapper, CheckResult };
 
 export default ChecksResults;
