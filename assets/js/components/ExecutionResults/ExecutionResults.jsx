faf3bb2c47088a7715033fb1da3b2e6b9b96acc6
Alessio Biancalana <alessio.biancalana@suse.com>
2022-11-14 17:42:28 +0100

    Add ExecutionResults component

--- assets/js/components/ExecutionResults/ExecutionResults.jsx
+++ assets/js/components/ExecutionResults/ExecutionResults.jsx
@@ -1,20 +1,185 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+import { logError } from '@lib/log';
+
+import Modal from '@components/Modal';
 import BackButton from '@components/BackButton';
+import WarningBanner from '@components/Banners/WarningBanner';
+import LoadingBox from '@components/LoadingBox';
+import {
+  ResultsContainer,
+  HostResultsWrapper,
+  CheckResult,
+} from '@components/ChecksResults';
+import { UNKNOWN_PROVIDER } from '@components/ClusterDetails/ClusterSettings';
 
-const defaultFetch = () => {};
+const defaultExecutionFetch = () => Promise.resolve({});
+const defaultCatalogFetch = () => Promise.resolve({});
+const truncatedClusterNameClasses =
+  'font-bold truncate w-60 inline-block align-top';
+
+const getCheckResults = (executionData) => {
+  if (!executionData) {
+    return [];
+  }
+  if (!executionData.check_results) {
+    return [];
+  }
+  return executionData.check_results;
+};
+
+const getHosts = (checkResults) => {
+  return checkResults.flatMap(({ agents_check_results }) =>
+    agents_check_results.map(({ agent_id }) => agent_id)
+  );
+};
+
+const getChecks = (checkResults) => {
+  return checkResults.map(({ check_id }) => check_id);
+};
+
+const getHealth = (checkResults, checkID, agentID) => {
+  const checkResult = checkResults.find(({ check_id }) => check_id === checkID);
+  if (checkResult) {
+    const agentCheckResult = checkResult.agents_check_results.find(
+      ({ agent_id }) => agent_id === agentID
+    );
+
+    const failedExpectationEvaluations =
+      agentCheckResult?.expectation_evaluations.filter(
+        (expectationEvaluation) => 'message' in expectationEvaluation
+      );
+
+    return {
+      expectations: checkResult.expectation_results.length,
+      failedExpectations: failedExpectationEvaluations.length,
+      health: failedExpectationEvaluations.length > 0 ? 'critical' : 'passing',
+    };
+  }
+};
 
 const ExecutionResults = ({
   clusterID,
   executionID,
-  onFetch = defaultFetch,
+  clusterName,
+  cloudProvider,
+  hostnames = [],
+  onExecutionFetch = defaultExecutionFetch,
+  onCatalogFetch = defaultCatalogFetch,
+  onCatalogRefresh = () => {},
 }) => {
+  const [loading, setLoading] = useState(false);
+  const [executionData, setExecutionData] = useState(null);
+  const [catalog, setCatalog] = useState(null);
+  const [selectedCheck, setSelectedCheck] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    Promise.all([onExecutionFetch(executionID), onCatalogFetch()])
+      .then(
+        ([{ data: fetchedExecutionData }, { data: fetchedCatalogData }]) => {
+          setLoading(false);
+          console.log(fetchedExecutionData);
+          setExecutionData(fetchedExecutionData);
+          setCatalog(fetchedCatalogData.items);
+        }
+      )
+      .catch((error) => {
+        setLoading(false);
+        logError(error);
+      });
+  }, [onExecutionFetch, onCatalogFetch, setExecutionData, setCatalog]);
+
+  if (loading) {
+    return <LoadingBox text="Loading checks execution..." />;
+  }
+
+  if (executionData?.status === 'running') {
+    return <LoadingBox text="Check execution currently running..." />;
+  }
+
+  const checkResults = getCheckResults(executionData);
+  const hosts = getHosts(checkResults);
+  const checks = getChecks(checkResults);
+
   return (
     <div>
+      <Modal
+        title={catalog?.find(({ id }) => id === selectedCheck)?.description}
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+      >
+        <ReactMarkdown className="markdown" remarkPlugins={[remarkGfm]}>
+          {catalog?.find(({ id }) => id === selectedCheck)?.description}
+        </ReactMarkdown>
+      </Modal>
       <BackButton url={`/clusters/${clusterID}`}>
         Back to Cluster Details
       </BackButton>
-      {executionID}
+      <div className="flex mb-4 justify-between">
+        <h1 className="text-3xl w-3/5">
+          <span className="font-medium">Checks Results for cluster</span>{' '}
+          <span
+            className={classNames('font-bold', truncatedClusterNameClasses)}
+          >
+            {clusterName}
+          </span>
+        </h1>
+      </div>
+      {cloudProvider == UNKNOWN_PROVIDER && (
+        <WarningBanner>
+          The following results are valid for on-premise bare metal platforms.
+          <br />
+          If you are running your HANA cluster on a different platform, please
+          use results with caution
+        </WarningBanner>
+      )}
+      <ResultsContainer
+        catalogError={false}
+        clusterID={clusterID}
+        hasAlreadyChecksResults
+        selectedChecks={checks}
+        onCatalogRefresh={onCatalogRefresh}
+      >
+        {hosts &&
+          hosts.map((agentID, idx) => (
+            <HostResultsWrapper
+              key={idx}
+              hostname={hostnames.find(({ id }) => agentID === id)?.hostname}
+              reachable
+              unreachableMessage=""
+            >
+              {checks.map((checkID) => {
+                const { health, failedExpectations, expectations } = getHealth(
+                  checkResults,
+                  checkID,
+                  agentID
+                );
+
+                return (
+                  <CheckResult
+                    key={checkID}
+                    checkId={checkID}
+                    description={
+                      catalog.find(({ id }) => id === checkID)?.description
+                    }
+                    executionState={executionData?.status}
+                    health={health}
+                    onClick={() => {
+                      setModalOpen(true);
+                      setSelectedCheck(checkID);
+                    }}
+                  />
+                );
+              })}
+            </HostResultsWrapper>
+          ))}
+      </ResultsContainer>
     </div>
   );
 };
---
+++ assets/js/components/ExecutionResults/ExecutionResults.stories.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import ExecutionResults from './';
+
+const executionID = '9fbcaec6-e65e-4adc-9cac-fc542c66717b';
+
+const runningExecution = {
+  data: {
+    check_results: null,
+    completed_at: null,
+    execution_id: '9fbcaec6-e65e-4adc-9cac-fc542c66717b',
+    group_id: '02acea9d-9658-4902-9806-0eef2bfbbf5d',
+    result: null,
+    started_at: '2022-11-09T15:11:31.436586Z',
+    status: 'running',
+    timeout: null,
+  },
+};
+
+const completedExecution = {
+  data: {
+    check_results: [
+      {
+        agents_check_results: [
+          {
+            agent_id: '22248a4c-216f-45d8-90ff-904d27900efc',
+            expectation_evaluations: [
+              {
+                name: 'expectation_example',
+                return_value: 123,
+                type: 'expect',
+              },
+            ],
+            facts: [
+              { check_id: '156F64', name: 'lol_this_is_a_fact', value: 123 },
+            ],
+            values: [],
+          },
+        ],
+        check_id: '156F64',
+        expectation_results: [
+          { name: 'expectation_example', result: true, type: 'expect' },
+        ],
+        result: 'passing',
+      },
+    ],
+    completed_at: '2022-11-09T17:02:20.629366Z',
+    execution_id: '9fbcaec6-e65e-4adc-9cac-fc542c66717b',
+    group_id: '02acea9d-9658-4902-9806-0eef2bfbbf5d',
+    result: 'passing',
+    started_at: '2022-11-09T15:11:31.436586Z',
+    status: 'completed',
+    timeout: [],
+  },
+};
+
+const hostnames = [
+  {
+    id: '22248a4c-216f-45d8-90ff-904d27900efc',
+    hostname: 'carbonarahost01',
+  },
+];
+
+const catalogData = {
+  data: {
+    items: [
+      {
+        description: 'Corosync `token` timeout is set to expected value\n',
+        expectations: [
+          {
+            expression:
+              'facts.corosync_token_timeout == values.expected_token_timeout',
+            name: 'timeout',
+            type: 'expect',
+          },
+        ],
+        facts: [
+          {
+            argument: 'totem.token',
+            gatherer: 'corosync.conf',
+            name: 'corosync_token_timeout',
+          },
+        ],
+        group: 'Corosync',
+        id: '156F64',
+        name: 'Corosync configuration file',
+        remediation:
+          '## Abstract\nThe value of the Corosync `token` timeout is not set as recommended.\n## Remediation\n...\n',
+        severity: 'critical',
+        values: [
+          {
+            conditions: [
+              {
+                expression: 'env.provider == "azure" || env.provider == "aws"',
+                value: 30000,
+              },
+              { expression: 'env.provider == "gcp"', value: 20000 },
+            ],
+            default: 5000,
+            name: 'expected_token_timeout',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const fetchRunning = () =>
+  new Promise((resolve, _reject) => {
+    setTimeout(() => resolve(runningExecution), 3000);
+  });
+
+const fetchCompleted = () =>
+  new Promise((resolve, _reject) => {
+    setTimeout(() => resolve(completedExecution), 3000);
+  });
+
+const fetchCatalog = () =>
+  new Promise((resolve, _reject) => {
+    setTimeout(() => resolve(catalogData), 3000);
+  });
+
+export default {
+  title: 'ExecutionResults',
+  components: ExecutionResults,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export const Running = () => (
+  <ExecutionResults
+    clusterID="lellellel"
+    executionID={executionID}
+    onExecutionFetch={fetchRunning}
+    onCatalogFetch={fetchCatalog}
+    hosts={hostnames}
+  />
+);
+
+export const Completed = () => (
+  <ExecutionResults
+    clusterID="EugenIsTheBest"
+    executionID={executionID}
+    onExecutionFetch={fetchCompleted}
+    onCatalogFetch={fetchCatalog}
+    hostnames={hostnames}
+  />
+);
