1b0ca48df2726cd66138554104caa0ccaf584975
Carmine Di Monaco <carmine.dimonaco@suse.com>
2022-11-14 14:29:31 +0100

    Refactored prometheus controller view namings

--- lib/trento_web/controllers/prometheus_controller.ex
+++ lib/trento_web/controllers/prometheus_controller.ex
@@ -8,13 +8,13 @@ defmodule TrentoWeb.PrometheusController do
   def targets(conn, _) do
     targets = Prometheus.get_targets()
 
-    render(conn, "targets.json", hosts: targets)
+    render(conn, "targets.json", targets: targets)
   end
 
   def exporters_status(conn, %{"id" => host_id}) do
     case Prometheus.get_exporters_status(host_id) do
       {:ok, exporters_status} ->
-        render(conn, "exporter_status.json", status: exporters_status)
+        render(conn, "exporters_status.json", status: exporters_status)
 
       {:error, reason} ->
         Logger.error("Failed to get exporters status:", error: inspect(reason))
--- lib/trento_web/views/prometheus_view.ex
+++ lib/trento_web/views/prometheus_view.ex
@@ -4,21 +4,21 @@ defmodule TrentoWeb.PrometheusView do
   @node_exporter_port 9100
   @node_exporter_name "Node Exporter"
 
-  def render("targets.json", %{hosts: hosts}) do
-    render_many(hosts, __MODULE__, "target.json", as: :host)
+  def render("targets.json", %{targets: targets}) do
+    render_many(targets, __MODULE__, "target.json", as: :target)
   end
 
-  def render("target.json", %{host: host}) do
+  def render("target.json", %{target: target}) do
     %{
-      targets: ["#{host.ssh_address}:#{@node_exporter_port}"],
+      targets: ["#{target.ssh_address}:#{@node_exporter_port}"],
       labels: %{
         # TODO: in the future renaeme this label which also is used by node_exporter json
-        agentID: "#{host.id}",
-        hostname: "#{host.hostname}",
+        agentID: "#{target.id}",
+        hostname: "#{target.hostname}",
         exporter_name: @node_exporter_name
       }
     }
   end
 
-  def render("exporter_status.json", %{status: status}), do: status
+  def render("exporters_status.json", %{status: status}), do: status
 end
