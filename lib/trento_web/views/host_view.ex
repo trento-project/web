f8438d50738763a26b5d43d7cb2202b8ca700644
Carmine Di Monaco <carmine.dimonaco@suse.com>
2022-11-14 14:55:29 +0100

    Host view render methods refactored

--- lib/trento_web/views/host_view.ex
+++ lib/trento_web/views/host_view.ex
@@ -5,12 +5,14 @@ defmodule TrentoWeb.HostView do
     render_many(hosts, __MODULE__, "host.json")
   end
 
-  def render("host.json", %{host: host}), do: host
-
-  def render("host_details_updated.json", %{host: host}) do
+  def render("host.json", %{host: host}) do
     host
     |> Map.from_struct()
     |> Map.delete(:__meta__)
+  end
+
+  def render("host_details_updated.json", %{host: host}) do
+    render("host.json", %{host: host})
     |> Map.delete(:sles_subscriptions)
     |> Map.delete(:tags)
     |> Map.delete(:cluster_id)
@@ -19,9 +21,7 @@ defmodule TrentoWeb.HostView do
   end
 
   def render("host_registered.json", %{host: host}) do
-    host
-    |> Map.from_struct()
-    |> Map.delete(:__meta__)
+    render("host.json", %{host: host})
     |> Map.delete(:sles_subscriptions)
     |> Map.delete(:tags)
   end
