c522d2d165accaa53c7730d10ae43ca6995eb677
arbulu89 <xarbulu@suse.com>
2022-11-16 09:55:09 +0100

    Document SAP system aggregate

--- lib/trento/domain/sap_system/commands/register_application_instance.ex
+++ lib/trento/domain/sap_system/commands/register_application_instance.ex
@@ -1,6 +1,16 @@
 defmodule Trento.Domain.Commands.RegisterApplicationInstance do
   @moduledoc """
   Register an application instance to the monitoring system.
+
+  In order to register an application instance a database entry associated to this application
+  must be already registered.
+
+  The database/application association consists of having:
+  - the application instance `tenant` field matching with an already registered database instance `tenant`
+  - the application `db_host` field matching with one of the IP addresses of the host where this database is running
+
+  Find the association protocol code [here](https://github.com/trento-project/web/blob/main/lib/trento/application/integration/discovery/protocol/enrich_register_application_instance.ex)
+  as reference.
   """
 
   @required_fields [
