180cf358156a9600241725041752cb32b0a41080
EMaksy <emaksymenko@suse.com>
2022-11-25 14:15:48 +0100

    Add test to the AboutPage component

---
+++ assets/js/components/AboutPage/AboutPage.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { aboutPageFactory } from '@lib/test-utils/factories';
+import AboutPage from './index';
+import { act, screen } from '@testing-library/react';
+import { renderWithRouter } from '@lib/test-utils';
+
+describe('AboutPage component', () => {
+  const apiRequestData = aboutPageFactory.build();
+  it('should render the about page with content from the api', async () => {
+    await act(async () => {
+      renderWithRouter(
+        <AboutPage onFetch={() => Promise.resolve({ data: apiRequestData })} />
+      );
+    });
+
+    expect(screen.getByText(apiRequestData.flavor)).toBeTruthy();
+    expect(screen.getByText(apiRequestData.version)).toBeTruthy();
+    expect(
+      screen.getByText(`${apiRequestData.sles_subscriptions} found`)
+    ).toBeTruthy();
+  });
+
+  it('should render the about page with state values if api get request fails', async () => {
+    const stateValues = { flavor: 'N/A', subscriptions: 0, version: 'v0.0.0' };
+    const errorMessage = { messages: "Get request '/api/about' failed" };
+    await act(async () => {
+      renderWithRouter(
+        <AboutPage onFetch={() => Promise.reject(errorMessage)} />
+      );
+    });
+
+    expect(screen.getByText(stateValues.flavor)).toBeTruthy();
+    expect(screen.getByText(stateValues.version)).toBeTruthy();
+    expect(screen.getByText(`${stateValues.subscriptions} found`)).toBeTruthy();
+  });
+});
