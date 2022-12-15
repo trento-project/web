f91afe0f18eaa01217a91a649b785f4a6ba0b443
arbulu89 <xarbulu@suse.com>
2022-12-15 15:20:58 +0100

    Add some new cluster selector functions

---
+++ assets/js/state/selectors/cluster.test.js
@@ -0,0 +1,52 @@
+import { getClusterHostIDs, getClusterSelectedChecks } from './cluster';
+
+describe('Cluster selector', () => {
+  it('should return the cluster hosts IDs', () => {
+    const state = {
+      hostsList: {
+        hosts: [
+          {
+            id: 'id1',
+            cluster_id: 'cluster1',
+          },
+          {
+            id: 'id2',
+            cluster_id: 'cluster1',
+          },
+          {
+            id: 'id3',
+            cluster_id: 'cluster2',
+          },
+        ],
+      },
+    };
+
+    expect(getClusterHostIDs('cluster1')(state)).toEqual(['id1', 'id2']);
+  });
+
+  it('should return the cluster selected checks', () => {
+    const state = {
+      clustersList: {
+        clusters: [
+          {
+            id: 'cluster1',
+            selected_checks: ['check1', 'check2'],
+          },
+          {
+            id: 'cluster2',
+            selected_checks: ['check3', 'check4'],
+          },
+          {
+            id: 'cluster3',
+            selected_checks: ['check5', 'check6'],
+          },
+        ],
+      },
+    };
+
+    expect(getClusterSelectedChecks('cluster1')(state)).toEqual([
+      'check1',
+      'check2',
+    ]);
+  });
+});
