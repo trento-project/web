b44f895ddf402bcaabcaa65f4c9b9515fbe27182
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-12-16 13:42:43 +0100

    Update roll-up event handler

---
+++ lib/trento/application/event_handlers/roll_up_event_handler.ex
@@ -0,0 +1,33 @@
+defmodule Trento.RollUpEventHandler do
+  @moduledoc """
+  This event handler is responsible for rolling-up aggregates when a requested event is received.
+  """
+
+  use Commanded.Event.Handler,
+    application: Trento.Commanded,
+    name: "roll_up_event_handler",
+    consistency: :strong,
+    start_from: :current
+
+  alias Trento.RollUp
+
+  alias Trento.Domain.Events.{
+    ClusterRolledUp,
+    ClusterRollUpRequested
+  }
+
+  def handle(
+        %ClusterRollUpRequested{cluster_id: stream_id, snapshot: snapshot},
+        _
+      ) do
+    roll_up_event = %ClusterRolledUp{
+      cluster_id: stream_id,
+      snapshot: snapshot
+    }
+
+    now = DateTime.to_iso8601(DateTime.utc_now())
+    archive_stream_id = "#{stream_id}-archived-#{now}"
+
+    RollUp.roll_up_aggregate(stream_id, roll_up_event, archive_stream_id)
+  end
+end
