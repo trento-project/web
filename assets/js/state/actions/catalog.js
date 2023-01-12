3a5075e929c464aedadc7e7b78a6670ccb22fd05
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-01-12 08:26:37 +0000

    Add default value for env in the action creator

--- assets/js/state/actions/catalog.js
+++ assets/js/state/actions/catalog.js
@@ -1,6 +1,6 @@
 export const UPDATE_CATALOG = 'UPDATE_CATALOG_NEW';
 
-export const updateCatalog = (env) => ({
+export const updateCatalog = (env = {}) => ({
   type: UPDATE_CATALOG,
   payload: env,
 });
