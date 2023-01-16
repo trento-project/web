e1eb3f526fb8927673d9c6a162d3b173285fa1ad
arbulu89 <xarbulu@suse.com>
2023-01-16 16:20:08 +0100

    Add premium pill to the check selection view

---
+++ assets/js/components/PremiumPill.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PremiumPill() {
+  return (
+    <p className="ml-1 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+      Premium
+    </p>
+  );
+}
+
+export default PremiumPill;
