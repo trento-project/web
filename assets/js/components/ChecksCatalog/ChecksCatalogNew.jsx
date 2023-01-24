d0b73e4d933b767a75959b987356567bde106d77
EMaksy <emaksymenko@suse.com>
2023-01-24 13:34:12 +0100

    remove unused div

--- assets/js/components/ChecksCatalog/ChecksCatalogNew.jsx
+++ assets/js/components/ChecksCatalog/ChecksCatalogNew.jsx
@@ -42,53 +42,51 @@ export function ChecksCatalogNew() {
     dispatch(updateCatalog(apiParams));
   }, [dispatch, selectedProvider]);
   return (
-    <div>
-      <CatalogContainer
-        onRefresh={() =>
-          dispatch(
-            updateCatalog({
-              provider:
-                getProviderByLabel(providerData, selectedProvider) || null,
-            })
-          )
-        }
-        isCatalogEmpty={catalogData.length === 0}
-        catalogError={catalogError}
-        loading={loading}
-      >
-        <ProviderSelection
-          providers={providerLabels}
-          selected={selectedProvider}
-          onChange={setProviderSelected}
-        />
-        <div>
-          {Object.entries(groupBy(catalogData, 'group')).map(
-            ([group, checks], idx) => (
-              <div
-                key={idx}
-                className="check-group bg-white shadow overflow-hidden sm:rounded-md mb-8"
-              >
-                <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-                  <h3 className="text-lg leading-6 font-medium text-gray-900">
-                    {group}
-                  </h3>
-                </div>
-                <ul className="divide-y divide-gray-200">
-                  {checks.map((check) => (
-                    <CheckItem
-                      key={check.id}
-                      checkID={check.id}
-                      premium={check.premium}
-                      description={check.description}
-                      remediation={check.remediation}
-                    />
-                  ))}
-                </ul>
+    <CatalogContainer
+      onRefresh={() =>
+        dispatch(
+          updateCatalog({
+            provider:
+              getProviderByLabel(providerData, selectedProvider) || null,
+          })
+        )
+      }
+      isCatalogEmpty={catalogData.length === 0}
+      catalogError={catalogError}
+      loading={loading}
+    >
+      <ProviderSelection
+        providers={providerLabels}
+        selected={selectedProvider}
+        onChange={setProviderSelected}
+      />
+      <div>
+        {Object.entries(groupBy(catalogData, 'group')).map(
+          ([group, checks], idx) => (
+            <div
+              key={idx}
+              className="check-group bg-white shadow overflow-hidden sm:rounded-md mb-8"
+            >
+              <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  {group}
+                </h3>
               </div>
-            )
-          )}
-        </div>
-      </CatalogContainer>
-    </div>
+              <ul className="divide-y divide-gray-200">
+                {checks.map((check) => (
+                  <CheckItem
+                    key={check.id}
+                    checkID={check.id}
+                    premium={check.premium}
+                    description={check.description}
+                    remediation={check.remediation}
+                  />
+                ))}
+              </ul>
+            </div>
+          )
+        )}
+      </div>
+    </CatalogContainer>
   );
 }
