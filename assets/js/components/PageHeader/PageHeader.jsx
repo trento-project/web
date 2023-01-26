4d4a246e0d972802e876163bd064fa768c687e84
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-01-26 11:45:16 +0000

    Remove w-3/4 from header

--- assets/js/components/PageHeader/PageHeader.jsx
+++ assets/js/components/PageHeader/PageHeader.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 function PageHeader({ className, children }) {
   return (
-    <h1 className={classNames('text-3xl pb-2 truncate w-3/4', className)}>
+    <h1 className={classNames('text-3xl pb-2 truncate', className)}>
       {children}
     </h1>
   );
