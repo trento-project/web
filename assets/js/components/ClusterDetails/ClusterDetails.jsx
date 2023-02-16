be3d8d06aaf95df1d1f1fda54b2a0a4fb43dc209
EMaksy <emaksymenko@suse.com>
2023-02-16 18:37:00 +0100

    Fix cluster page button width

--- assets/js/components/ClusterDetails/ClusterDetails.jsx
+++ assets/js/components/ClusterDetails/ClusterDetails.jsx
@@ -123,46 +123,50 @@ export function ClusterDetails() {
           </PageHeader>
         </div>
         <div className="flex w-1/2 justify-end">
-          <Button
-            type="primary-white"
-            className="w-fit inline-block mx-0.5 border-green-500 border "
-            size="small"
-            onClick={() => navigate(`/clusters/${clusterID}/settings`)}
-          >
-            <EOS_SETTINGS className="inline-block fill-jungle-green-500" />{' '}
-            Settings
-          </Button>
+          <div className="flex w-fit whitespace-nowrap">
+            <Button
+              type="primary-white"
+              className="inline-block mx-0.5 border-green-500 border "
+              size="small"
+              onClick={() => navigate(`/clusters/${clusterID}/settings`)}
+            >
+              <EOS_SETTINGS className="inline-block fill-jungle-green-500" />{' '}
+              Settings
+            </Button>
 
-          <Button
-            type="primary-white"
-            className="w-fit mx-0.5 border-green-500 border"
-            size="small"
-            onClick={() => navigate(`/clusters/${clusterID}/executions/last`)}
-          >
-            <EOS_CLEAR_ALL className="inline-block fill-jungle-green-500" />{' '}
-            Show Results
-          </Button>
+            <Button
+              type="primary-white"
+              className="mx-0.5 border-green-500 border"
+              size="small"
+              onClick={() => navigate(`/clusters/${clusterID}/executions/last`)}
+            >
+              <EOS_CLEAR_ALL className="inline-block fill-jungle-green-500" />{' '}
+              Show Results
+            </Button>
 
-          <TriggerChecksExecutionRequest
-            cssClasses="rounded relative w-fit ml-0.5 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-gray-400"
-            clusterId={clusterID}
-            disabled={!hasSelectedChecks}
-            hosts={hosts}
-            checks={cluster.selected_checks}
-            onStartExecution={(_, hostList, selectedChecks) =>
-              dispatch(executionRequested(clusterID, hostList, selectedChecks))
-            }
-          >
-            <EOS_PLAY_CIRCLE
-              className={classNames('inline-block fill-jungle-green-500', {
-                'fill-slate-500': !hasSelectedChecks,
-              })}
-            />{' '}
-            Start Execution
-            {!hasSelectedChecks && (
-              <Tooltip tooltipText="Select some Checks first!" />
-            )}
-          </TriggerChecksExecutionRequest>
+            <TriggerChecksExecutionRequest
+              cssClasses="flex rounded relative ml-0.5 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-gray-400"
+              clusterId={clusterID}
+              disabled={!hasSelectedChecks}
+              hosts={hosts}
+              checks={cluster.selected_checks}
+              onStartExecution={(_, hostList, selectedChecks) =>
+                dispatch(
+                  executionRequested(clusterID, hostList, selectedChecks)
+                )
+              }
+            >
+              <EOS_PLAY_CIRCLE
+                className={classNames('inline-block fill-jungle-green-500', {
+                  'fill-slate-500': !hasSelectedChecks,
+                })}
+              />{' '}
+              <span>Start Execution</span>
+              {!hasSelectedChecks && (
+                <Tooltip tooltipText="Select some Checks first!" />
+              )}
+            </TriggerChecksExecutionRequest>
+          </div>
         </div>
       </div>
       <div className="flex xl:flex-row flex-col">
