79261aab59a8e20c1a6229e670e0d6bdd4668c36
Alessio Biancalana <alessio.biancalana@suse.com>
2023-03-01 18:04:11 +0100

    Add ObjectTree React component

---
+++ assets/js/components/ObjectTree/tree.js
@@ -0,0 +1,62 @@
+export const flattenTree = (tree, parent, id = 0) => {
+  const { count, children } =
+    tree.children == null || tree.children.length === 0
+      ? { count: id, children: [] }
+      : tree.children.reduce(
+          (
+            { count: accumulatedCount, children: accumulatedChildren },
+            currentChild
+          ) => {
+            const { count: newCount, children: newChildren } = flattenTree(
+              currentChild,
+              id,
+              accumulatedCount + 1
+            );
+            return {
+              count: newCount,
+              children: [...accumulatedChildren, ...newChildren],
+            };
+          },
+          {
+            count: id,
+            children: [],
+          }
+        );
+
+  const childrenIds = [
+    ...children
+      .filter(({ parent: parentId }) => parentId === id)
+      .map(({ id: nodeId }) => nodeId),
+  ];
+
+  const node = {
+    ...tree,
+    id,
+    name: tree.name,
+    children: childrenIds,
+    parent,
+  };
+
+  return { count, children: [node, ...children] };
+};
+
+export const treeify = (data) => ({
+  name: '',
+  children: Object.keys(data).map((key) => {
+    const { [key]: element } = data;
+    const dataType = typeof element;
+
+    if (dataType === 'object') {
+      const { children } = treeify(element);
+      return { name: key, children };
+    }
+
+    return { name: key, value: element };
+  }),
+});
+
+export const getTreeRepresentation = (data) => {
+  const tree = treeify(data);
+  const { children: flattenedTree } = flattenTree(tree, null, 0);
+  return flattenedTree;
+};
