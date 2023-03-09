e4bdbde2486bd30d17ded4596b1cad1b744e41ac
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-03-09 18:21:05 +0100

    Add Not Found and Bad Request schema

---
+++ lib/trento_web/openapi/schema/bad_request.ex
@@ -0,0 +1,38 @@
+defmodule TrentoWeb.OpenApi.Schema.BadRequest do
+  @moduledoc """
+  Bad Request
+  """
+
+  require OpenApiSpex
+
+  alias OpenApiSpex.Operation
+  alias OpenApiSpex.Schema
+
+  OpenApiSpex.schema(%{
+    title: "BadRequest",
+    type: :object,
+    properties: %{
+      errors: %Schema{
+        type: :array,
+        items: %Schema{
+          type: :object,
+          properties: %{
+            detail: %Schema{
+              type: :string,
+              example: "Invalid request payload."
+            },
+            title: %Schema{type: :string, example: "Bad Request"}
+          }
+        }
+      }
+    }
+  })
+
+  def response do
+    Operation.response(
+      "Bad Request",
+      "application/json",
+      __MODULE__
+    )
+  end
+end
