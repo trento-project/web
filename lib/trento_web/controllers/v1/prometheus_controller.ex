73c7c957fb749a4a1ea3b8b503b05b65210460ff
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2023-03-15 13:36:36 +0100

    Add Openapi spec to prometheus controller

--- lib/trento_web/controllers/v1/prometheus_controller.ex
+++ lib/trento_web/controllers/v1/prometheus_controller.ex
@@ -1,17 +1,41 @@
 defmodule TrentoWeb.V1.PrometheusController do
   use TrentoWeb, :controller
 
+  use OpenApiSpex.ControllerSpecs
+
   alias Trento.Integration.Prometheus
+  alias TrentoWeb.OpenApi.Schema
 
   require Logger
 
   action_fallback TrentoWeb.FallbackController
+
+  operation :targets,
+    summary: "Get Prometheus exporters status",
+    tags: ["Target Infrastructure"],
+    description: "Get Prometheus targets with the Http Discovery format",
+    responses: [
+      ok:
+        {"A collection of HttpSTD targets", "application/json",
+         TrentoWeb.OpenApi.Schema.HttpStd.TargetList}
+    ]
 
   def targets(conn, _) do
     targets = Prometheus.get_targets()
 
     render(conn, "targets.json", targets: targets)
   end
+
+  operation :exporters_status,
+    summary: "Get prometheus exporters status",
+    tags: ["Target Infrastructure"],
+    description: "Get Prometheus exporters status for a host identified by host id",
+    responses: [
+      ok:
+        {"The status for the prometheus exporter", "application/json",
+         TrentoWeb.OpenApi.Schema.Prometheus.ExporterStatus},
+      not_found: Schema.NotFound.response()
+    ]
 
   def exporters_status(conn, %{"id" => host_id}) do
     case Prometheus.get_exporters_status(host_id) do
---
+++ lib/trento_web/openapi/schema/prometheus.ex
@@ -0,0 +1,20 @@
+defmodule TrentoWeb.OpenApi.Schema.Prometheus do
+  @moduledoc false
+
+  require OpenApiSpex
+  alias OpenApiSpex.Schema
+
+  defmodule ExporterStatus do
+    @moduledoc false
+
+    OpenApiSpex.schema(%{
+      title: "PrometheusExporterStatus",
+      type: :object,
+      additionalProperties: %Schema{
+        description:
+          "Status of the exporter, the value could be one of passing, critical, unknown",
+        type: :string
+      }
+    })
+  end
+end
