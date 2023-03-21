150989f47ca5c6e21f18ac17aa9aa18a4be1c86d
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-03-21 14:14:25 +0000

    Add sap_system rollup events and commands

---
+++ lib/trento/domain/sap_system/events/sap_system_rolled_up.ex
@@ -0,0 +1,13 @@
+defmodule Trento.Domain.Events.SapSystemRolledUp do
+  @moduledoc """
+  This event is emitted when a sap system roll-up is requested.
+  It is used to trigger the stream archiving process and it contains the snapshot of the host aggregate.
+  """
+
+  use Trento.Event
+
+  defevent resource: "sap_system" do
+    field :sap_system_id, Ecto.UUID
+    embeds_one :snapshot, Trento.Domain.SapSystem
+  end
+end
