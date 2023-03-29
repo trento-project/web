1ee9b1ab99952c331a280e7ab69c1aba7fe27d45
Nelson Kopliku <nelson.kopliku@suse.com>
2023-03-29 11:59:28 +0200

    Check Result Detail pt2 (#1264)
    * Chore: extract and test transformation functions from CheckResultOutline
    
    * Implement a Check detail for a Host target
    
    * Determine the target id based on target type in CheckResultDetailPage
    
    * Split CheckResultDetail subcomponents in dedicated files
    
    * Address review feedbacks
    
    * Improve naming

--- assets/js/components/ExecutionResults/CheckResultDetail/CheckResultDetail.jsx
+++ assets/js/components/ExecutionResults/CheckResultDetail/CheckResultDetail.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 
-import ObjectTree from '@components/ObjectTree/ObjectTree';
+import {
+  getAgentCheckResultByAgentID,
+  getExpectStatements,
+  isAgentCheckError,
+  isTargetHost,
+} from '../checksUtils';
+import ExpectationsResults from './ExpectationsResults';
+import ExpectedValues from './ExpectedValues';
+import GatheredFacts from './GatheredFacts';
 
-function CheckResultDetail({ executionData = {} }) {
-  return (
+function CheckResultDetail({ checkID, targetID, targetType, executionData }) {
+  const targetHost = isTargetHost(targetType);
+
+  const checkResult = getAgentCheckResultByAgentID(
+    executionData,
+    checkID,
+    targetID
+  );
+
+  const {
+    expectation_evaluations = [],
+    values = [],
+    facts = [],
+    message,
+  } = checkResult;
+
+  const isError = isAgentCheckError(checkResult);
+
+  const expectStatementsEvaluations = getExpectStatements(
+    expectation_evaluations
+  );
+
+  return targetHost ? (
     <>
-      <div className="tn-cluster-details w-full my-4 mr-4 bg-white shadow rounded-lg px-8 py-4">
-        Expectations
-      </div>
-      <div className="tn-cluster-details w-full my-4 mr-4 bg-white shadow rounded-lg px-8 py-4">
-        Values
-      </div>
-      <div className="tn-cluster-details w-full my-4 mr-4 bg-white shadow rounded-lg px-8 py-4">
-        Facts
-        <div className="mt-3">
-          <ObjectTree data={executionData} />
-        </div>
-      </div>
+      <ExpectationsResults
+        isError={isError}
+        results={expectStatementsEvaluations}
+        errorMessage={message}
+      />
+      <ExpectedValues isError={isError} expectedValues={values} />
+      <GatheredFacts gatheredFacts={facts || []} />
     </>
+  ) : (
+    <div>Cluster wide check</div>
   );
 }
 
---
+++ assets/js/components/ExecutionResults/CheckResultDetail/ExpectationsResults.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { faker } from '@faker-js/faker';
+
+import { executionExpectationEvaluationFactory } from '@lib/test-utils/factories';
+
+import '@testing-library/jest-dom';
+import ExpectationsResults from './ExpectationsResults';
+
+describe('ExpectationsResults Component', () => {
+  it('should render expect statements results', async () => {
+    const results = [
+      ...executionExpectationEvaluationFactory.buildList(3, {
+        return_value: true,
+      }),
+      ...executionExpectationEvaluationFactory.buildList(2, {
+        return_value: false,
+      }),
+    ];
+
+    const [{ name: expectationName1 }, _, { name: expectationName3 }] = results;
+
+    render(<ExpectationsResults results={results} />);
+
+    expect(screen.getAllByText('Passing')).toHaveLength(3);
+    expect(screen.getAllByText('Failing')).toHaveLength(2);
+    expect(screen.getByText(expectationName1)).toBeVisible();
+    expect(screen.getByText(expectationName3)).toBeVisible();
+  });
+
+  it('should render an error', async () => {
+    const errorMessage = faker.lorem.sentence();
+    render(
+      <ExpectationsResults results={[]} isError errorMessage={errorMessage} />
+    );
+
+    expect(screen.getByText(errorMessage)).toBeVisible();
+  });
+});
