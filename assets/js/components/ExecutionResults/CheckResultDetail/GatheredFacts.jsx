623aa9e8580d1106e6fd9a89714009a4e8e78346
Nelson Kopliku <nelson.kopliku@suse.com>
2023-03-31 10:26:35 +0200

    Add support for both host and cluster wide facts to GatheredFacts component

--- assets/js/components/ExecutionResults/CheckResultDetail/GatheredFacts.jsx
+++ assets/js/components/ExecutionResults/CheckResultDetail/GatheredFacts.jsx
@@ -3,36 +3,40 @@ import React from 'react';
 import ListView from '@components/ListView';
 import ObjectTree from '@components/ObjectTree';
 
-function GatheredFacts({ gatheredFacts = [] }) {
-  const facts = gatheredFacts.map(({ name, value, type, message }) => {
-    const isFactError = !!type;
+const gatheredFactsToListView = (gatheredFacts) =>
+  gatheredFacts.map(({ name, value, type, message }) => ({
+    title: name,
+    content: value,
+    render: (factValue = {}) =>
+      type ? (
+        <span className="text-red-500">{message}</span>
+      ) : (
+        <ObjectTree className="mt-3 text-sm" data={factValue} />
+      ),
+  }));
 
-    return {
-      title: name,
-      content: value,
-      render: (factValue = {}) =>
-        isFactError ? (
-          <span className="text-red-500">{message}</span>
-        ) : (
-          <ObjectTree className="mt-3" data={factValue} />
-        ),
-    };
-  });
-
+function GatheredFacts({ isTargetHost = true, gatheredFacts = [] }) {
   return (
     <div className="w-full my-4 mr-4 bg-white shadow rounded-lg px-8 py-4">
       <div className="text-lg font-bold">Facts</div>
 
-      {facts.length === 0 ? (
+      {gatheredFacts.length === 0 && (
         <div className="mt-3 text-red-500">No facts were gathered</div>
-      ) : (
+      )}
+
+      {isTargetHost && (
         <ListView
           className="grid-flow-row mt-3"
           titleClassName="text-sm"
           orientation="horizontal"
-          data={facts}
+          data={gatheredFactsToListView(gatheredFacts)}
         />
       )}
+
+      {!isTargetHost &&
+        gatheredFacts.map(({ name, value }) => (
+          <ObjectTree key={name} className="mt-3 text-sm" data={value} />
+        ))}
     </div>
   );
 }
