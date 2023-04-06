9b57bd90537e39712d3292f949e100b9edb3a2b4
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2023-04-06 11:13:38 +0200

    Run through prettier

--- assets/js/components/Tooltip/Tooltip.stories.jsx
+++ assets/js/components/Tooltip/Tooltip.stories.jsx
@@ -6,15 +6,17 @@ export default {
   title: 'Tooltip',
   component: Tooltip,
   args: {
-    tooltipText: "Hello World",
-    width: "w-full"
+    tooltipText: 'Hello World',
+    width: 'w-full',
   },
 };
 
 export function Default(args) {
-    return <div class="p-12">
-        <div class="relative">
-            <Tooltip {...args}>Hover me!</Tooltip>
-            </div>
-        </div>;
-  }
+  return (
+    <div class="p-12">
+      <div class="relative">
+        <Tooltip {...args}>Hover me!</Tooltip>
+      </div>
+    </div>
+  );
+}
