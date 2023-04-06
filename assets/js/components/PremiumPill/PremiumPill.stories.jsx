921c8b6ff9203566324cd423677090a1d40d876d
Eugen Maksymenko <emaksymenko@suse.com>
2023-04-06 17:34:53 +0200

    Add PremiumPill directory with a story (#1308)
    * Add PremiumPill directory with a story

---
+++ assets/js/components/PremiumPill/PremiumPill.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PremiumPill from '.';
+
+export default {
+  title: 'PremiumPill',
+  component: PremiumPill,
+};
+
+export function Default() {
+  return <PremiumPill />;
+}
