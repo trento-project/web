a602702d91c9842549e742ce8d46ec0dcd9c3475
EMaksy <eugen.maksymenko@suse.com>
2023-04-06 17:26:51 +0200

    Add stories for the AboutPage components

---
+++ assets/js/components/AboutPage/AboutPageLogo.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import AboutPageLogo from './AboutPageLogo';
+
+export default {
+  title: 'AboutPageLogo',
+  component: AboutPageLogo,
+};
+
+export function Default() {
+  return <AboutPageLogo />;
+}
---
+++ assets/js/components/AboutPage/AboutPageText.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import AboutPageText from './AboutPageText';
+
+export default {
+  title: 'AboutPageText',
+  component: AboutPageText,
+};
+
+export function Default() {
+  return <AboutPageText />;
+}
