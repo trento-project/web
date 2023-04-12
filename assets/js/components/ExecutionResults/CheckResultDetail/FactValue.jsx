e1ee002d4e128a0791d9ec0c3051fa8568569a85
Alessio Biancalana <alessio.biancalana@suse.com>
2023-04-12 11:34:03 +0200

    Fact value primitive types (#1317)
    * Display raw values for primitive types
    
    * Add FactValue tests and detail stories
    
    * Fix factories using transient params
    
    * Fix props in CheckResultDetail test

---
+++ assets/js/components/ExecutionResults/CheckResultDetail/FactValue.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ObjectTree from '@components/ObjectTree';
+
+function FactValue({ className, data }) {
+  return typeof data === 'object' ? (
+    <ObjectTree className={className} data={data} />
+  ) : (
+    <span className={className}>{data}</span>
+  );
+}
+
+export default FactValue;
