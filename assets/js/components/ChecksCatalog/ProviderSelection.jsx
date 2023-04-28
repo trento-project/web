2078e08f03571798173c36ce92fdb2939dd6b148
Nelson Kopliku <nelson.kopliku@suse.com>
2023-04-28 15:12:11 +0200

    Collapsible checks catalog (#1369)
    * Add an Accordion Component
    
    * Refactor ChecksCatalog to have collapsible groups
    
    * Adjust cypress test to select correct item in the DOM
    
    * Refactor CheckItem to use the new Accordion component
    
    * Refactor ChecksSelectionGroup to use the new Accordion component
    
    * Adjust z-index for ProviderSelection

--- assets/js/components/ChecksCatalog/ProviderSelection.jsx
+++ assets/js/components/ChecksCatalog/ProviderSelection.jsx
@@ -36,7 +36,7 @@ function ProviderSelection({ className, providers, selected, onChange }) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-[1]">
               {providers.map((provider, providerIdx) => (
                 <Listbox.Option
                   key={/* eslint-disable */ providerIdx}
