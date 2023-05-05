77aa0d385e511293a6b5023bbcd22cb76c25185a
Alessio Biancalana <alessio.biancalana@suse.com>
2023-05-05 13:07:09 +0200

    Fix text size rendering expectation results

--- assets/js/components/ExecutionResults/CheckResultDetail/ExpectedValues.jsx
+++ assets/js/components/ExecutionResults/CheckResultDetail/ExpectedValues.jsx
@@ -7,10 +7,12 @@ function ExpectedValues({ expectedValues = [], isError = false }) {
     <div className="w-full my-4 mr-4 bg-white shadow rounded-lg px-8 py-4">
       <div className="text-lg font-bold">Values</div>
       {isError ? (
-        <div className="mt-3 text-red-500">Expected Values unavailable</div>
+        <div className="mt-3 text-red-500 text-sm">
+          Expected Values unavailable
+        </div>
       ) : (
         <ListView
-          className="mt-3"
+          className="mt-3 text-sm"
           titleClassName="text-sm"
           orientation="horizontal"
           data={expectedValues.map(({ name, value }) => ({
