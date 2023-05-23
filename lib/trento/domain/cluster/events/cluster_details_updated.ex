fb6b53faaa935234a29f8c0cd1238e5efed9ee3d
arbulu89 <xarbulu@suse.com>
2023-05-23 10:33:01 +0200

    Set default value to additional_sids in cluster events

--- lib/trento/domain/cluster/events/cluster_details_updated.ex
+++ lib/trento/domain/cluster/events/cluster_details_updated.ex
@@ -18,7 +18,7 @@ defmodule Trento.Domain.Events.ClusterDetailsUpdated do
     field :name, :string
     field :type, Ecto.Enum, values: ClusterType.values()
     field :sid, :string
-    field :additional_sids, {:array, :string}
+    field :additional_sids, {:array, :string}, default: []
     field :provider, Ecto.Enum, values: Provider.values()
     field :resources_number, :integer
     field :hosts_number, :integer
