ada8330a2ae5b55cafa2ad3bb2f1dc91fc3b1c6c
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-06-19 17:18:39 +0100

    Add test for instances removal notify

--- assets/js/state/sagas/databases.test.js
+++ assets/js/state/sagas/databases.test.js
@@ -3,14 +3,15 @@ import { databaseInstanceDeregistered } from '@state/sagas/databases';
 import { removeDatabaseInstance } from '@state/databases';
 import { removeDatabaseInstanceFromSapSystem } from '@state/sapSystems';
 import { databaseInstanceFactory } from '@lib/test-utils/factories';
+import { notify } from '@state/actions/notifications';
 
 describe('SAP Systems sagas', () => {
   it('should remove the database instance', async () => {
-    const { sap_system_id, host_id, instance_number } =
+    const { sap_system_id, host_id, instance_number, sid } =
       databaseInstanceFactory.build();
 
     const dispatched = await recordSaga(databaseInstanceDeregistered, {
-      payload: { sap_system_id, host_id, instance_number },
+      payload: { sap_system_id, host_id, instance_number, sid },
     });
 
     expect(dispatched).toContainEqual(
@@ -18,10 +19,17 @@ describe('SAP Systems sagas', () => {
         sap_system_id,
         host_id,
         instance_number,
+        sid,
       })
     );
     expect(dispatched).toContainEqual(
-      removeDatabaseInstance({ sap_system_id, host_id, instance_number })
+      removeDatabaseInstance({ sap_system_id, host_id, instance_number, sid })
+    );
+    expect(dispatched).toContainEqual(
+      notify({
+        text: `The database instance ${instance_number} has been deregistered from ${sid}.`,
+        icon: 'ℹ️',
+      })
     );
   });
 });
