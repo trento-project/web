cb94d0dee33042a4a78bb953cdff67a769fa2a6b
arbulu89 <xarbulu@suse.com>
2023-06-21 13:19:10 +0200

    Add ensa version default value as backend in other branch

--- assets/js/components/SapSystemDetails/GenericSystemDetails.jsx
+++ assets/js/components/SapSystemDetails/GenericSystemDetails.jsx
@@ -50,7 +50,7 @@ export function GenericSystemDetails({ title, type, system }) {
               ? [
                   {
                     title: 'ENSA version',
-                    content: system.ensaVersion,
+                    content: system.ensaVersion || '-',
                     render: (content) => renderEnsaVersion(content),
                   },
                 ]
