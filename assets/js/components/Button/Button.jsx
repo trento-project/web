e98f4b5d026c22da8502e86088db7f217e39ccf3
Jamie Rodríguez <jamie.rodriguez@suse.com>
2023-06-22 10:34:47 +0200

    Add Deregistration modal (#1537)

--- assets/js/components/Button/Button.jsx
+++ assets/js/components/Button/Button.jsx
@@ -14,10 +14,14 @@ const getButtonClasses = (type) => {
   switch (type) {
     case 'primary-white':
       return 'bg-white hover:opacity-75 focus:outline-none text-jungle-green-500 w-full transition ease-in duration-200 text-center font-semibold rounded shadow';
+    case 'primary-white-fit':
+      return 'bg-white hover:opacity-75 focus:outline-none text-jungle-green-500 w-fit transition ease-in duration-200 text-center font-semibold rounded shadow';
     case 'transparent':
       return 'bg-transparent hover:opacity-75 focus:outline-none w-full transition ease-in duration-200 font-semibold';
     case 'secondary':
-      return 'bg-persimmon hover:opacity-75 focus:outline-none  text-gray-800 w-full transition ease-in duration-200 text-center font-semibold rounded shadow';
+      return 'bg-persimmon hover:opacity-75 focus:outline-none text-gray-800 w-full transition ease-in duration-200 text-center font-semibold rounded shadow';
+    case 'default-fit':
+      return 'bg-jungle-green-500 hover:opacity-75 focus:outline-none text-white w-fit transition ease-in duration-200 text-center font-semibold rounded shadow';
     default:
       return 'bg-jungle-green-500 hover:opacity-75 focus:outline-none text-white w-full transition ease-in duration-200 text-center font-semibold rounded shadow';
   }
---
+++ assets/js/components/DeregistrationModal/DeregistrationModal.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { EOS_CLEANING_SERVICES } from 'eos-icons-react';
+
+import Modal from '@components/Modal';
+import Button from '@components/Button';
+
+function DeregistrationModal({
+  hostname,
+  isOpen = false,
+  onCleanUp,
+  onCancel,
+}) {
+  return (
+    <Modal
+      title={`Clean up data discovered by agent on host ${hostname}`}
+      open={isOpen}
+      onClose={onCancel}
+    >
+      <div className="text-gray-500">
+        This action will cause Trento to stop tracking all the components
+        discovered by the agent in this host, including the host itself and any
+        other component depending on it.
+      </div>
+      <div className="flex justify-start gap-2 mt-4">
+        <Button
+          type="default-fit"
+          className="inline-block mx-0.5 border-green-500 border w-fit"
+          size="small"
+          onClick={onCleanUp}
+        >
+          <EOS_CLEANING_SERVICES size="base" className="fill-white inline" />
+          <span className="text-white text-sm font-bold pl-1.5">Clean up</span>
+        </Button>
+        <Button
+          type="primary-white-fit"
+          className="inline-block mx-0.5 border-green-500 border"
+          size="small"
+          onClick={onCancel}
+        >
+          Cancel
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default DeregistrationModal;
