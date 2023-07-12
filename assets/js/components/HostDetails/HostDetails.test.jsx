a720d46e0e77fa1f3722db68be34144fee16f9e8
Jamie Rodriguez <jamie.rodriguez@suse.com>
2023-07-12 14:52:44 +0200

    Organise tests inside `describe` blocks

--- assets/js/components/HostDetails/HostDetails.test.jsx
+++ assets/js/components/HostDetails/HostDetails.test.jsx
@@ -22,160 +22,166 @@ describe('HostDetails component', () => {
     axiosMock.reset();
   });
 
-  it('should not show any warning message if the agent version is correct', () => {
-    const hosts = hostFactory.buildList(1, { agent_version: '2.0.0' });
-    const { id: hostID } = hosts[0];
-    const state = {
-      ...defaultInitialState,
-      hostsList: {
-        hosts,
-      },
-    };
-    const [StatefulHostDetails] = withState(<HostDetails />, state);
+  describe('agent version', () => {
+    it('should not show any warning message if the agent version is correct', () => {
+      const hosts = hostFactory.buildList(1, { agent_version: '2.0.0' });
+      const { id: hostID } = hosts[0];
+      const state = {
+        ...defaultInitialState,
+        hostsList: {
+          hosts,
+        },
+      };
+      const [StatefulHostDetails] = withState(<HostDetails />, state);
 
-    renderWithRouterMatch(StatefulHostDetails, {
-      path: '/hosts/:hostID',
-      route: `/hosts/${hostID}`,
+      renderWithRouterMatch(StatefulHostDetails, {
+        path: '/hosts/:hostID',
+        route: `/hosts/${hostID}`,
+      });
+
+      expect(
+        screen.queryByText(
+          'Agent version 2.0.0 or greater is required for the new checks engine.'
+        )
+      ).not.toBeInTheDocument();
     });
 
-    expect(
-      screen.queryByText(
-        'Agent version 2.0.0 or greater is required for the new checks engine.'
-      )
-    ).not.toBeInTheDocument();
+    it('should show 2.0.0 version required warning message', () => {
+      const hosts = hostFactory.buildList(1, { agent_version: '1.0.0' });
+      const { id: hostID } = hosts[0];
+      const state = {
+        ...defaultInitialState,
+        hostsList: {
+          hosts,
+        },
+      };
+      const [StatefulHostDetails] = withState(<HostDetails />, state);
+
+      renderWithRouterMatch(StatefulHostDetails, {
+        path: '/hosts/:hostID',
+        route: `/hosts/${hostID}`,
+      });
+
+      expect(
+        screen.getByText(
+          'Agent version 2.0.0 or greater is required for the new checks engine.'
+        )
+      ).toBeInTheDocument();
+    });
   });
 
-  it('should show 2.0.0 version required warning message', () => {
-    const hosts = hostFactory.buildList(1, { agent_version: '1.0.0' });
-    const { id: hostID } = hosts[0];
-    const state = {
-      ...defaultInitialState,
-      hostsList: {
-        hosts,
-      },
-    };
-    const [StatefulHostDetails] = withState(<HostDetails />, state);
+  describe('deregistration', () => {
+    it('should not display clean up button when host is not deregisterable', () => {
+      const hosts = hostFactory.buildList(1, { deregisterable: false });
+      const state = {
+        ...defaultInitialState,
+        hostsList: {
+          hosts,
+        },
+      };
+      const [StatefulHostDetails] = withState(<HostDetails />, state);
 
-    renderWithRouterMatch(StatefulHostDetails, {
-      path: '/hosts/:hostID',
-      route: `/hosts/${hostID}`,
+      const { id } = hosts[0];
+      renderWithRouterMatch(StatefulHostDetails, {
+        path: '/hosts/:hostID',
+        route: `/hosts/${id}`,
+      });
+
+      const cleanUpButton = screen.queryByRole('button', { name: 'Clean up' });
+      expect(cleanUpButton).not.toBeInTheDocument();
     });
 
-    expect(
-      screen.getByText(
-        'Agent version 2.0.0 or greater is required for the new checks engine.'
-      )
-    ).toBeInTheDocument();
-  });
+    it('should display clean up button when host is deregisterable', () => {
+      const hosts = hostFactory.buildList(1, { deregisterable: true });
 
-  it('should not display clean up button when host is not deregisterable', () => {
-    const hosts = hostFactory.buildList(1, { deregisterable: false });
-    const state = {
-      ...defaultInitialState,
-      hostsList: {
-        hosts,
-      },
-    };
-    const [StatefulHostDetails] = withState(<HostDetails />, state);
+      const state = {
+        ...defaultInitialState,
+        hostsList: {
+          hosts,
+        },
+      };
+      const [StatefulHostDetails] = withState(<HostDetails />, state);
 
-    const { id } = hosts[0];
-    renderWithRouterMatch(StatefulHostDetails, {
-      path: '/hosts/:hostID',
-      route: `/hosts/${id}`,
+      const { id } = hosts[0];
+
+      renderWithRouterMatch(StatefulHostDetails, {
+        path: '/hosts/:hostID',
+        route: `/hosts/${id}`,
+      });
+
+      expect(
+        screen.getByRole('button', { name: 'Clean up' })
+      ).toBeInTheDocument();
     });
 
-    const cleanUpButton = screen.queryByRole('button', { name: 'Clean up' });
-    expect(cleanUpButton).not.toBeInTheDocument();
-  });
+    it('should show the host in deregistering state', () => {
+      const host = hostFactory.build({
+        deregisterable: true,
+        deregistering: true,
+      });
+      const state = {
+        ...defaultInitialState,
+        hostsList: {
+          hosts: [host],
+        },
+      };
 
-  it('should display clean up button when host is deregisterable', () => {
-    const hosts = hostFactory.buildList(1, { deregisterable: true });
+      const [StatefulHostDetails] = withState(<HostDetails />, state);
 
-    const state = {
-      ...defaultInitialState,
-      hostsList: {
-        hosts,
-      },
-    };
-    const [StatefulHostDetails] = withState(<HostDetails />, state);
+      const { id } = host;
 
-    const { id } = hosts[0];
+      renderWithRouterMatch(StatefulHostDetails, {
+        path: '/hosts/:hostID',
+        route: `/hosts/${id}`,
+      });
 
-    renderWithRouterMatch(StatefulHostDetails, {
-      path: '/hosts/:hostID',
-      route: `/hosts/${id}`,
+      const cleanUpButton = screen.queryByRole('button', { name: 'Clean up' });
+      expect(cleanUpButton).not.toBeInTheDocument();
+
+      const loadingSpinner = screen.getByRole('alert', { name: 'Loading' });
+      expect(loadingSpinner).toBeInTheDocument();
     });
 
-    expect(
-      screen.getByRole('button', { name: 'Clean up' })
-    ).toBeInTheDocument();
-  });
+    it('should request a deregistration when the clean up button in the modal is clicked', async () => {
+      const host = hostFactory.build({ deregisterable: true });
+      const state = {
+        ...defaultInitialState,
+        hostsList: {
+          hosts: [host],
+        },
+      };
 
-  it('should show the host in deregistering state', () => {
-    const host = hostFactory.build({
-      deregisterable: true,
-      deregistering: true,
+      const [StatefulHostDetails, store] = withState(<HostDetails />, state);
+
+      const { id, hostname } = host;
+
+      renderWithRouterMatch(StatefulHostDetails, {
+        path: '/hosts/:hostID',
+        route: `/hosts/${id}`,
+      });
+
+      const cleanUpButton = screen.getByRole('button', { name: 'Clean up' });
+      fireEvent.click(cleanUpButton);
+
+      expect(
+        screen.getByText(
+          `Clean up data discovered by agent on host ${hostname}`
+        )
+      ).toBeInTheDocument();
+
+      const cleanUpModalButton = screen.getAllByRole('button', {
+        name: 'Clean up',
+      })[1];
+      fireEvent.click(cleanUpModalButton);
+
+      const actions = store.getActions();
+      const expectedActions = [
+        {
+          type: 'DEREGISTER_HOST',
+          payload: expect.objectContaining({ id, hostname }),
+        },
+      ];
+      expect(actions).toEqual(expect.arrayContaining(expectedActions));
     });
-    const state = {
-      ...defaultInitialState,
-      hostsList: {
-        hosts: [host],
-      },
-    };
-
-    const [StatefulHostDetails] = withState(<HostDetails />, state);
-
-    const { id } = host;
-
-    renderWithRouterMatch(StatefulHostDetails, {
-      path: '/hosts/:hostID',
-      route: `/hosts/${id}`,
-    });
-
-    const cleanUpButton = screen.queryByRole('button', { name: 'Clean up' });
-    expect(cleanUpButton).not.toBeInTheDocument();
-
-    const loadingSpinner = screen.getByRole('alert', { name: 'Loading' });
-    expect(loadingSpinner).toBeInTheDocument();
-  });
-
-  it('should request a deregistration when the clean up button in the modal is clicked', async () => {
-    const host = hostFactory.build({ deregisterable: true });
-    const state = {
-      ...defaultInitialState,
-      hostsList: {
-        hosts: [host],
-      },
-    };
-
-    const [StatefulHostDetails, store] = withState(<HostDetails />, state);
-
-    const { id, hostname } = host;
-
-    renderWithRouterMatch(StatefulHostDetails, {
-      path: '/hosts/:hostID',
-      route: `/hosts/${id}`,
-    });
-
-    const cleanUpButton = screen.getByRole('button', { name: 'Clean up' });
-    fireEvent.click(cleanUpButton);
-
-    expect(
-      screen.getByText(`Clean up data discovered by agent on host ${hostname}`)
-    ).toBeInTheDocument();
-
-    const cleanUpModalButton = screen.getAllByRole('button', {
-      name: 'Clean up',
-    })[1];
-    fireEvent.click(cleanUpModalButton);
-
-    const actions = store.getActions();
-    const expectedActions = [
-      {
-        type: 'DEREGISTER_HOST',
-        payload: expect.objectContaining({ id, hostname }),
-      },
-    ];
-    expect(actions).toEqual(expect.arrayContaining(expectedActions));
   });
 });
