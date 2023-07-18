39cf8a058872ef4d2ca0db8c95e53bd81a836344
Nelson Kopliku <nelson.kopliku@suse.com>
2023-07-18 14:54:58 +0200

    Add getClusterCheckSelection selector on top of the centralized checks selection state

--- assets/js/state/selectors/checksSelection.js
+++ assets/js/state/selectors/checksSelection.js
@@ -1,6 +1,11 @@
-import { TARGET_HOST } from '@lib/model';
+import { TARGET_HOST, TARGET_CLUSTER } from '@lib/model';
 
 export const getHostCheckSelection =
   (hostID) =>
   ({ checksSelection }) =>
     checksSelection?.[TARGET_HOST][hostID] || {};
+
+export const getClusterCheckSelection =
+  (clusterID) =>
+  ({ checksSelection }) =>
+    checksSelection?.[TARGET_CLUSTER][clusterID] || {};
--- assets/js/state/selectors/checksSelection.test.js
+++ assets/js/state/selectors/checksSelection.test.js
@@ -1,5 +1,8 @@
 import { faker } from '@faker-js/faker';
-import { getHostCheckSelection } from './checksSelection';
+import {
+  getHostCheckSelection,
+  getClusterCheckSelection,
+} from './checksSelection';
 
 describe('Checks Selection selector', () => {
   it(`should get a host's check selection state`, () => {
@@ -8,12 +11,12 @@ describe('Checks Selection selector', () => {
       getHostCheckSelection(hostID)({
         checksSelection: {
           host: {
-            [faker.datatype.uuid()]: { status: 'successfully_saved' },
-            [hostID]: { status: 'saving' },
+            [faker.datatype.uuid()]: { status: 'SUCCESSFULLY_SAVED' },
+            [hostID]: { status: 'SAVING' },
           },
         },
       })
-    ).toEqual({ status: 'saving' });
+    ).toEqual({ status: 'SAVING' });
   });
 
   it(`should not get a host's check selection state if not present`, () => {
@@ -22,8 +25,36 @@ describe('Checks Selection selector', () => {
       getHostCheckSelection(hostWithoutSelection)({
         checksSelection: {
           host: {
-            [faker.datatype.uuid()]: { status: 'successfully_saved' },
-            [faker.datatype.uuid()]: { status: 'saving' },
+            [faker.datatype.uuid()]: { status: 'SUCCESSFULLY_SAVED' },
+            [faker.datatype.uuid()]: { status: 'SAVING' },
+          },
+        },
+      })
+    ).toEqual({});
+  });
+
+  it(`should get a cluster's check selection state`, () => {
+    const clusterID = faker.datatype.uuid();
+    expect(
+      getClusterCheckSelection(clusterID)({
+        checksSelection: {
+          cluster: {
+            [faker.datatype.uuid()]: { status: 'SUCCESSFULLY_SAVED' },
+            [clusterID]: { status: 'SAVING' },
+          },
+        },
+      })
+    ).toEqual({ status: 'SAVING' });
+  });
+
+  it(`should not get a cluster's check selection state if not present`, () => {
+    const clustertWithoutSelection = faker.datatype.uuid();
+    expect(
+      getClusterCheckSelection(clustertWithoutSelection)({
+        checksSelection: {
+          cluster: {
+            [faker.datatype.uuid()]: { status: 'SUCCESSFULLY_SAVED' },
+            [faker.datatype.uuid()]: { status: 'SAVING' },
           },
         },
       })
