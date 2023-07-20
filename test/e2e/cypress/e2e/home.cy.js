30679ecefab221fa09ddcabff055460b03acb169
arbulu89 <xarbulu@suse.com>
2023-07-20 16:42:15 +0200

    Add e2e test

---
+++ test/e2e/cypress/e2e/home.cy.js
@@ -0,0 +1,19 @@
+context('Homepage', () => {
+  before(() => {
+    cy.visit('/');
+    cy.url().should('include', '/');
+  });
+
+  describe('Deregistration', () => {
+    const sapSystemNwp = {
+      sid: 'NWP',
+      hostId: '9cd46919-5f19-59aa-993e-cf3736c71053',
+    };
+
+    it(`should not display SAP System ${sapSystemNwp.sid} after it is deregistered`, () => {
+      cy.contains(sapSystemNwp.sid).should('exist');
+      cy.deregisterHost(sapSystemNwp.hostId);
+      cy.contains(sapSystemNwp.sid).should('not.exist');
+    });
+  });
+});
