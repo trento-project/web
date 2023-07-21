0fe593dcac0441442e0915c52cdadb6ddb665627
Nelson Kopliku <nelson.kopliku@suse.com>
2023-07-21 10:35:57 +0200

    Add Checks related Call to actions in host detail

--- assets/js/components/HostDetails/HostDetails.jsx
+++ assets/js/components/HostDetails/HostDetails.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import classNames from 'classnames';
+
+import { EOS_CLEAR_ALL, EOS_PLAY_CIRCLE, EOS_SETTINGS } from 'eos-icons-react';
 
 import { networkClient } from '@lib/network';
 import { agentVersionWarning } from '@lib/agent';
 
+import Button from '@components/Button';
+import TriggerChecksExecutionRequest from '@components/TriggerChecksExecutionRequest';
 import ListView from '@components/ListView';
 import Table from '@components/Table';
 import PageHeader from '@components/PageHeader';
@@ -30,6 +35,9 @@ import {
 
 function HostDetails() {
   const { hostID } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const host = useSelector(getHost(hostID));
   const cluster = useSelector(getClusterByHost(hostID));
   const sapSystems = useSelector(getInstancesOnHost(hostID));
@@ -39,9 +47,6 @@ function HostDetails() {
 
   const [exportersStatus, setExportersStatus] = useState([]);
   const [cleanUpModalOpen, setCleanUpModalOpen] = useState(false);
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const cleanUpHost = ({ id, hostname }) => {
     setCleanUpModalOpen(false);
@@ -79,38 +84,81 @@ function HostDetails() {
       />
       <div>
         <BackButton url="/hosts">Back to Hosts</BackButton>
-        <div className="flex">
-          <PageHeader>
-            Host Details: <span className="font-bold">{host.hostname}</span>
-          </PageHeader>
-          <StatusPill
-            className="self-center ml-4 shadow"
-            heartbeat={host.heartbeat}
-          >
-            Agent
-          </StatusPill>
-
-          {Object.entries(exportersStatus).map(
-            ([exporterName, exporterStatus]) => (
-              <StatusPill
-                key={exporterName}
-                className="self-center ml-4 shadow"
-                heartbeat={exporterStatus}
+        <div className="flex flex-wrap">
+          <div className="flex w-1/2 h-auto overflow-hidden overflow-ellipsis break-words">
+            <PageHeader>
+              Host Details: <span className="font-bold">{host.hostname}</span>
+            </PageHeader>
+          </div>
+          <div className="flex w-1/2 justify-end">
+            <div className="flex w-fit whitespace-nowrap">
+              {host.deregisterable && (
+                <CleanUpButton
+                  cleaning={host.deregistering}
+                  onClick={() => {
+                    setCleanUpModalOpen(true);
+                  }}
+                />
+              )}
+              <Button
+                type="primary-white"
+                className="inline-block mx-0.5 border-green-500 border"
+                size="small"
+                onClick={() => navigate(`/hosts/${hostID}/settings`)}
               >
-                {exporterName}
-              </StatusPill>
-            )
-          )}
-          {host.deregisterable && (
-            <div className="ml-auto my-auto">
-              <CleanUpButton
-                cleaning={host.deregistering}
-                onClick={() => {
-                  setCleanUpModalOpen(true);
-                }}
-              />
+                <EOS_SETTINGS className="inline-block fill-jungle-green-500" />{' '}
+                Check Selection
+              </Button>
+
+              <Button
+                type="primary-white"
+                className="mx-0.5 border-green-500 border disabled:bg-slate-50 disabled:text-slate-500 disabled:border-gray-400"
+                size="small"
+                onClick={() => navigate(`/hosts/${hostID}/executions/last`)}
+                disabled
+              >
+                <EOS_CLEAR_ALL
+                  className={classNames('inline-block ', {
+                    'fill-jungle-green-500': false,
+                    'fill-slate-500': true,
+                  })}
+                />{' '}
+                <span>Show Results</span>
+              </Button>
+
+              <TriggerChecksExecutionRequest
+                cssClasses="flex rounded relative ml-0.5 disabled:bg-gray-400 disabled:text-gray-200 disabled:border-gray-400"
+                disabled
+              >
+                <EOS_PLAY_CIRCLE
+                  className={classNames('inline-block mr-1', {
+                    'fill-jungle-green-500': false,
+                    'fill-gray-200': true,
+                  })}
+                />{' '}
+                <span>Start Execution</span>
+              </TriggerChecksExecutionRequest>
             </div>
-          )}
+          </div>
+          <div className="pb-3">
+            <StatusPill
+              className="self-center shadow"
+              heartbeat={host.heartbeat}
+            >
+              Agent
+            </StatusPill>
+            {Object.entries(exportersStatus).map(
+              ([exporterName, exporterStatus]) => (
+                <StatusPill
+                  key={exporterName}
+                  className="self-center ml-4 shadow"
+                  heartbeat={exporterStatus}
+                >
+                  {exporterName}
+                </StatusPill>
+              )
+            )}
+          </div>
         </div>
         {versionWarningMessage && (
           <WarningBanner>{versionWarningMessage}</WarningBanner>
@@ -128,7 +176,6 @@ function HostDetails() {
             ]}
           />
         </div>
-
         <div className="mt-8 bg-white shadow rounded-lg py-4 px-8">
           <iframe
             title="node-exporter chart"
