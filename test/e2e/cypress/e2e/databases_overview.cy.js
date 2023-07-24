cbacc96b7a060729fa8654fec6cd37a0655e48c1
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-07-24 14:45:22 +0100

    Add restoration test for db overview

--- test/e2e/cypress/e2e/databases_overview.cy.js
+++ test/e2e/cypress/e2e/databases_overview.cy.js
@@ -18,4 +18,19 @@ context('Databases Overview', () => {
       cy.contains(hdqDatabase.sid).should('not.exist');
     });
   });
+
+  describe('Restoration', () => {
+    const databaseToRestore = {
+      sid: 'HDQ',
+      hanaPrimary: {
+        name: 'vmhdbqas01',
+        id: '99cf8a3a-48d6-57a4-b302-6e4482227ab6',
+      },
+    };
+
+    it(`should display DB ${databaseToRestore.sid} again after restoring the primary instance`, () => {
+      cy.loadScenario(`host-${databaseToRestore.hanaPrimary.name}-restore`);
+      cy.contains(databaseToRestore.sid).should('exist');
+    });
+  });
 });
