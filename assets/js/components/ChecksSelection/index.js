21fc3005b9e41e62f3bc5db67d18b38f06b66050
Alessio Biancalana <alessio.biancalana@suse.com>
2023-07-28 16:59:05 +0200

    Update ChecksSelection component to new UX

--- assets/js/components/ChecksSelection/index.js
+++ assets/js/components/ChecksSelection/index.js
@@ -1,3 +1,6 @@
 import ChecksSelection from './ChecksSelection';
 
+export const canStartExecution = (selectedChecks, savingSuccessful) =>
+  savingSuccessful && selectedChecks.length > 0;
+
 export default ChecksSelection;
