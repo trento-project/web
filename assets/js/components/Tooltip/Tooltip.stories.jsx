56d088a7c8ba2ad30d36dd5dad2ab8bca54c17ee
Nelson Kopliku <nelson.kopliku@suse.com>
2023-07-31 15:50:37 +0200

    Update Tooltip stories

--- assets/js/components/Tooltip/Tooltip.stories.jsx
+++ assets/js/components/Tooltip/Tooltip.stories.jsx
@@ -1,22 +1,68 @@
 import React from 'react';
 
-import Tooltip from '.';
+import { TooltipNext } from '.';
+import { PLACES } from './Tooltip';
 
 export default {
   title: 'Tooltip',
-  component: Tooltip,
+  component: TooltipNext,
+  argTypes: {
+    content: {
+      type: 'string',
+      description: 'Content to be rendered int he tooltip',
+      options: PLACES,
+      control: { type: 'text' },
+    },
+    place: {
+      type: 'string',
+      description: 'Position of the tooltip',
+      options: PLACES,
+      control: { type: 'radio' },
+    },
+    offset: {
+      type: 'number',
+      description: 'Space between the tooltip and the anchor',
+      control: { type: 'number' },
+    },
+    isEnabled: {
+      type: 'boolean',
+      description: 'Whether the tooltip is enabled',
+      control: { type: 'boolean' },
+    },
+    isOpen: {
+      type: 'boolean',
+      description: 'Whether the tooltip should be open',
+      control: { type: 'boolean' },
+    },
+  },
+  render: (args) => (
+    <div className="p-12 flex items-center justify-center">
+      <TooltipNext {...args}>
+        <div className="bg-sky-400 p-2 text-white font-semibold rounded">
+          Hover me!
+        </div>
+      </TooltipNext>
+    </div>
+  ),
+};
+
+export const Default = {
   args: {
-    tooltipText: 'Hello World',
-    width: 'w-full',
+    content: 'Hello World',
   },
 };
 
-export function Default(args) {
-  return (
-    <div className="p-12">
-      <div className="relative">
-        <Tooltip {...args}>Hover me!</Tooltip>
-      </div>
-    </div>
-  );
-}
+export const Positioning = {
+  args: {
+    ...Default.args,
+    isOpen: true,
+  },
+};
+
+export const WithOffset = {
+  args: {
+    ...Default.args,
+    isOpen: true,
+    offset: -1,
+  },
+};
