3abf8ea672b57d77f27246ce677d1e145f7f35ea
Jamie Rodriguez <jamie.rodriguez@suse.com>
2023-08-01 15:12:32 +0200

    Don't load host data for `DatabaseRestored` event

--- lib/trento/application/projectors/database_projector.ex
+++ lib/trento/application/projectors/database_projector.ex
@@ -302,7 +302,7 @@ defmodule Trento.DatabaseProjector do
     database =
       DatabaseReadModel
       |> Repo.get!(sap_system_id)
-      |> Repo.preload([:tags, :database_instances, [database_instances: :host]])
+      |> Repo.preload([:tags, :database_instances])
 
     TrentoWeb.Endpoint.broadcast(
       @databases_topic,
