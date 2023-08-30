7d38ba688bd8041465cefa09f1f664be27f940b6
arbulu89 <xarbulu@suse.com>
2023-08-30 10:08:49 +0200

    Update deregistration modal to include instances content

--- assets/js/components/DeregistrationModal/DeregistrationModal.jsx
+++ assets/js/components/DeregistrationModal/DeregistrationModal.jsx
@@ -5,23 +5,45 @@ import { EOS_CLEANING_SERVICES } from 'eos-icons-react';
 import Modal from '@components/Modal';
 import Button from '@components/Button';
 
+import { APPLICATION_TYPE, DATABASE_TYPE } from '@lib/model';
+
+const getContentByType = (type, data) => {
+  switch (type) {
+    case APPLICATION_TYPE:
+      return {
+        title: `Clean up absent instance ${data.instanceNumber} from ${data.sid} system`,
+        body: `In the case of an ASCS instance or a unique Application Server Instance, 
+          this action will cause the complete deregistration of the system.`,
+      };
+    case DATABASE_TYPE:
+      return {
+        title: `Clean up absent instance ${data.instanceNumber} from ${data.sid} database`,
+        body: `In the case of the last database instance, or the last Primary instance in 
+          a system replication setup, this action will cause the complete deregistration 
+          of the database and the system above if any.`,
+      };
+    default:
+      return {
+        title: `Clean up data discovered by agent on host ${data.hostname}`,
+        body: `This action will cause Trento to stop tracking all the components
+          discovered by the agent in this host, including the host itself and any
+          other component depending on it.`,
+      };
+  }
+};
+
 function DeregistrationModal({
-  hostname,
+  contentType = 'host',
   isOpen = false,
   onCleanUp,
   onCancel,
+  ...rest
 }) {
+  const { title, body } = getContentByType(contentType, rest);
+
   return (
-    <Modal
-      title={`Clean up data discovered by agent on host ${hostname}`}
-      open={isOpen}
-      onClose={onCancel}
-    >
-      <div className="text-gray-500">
-        This action will cause Trento to stop tracking all the components
-        discovered by the agent in this host, including the host itself and any
-        other component depending on it.
-      </div>
+    <Modal title={title} open={isOpen} onClose={onCancel}>
+      <div className="text-gray-500">{body}</div>
       <div className="flex justify-start gap-2 mt-4">
         <Button
           type="default-fit"
--- assets/js/components/DeregistrationModal/DeregistrationModal.test.jsx
+++ assets/js/components/DeregistrationModal/DeregistrationModal.test.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { faker } from '@faker-js/faker';
 
+import { APPLICATION_TYPE, DATABASE_TYPE } from '@lib/model';
+
 import DeregistrationModal from '.';
 
 describe('Deregistration Modal component', () => {
-  it('should render deregistration modal correctly', async () => {
+  it('should render a host deregistration modal correctly', async () => {
     const hostname = faker.name.firstName();
 
     render(
@@ -19,8 +21,66 @@ describe('Deregistration Modal component', () => {
 
     expect(await screen.findByText(hostname, { exact: false })).toBeTruthy();
     expect(
+      await screen.findByText(
+        'This action will cause Trento to stop tracking',
+        { exact: false }
+      )
+    ).toBeTruthy();
+    expect(
       await screen.findByRole('button', { name: /Clean up/i })
     ).toBeTruthy();
     expect(await screen.findByRole('button', { name: /Cancel/i })).toBeTruthy();
+  });
+
+  it('should render an application instance deregistration modal correctly', async () => {
+    const sid = 'PRD';
+    const instanceNumber = '00';
+
+    render(
+      <DeregistrationModal
+        contentType={APPLICATION_TYPE}
+        sid={sid}
+        instanceNumber={instanceNumber}
+        isOpen
+        onCleanUp={() => {}}
+        onCancel={() => {}}
+      />
+    );
+
+    expect(await screen.findByText(sid, { exact: false })).toBeTruthy();
+    expect(
+      await screen.findByText(instanceNumber, { exact: false })
+    ).toBeTruthy();
+    expect(
+      await screen.findByText('In the case of an ASCS instance', {
+        exact: false,
+      })
+    ).toBeTruthy();
+  });
+
+  it('should render an application instance deregistration modal correctly', async () => {
+    const sid = 'PRD';
+    const instanceNumber = '00';
+
+    render(
+      <DeregistrationModal
+        contentType={DATABASE_TYPE}
+        sid={sid}
+        instanceNumber={instanceNumber}
+        isOpen
+        onCleanUp={() => {}}
+        onCancel={() => {}}
+      />
+    );
+
+    expect(await screen.findByText(sid, { exact: false })).toBeTruthy();
+    expect(
+      await screen.findByText(instanceNumber, { exact: false })
+    ).toBeTruthy();
+    expect(
+      await screen.findByText('In the case of the last database instance', {
+        exact: false,
+      })
+    ).toBeTruthy();
   });
 });
