01b0dfe3071cfc8f7a4c07c005cef8ef9f60d14e
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-08-31 09:04:11 +0100

    Remove unused import

--- assets/js/components/InstanceOverview/InstanceOverview.test.jsx
+++ assets/js/components/InstanceOverview/InstanceOverview.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, waitFor, act, within } from '@testing-library/react';
+import { screen, waitFor, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { databaseInstanceFactory } from '@lib/test-utils/factories';
