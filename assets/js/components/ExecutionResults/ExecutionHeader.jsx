25e33ae0eddac049b3386f3f76150c7386564e3f
Nelson Kopliku <nelson.kopliku@suse.com>
2023-09-07 14:09:04 +0200

    Generalize ExecutionHeader on target type

--- assets/js/components/ExecutionResults/ExecutionHeader.jsx
+++ assets/js/components/ExecutionResults/ExecutionHeader.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import {
+  UNKNOWN_PROVIDER,
+  VMWARE_PROVIDER,
+  TARGET_CLUSTER,
+  TARGET_HOST,
+} from '@lib/model';
+
 import BackButton from '@components/BackButton';
 import { ClusterInfoBox } from '@components/ClusterDetails';
+import HostInfoBox from '@components/HostDetails/HostInfoBox';
 
 import ChecksResultFilters from '@components/ExecutionResults/ChecksResultFilters';
-import { UNKNOWN_PROVIDER, VMWARE_PROVIDER } from '@lib/model';
 import WarningBanner from '@components/Banners/WarningBanner';
+import { isTargetCluster } from './checksUtils';
 
 function ExecutionHeader({
-  clusterID,
-  clusterName,
-  cloudProvider,
-  clusterScenario,
+  targetID,
+  targetName,
+  targetType,
+  target,
   savedFilters,
   onFilterChange = () => {},
   onFilterSave = () => {},
 }) {
   return (
     <>
-      <BackButton url={`/clusters/${clusterID}`}>
-        Back to Cluster Details
-      </BackButton>
+      <BackToTargetDetails targetType={targetType} targetID={targetID} />
       <div className="flex mb-4 justify-between">
         <h1 className="text-3xl w-3/5">
-          <span className="font-medium">Checks Results for cluster</span>{' '}
+          <span className="font-medium">Checks Results for {targetType}</span>{' '}
           <span
             className={classNames(
               'font-bold truncate w-60 inline-block align-top'
             )}
           >
-            {clusterName}
+            {targetName}
           </span>
         </h1>
         <ChecksResultFilters
@@ -39,24 +45,71 @@ function ExecutionHeader({
           onSave={onFilterSave}
         />
       </div>
-      {cloudProvider === UNKNOWN_PROVIDER && (
+      {isTargetCluster(targetType) && (
+        <ClusterWarningBanner cloudProvider={target.provider} />
+      )}
+      <TargetInfoBox targetType={targetType} target={target} />
+    </>
+  );
+}
+
+function BackToTargetDetails({ targetType, targetID }) {
+  let backUrl;
+  let backText;
+  switch (targetType) {
+    case TARGET_CLUSTER:
+      backUrl = `/clusters/${targetID}`;
+      backText = 'Back to Cluster Details';
+      break;
+    case TARGET_HOST:
+      backUrl = `/hosts/${targetID}`;
+      backText = 'Back to Host Details';
+      break;
+    default:
+  }
+  return <BackButton url={backUrl}>{backText}</BackButton>;
+}
+
+function TargetInfoBox({ targetType, target }) {
+  switch (targetType) {
+    case TARGET_CLUSTER:
+      return (
+        <ClusterInfoBox haScenario={target.type} provider={target.provider} />
+      );
+    case TARGET_HOST:
+      return (
+        <HostInfoBox
+          provider={target.provider}
+          agentVersion={target.agent_version}
+        />
+      );
+    default:
+  }
+  return null;
+}
+
+function ClusterWarningBanner({ cloudProvider }) {
+  switch (cloudProvider) {
+    case UNKNOWN_PROVIDER:
+      return (
         <WarningBanner>
           The following results are valid for on-premise bare metal platforms.
           <br />
           If you are running your HANA cluster on a different platform, please
           use results with caution
         </WarningBanner>
-      )}
-      {cloudProvider === VMWARE_PROVIDER && (
+      );
+    case VMWARE_PROVIDER:
+      return (
         <WarningBanner>
           Configuration checks for HANA scale-up performance optimized clusters
           on VMware are still in experimental phase. Please use results with
           caution.
         </WarningBanner>
-      )}
-      <ClusterInfoBox haScenario={clusterScenario} provider={cloudProvider} />
-    </>
-  );
+      );
+    default:
+  }
+  return null;
 }
 
 export default ExecutionHeader;
--- assets/js/components/ExecutionResults/ExecutionHeader.test.jsx
+++ assets/js/components/ExecutionResults/ExecutionHeader.test.jsx
@@ -3,52 +3,130 @@ import { screen } from '@testing-library/react';
 
 import { faker } from '@faker-js/faker';
 import { renderWithRouter } from '@lib/test-utils';
+import { clusterFactory, hostFactory } from '@lib/test-utils/factories';
 
 import '@testing-library/jest-dom';
 import ExecutionHeader from './ExecutionHeader';
 
-describe('Cluster Checks results ExecutionHeader Component', () => {
-  it('should render a header with expected information', () => {
-    const clusterID = faker.datatype.uuid();
-    const clusterName = faker.animal.bear();
-    const cloudProvider = 'azure';
-    const clusterScenario = 'hana_scale_up';
+describe('Checks results ExecutionHeader Component', () => {
+  describe('With target Cluster', () => {
+    it('should render a header with expected cluster information', () => {
+      const clusterID = faker.datatype.uuid();
+      const clusterName = faker.animal.bear();
+      const cloudProvider = 'azure';
+      const clusterScenario = 'hana_scale_up';
 
-    renderWithRouter(
-      <ExecutionHeader
-        clusterID={clusterID}
-        clusterName={clusterName}
-        cloudProvider={cloudProvider}
-        clusterScenario={clusterScenario}
-      />
-    );
+      const target = clusterFactory.build({
+        id: clusterID,
+        name: clusterName,
+        provider: cloudProvider,
+        type: clusterScenario,
+      });
 
-    expect(screen.getByText('Azure')).toBeTruthy();
-    expect(screen.getByText('HANA scale-up')).toBeTruthy();
-    expect(screen.getByText(clusterName));
+      renderWithRouter(
+        <ExecutionHeader
+          targetID={clusterID}
+          targetName={clusterName}
+          targetType="cluster"
+          target={target}
+        />
+      );
+
+      expect(screen.getByText('Back to Cluster Details')).toBeTruthy();
+      expect(screen.getByText('Azure')).toBeTruthy();
+      expect(screen.getByText('HANA scale-up')).toBeTruthy();
+      expect(screen.getByText('Checks Results for cluster')).toBeTruthy();
+      expect(screen.getByText(clusterName)).toBeTruthy();
+    });
+
+    it('should render a header with a warning banner on an unknown provider detection', () => {
+      const clusterID = faker.datatype.uuid();
+      const clusterName = faker.animal.bear();
+      const cloudProvider = 'unknown';
+      const clusterScenario = 'hana_scale_up';
+
+      const target = clusterFactory.build({
+        id: clusterID,
+        name: clusterName,
+        provider: cloudProvider,
+        type: clusterScenario,
+      });
+
+      renderWithRouter(
+        <ExecutionHeader
+          targetID={clusterID}
+          targetName={clusterName}
+          targetType="cluster"
+          target={target}
+        />
+      );
+
+      expect(screen.getByText('Provider not recognized')).toBeTruthy();
+      expect(screen.getByText('HANA scale-up')).toBeTruthy();
+      expect(screen.getByText('Checks Results for cluster')).toBeTruthy();
+      expect(
+        screen.getByText(
+          /The following results are valid for on-premise bare metal platforms./
+        )
+      ).toBeTruthy();
+    });
   });
 
-  it('should render a header with a warning banner on an unknown provider detection', () => {
-    const clusterID = faker.datatype.uuid();
-    const clusterName = faker.animal.bear();
-    const cloudProvider = 'unknown';
-    const clusterScenario = 'hana_scale_up';
+  describe('With target Host', () => {
+    it('should render a header with expected host information', () => {
+      const hostID = faker.datatype.uuid();
+      const hostName = faker.animal.bear();
+      const cloudProvider = 'aws';
+      const agentVersion = '2.1.1';
 
-    renderWithRouter(
-      <ExecutionHeader
-        clusterID={clusterID}
-        clusterName={clusterName}
-        cloudProvider={cloudProvider}
-        clusterScenario={clusterScenario}
-      />
-    );
+      const target = hostFactory.build({
+        id: hostID,
+        name: hostName,
+        provider: cloudProvider,
+        agent_version: agentVersion,
+      });
 
-    expect(screen.getByText('Provider not recognized')).toBeTruthy();
-    expect(screen.getByText('HANA scale-up')).toBeTruthy();
-    expect(
-      screen.getByText(
-        /The following results are valid for on-premise bare metal platforms./
-      )
-    ).toBeTruthy();
+      renderWithRouter(
+        <ExecutionHeader
+          targetID={hostID}
+          targetName={hostName}
+          targetType="host"
+          target={target}
+        />
+      );
+
+      expect(screen.getByText('Back to Host Details')).toBeTruthy();
+      expect(screen.getByText('AWS')).toBeTruthy();
+      expect(screen.getByText('Checks Results for host')).toBeTruthy();
+      expect(screen.getByText(hostName)).toBeTruthy();
+      expect(screen.getByText('Agent version')).toBeTruthy();
+      expect(screen.getByText(agentVersion)).toBeTruthy();
+    });
+
+    it('should not render a warning banner within the header on an unknown provider detection', () => {
+      const hostID = faker.datatype.uuid();
+      const hostName = faker.animal.bear();
+      const cloudProvider = 'unknown';
+
+      const target = hostFactory.build({
+        id: hostID,
+        name: hostName,
+        provider: cloudProvider,
+      });
+
+      renderWithRouter(
+        <ExecutionHeader
+          targetID={hostID}
+          targetName={hostName}
+          targetType="host"
+          target={target}
+        />
+      );
+
+      expect(screen.getByText('Provider not recognized')).toBeTruthy();
+      expect(
+        screen.queryByText(/The following results are valid/)
+      ).not.toBeTruthy();
+    });
   });
 });
