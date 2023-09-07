4646f8cc088bf62dea0805ba21823decb2085dad
Nelson Kopliku <nelson.kopliku@suse.com>
2023-09-07 15:00:03 +0200

    Initial generalization of CheckResultDetailPage on target type

--- assets/js/components/ExecutionResults/CheckResultDetail/CheckResultDetailPage.jsx
+++ assets/js/components/ExecutionResults/CheckResultDetail/CheckResultDetailPage.jsx
@@ -25,27 +25,31 @@ import {
   getCheckDescription,
   getCheckExpectations,
   isTargetHost,
+  isTargetCluster,
   getClusterCheckResults,
 } from '../checksUtils';
 import CheckDetailHeader from './CheckDetailHeader';
 
-function CheckResultDetailPage() {
-  const { clusterID, checkID, targetType, targetName } = useParams();
+function CheckResultDetailPage({ targetType }) {
+  const { targetID, checkID, resultTargetType, resultTargetName } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const {
-    clusterHosts,
-    cluster,
+    targetHosts,
+    target,
     catalog: { loading: catalogLoading, data: catalog, error: catalogError },
     lastExecution: {
       data: executionData,
       error: executionError,
       loading: executionLoading,
     },
-  } = useSelector((state) => getLastExecutionData(state, clusterID));
+  } = useSelector((state) => getLastExecutionData(state, targetID, targetType));
   const clustersIDList = useSelector((state) =>
-    state.clustersList.clusters.map((clusterObj) => clusterObj.id)
+    state.clustersList.clusters.map(({ id }) => id)
+  );
+  const hostsIDList = useSelector((state) =>
+    state.hostsList.hosts.map(({ id }) => id)
   );
 
   useEffect(() => {
@@ -53,20 +57,40 @@ function CheckResultDetailPage() {
       dispatch(updateCatalog());
     }
     if (!executionData) {
-      dispatch(updateLastExecution(clusterID));
+      dispatch(updateLastExecution(targetID));
     }
   }, []);
 
-  if (!clustersIDList.includes(clusterID) && clustersIDList.length > 0) {
+  const isHostExecution = isTargetHost(targetType);
+  const isClusterExecution = isTargetCluster(targetType);
+
+  if (
+    isClusterExecution &&
+    !clustersIDList.includes(targetID) &&
+    clustersIDList.length > 0
+  ) {
     return (
       <NotFound
-        buttonText="Go back to cluster overview"
-        onNavigate={() => navigate('/clusters/')}
+        buttonText="Go back to clusters overview"
+        onNavigate={() => navigate('/clusters')}
       />
     );
   }
 
-  if (!cluster || !executionData || executionData.status === 'running') {
+  if (
+    isHostExecution &&
+    !hostsIDList.includes(targetID) &&
+    hostsIDList.length > 0
+  ) {
+    return (
+      <NotFound
+        buttonText="Go back to hosts overview"
+        onNavigate={() => navigate('/hosts')}
+      />
+    );
+  }
+
+  if (!target || !executionData || executionData.status === 'running') {
     return (
       <div>
         <LoadingBox text="Loading..." />
@@ -74,32 +98,64 @@ function CheckResultDetailPage() {
     );
   }
 
-  const targetHost = isTargetHost(targetType);
-  const validClusterID = cluster.id === clusterID;
+  const isHostCheck = isTargetHost(resultTargetType);
+  const isClusterWideCheck = isTargetCluster(resultTargetType);
+
+  const validateTargetResult = () => {
+    switch (true) {
+      case isHostCheck:
+        return targetHosts.some(
+          ({ hostname }) => hostname === resultTargetName
+        );
+      case isClusterWideCheck:
+        return resultTargetName === target.name;
+      default:
+        return false;
+    }
+  };
+  const validTargetID = target.id === targetID;
   const validTarget =
-    isValidTargetType(targetType) &&
-    (targetHost
-      ? clusterHosts.some(({ hostname }) => hostname === targetName)
-      : targetName === cluster.name);
+    isValidTargetType(resultTargetType) && validateTargetResult();
 
   const validCheckID = executionData?.check_results.some(
     ({ check_id }) => check_id === checkID
   );
 
-  if (!validClusterID || !validTarget || !validCheckID) {
+  if (!validTargetID || !validTarget || !validCheckID) {
     return (
       <NotFound
         buttonText="Go back to last execution"
-        onNavigate={() => navigate(`/clusters/${clusterID}/executions/last`)}
+        onNavigate={() => {
+          switch (true) {
+            case isClusterExecution:
+              navigate(`/clusters/${targetID}/executions/last`);
+              break;
+            case isHostExecution:
+              navigate(`/hosts/${targetID}/executions/last`);
+              break;
+            default:
+          }
+        }}
       />
     );
   }
 
   const checkDescription = getCheckDescription(catalog, checkID);
 
-  const targetID = isTargetHost(targetType)
-    ? (clusterHosts.find(({ hostname }) => hostname === targetName) || {})?.id
-    : clusterID;
+  const getResultTargetID = () => {
+    switch (true) {
+      case isHostCheck:
+        return (
+          targetHosts.find(({ hostname }) => hostname === resultTargetName) ||
+          {}
+        )?.id;
+      case isClusterWideCheck:
+        return targetID;
+      default:
+        return null;
+    }
+  };
+  const resultTargetID = getResultTargetID();
 
   return (
     <ExecutionContainer
@@ -109,12 +165,12 @@ function CheckResultDetailPage() {
       executionRunning={RUNNING_STATES.includes(executionData?.status)}
     >
       <CheckDetailHeader
-        clusterID={clusterID}
+        clusterID={targetID}
         checkID={checkID}
         checkDescription={checkDescription}
-        targetType={targetType}
-        targetName={targetName}
-        cloudProvider={cluster?.provider}
+        targetType={resultTargetType}
+        targetName={resultTargetName}
+        cloudProvider={target?.provider}
         result={getClusterCheckResults(executionData, checkID)?.result}
       />
       <ResultsContainer
@@ -123,16 +179,16 @@ function CheckResultDetailPage() {
           catalogError ? `Failed loading catalog: ${catalogError}` : null,
           executionError ? `Failed loading execution: ${executionError}` : null,
         ]}
-        clusterID={clusterID}
+        targetID={targetID}
         hasAlreadyChecksResults={!!(executionData || executionLoading)}
-        selectedChecks={cluster?.selected_checks}
-        hosts={clusterHosts.map(getHostID)}
+        selectedChecks={target?.selected_checks}
+        hosts={targetHosts.map(getHostID)}
         onContentRefresh={() => {
           if (catalogError) {
             dispatch(updateCatalog());
           }
           if (executionError) {
-            dispatch(updateLastExecution(clusterID));
+            dispatch(updateLastExecution(targetID));
           }
         }}
         onStartExecution={(clusterId, hosts, selectedChecks, onNavigate) =>
@@ -144,10 +200,10 @@ function CheckResultDetailPage() {
         <CheckResultDetail
           checkID={checkID}
           expectations={getCheckExpectations(catalog, checkID)}
-          targetID={targetID}
-          targetType={targetType}
+          targetID={resultTargetID}
+          targetType={resultTargetType}
           executionData={executionData}
-          clusterHosts={clusterHosts}
+          clusterHosts={targetHosts}
         />
       </ResultsContainer>
     </ExecutionContainer>
