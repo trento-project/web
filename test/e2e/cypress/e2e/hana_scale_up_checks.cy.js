b5158f054efe5f502530664c87fedd0d65b79489
Alessio Biancalana <alessio.biancalana@suse.com>
2023-09-09 10:53:24 +0200

    Use fake agents for hana scale up checks

--- test/e2e/cypress/e2e/hana_scale_up_checks.cy.js
+++ test/e2e/cypress/e2e/hana_scale_up_checks.cy.js
@@ -1,67 +1,49 @@
-if (Cypress.env('REAL_CLUSTER_TESTS')) {
-  context('HANA scale-up checks', () => {
-    const clusterID = 'd2522281-2c76-52dc-8500-10bdf2cc6664';
+context('HANA scale-up checks', () => {
+  const clusterID = '597a00db-5920-5033-b3b4-1dc98ca8718a';
 
-    const PASSING = 'fill-jungle-green-500';
-    const WARNING = 'fill-yellow-500';
-    const CRITICAL = 'fill-red-500';
+  const PASSING = 'fill-jungle-green-500';
+  // eslint-disable-next-line no-unused-vars
+  const WARNING = 'fill-yellow-500';
+  // eslint-disable-next-line no-unused-vars
+  const CRITICAL = 'fill-red-500';
 
-    const expectedCheckResults = [
-      ['00081D', PASSING],
-      ['156F64', PASSING],
-      ['15F7A8', PASSING],
-      ['21FCA6', PASSING],
-      ['24ABCB', PASSING],
-      ['32CFC6', WARNING],
-      ['33403D', PASSING],
-      ['53D035', PASSING],
-      ['6E9B82', PASSING],
-      ['7E0221', PASSING],
-      ['822E47', PASSING],
-      ['845CC9', PASSING],
-      ['A1244C', PASSING],
-      ['C620DC', PASSING],
-      ['D78671', PASSING],
-      ['DA114A', WARNING],
-      ['FB0E0D', PASSING],
-      ['205AF7', PASSING],
-      ['373DB8', CRITICAL],
-      ['790926', WARNING],
-      ['9FAAD0', PASSING],
-      ['9FEFB0', PASSING],
-      ['CAEFF1', PASSING],
-      ['D028B9', PASSING],
-      ['DC5429', PASSING],
-      ['F50AF5', PASSING],
-    ];
+  const expectedCheckResults = {
+    A1244C: CRITICAL,
+    C620DC: PASSING,
+    '33403D': PASSING,
+    DA114A: WARNING,
+    790926: PASSING,
+    '61451E': WARNING,
+  };
 
-    before(() => {
-      cy.visit(`/clusters/${clusterID}`);
-      cy.url().should('include', `/clusters/${clusterID}`);
-    });
+  before(() => {
+    cy.visit(`/clusters/${clusterID}`);
+    cy.url().should('include', `/clusters/${clusterID}`);
+  });
 
-    describe('Run checks', () => {
-      it('should run checks with expected results', () => {
-        cy.get('button').contains('Settings').click();
+  describe('Run checks', () => {
+    it('should run checks with expected results', () => {
+      cy.get('button').contains('Check Selection').click();
 
-        cy.get('.tn-check-switch').click({ multiple: true });
-        cy.get('button').contains('Select Checks for Execution').click();
-        cy.get('button').contains('Back to Cluster Details').click();
-        cy.get('button').contains('Start Execution').click();
+      cy.get('.tn-check-switch').click({ multiple: true });
+      cy.get('button').contains('Save Checks Selection').click();
+      cy.get('button').contains('Back to Cluster Details').click();
+      cy.get('button').contains('Start Execution').click();
 
-        cy.get('table').each((_, index) => {
-          cy.get('table').eq(index).find('.tn-check-result-row').as('rows');
+      cy.get('table').each((_, index) => {
+        cy.get('table').eq(index).find('.tn-check-result-row').as('rows');
 
-          cy.get('@rows').each((_, index) => {
-            const [id, result] = expectedCheckResults[index];
+        cy.get('@rows').each((elem) => {
+          const checkID = elem.find('td').get(0).innerText;
+          const result = expectedCheckResults[checkID];
 
-            cy.get('@rows').eq(index).as('row').contains(id);
-            cy.get('@row')
+          if (result) {
+            cy.get(elem)
               .find('[data-testid="eos-svg-component"]')
               .should('have.class', result);
-          });
+          }
         });
       });
     });
   });
-}
+});
--- test/e2e/cypress/e2e/hosts_overview.cy.js
+++ test/e2e/cypress/e2e/hosts_overview.cy.js
@@ -54,7 +54,7 @@ context('Hosts Overview', () => {
           .then((i) => {
             cy.get('@hostRow')
               .eq(i)
-              .should('contain', host.agentVersion.slice(0, 15));
+              .should('contain', host.agentVersion.slice(0, 13));
           });
       });
     });
