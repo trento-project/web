dd9fe3686651b8ca1dfb459433b009b871b7ada9
EMaksy <eugen.maksymenko@suse.com>
2023-10-13 10:36:47 +0200

    Refactor test by using variables instead of the whole div

--- test/e2e/cypress/e2e/saptune_details.cy.js
+++ test/e2e/cypress/e2e/saptune_details.cy.js
@@ -5,69 +5,52 @@ describe('Saptune Details page', () => {
 
   it('should show information from the agent with unsupported version', () => {
     cy.loadScenario('host-vmhdbdev01-saptune-unsupported');
-    cy.get('div').should('contain', 'Saptune Details Not Found');
+    const notFoundContainer= cy.get('.pb-24')
+    notFoundContainer.should('contain', 'Saptune Details Not Found');
   });
 
   it('should show information from the agent saptune not installed', () => {
     cy.loadScenario('host-vmhdbdev01-saptune-uninstalled');
-    cy.get('div').should('contain', 'Saptune Details Not Found');
+    const notFoundContainer= cy.get('.pb-24')
+    notFoundContainer.should('contain', 'Saptune Details Not Found');
   });
 
-  it('should show not tuned saptune details', () => {
+  it('should display saptune details view when saptune is not tuning', () => {
     cy.loadScenario('host-vmhdbdev01-saptune-not-tuned');
+    const versionContainer = cy.get('.max-w-7xl > :nth-child(1) > :nth-child(1) > :nth-child(3)')
+    versionContainer.should('contain', 'Package').should('contain', '3.1.0')
+    versionContainer.should('contain', 'Configured Version').should('contain', '3');
+    versionContainer.should('contain', 'Tuning').should('contain', 'No tuning');
+    versionContainer.should('contain', 'Tuning').find('svg').should('exist').should('have.class', 'fill-yellow-500');
 
-    cy.get('div').should('contain', 'Package').should('contain', '3.1.0');
-    cy.get('div')
-      .should('contain', 'Configured Version')
-      .should('contain', '3');
-
-    cy.get('div').should('contain', 'Tuning').find('svg').should('exist');
-    cy.get('div')
-      .should('contain', 'Tuning')
-      .find('svg')
-      .should('exist')
-      .should('have.class', 'fill-yellow-500');
-    cy.get('div').should('contain', 'Tuning').should('contain', 'No tuning');
   });
 
   it('should show compliant saptune details', () => {
     cy.loadScenario('host-vmhdbdev01-saptune-compliant');
 
-    cy.get('div').should('contain', 'Package').should('contain', '3.1.0');
-    cy.get('div')
-      .should('contain', 'Configured Version')
-      .should('contain', '3');
-    cy.get('div').should('contain', 'Tuning').should('contain', 'Compliant');
-    cy.get('div')
-    .should('contain', 'saptune.service')
-    .find('svg')
-    .should('exist')
-    .should('have.class', 'fill-yellow-500');
-    cy.get('div')
-      .should('contain', 'saptune.service')
-      .should('contain', 'enabled/inactive');
-    cy.get('div').should('contain', 'sapconf.service').should('contain', '-');
-    cy.get('div').should('contain', 'tuned.service').should('contain', '-');
-    cy.get('div')
-      .should('contain', 'Enabled Solution')
-      .should(
-        'contain',
-        'NETWEAVER (941735, 1771258, 2578899, 2993054, 1656250, 900929)'
-      );
-    cy.get('div')
-      .should('contain', 'Applied Solution')
-      .should(
-        'contain',
-        'NETWEAVER (941735, 1771258, 2578899, 2993054, 1656250, 900929)'
-      );
-    cy.get('div')
-      .should('contain', 'Enabled Notes')
-      .should('contain', '941735, 1771258, 2578899, 2993054, 1656250, 900929');
-    cy.get('div')
-      .should('contain', 'Applied Notes')
-      .should('contain', '941735, 1771258, 2578899, 2993054, 1656250, 900929');
-    cy.get('div').should('contain', 'Staging').should('contain', 'Disabled');
-    cy.get('div').should('contain', 'Staged Notes').should('contain', '-');
-    cy.get('div').should('contain', 'Staged Solutions').should('contain', '-');
+    const versionContainer = cy.get('.max-w-7xl > :nth-child(1) > :nth-child(1) > :nth-child(3)')
+    versionContainer.should('contain', 'Tuning').should('contain', 'Compliant');
+
+    const saptuneServiceStatusContainer = cy.get('.max-w-7xl > :nth-child(1) > :nth-child(1) > :nth-child(5)')
+
+    saptuneServiceStatusContainer.should('contain','saptune.service').should('contain', 'saptune.service')
+    saptuneServiceStatusContainer.should('contain', 'sapconf.service').should('contain', '-');
+    saptuneServiceStatusContainer.should('contain', 'tuned.service').should('contain', '-');
+    saptuneServiceStatusContainer.should('contain', 'saptune.service').find('svg').should('exist').should('have.class', 'fill-yellow-500')
+
+
+    const saptuneTuningSolutionsContainer = cy.get('.max-w-7xl > :nth-child(1) > :nth-child(1) > :nth-child(7)')
+    saptuneTuningSolutionsContainer.should('contain', 'Enabled Solution').should('contain','NETWEAVER (941735, 1771258, 2578899, 2993054, 1656250, 900929)');
+    saptuneTuningSolutionsContainer.should('contain', 'Applied Solution').should('contain','NETWEAVER (941735, 1771258, 2578899, 2993054, 1656250, 900929)');
+
+    const saptuneTuningNotesContainer= cy.get('.max-w-7xl > :nth-child(1) > :nth-child(1) > :nth-child(9)')
+
+    saptuneTuningNotesContainer.should('contain', 'Enabled Notes').should('contain', '941735, 1771258, 2578899, 2993054, 1656250, 900929');
+    saptuneTuningNotesContainer.should('contain', 'Applied Notes').should('contain', '941735, 1771258, 2578899, 2993054, 1656250, 900929');
+    
+    const saptuneStagingStatusContainer=cy.get(':nth-child(11)')
+    saptuneStagingStatusContainer.should('contain', 'Staging').should('contain', 'Disabled');
+    saptuneStagingStatusContainer.should('contain', 'Staged Notes').should('contain', '-');
+    saptuneStagingStatusContainer.should('contain', 'Staged Solutions').should('contain', '-');
   });
 });
