e5b029e89084b5a1c6f79e0672855d68bf284519
EMaksy <eugen.maksymenko@suse.com>
2023-10-16 15:10:13 +0200

    Add saptune details data fixture

---
+++ test/e2e/cypress/fixtures/saptune-details/saptune_details_data.js
@@ -0,0 +1,94 @@
+export const saptuneDetailsData = {
+  appliedNotes: [
+    {
+      additionally_enabled: false,
+      id: '941735',
+    },
+    {
+      additionally_enabled: false,
+      id: '1771258',
+    },
+    {
+      additionally_enabled: false,
+      id: '2578899',
+    },
+    {
+      additionally_enabled: false,
+      id: '2993054',
+    },
+    {
+      additionally_enabled: false,
+      id: '1656250',
+    },
+    {
+      additionally_enabled: false,
+      id: '900929',
+    },
+  ],
+  appliedSolution: [
+    '941735',
+    '1771258',
+    '2578899',
+    '2993054',
+    '1656250',
+    '900929',
+  ],
+  enabledNotes: [
+    {
+      additionally_enabled: false,
+      id: '941735',
+    },
+    {
+      additionally_enabled: false,
+      id: '1771258',
+    },
+    {
+      additionally_enabled: false,
+      id: '2578899',
+    },
+    {
+      additionally_enabled: false,
+      id: '2993054',
+    },
+    {
+      additionally_enabled: false,
+      id: '1656250',
+    },
+    {
+      additionally_enabled: false,
+      id: '900929',
+    },
+  ],
+  enabledSolutions: {
+    id: 'NETWEAVER',
+    notes: ['941735', '1771258', '2578899', '2993054', '1656250', '900929'],
+    partial: false,
+  },
+  configuredVersion: '3',
+  hostname: 'vmhdbdev01',
+  hostID: '13e8c25c-3180-5a9a-95c8-51ec38e50cfc',
+  packageVersion: '3.1.0',
+  services: [
+    {
+      active: null,
+      enabled: null,
+      name: 'sapconf',
+    },
+    {
+      active: 'inactive',
+      enabled: 'enabled',
+      name: 'saptune',
+    },
+    {
+      active: null,
+      enabled: null,
+      name: 'tuned',
+    },
+  ],
+  staging: {
+    enabled: false,
+    notes: [],
+    solutions_ids: [],
+  },
+  tuningStatus: ' compliant',
+};
