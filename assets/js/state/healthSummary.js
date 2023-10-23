4104818f5de0d113d54bd202080c932a46256d0e
arbulu89 <xarbulu@suse.com>
2023-10-23 15:54:49 +0200

    Remove outdated comments

--- assets/js/state/healthSummary.js
+++ assets/js/state/healthSummary.js
@@ -1,24 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// interface SapSystemHealthSummary {
-//  application_cluster_health: string,
-//  application_cluster_id:     string,
-//  cluster_id:                 string,
-//  clusters_health:            string,
-//  database_cluster_health:    string,
-//  database_cluster_id:        string,
-//  database_health:            string,
-//  database_id:                string,
-//  hosts_health:               string,
-//  id:                         string,
-//  sapsystem_health:           string,
-//  sid:                        string,
-//  tenant:                     string,
-// }
-
 const initialState = {
   loading: false,
-  sapSystemsHealth: [], // SapSystemHealthSummary[]
+  sapSystemsHealth: [],
   error: '',
 };
 
