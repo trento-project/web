5d744ff6f7f7825d9f5d6c9e1262194e88d4526f
arbulu89 <xarbulu@suse.com>
2023-10-24 11:56:32 +0200

    Add ChecksCatalog stories

---
+++ assets/js/components/ChecksCatalog/ChecksCatalog.stories.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { faker } from '@faker-js/faker';
+
+import { catalogCheckFactory } from '@lib/test-utils/factories';
+import ChecksCatalog from './ChecksCatalog';
+
+const groupName1 = faker.string.uuid();
+const groupName2 = faker.string.uuid();
+const groupName3 = faker.string.uuid();
+const group1 = catalogCheckFactory.buildList(5, { group: groupName1 });
+const group2 = catalogCheckFactory.buildList(5, { group: groupName2 });
+const group3 = catalogCheckFactory.buildList(5, { group: groupName3 });
+const catalogData = group1.concat(group2).concat(group3);
+
+function ContainerWrapper({ children }) {
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">{children}</div>
+  );
+}
+
+export default {
+  title: 'Layouts/ChecksCatalog',
+  component: ChecksCatalog,
+  argTypes: {
+    catalogData: {
+      control: 'object',
+      description: 'Catalog content',
+    },
+    catalogError: {
+      control: 'text',
+      description: 'Error message getting catalog data',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    loading: {
+      control: { type: 'boolean' },
+      description: 'Catalog data is being loaded',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: false },
+      },
+    },
+    updateCatalog: {
+      action: 'Update catalog',
+      description: 'Update catalog content',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+  render: (args) => (
+    <ContainerWrapper>
+      <ChecksCatalog {...args} />
+    </ContainerWrapper>
+  ),
+};
+
+export const Default = {
+  args: {
+    catalogData,
+  },
+};
+
+export const Loading = {
+  args: {
+    ...Default.args,
+    loading: true,
+  },
+};
+
+export const Error = {
+  args: {
+    ...Default.args,
+    catalogError: 'Error loading catalog',
+  },
+};
