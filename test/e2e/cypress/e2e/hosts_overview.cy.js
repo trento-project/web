85448e2a79bf0f9df7166eccafb34311dae00ca6
Alessio Biancalana <alessio.biancalana@suse.com>
2023-10-27 12:57:32 +0200

    Fix end to end tests for saptune

--- test/e2e/cypress/e2e/hosts_overview.cy.js
+++ test/e2e/cypress/e2e/hosts_overview.cy.js
@@ -179,6 +179,7 @@ context('Hosts Overview', () => {
       });
 
       it('should change the health to warning if saptune is not installed', () => {
+        cy.contains('button', '2').click();
         cy.loadScenario(`host-${hostWithSap}-saptune-uninstalled`);
         cy.contains('tr', hostWithSap).within(() => {
           cy.get('td:nth-child(1) svg').should('have.class', 'fill-yellow-500');
@@ -186,6 +187,7 @@ context('Hosts Overview', () => {
       });
 
       it('should change the health to warning if saptune version is unsupported', () => {
+        cy.contains('button', '2').click();
         cy.loadScenario(`host-${hostWithSap}-saptune-unsupported`);
         cy.contains('tr', hostWithSap).within(() => {
           cy.get('td:nth-child(1) svg').should('have.class', 'fill-yellow-500');
@@ -213,6 +215,7 @@ context('Hosts Overview', () => {
         },
       ].forEach(({ state, scenario, health, icon }) => {
         it(`should change the health to ${health} if saptune tuning state is ${state}`, () => {
+          cy.contains('button', '2').click();
           cy.loadScenario(`host-${hostWithSap}-saptune-${scenario}`);
           cy.contains('tr', hostWithSap).within(() => {
             cy.get('td:nth-child(1) svg').should('have.class', icon);
@@ -220,6 +223,7 @@ context('Hosts Overview', () => {
         });
       });
     });
+
     describe('Health is changed to critical when the heartbeat is not sent', () => {
       before(() => {
         cy.visit('/hosts');
@@ -227,7 +231,7 @@ context('Hosts Overview', () => {
       });
       it('should show health status of the entire cluster of 27 hosts with critical health', () => {
         cy.get('.tn-health-container .tn-health-critical', {
-          timeout: 15000,
+          timeout: 30000,
         }).should('contain', 27);
       });
 
