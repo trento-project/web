2f2e78961f8588c90e4614ed7ff824b7da0f75a2
Alessio Biancalana <alessio.biancalana@suse.com>
2023-11-27 16:45:15 +0100

    Refactor to createOptionRenderer helper (#2034)

--- assets/js/components/Select/index.js
+++ assets/js/components/Select/index.js
@@ -1,5 +1,12 @@
 import Select, { OPTION_ALL } from './Select';
 
+export const createOptionRenderer = (label, renderer) => (option) => {
+  if (option === OPTION_ALL) {
+    return label;
+  }
+  return renderer(option);
+};
+
 export { OPTION_ALL };
 
 export default Select;
