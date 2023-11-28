5eecc74a30d90cb7bdebc9b5b2a76a525d3aea09
arbulu89 <xarbulu@suse.com>
2023-11-28 09:09:54 +0100

    Move functions to clusters saga

--- assets/js/state/sagas/clusters.js
+++ assets/js/state/sagas/clusters.js
@@ -2,13 +2,36 @@ import { put, takeEvery } from 'redux-saga/effects';
 
 import { notify } from '@state/notifications';
 import {
+  CLUSTER_REGISTERED,
+  CLUSTER_CIB_LAST_WRITTEN_UPDATED,
+  CLUSTER_DETAILS_UPDATED,
   CLUSTER_DEREGISTERED,
   CLUSTER_RESTORED,
   CLUSTER_HEALTH_CHANGED,
   appendCluster,
   removeCluster,
+  updateCluster,
   updateClusterHealth,
+  updateCibLastWritten,
 } from '@state/clusters';
+
+export function* clusterRegistered({ payload }) {
+  yield put(appendCluster(payload));
+  yield put(
+    notify({
+      text: `A new cluster, ${payload.name}, has been discovered.`,
+      icon: 'ℹ️',
+    })
+  );
+}
+
+function* cibLastWrittenUpdated({ payload }) {
+  yield put(updateCibLastWritten(payload));
+}
+
+function* clusterDetailsUpdated({ payload }) {
+  yield put(updateCluster(payload));
+}
 
 export function* clusterDeregistered({ payload: { name, id } }) {
   yield put(removeCluster({ id }));
@@ -42,14 +65,11 @@ export function* clusterHealthChanged({
   );
 }
 
-export function* watchClusterHealthChanged() {
+export function* watchClusterEvents() {
+  yield takeEvery(CLUSTER_REGISTERED, clusterRegistered);
+  yield takeEvery(CLUSTER_CIB_LAST_WRITTEN_UPDATED, cibLastWrittenUpdated);
+  yield takeEvery(CLUSTER_DETAILS_UPDATED, clusterDetailsUpdated);
   yield takeEvery(CLUSTER_HEALTH_CHANGED, clusterHealthChanged);
-}
-
-export function* watchClusterDeregistered() {
   yield takeEvery(CLUSTER_DEREGISTERED, clusterDeregistered);
-}
-
-export function* watchClusterRestored() {
   yield takeEvery(CLUSTER_RESTORED, clusterRestored);
 }
