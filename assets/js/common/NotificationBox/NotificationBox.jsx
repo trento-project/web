bd30bb26a7e2a9a6392e17fb638238d7e2f4ef41
Alessio Biancalana <alessio.biancalana@suse.com>
2023-12-11 17:32:57 +0100

    Add title prop to NotificationBox

--- assets/js/common/NotificationBox/NotificationBox.jsx
+++ assets/js/common/NotificationBox/NotificationBox.jsx
@@ -13,17 +13,22 @@ const extractTextContent = (text) =>
         ))
     : text;
 
-function NotificationBox({ icon, text, buttonText, buttonOnClick }) {
+function NotificationBox({ icon, title, text, buttonText, buttonOnClick }) {
   return (
     <div className="shadow-lg rounded-2xl p-4 bg-white dark:bg-gray-800">
       <div className="w-full h-full text-center">
         <div className="flex h-full flex-col justify-between">
           {icon}
-          <p className="text-gray-600 dark:text-gray-100 text-md py-2 px-6">
+          {title && (
+            <p className="text-gray-900 dark:text-gray-100 font-bold text-3xl py-2 px-6">
+              {title}
+            </p>
+          )}
+          <p className="text-gray-900 dark:text-gray-100 text-md py-2 px-6">
             {extractTextContent(text)}
           </p>
           {buttonText ? (
-            <div className="flex items-center justify-center gap-4 mt-4 w-1/4 m-auto">
+            <div className="flex items-center justify-center gap-4 mt-4 w-1/8 m-auto">
               <Button type="primary" className="w-32" onClick={buttonOnClick}>
                 {buttonText}
               </Button>
