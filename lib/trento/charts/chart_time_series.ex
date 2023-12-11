8a943bbb0b872defdc1cf107c43da3f02a73380c
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2023-12-11 12:33:54 +0100

    Add ChartTimeSeries data structure to charts context

---
+++ lib/trento/charts/chart_time_series.ex
@@ -0,0 +1,25 @@
+defmodule Trento.Charts.ChartTimeSeries do
+  @moduledoc """
+   Represents a time series of a chart
+   The series has a label and the samples distributed through time
+  """
+  @required_fields :all
+
+  use Trento.Support.Type
+
+  defmodule Sample do
+    @required_fields :all
+
+    use Trento.Support.Type
+
+    deftype do
+      field :timestamp, :utc_datetime_usec
+      field :value, :decimal
+    end
+  end
+
+  deftype do
+    field :label, :string
+    embeds_many :series, Sample
+  end
+end
