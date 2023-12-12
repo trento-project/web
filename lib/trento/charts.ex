922f87f74eecb8caaa41aa8201b76e138bb96b5c
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2023-12-12 18:34:38 +0100

    Charts context entrypoint performs charts queries

--- lib/trento/charts.ex
+++ lib/trento/charts.ex
@@ -10,63 +10,22 @@ defmodule Trento.Charts do
           {:ok, HostCpuChart.t()} | {:error, any}
   def host_cpu_chart(host_id, from, to) do
     with {:ok, cpu_busy_iowait_samples} <-
-           host_data_fetcher().get_cpu_busy_iowait(host_id, from, to),
-         {:ok, cpu_idle_samples} <- host_data_fetcher().get_cpu_idle(host_id, from, to),
+           host_data_fetcher().cpu_busy_iowait(host_id, from, to),
+         {:ok, cpu_idle_samples} <- host_data_fetcher().cpu_idle(host_id, from, to),
          {:ok, cpu_busy_system_samples} <-
-           host_data_fetcher().get_cpu_busy_system(host_id, from, to),
-         {:ok, cpu_busy_user_samples} <- host_data_fetcher().get_cpu_busy_user(host_id, from, to),
+           host_data_fetcher().cpu_busy_system(host_id, from, to),
+         {:ok, cpu_busy_user_samples} <- host_data_fetcher().cpu_busy_user(host_id, from, to),
+         {:ok, cpu_busy_irqs_samples} <- host_data_fetcher().cpu_busy_irqs(host_id, from, to),
          {:ok, cpu_busy_other_samples} <-
-           host_data_fetcher().get_cpu_busy_other(host_id, from, to),
-         {:ok,
-          %{
-            cpu_busy_iowait_ts: cpu_busy_iowait_ts,
-            cpu_idle_ts: cpu_idle_ts,
-            cpu_busy_system_ts: cpu_busy_system_ts,
-            cpu_busy_user_ts: cpu_busy_user_ts,
-            cpu_busy_other_ts: cpu_busy_other_ts
-          }} <-
-           build_host_chart_timeseries(%{
-             cpu_busy_iowait_samples: cpu_busy_iowait_samples,
-             cpu_idle_samples: cpu_idle_samples,
-             cpu_busy_system_samples: cpu_busy_system_samples,
-             cpu_busy_user_samples: cpu_busy_user_samples,
-             cpu_busy_other_samples: cpu_busy_other_samples
-           }) do
-      HostCpuChart.new(%{
-        busy_iowait: cpu_busy_iowait_ts,
-        idle: cpu_idle_ts,
-        busy_system: cpu_busy_system_ts,
-        busy_user: cpu_busy_user_ts,
-        busy_other: cpu_busy_other_ts
-      })
-    end
-  end
-
-  defp build_host_chart_timeseries(%{
-         cpu_idle_samples: cpu_idle_samples,
-         cpu_busy_iowait_samples: cpu_busy_iowait_samples,
-         cpu_busy_system_samples: cpu_busy_system_samples,
-         cpu_busy_user_samples: cpu_busy_user_samples,
-         cpu_busy_other_samples: cpu_busy_other_samples
-       }) do
-    with {:ok, cpu_busy_iowait_ts} <-
-           ChartTimeSeries.new(%{label: "cpu_busy_iowait", series: cpu_busy_iowait_samples}),
-         {:ok, cpu_idle_ts} <-
-           ChartTimeSeries.new(%{label: "cpu_idle", series: cpu_idle_samples}),
-         {:ok, cpu_busy_system_ts} <-
-           ChartTimeSeries.new(%{label: "cpu_busy_system", series: cpu_busy_system_samples}),
-         {:ok, cpu_busy_user_ts} <-
-           ChartTimeSeries.new(%{label: "cpu_busy_user", series: cpu_busy_user_samples}),
-         {:ok, cpu_busy_other_ts} <-
-           ChartTimeSeries.new(%{label: "cpu_busy_other", series: cpu_busy_other_samples}) do
-      {:ok,
-       %{
-         cpu_busy_iowait_ts: cpu_busy_iowait_ts,
-         cpu_idle_ts: cpu_idle_ts,
-         cpu_busy_system_ts: cpu_busy_system_ts,
-         cpu_busy_user_ts: cpu_busy_user_ts,
-         cpu_busy_other_ts: cpu_busy_other_ts
-       }}
+           host_data_fetcher().cpu_busy_other(host_id, from, to) do
+      %HostCpuChart{
+        busy_iowait: %ChartTimeSeries{label: "cpu_busy_iowait", series: cpu_busy_iowait_samples},
+        idle: %ChartTimeSeries{label: "cpu_idle", series: cpu_idle_samples},
+        busy_system: %ChartTimeSeries{label: "cpu_busy_system", series: cpu_busy_system_samples},
+        busy_user: %ChartTimeSeries{label: "cpu_busy_user", series: cpu_busy_user_samples},
+        busy_other: %ChartTimeSeries{label: "cpu_busy_other", series: cpu_busy_other_samples},
+        busy_irqs: %ChartTimeSeries{label: "cpu_busy_irqs", series: cpu_busy_irqs_samples}
+      }
     end
   end
 
