5e9b3533bfed6899c531f359a2960a2c9fa8184f
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2023-12-12 12:26:56 +0100

    Prometheus api, implementing host data fetcher

--- lib/trento/infrastructure/prometheus/adapter/prometheus_api.ex
+++ lib/trento/infrastructure/prometheus/adapter/prometheus_api.ex
@@ -12,28 +12,48 @@ defmodule Trento.Infrastructure.Prometheus.PrometheusApi do
   require Logger
 
   @behaviour Trento.Infrastructure.Prometheus.Gen
+  @behaviour Trento.Charts.HostDataFetcher
 
-  def get_cpu_busy_system(host_id, from, to) do
+  def cpu_busy_irqs(host_id, from, to) do
+    query =
+      "sum by (instance)(irate(node_cpu_seconds_total{mode=~\".*irq\",agentID=\"#{host_id}\"}[5m])) * 100"
+
+    perform_query_range(query, from, to)
+  end
+
+  def cpu_busy_other(host_id, from, to) do
+    query =
+      "sum (irate(node_cpu_seconds_total{mode!='idle',mode!='user',mode!='system',mode!='iowait',mode!='irq',mode!='softirq',agentID=\"#{host_id}\"}[5m])) * 100"
+
+    perform_query_range(query, from, to)
+  end
+
+  def cpu_busy_iowait(host_id, from, to) do
+    query =
+      "sum by (instance)(irate(node_cpu_seconds_total{mode=\"iowait\", agentID=\"#{host_id}\"}[5m])) * 100"
+
+    perform_query_range(query, from, to)
+  end
+
+  def cpu_busy_user(host_id, from, to) do
+    query =
+      "sum by (instance)(irate(node_cpu_seconds_total{mode=\"user\", agentID=\"#{host_id}\"}[5m])) * 100"
+
+    perform_query_range(query, from, to)
+  end
+
+  def cpu_idle(host_id, from, to) do
+    query =
+      "sum by (mode)(irate(node_cpu_seconds_total{mode='idle',agentID=\"#{host_id}\"}[5m])) * 100"
+
+    perform_query_range(query, from, to)
+  end
+
+  def cpu_busy_system(host_id, from, to) do
     query =
       "sum by (instance)(irate(node_cpu_seconds_total{mode=\"system\", agentID=\"#{host_id}\"}[5m])) * 100"
 
-    with {:ok, [%{"values" => query_values}]} <-
-           perform_query_range(query, from, to),
-         {:ok, samples} <- ChartIntegration.query_values_to_samples(query_values) do
-      {:ok, samples}
-    else
-      %HTTPoison.Response{status_code: status_code, body: body} ->
-        Logger.error(
-          "Unexpected response from Prometheus API, status code: #{status_code}, body: #{inspect(body)}."
-        )
-
-        {:error, :unexpected_response}
-
-      {:error, reason} = error ->
-        Logger.error("Error getting cpu busy system data from Prometheus API: #{inspect(reason)}")
-
-        error
-    end
+    perform_query_range(query, from, to)
   end
 
   def get_exporters_status(host_id) do
@@ -93,8 +113,21 @@ defmodule Trento.Infrastructure.Prometheus.PrometheusApi do
     }
 
     with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- HTTPoison.request(request),
-         {:ok, %{"data" => %{"result" => results}}} <- Jason.decode(body) do
-      {:ok, results}
+         {:ok, %{"data" => %{"result" => [%{"values" => query_values}]}}} <- Jason.decode(body),
+         {:ok, samples} <- ChartIntegration.query_values_to_samples(query_values) do
+      {:ok, samples}
+    else
+      %HTTPoison.Response{status_code: status_code, body: body} ->
+        Logger.error(
+          "Unexpected response from Prometheus API, status code: #{status_code}, body: #{inspect(body)}."
+        )
+
+        {:error, :unexpected_response}
+
+      {:error, reason} = error ->
+        Logger.error("Error getting cpu busy system data from Prometheus API: #{inspect(reason)}")
+
+        error
     end
   end
 end
