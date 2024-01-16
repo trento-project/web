33a0a5317311a6a230c5177bfeaae0eb1ab63569
Jamie Rodriguez <jamie.rodriguez@suse.com>
2024-01-16 08:52:53 +0100

    Use correct `EnsaVersion` enum in `ClusterExecutionEnv`

--- lib/trento/infrastructure/checks/cluster_execution_env.ex
+++ lib/trento/infrastructure/checks/cluster_execution_env.ex
@@ -9,7 +9,7 @@ defmodule Trento.Infrastructure.Checks.ClusterExecutionEnv do
   require Trento.Enums.Provider, as: Provider
   require Trento.Clusters.Enums.ClusterType, as: ClusterType
   require Trento.Clusters.Enums.FilesystemType, as: FilesystemType
-  require Trento.SapSystems.Enums.EnsaVersion, as: EnsaVersion
+  require Trento.Clusters.Enums.EnsaVersion, as: EnsaVersion
 
   deftype do
     field :cluster_type, Ecto.Enum, values: ClusterType.values()
