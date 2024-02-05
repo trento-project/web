6f8d3dfc8426013f25fc6b2d01d960df6634178e
Jamie Rodriguez <jamie.rodriguez@suse.com>
2024-02-05 14:22:28 +0100

    Make `SoftwareUpdatesView` test more specific

--- test/trento_web/views/v1/software_updates_view_test.exs
+++ test/trento_web/views/v1/software_updates_view_test.exs
@@ -2,17 +2,21 @@ defmodule TrentoWeb.V1.SettingsViewTest do
   use TrentoWeb.ConnCase, async: true
 
   import Phoenix.View
-  import Trento.Factory
 
-  alias Trento.SoftwareUpdates.Settings
   alias TrentoWeb.V1.SoftwareUpdatesView
 
   describe "renders software_updates_settings.json" do
     test "should render relevant fields" do
-      %Settings{url: url, username: username, ca_uploaded_at: ca_uploaded_at} =
-        settings = build(:software_updates_settings)
+      %{url: url, username: username, ca_uploaded_at: ca_uploaded_at} =
+        settings = %{
+          url: Faker.Internet.url(),
+          username: Faker.Internet.user_name(),
+          password: Faker.Lorem.word(),
+          ca_cert: Faker.Lorem.sentence(),
+          ca_uploaded_at: DateTime.utc_now()
+        }
 
-      assert %{url: ^url, username: ^username, ca_uploaded_at: ^ca_uploaded_at} =
+      assert %{url: url, username: username, ca_uploaded_at: ca_uploaded_at} ==
                render(SoftwareUpdatesView, "software_updates_settings.json", %{settings: settings})
     end
   end
