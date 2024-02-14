391ef0e9bb8fabe0b780108abd2440e91db25ba0
Nelson Kopliku <nelson.kopliku@suse.com>
2024-02-14 14:46:45 +0100

    Add support to PATCH requests in network client

--- assets/js/lib/network/index.js
+++ assets/js/lib/network/index.js
@@ -86,6 +86,15 @@ export const del = function del(url, config = null) {
     });
 };
 
+export const patch = function patch(url, data, config = null) {
+  return networkClient
+    .patch(url, data, config)
+    .then(handleResponseStatus)
+    .catch((error) => {
+      handleError(error);
+    });
+};
+
 export const put = function put(url, data, config = null) {
   return networkClient
     .put(url, data, config)
