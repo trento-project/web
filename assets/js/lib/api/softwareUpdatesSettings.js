bc6238a46b10799c234cc0972238f0a939a10185
Nelson Kopliku <nelson.kopliku@suse.com>
2024-02-15 09:37:15 +0100

    Add network requests for software updates settings interactions (#2312)

---
+++ assets/js/lib/api/softwareUpdatesSettings.js
@@ -0,0 +1,13 @@
+import { networkClient } from '@lib/network';
+
+export const getSettings = () =>
+  networkClient.get(`/settings/suma_credentials`);
+
+export const saveSettings = (settings) =>
+  networkClient.post(`/settings/suma_credentials`, settings);
+
+export const updateSettings = (settings) =>
+  networkClient.patch(`/settings/suma_credentials`, settings);
+
+export const clearSettings = () =>
+  networkClient.delete(`/settings/suma_credentials`);
