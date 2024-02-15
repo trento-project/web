28bc9b02ff0aabedacf19d8a2d15b1b1d41ba730
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2024-02-15 15:35:20 +0000

    Allow markdown for the saptune compliancy tooltip

--- assets/js/pages/SaptuneDetails/SaptuneTuningState.jsx
+++ assets/js/pages/SaptuneDetails/SaptuneTuningState.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-import Tooltip from '@common/Tooltip';
 import HealthIcon from '@common/HealthIcon';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import Tooltip from '@common/Tooltip';
 
 function SaptuneTuningState({ state }) {
+  const TooltipContent = () => (
+    <ReactMarkdown className="markdown" remarkPlugins={[remarkGfm]}>
+      Run `saptune note verify` in the host for further details
+    </ReactMarkdown>
+  );
   switch (state) {
     case 'compliant':
       return <span>Compliant</span>;
     case 'not compliant':
       return (
         <div className="flex">
-          <Tooltip
-            content="Run `saptune note verify` in the host for further details"
-            place="bottom"
-          >
+          <Tooltip place="bottom" content={<TooltipContent />}>
             <HealthIcon health="critical" />
           </Tooltip>
           <span className="ml-1">Not compliant</span>
