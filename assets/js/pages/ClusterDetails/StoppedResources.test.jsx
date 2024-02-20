80921a525399343a89d958bd5dab8cc6e7a80e06
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2024-02-20 12:57:02 +0000

    Use render instead of renderWithRouter

--- assets/js/pages/ClusterDetails/StoppedResources.test.jsx
+++ assets/js/pages/ClusterDetails/StoppedResources.test.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
-import { renderWithRouter } from '@lib/test-utils';
 
 import StoppedResources from './StoppedResources';
 
 it('should display a "No resources to display." message when there are no resources', async () => {
-  renderWithRouter(<StoppedResources resources={[]} />);
+  render(<StoppedResources resources={[]} />);
 
   expect(screen.getByText('No resources to display.')).toBeInTheDocument();
 });
