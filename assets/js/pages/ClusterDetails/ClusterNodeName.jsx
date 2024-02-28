a08292adaf782a6a3ffd5f89428ea57a596282f3
EMaksy <emaksymenko@suse.com>
2024-02-28 16:35:43 +0100

    Refactor ClusterNodeName

--- assets/js/pages/ClusterDetails/ClusterNodeName.jsx
+++ assets/js/pages/ClusterDetails/ClusterNodeName.jsx
@@ -6,39 +6,52 @@ import {
   EOS_POWER_OFF_OUTLINED,
 } from 'eos-icons-react';
 
-import { countBy } from 'lodash';
-
 import Tooltip from '@common/Tooltip';
 import ClusterNodeLink from './ClusterNodeLink';
 
-const getNodeStatusIcon = (status) => {
+const getNodeStatusIcon = (status, resources) => {
   switch (status) {
     case 'Online': {
-      return <EOS_BOLT_FILLED className="tn-online" />;
+      const unmanagedResourcesCount = resources.filter(
+        ({ managed }) => !managed
+      ).length;
+
+      if (unmanagedResourcesCount > 0) {
+        return {
+          icon: <EOS_BUILD_OUTLINED className="tn-online" />,
+          message: `${unmanagedResourcesCount} unmanaged resources`,
+        };
+      }
+
+      return {
+        icon: <EOS_BOLT_FILLED className="tn-online" />,
+        message: status,
+      };
     }
     case 'Offline': {
-      return <EOS_POWER_OFF_OUTLINED className="tn-offline" />;
+      return {
+        icon: <EOS_POWER_OFF_OUTLINED className="tn-offline" />,
+        message: status,
+      };
     }
     case 'Maintenance': {
-      return <EOS_BUILD_OUTLINED className="tn-maintenance" />;
+      return {
+        icon: <EOS_BUILD_OUTLINED className="tn-maintenance" />,
+        message: status,
+      };
     }
+
     default: {
-      return <EOS_WARNING_OUTLINED className="tn-unknown" />;
+      return {
+        icon: <EOS_WARNING_OUTLINED className="tn-unknown" />,
+        message: status,
+      };
     }
   }
 };
 
-const getNodeMessageAndIcon = (status, resources) => {
-  const { unmanaged } = countBy(resources, (resource) =>
-    resource.managed ? 'managed' : 'unmanaged'
-  );
-  const message = unmanaged > 0 ? `${unmanaged} resources unmanaged` : status;
-  const icon = getNodeStatusIcon(status);
-
-  return { icon, message };
-};
-function ClusterNodeName({ status, hostId, children, resources = [] }) {
-  const { icon, message } = getNodeMessageAndIcon(status, resources);
+function ClusterNodeName({ status, hostId, resources, children }) {
+  const { icon, message } = getNodeStatusIcon(status, resources);
   return (
     <span className="group flex items-center relative space-x-2">
       <Tooltip content={message} place="bottom">
