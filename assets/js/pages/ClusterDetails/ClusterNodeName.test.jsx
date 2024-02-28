8bc19ff9ff8a5bdeb657c5990413248496f62814
EMaksy <emaksymenko@suse.com>
2024-02-28 17:57:36 +0100

    Update test for ClusterNodeName

--- assets/js/pages/ClusterDetails/ClusterNodeName.test.jsx
+++ assets/js/pages/ClusterDetails/ClusterNodeName.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, act } from '@testing-library/react';
+import { screen, act, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { renderWithRouter } from '@lib/test-utils';
@@ -11,35 +11,51 @@ describe('ClusterNodeName', () => {
   it.each([
     {
       status: 'Online',
+      resources: [],
       testID: 'tn-online',
+      expectedMessage: 'Online',
+    },
+    {
+      status: 'Online',
+      resources: [{ managed: true }, { managed: false }],
+      testID: 'tn-online',
+      expectedMessage: '1 unmanaged resources',
     },
     {
       status: 'Offline',
+      resources: [],
       testID: 'tn-offline',
+      expectedMessage: 'Offline',
     },
     {
       status: 'Maintenance',
+      resources: [],
       testID: 'tn-maintenance',
+      expectedMessage: 'Maintenance',
     },
     {
       status: 'Other',
+      resources: [],
       testID: 'tn-unknown',
+      expectedMessage: 'Other',
     },
-  ])('renders correct icon', async ({ status, testID }) => {
-    const user = userEvent.setup();
+  ])(
+    'renders correct icon',
+    async ({ status, resources, testID, expectedMessage }) => {
+      const user = userEvent.setup();
 
-    const { id, hostname } = hostFactory.build();
+      const { id, hostname } = hostFactory.build();
+      renderWithRouter(
+        <ClusterNodeName status={status} hostId={id} resources={resources}>
+          {hostname}
+        </ClusterNodeName>
+      );
 
-    renderWithRouter(
-      <ClusterNodeName status={status} hostId={id}>
-        {hostname}
-      </ClusterNodeName>
-    );
-
-    const icon = screen.getByTestId('eos-svg-component');
-    expect(icon).toHaveClass(testID);
-    await act(async () => user.hover(icon));
-
-    expect(screen.getByText(status)).toBeVisible();
-  });
+      const icon = screen.getByTestId('eos-svg-component');
+      expect(icon).toHaveClass(testID);
+      await act(async () => user.hover(icon));
+      const tooltip = screen.getByRole('tooltip');
+      expect(within(tooltip).getByText(expectedMessage)).toBeVisible();
+    }
+  );
 });
