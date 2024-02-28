267b732294e89f2645ca8fd98b5a1d7c3f1530bf
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-02-28 17:37:33 +0100

    Installation controller returns new api key from settings

--- lib/trento_web/controllers/v1/installation_controller.ex
+++ lib/trento_web/controllers/v1/installation_controller.ex
@@ -3,8 +3,11 @@ defmodule TrentoWeb.V1.InstallationController do
   use OpenApiSpex.ControllerSpecs
 
   alias OpenApiSpex.Schema
-
+  alias Trento.Settings
   alias TrentoWeb.Auth.ApiKey
+
+  plug OpenApiSpex.Plug.CastAndValidate, json_render_error_v2: true
+  action_fallback TrentoWeb.FallbackController
 
   operation :get_api_key,
     summary: "Retrieve API Key",
@@ -26,8 +29,18 @@ defmodule TrentoWeb.V1.InstallationController do
 
   @spec get_api_key(Plug.Conn.t(), any) :: Plug.Conn.t()
   def get_api_key(conn, _) do
-    key = ApiKey.get_api_key()
+    with {:ok, api_key_settings} <- Settings.get_api_key_settings() do
+      render(conn, "api_key.json", %{
+        api_key: generate_api_key!(api_key_settings)
+      })
+    end
+  end
 
-    render(conn, "api_key.json", api_key: key)
+  defp generate_api_key!(%{
+         jti: jti,
+         api_key_expire_at: api_key_expire_at,
+         api_key_created_at: api_key_created_at
+       }) do
+    ApiKey.generate_api_key!(%{"jti" => jti}, api_key_created_at, api_key_expire_at)
   end
 end
