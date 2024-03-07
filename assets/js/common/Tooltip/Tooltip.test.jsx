3d609571ec2a542327858f131101f24b251926c3
Alessio Biancalana <alessio.biancalana@suse.com>
2024-03-07 12:44:06 +0100

    Add wrap prop to tooltip (#2389)

--- assets/js/common/Tooltip/Tooltip.test.jsx
+++ assets/js/common/Tooltip/Tooltip.test.jsx
@@ -24,4 +24,24 @@ describe('Tooltip', () => {
       expect(screen.queryByText('This is my tooltip text')).toBeVisible()
     );
   });
+
+  it('should show a text when mouse is hovering and wrap is false', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <div>
+        <Tooltip content="This is my tooltip text" wrap={false}>
+          <div>This is my anchor</div>
+        </Tooltip>
+      </div>
+    );
+
+    expect(screen.queryByText('This is my tooltip text')).toBeNull();
+
+    await act(async () => user.hover(screen.queryByText('This is my anchor')));
+
+    await waitFor(() =>
+      expect(screen.queryByText('This is my tooltip text')).toBeVisible()
+    );
+  });
 });
