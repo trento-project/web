37a849f3600f1f36ff013cfa162f80d32cc41d9d
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-03-11 12:46:14 +0100

    ApiKeySettings modal with story

--- assets/js/common/ApiKeySettingsModal/ApiKeySettingsModal.jsx
+++ assets/js/common/ApiKeySettingsModal/ApiKeySettingsModal.jsx
@@ -1,14 +1,24 @@
-import { addDays, addMonths, addYears, setHours, setMinutes } from 'date-fns';
+import {
+  addDays,
+  addMonths,
+  addYears,
+  format,
+  parseISO,
+  setHours,
+  setMinutes,
+} from 'date-fns';
 import React, { useState } from 'react';
-import { noop } from 'lodash';
-
+import { noop, truncate } from 'lodash';
+import { EOS_CONTENT_COPY, EOS_INFO_OUTLINED } from 'eos-icons-react';
 import Button from '@common/Button';
 import Modal from '@common/Modal';
-import Input, { Password, Textarea } from '@common/Input';
+import Input from '@common/Input';
 import Label from '@common/Label';
 import Select from '@common/Select';
+import Switch from '@common/Switch';
 
-const normalizeExpiration = (expiration) => setMinutes(setHours(0), 0);
+const normalizeExpiration = (expiration) =>
+  setMinutes(setHours(expiration, 0), 0);
 
 const availableTimeOptions = [
   {
@@ -31,6 +41,7 @@ function ApiKeySettingsModal({
   onSave = noop,
   onClose = noop,
   generatedApiKey,
+  generatedApiKeyExpiration,
 }) {
   const timeOptions = availableTimeOptions.map((o) => o.type);
   const [timeQuantity, setTimeQuantity] = useState(0);
@@ -39,42 +50,123 @@ function ApiKeySettingsModal({
   const [quantityError, setQuantityError] = useState(false);
   const [timeFormEnabled, setTimeFormEnabled] = useState(true);
 
+  const saveFormState = () => {
+    if (!timeFormEnabled) {
+      onSave({ apiKeyExpiration: null });
+      return;
+    }
+    if (timeQuantity === 0 || !timeQuantity) {
+      setQuantityError(true);
+      return;
+    }
+    const timeQuantitySettings = availableTimeOptions.find(
+      (q) => q.type === timeQuantityType
+    );
+    const apiKeyExpiration = timeQuantitySettings.timeGenerator(timeQuantity);
+
+    onSave({ apiKeyExpiration: normalizeExpiration(apiKeyExpiration) });
+  };
+
   return (
     <Modal
       title="API Key Settings"
-      className="!w-2/4"
+      className="!w-3/4 !max-w-3xl"
       open={open}
       onClose={onClose}
     >
-      <div className="flex items-center my-5 space-x-2">
-        <div className="w-1/3">
-          <Label>Key Expiration</Label>
-        </div>
+      <div className="flex flex-col my-5">
+        <span className="my-2 mb-4 text-gray-500">
+          {' '}
+          By generating a new key, you will need to replace the API key on all
+          hosts.{' '}
+        </span>
 
-        <div className="w-2/4">
-          <Input
-            value={timeQuantity}
-            className="h-4/5"
-            type="number"
-            disabled={!timeFormEnabled}
-            error={quantityError}
-            onChange={({ target: { value } }) => {
-              setTimeQuantity(value);
+        <div className="flex space-x-1">
+          <div className="w-1/5">
+            <Label>Never Expires</Label>
+          </div>
+
+          <Switch
+            selected={!timeFormEnabled}
+            onChange={() => {
+              setTimeFormEnabled((enabled) => !enabled);
               setQuantityError(false);
             }}
           />
         </div>
-        <div className="w-2/4 pt-4">
-          <Select
-            optionsName=""
-            options={timeOptions}
-            disabled={!timeFormEnabled}
-            value={timeQuantityType}
-            onChange={(value) => setTimeQuantityType(value)}
-          />
+        <div className="flex items-center my-2 space-x-2">
+          <div className="w-1/3">
+            <Label>Key Expiration</Label>
+          </div>
+
+          <div className="w-2/4">
+            <Input
+              value={timeQuantity}
+              className="h-3/4"
+              type="number"
+              disabled={!timeFormEnabled}
+              error={quantityError}
+              onChange={({ target: { value } }) => {
+                setTimeQuantity(parseInt(value, 10));
+                setQuantityError(false);
+              }}
+            />
+          </div>
+          <div className="w-2/4 pt-4">
+            <Select
+              optionsName=""
+              options={timeOptions}
+              disabled={!timeFormEnabled}
+              value={timeQuantityType}
+              onChange={(value) => setTimeQuantityType(value)}
+            />
+          </div>
+          <div className="w-1/6 h-4/5">
+            <Button
+              onClick={() => saveFormState()}
+              disabled={quantityError || loading}
+            >
+              Generate
+            </Button>
+          </div>
         </div>
+        {quantityError && (
+          <span className="my-1 mb-4 text-red-500">
+            {' '}
+            Key expiration value needs to be greater than 0{' '}
+          </span>
+        )}
+        {generatedApiKey && (
+          <div className="flex flex-col my-1 mb-4">
+            <div className="flex space-x-2">
+              <div className="w-full break-words p-2 pr-2 rounded-lg bg-white border-gray-300 border">
+                <code> {truncate(generatedApiKey, { length: 65 })} </code>
+              </div>
+              <button
+                type="button"
+                aria-label="generate api key"
+                onClick={() => {
+                  window.navigator.clipboard.writeText(generatedApiKey);
+                }}
+              >
+                <EOS_CONTENT_COPY role="button" size="25" />
+              </button>
+            </div>
+            <div className="flex space-x-2">
+              <EOS_INFO_OUTLINED size="20" className="mt-2" />
+
+              <div className="mt-2 text-gray-600 text-sm">
+                {' '}
+                Key Will Expire{' '}
+                {format(parseISO(generatedApiKeyExpiration), 'd LLL yyyy')}
+              </div>
+            </div>
+          </div>
+        )}
         <div className="w-1/6 h-4/5">
-          <Button>Generate</Button>
+          <Button type="primary-white" onClick={onClose}>
+            Close
+          </Button>
         </div>
       </div>
     </Modal>
--- assets/js/common/ApiKeySettingsModal/ApiKeySettingsModal.stories.jsx
+++ assets/js/common/ApiKeySettingsModal/ApiKeySettingsModal.stories.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-console, react/function-component-definition */
+import React from 'react';
+import { useArgs } from '@storybook/client-api';
 import ApiKeySettingsModal from './ApiKeySettingsModal';
 
 export default {
@@ -16,11 +19,56 @@ export default {
         type: 'boolean',
       },
     },
+    generatedApiKeyExpiration: {
+      description:
+        'The new generated api key expiration expressed in ISO8601 Timestamp',
+    },
+    generatedApiKey: {
+      description: 'The new generated api key',
+    },
+  },
+  args: {
+    loading: false,
+    open: false,
+    generatedApiKeyExpiration: new Date().toISOString(),
+    generatedApiKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0cmVudG9fYXBpX2tleSIsImV4cCI6MTcxMjE1MzE3MCwiaWF0IjoxNzA5NzMzOTcxLCJpc3MiOiJodHRwczovL2dpdGh1Yi5jb20vdHJlbnRvLXByb2plY3Qvd2ViIiwianRpIjoiYmZmMjA0YjUtMzJmMS00YmVlLThiMGItY2IxZGQwNTlmNGRjIiwibmJmIjoxNzA5NzMzOTcxLCJ0eXAiOiJCZWFyZXIifQ.0Lz0MwZaFpIGbSohnkiJ6AN5FFb5Vg5ZVhqM3fdUf3M',
+    onSave: ({ apiKeyExpiration }) =>
+      console.log('api key expiration', apiKeyExpiration),
   },
 };
 
-export const Default = {
-  args: {
-    open: false,
-  },
+export const Default = (args) => {
+  const [{ open }, updateArgs] = useArgs();
+  const handleClose = () => updateArgs({ open: !open });
+
+  return (
+    <>
+      <button type="button" onClick={() => handleClose()}>
+        {' '}
+        Toggle modal{' '}
+      </button>
+      <ApiKeySettingsModal {...args} onClose={handleClose} />
+    </>
+  );
+};
+
+export const OnlyGenerationForm = (args) => {
+  const [{ open }, updateArgs] = useArgs();
+  const handleClose = () => updateArgs({ open: !open });
+
+  return (
+    <>
+      <button type="button" onClick={() => handleClose()}>
+        {' '}
+        Toggle modal{' '}
+      </button>
+      <ApiKeySettingsModal
+        {...args}
+        generatedApiKeyExpiration={null}
+        generatedApiKey={null}
+        onClose={handleClose}
+      />
+    </>
+  );
 };
