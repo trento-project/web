f4658d99c96d8be157dc4193a90ab7d68eb71394
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-03-12 16:09:25 +0100

    Add api key settings e2e tests

--- test/e2e/cypress/e2e/settings.cy.js
+++ test/e2e/cypress/e2e/settings.cy.js
@@ -10,6 +10,40 @@ context('Settings page', () => {
     cy.updateApiKeyExpiration(null);
   });
 
+  describe('Api key display', () => {
+    it('should display the api key with the copy button', () => {
+      cy.updateApiKeyExpiration(null);
+
+      cy.reload();
+
+      cy.get('body').should('contain', 'Key will never expire');
+      cy.get('code').should('not.be.empty');
+      cy.get('button').contains('Copy Key').should('be.visible');
+    });
+  });
+
+  describe('Api Key generation', () => {
+    it('should generate a new api key', () => {
+      cy.get('button').contains('Generate Key').click();
+      cy.get('.rc-input-number-input').as('quantityInput');
+      cy.get('.bg-jungle-green-500').as('generateButton');
+
+      cy.get('@quantityInput').type('2');
+      cy.get('@generateButton').click();
+
+      cy.get(':nth-child(1) > .w-full > code').as('generatedApiKey');
+      cy.get('@generatedApiKey').should('not.be.empty');
+
+      cy.get('.flex-col > :nth-child(1) > button').as('copyApiKey');
+      cy.get('@copyApiKey').should('be.visible');
+
+      cy.get('.flex-col > :nth-child(2) > .text-gray-600').as('expirationDate');
+
+      cy.get('@expirationDate').should('contain', 'Key Will Expire');
+      cy.get('button').contains('Close').click();
+    });
+  });
+
   describe('Api key expiration notifications', () => {
     it('should show api key expired notification when first loading the page, when the api key is expired', () => {
       cy.updateApiKeyExpiration(subDays(new Date(), 1));
