74842434e822d3e8b50f1f522049f5cb0f93d0b3
Jamie Rodriguez <jamie.rodriguez@suse.com>
2024-03-19 14:39:27 +0100

    Fix *get software updates* URL

--- assets/js/lib/api/softwareUpdates.js
+++ assets/js/lib/api/softwareUpdates.js
@@ -1,4 +1,4 @@
 import { networkClient } from '@lib/network';
 
 export const getSoftwareUpdates = (hostId) =>
-  networkClient.get(`/api/v1/hosts/${hostId}/software_updates`);
+  networkClient.get(`/hosts/${hostId}/software_updates`);
