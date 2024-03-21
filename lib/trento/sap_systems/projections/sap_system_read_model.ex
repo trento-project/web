fed58f60d13f93df1037e4e922c9ecd5c9c6b636
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-03-21 16:50:56 +0100

    Refactor database instances relationship in sap systems rm

--- lib/trento/sap_systems/projections/sap_system_read_model.ex
+++ lib/trento/sap_systems/projections/sap_system_read_model.ex
@@ -29,8 +29,10 @@ defmodule Trento.SapSystems.Projections.SapSystemReadModel do
     field :health, Ecto.Enum, values: Health.values()
     field :ensa_version, Ecto.Enum, values: EnsaVersion.values(), default: EnsaVersion.no_ensa()
 
+    belongs_to :database, DatabaseReadModel, type: :binary_id
+
     has_many :database_instances, DatabaseInstanceReadModel,
-      references: :id,
+      references: :database_id,
       foreign_key: :sap_system_id,
       preload_order: [asc: :instance_number, asc: :host_id]
 
@@ -40,8 +42,6 @@ defmodule Trento.SapSystems.Projections.SapSystemReadModel do
       preload_order: [asc: :instance_number, asc: :host_id]
 
     has_many :tags, Tag, foreign_key: :resource_id
-
-    belongs_to :database, DatabaseReadModel, type: :binary_id
 
     field :deregistered_at, :utc_datetime_usec
 
