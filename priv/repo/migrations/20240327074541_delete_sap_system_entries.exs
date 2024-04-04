419cd4dfcc2e7d8a819315965e12998dadaaa53a
arbulu89 <xarbulu@suse.com>
2024-04-04 11:21:44 +0200

    Make the rollback possible in the migration

--- priv/repo/migrations/20240327074541_delete_sap_system_entries.exs
+++ priv/repo/migrations/20240327074541_delete_sap_system_entries.exs
@@ -2,7 +2,13 @@ defmodule Trento.Repo.Migrations.DeleteSapSystemEntries do
   use Ecto.Migration
 
   def change do
-    execute "DELETE FROM sap_systems"
-    execute "DELETE FROM application_instances"
+    execute(&execute_up/0, &execute_down/0)
   end
+
+  defp execute_up do
+    repo().delete_all("sap_systems")
+    repo().delete_all("application_instances")
+  end
+
+  defp execute_down, do: nil
 end
