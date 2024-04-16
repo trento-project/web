3d37636eae06b249c0a8c47ba3e6a0e42d5d952c
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-04-16 16:28:39 +0200

    User controller refactored for the new users context

--- lib/trento_web/controllers/v1/user_controller.ex
+++ lib/trento_web/controllers/v1/user_controller.ex
@@ -7,6 +7,8 @@ defmodule TrentoWeb.V1.UserController do
   action_fallback TrentoWeb.FallbackController
 
   def index(conn, _params) do
+    cu = Pow.Plug.current_user(conn)
+    # dbg()
     users = Users.list_users()
     render(conn, "index.json", users: users)
   end
@@ -20,22 +22,21 @@ defmodule TrentoWeb.V1.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-    user = Users.get_user!(id)
-    render(conn, "show.json", user: user)
+    with {:ok, user} <- Users.get_user(id) do
+      render(conn, "show.json", user: user)
+    end
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Users.get_user!(id)
-
-    with {:ok, %User{} = user} <- Users.update_user(user, user_params) do
+    with {:ok, user} <- Users.get_user(id),
+         {:ok, %User{} = user} <- Users.update_user(user, user_params) do
       render(conn, "show.json", user: user)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    user = Users.get_user!(id)
-
-    with {:ok, %User{}} <- Users.delete_user(user) do
+    with {:ok, user} <- Users.get_user(id),
+         {:ok, %User{}} <- Users.delete_user(user) do
       send_resp(conn, :no_content, "")
     end
   end
