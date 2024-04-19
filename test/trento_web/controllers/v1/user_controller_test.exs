a10dc58ab61b435a310a5a736d044469f2a9fbc0
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-04-19 15:32:25 +0200

    removed comments from user_controller_test

--- test/trento_web/controllers/v1/user_controller_test.exs
+++ test/trento_web/controllers/v1/user_controller_test.exs
@@ -216,62 +216,6 @@ defmodule TrentoWeb.V1.UserControllerTest do
     end
   end
 
-  # describe "create user" do
-  #   test "renders user when data is valid", %{conn: conn} do
-  #     conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
-  #     assert %{"id" => id} = json_response(conn, 201)["data"]
-
-  #     conn = get(conn, Routes.user_path(conn, :show, id))
-
-  #     assert %{
-  #              "id" => ^id,
-  #              "fullname" => "some fullname",
-  #              "name" => "some name",
-  #              "password" => "some password"
-  #            } = json_response(conn, 200)["data"]
-  #   end
-
-  #   test "renders errors when data is invalid", %{conn: conn} do
-  #     conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
-  #     assert json_response(conn, 422)["errors"] != %{}
-  #   end
-  # end
-
-  # describe "update user" do
-  #   setup [:create_user]
-
-  #   test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
-  #     conn = put(conn, Routes.user_path(conn, :update, user), user: @update_attrs)
-  #     assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-  #     conn = get(conn, Routes.user_path(conn, :show, id))
-
-  #     assert %{
-  #              "id" => ^id,
-  #              "fullname" => "some updated fullname",
-  #              "name" => "some updated name",
-  #              "password" => "some updated password"
-  #            } = json_response(conn, 200)["data"]
-  #   end
-
-  #   test "renders errors when data is invalid", %{conn: conn, user: user} do
-  #     conn = put(conn, Routes.user_path(conn, :update, user), user: @invalid_attrs)
-  #     assert json_response(conn, 422)["errors"] != %{}
-  #   end
-  # end
-
-  # describe "delete user" do
-  #   setup [:create_user]
-
-  #   test "deletes chosen user", %{conn: conn, user: user} do
-  #     conn = delete(conn, Routes.user_path(conn, :delete, user))
-  #     assert response(conn, 204)
-
-  #     assert_error_sent 404, fn ->
-  #       get(conn, Routes.user_path(conn, :show, user))
-  #     end
-  #   end
-
   defp create_user do
     password = "themightypassword8897"
 
