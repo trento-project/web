e3b075b0347118b2e5ccd930b86b5ad8f97438dc
Nelson Kopliku <nelson.kopliku@suse.com>
2024-04-22 09:42:43 +0200

    Improve timeout handling in TaskCase

--- test/support/task_case.ex
+++ test/support/task_case.ex
@@ -1,13 +1,13 @@
 defmodule Trento.TaskCase do
   @moduledoc """
-  This module defines the test case to be used by tests that require operating on asynv tasks.
+  This module defines the test case to be used by tests that require operating on async tasks.
   """
 
   use ExUnit.CaseTemplate
 
   using do
     quote do
-      def wait_for_tasks_completion do
+      def wait_for_tasks_completion(timeout \\ 5_000) do
         Trento.TasksSupervisor
         |> Task.Supervisor.children()
         |> Enum.map(fn pid ->
@@ -15,17 +15,19 @@ defmodule Trento.TaskCase do
 
           pid
         end)
-        |> wait_for_pids()
+        |> wait_for_pids(timeout)
       end
 
-      defp wait_for_pids([]), do: nil
+      defp wait_for_pids([], timeout), do: nil
 
-      defp wait_for_pids(pids) do
+      defp wait_for_pids(pids, timeout) do
         receive do
           {:DOWN, _ref, :process, pid, _reason} ->
             pids
             |> List.delete(pid)
-            |> wait_for_pids()
+            |> wait_for_pids(timeout)
+        after
+          timeout -> :timeout
         end
       end
     end
