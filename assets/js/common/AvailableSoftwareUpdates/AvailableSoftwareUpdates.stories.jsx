a92b416301a52e65f742699c6ea8b3fd9fbbdca1
Jamie Rodriguez <jamie.rodriguez@suse.com>
2024-04-23 09:59:49 +0200

    Update Storybook

--- assets/js/common/AvailableSoftwareUpdates/AvailableSoftwareUpdates.stories.jsx
+++ assets/js/common/AvailableSoftwareUpdates/AvailableSoftwareUpdates.stories.jsx
@@ -4,6 +4,10 @@ export default {
   title: 'Components/AvailableSoftwareUpdates',
   component: AvailableSoftwareUpdates,
   argTypes: {
+    settingsConfigured: {
+      type: 'bool',
+      description: 'Have settings been saved for the software updates service',
+    },
     relevantPatches: {
       type: 'number',
       description: 'Number of relevant patches available for the system',
@@ -26,17 +30,44 @@ export default {
       type: 'bool',
       description: 'Is data being fetched?',
     },
+    connectionError: {
+      type: 'bool',
+      description: 'There an error connecting to the software updates service',
+    },
+    onBackToSettings: {
+      description:
+        'Callback function to trigger when the "Settings" button is clicked',
+      control: {
+        type: 'function',
+      },
+    },
   },
 };
 
 export const Default = {
-  args: { relevantPatches: 412, upgradablePackages: 234 },
+  args: {
+    relevantPatches: 412,
+    upgradablePackages: 234,
+    settingsConfigured: true,
+  },
 };
 
 export const Cool = {
-  args: { relevantPatches: 0, upgradablePackages: 42 },
+  args: {
+    relevantPatches: 0,
+    upgradablePackages: 42,
+    settingsConfigured: true,
+  },
 };
 
-export const Unknown = { args: { tooltip: 'SUSE Manager is not available' } };
+export const NoSettingsConfigured = { args: { settingsConfigured: false } };
 
-export const Loading = { args: { loading: true } };
+export const Unknown = {
+  args: { tooltip: 'SUSE Manager is not available', settingsConfigured: true },
+};
+
+export const Loading = { args: { loading: true, settingsConfigured: true } };
+
+export const ConnectionError = {
+  args: { connectionError: true, settingsConfigured: true },
+};
