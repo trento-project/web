70665f023ea18c879e9b50241fa5a57a5428086b
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-05-03 15:19:44 +0200

    Profile controller

--- lib/trento_web/controllers/v1/profile_controller.ex
+++ lib/trento_web/controllers/v1/profile_controller.ex
@@ -2,10 +2,12 @@ defmodule TrentoWeb.V1.ProfileController do
   use TrentoWeb, :controller
   use OpenApiSpex.ControllerSpecs
 
-  alias TrentoWeb.OpenApi.V1.Schema
   alias Trento.Users
+  alias Trento.Users.User
+  alias TrentoWeb.OpenApi.V1.Schema
 
   plug OpenApiSpex.Plug.CastAndValidate, json_render_error_v2: true
+  plug TrentoWeb.Plugs.LoadUserPlug
   action_fallback TrentoWeb.FallbackController
 
   operation :show,
@@ -17,11 +19,8 @@ defmodule TrentoWeb.V1.ProfileController do
     ]
 
   def show(conn, _) do
-    with %{"user_id" => user_id} <- Pow.Plug.current_user(conn),
-         {user_id, _} <- Integer.parse(user_id),
-         {:ok, user} <- Users.get_user(user_id) do
-      render(conn, "profile.json", user: user)
-    end
+    %User{} = user = Pow.Plug.current_user(conn)
+    render(conn, "profile.json", user: user)
   end
 
   operation :update,
@@ -30,10 +29,15 @@ defmodule TrentoWeb.V1.ProfileController do
     request_body:
       {"UserProfileUpdateRequest", "application/json", Schema.User.UserProfileUpdateRequest},
     responses: [
-      created: {"Profile updated successfully", "application/json", Schema.User.UserProfile},
+      ok: {"Profile updated successfully", "application/json", Schema.User.UserProfile},
       unprocessable_entity: Schema.UnprocessableEntity.response()
     ]
 
-  def update(conn, _) do
+  def update(%{body_params: profile_params} = conn, _) do
+    %User{} = user = Pow.Plug.current_user(conn)
+
+    with {:ok, %User{} = updated_user} <- Users.update_user_profile(user, profile_params) do
+      render(conn, "profile.json", user: updated_user)
+    end
   end
 end
