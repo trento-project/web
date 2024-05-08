f69c7e49b8ed77b5ebccc9f189560936c450df7d
Xabier Arbulu Insausti <xarbulu@suse.com>
2024-05-08 13:03:17 +0200

    Multi select component (#2600)
    * Add react-select dependency
    
    * Create MultiSelect component
    
    * Add className to component
    
    * Update classNames usage
    
    * Update value containers color

---
+++ assets/js/common/MultiSelect/MultiSelect.test.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import userEvent from '@testing-library/user-event';
+import MultiSelect from '.';
+
+const options = [
+  { value: 1, label: 'Orange', tooltip: 'A nice orange' },
+  { value: 2, label: 'Apple', tooltip: 'A nice apple' },
+  { value: 3, label: 'Banana', tooltip: 'A nice banana' },
+];
+
+describe('MultiSelect Component', () => {
+  it('should display options', async () => {
+    const user = userEvent.setup();
+
+    render(<MultiSelect options={options} />);
+
+    await user.click(screen.getByText('Select...'));
+    options.forEach(({ label }) => {
+      expect(screen.getByText(label)).toBeVisible();
+    });
+  });
+
+  it('should display initial values', async () => {
+    const values = options.slice(0, 1);
+
+    render(<MultiSelect options={options} values={values} />);
+
+    expect(screen.getByText(values[0].label)).toBeVisible();
+  });
+
+  it('should change the values', async () => {
+    const user = userEvent.setup();
+    const mockOnChange = jest.fn();
+
+    render(<MultiSelect options={options} onChange={mockOnChange} />);
+
+    await user.click(screen.getByText('Select...'));
+    await user.click(screen.getByText(options[0].label));
+
+    expect(mockOnChange).toHaveBeenCalledWith(
+      options.slice(0, 1),
+      expect.anything()
+    );
+
+    await user.click(screen.getByText(options[0].label));
+    await user.click(screen.getByText(options[1].label));
+
+    expect(mockOnChange).toHaveBeenCalledWith(
+      options.slice(0, 2),
+      expect.anything()
+    );
+
+    const deleteIcons = screen.getAllByRole('button');
+    await user.click(deleteIcons[0]);
+
+    expect(mockOnChange).toHaveBeenCalledWith(
+      options.slice(1, 2),
+      expect.anything()
+    );
+
+    const deleteAll = document.querySelector('[data-slot="icon"]');
+    await user.click(deleteAll);
+
+    expect(mockOnChange).toHaveBeenCalledWith([], expect.anything());
+    expect(screen.getByText('Select...')).toBeVisible();
+  });
+});
