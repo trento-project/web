3244f85142c5ddf0a80173790e2a04d7dd843171
arbulu89 <xarbulu@suse.com>
2024-05-09 15:22:01 +0200

    Interpolate validation errors in error view

--- lib/trento_web/views/error_view.ex
+++ lib/trento_web/views/error_view.ex
@@ -49,7 +49,11 @@ defmodule TrentoWeb.ErrorView do
     error =
       Ecto.Changeset.traverse_errors(
         changeset,
-        fn {message, _} -> message end
+        fn {message, opts} ->
+          Regex.replace(~r"%{(\w+)}", message, fn _, key ->
+            opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+          end)
+        end
       )
 
     %{
