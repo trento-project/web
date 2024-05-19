245da983e8c82874be0506f35d2d8ffbffa6e941
EMaksy <emaksymenko@suse.com>
2024-05-19 20:46:06 +0200

    Refactor generatePassword function

--- assets/js/pages/Users/generatePassword.js
+++ assets/js/pages/Users/generatePassword.js
@@ -6,10 +6,46 @@ const SPECIAL_CHARACTERS = '~!@#$%^&*()_+={}[]|:;<>,.?/-\'"`';
 const PASSWORD_CHARACTERS =
   DIGITS + UPPERCASE_LETTERS + LOWERCASE_LETTERS + SPECIAL_CHARACTERS;
 
-export const generatePassword = (
-  length = 16,
-  characters = PASSWORD_CHARACTERS
-) =>
+export const hasValidLength = (password, pwdLength = 8) =>
+  password.length >= pwdLength;
+
+export const validateNoRepetitiveCharacters = (password) =>
+  !/(.)\1{2,}/.test(password);
+
+export const validateNoSequentialCharacters = (password) => {
+  if (password.length < 3) {
+    return true;
+  }
+  for (let i = 2; i < password.length; i += 1) {
+    const firstCharCode = password.charCodeAt(i - 2);
+    const secondCharCode = password.charCodeAt(i - 1);
+    const thirdCharCode = password.charCodeAt(i);
+
+    if (
+      (secondCharCode === firstCharCode + 1 &&
+        thirdCharCode === secondCharCode + 1) ||
+      (secondCharCode === firstCharCode - 1 &&
+        thirdCharCode === secondCharCode - 1)
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generatePassword = (length = 16, characters = PASSWORD_CHARACTERS) =>
   Array.from(crypto.getRandomValues(new Uint32Array(length)))
     .map((char) => characters[char % characters.length])
     .join('');
+
+export const generateValidPassword = (length = 16) => {
+  const password = generatePassword(length);
+  if (
+    hasValidLength(password) &&
+    validateNoRepetitiveCharacters(password) &&
+    validateNoSequentialCharacters(password)
+  ) {
+    return password;
+  }
+  return generateValidPassword(length);
+};
--- assets/js/pages/Users/generatePassword.test.js
+++ assets/js/pages/Users/generatePassword.test.js
@@ -1,20 +1,56 @@
-import { generatePassword } from './generatePassword';
+import {
+  hasValidLength,
+  validateNoRepetitiveCharacters,
+  validateNoSequentialCharacters,
+  generateValidPassword,
+} from './generatePassword';
 
 describe('generatePassword', () => {
   test('generates a password of default length 16', () => {
-    const password = generatePassword();
+    const password = generateValidPassword();
     expect(password).toHaveLength(16);
   });
 
   test('generates a password of specific length', () => {
     const length = 20;
-    const password = generatePassword(length);
+    const password = generateValidPassword(length);
     expect(password).toHaveLength(length);
   });
 
   test('generates passwords that vary', () => {
-    const firstPassword = generatePassword();
-    const secondPassword = generatePassword();
+    const firstPassword = generateValidPassword();
+    const secondPassword = generateValidPassword();
     expect(firstPassword).not.toBe(secondPassword);
+  });
+});
+
+describe('Validation helpers', () => {
+  test('validates password length to be minimum of 8', () => {
+    const validPwdLength = '12345678';
+    const invalidPWDLength = '1234567';
+    expect(hasValidLength(validPwdLength)).toBe(true);
+    expect(hasValidLength(invalidPWDLength)).toBe(false);
+  });
+
+  test('checks for repetitive characters', () => {
+    const validPwdWithoutRepetition = 'abcdef';
+    const invalidPwdWithRepetition = 'aaabbb';
+    expect(validateNoRepetitiveCharacters(validPwdWithoutRepetition)).toBe(
+      true
+    );
+    expect(validateNoRepetitiveCharacters(invalidPwdWithRepetition)).toBe(
+      false
+    );
+  });
+
+  test('checks for sequential characters', () => {
+    const validPwdWithoutSequentialCharacters = 'abc';
+    const invalidPwdWithSequentialCharacters = 'abd';
+    expect(
+      validateNoSequentialCharacters(validPwdWithoutSequentialCharacters)
+    ).toBe(false);
+    expect(
+      validateNoSequentialCharacters(invalidPwdWithSequentialCharacters)
+    ).toBe(true);
   });
 });
