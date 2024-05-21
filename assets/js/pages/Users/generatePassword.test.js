22e16786a9914b85c5a50ae2963cddd181516754
EMaksy <emaksymenko@suse.com>
2024-05-21 11:32:14 +0200

    improve test description

--- assets/js/pages/Users/generatePassword.test.js
+++ assets/js/pages/Users/generatePassword.test.js
@@ -5,52 +5,44 @@ import {
   generateValidPassword,
 } from './generatePassword';
 
-describe('generatePassword', () => {
-  test('generates a password of default length 16', () => {
+describe('Validation helper', () => {
+  test('should check password length to be minimum of 8', () => {
+    const validPwd = '12345678';
+    const invalidPwd = '1234567';
+    expect(hasValidPwdLength(validPwd)).toBe(true);
+    expect(hasValidPwdLength(invalidPwd)).toBe(false);
+  });
+
+  test('should check for repetitive characters', () => {
+    const validPwd = 'abcdef';
+    const invalidPwd = 'aaabbb';
+    expect(validateNoRepetitiveCharacters(validPwd)).toBe(true);
+    expect(validateNoRepetitiveCharacters(invalidPwd)).toBe(false);
+  });
+
+  test('should check for sequential characters', () => {
+    const validPwd = 'abd';
+    const invalidPwd = 'abc';
+    expect(validateNoSequentialCharacters(validPwd)).toBe(true);
+    expect(validateNoSequentialCharacters(invalidPwd)).toBe(false);
+  });
+});
+
+describe('generateValidPassword', () => {
+  test('should generate a password with a default length of 16', () => {
     const password = generateValidPassword();
     expect(password).toHaveLength(16);
   });
 
-  test('generates a password of specific length', () => {
+  test('should generate a password of specific length', () => {
     const length = 20;
     const password = generateValidPassword(length);
     expect(password).toHaveLength(length);
   });
 
-  test('generates passwords that vary', () => {
+  test('should generate passwords that vary', () => {
     const firstPassword = generateValidPassword();
     const secondPassword = generateValidPassword();
     expect(firstPassword).not.toBe(secondPassword);
-  });
-});
-
-describe('Validation helper', () => {
-  test('should check password length to be minimum of 8', () => {
-    const validPwdLength = '12345678';
-    const invalidPwdLength = '1234567';
-    expect(hasValidPwdLength(validPwdLength)).toBe(true);
-    expect(hasValidPwdLength(invalidPwdLength)).toBe(false);
-  });
-
-  test('should check for repetitive characters', () => {
-    const validPwdWithoutRepetition = 'abcdef';
-    const invalidPwdWithRepetition = 'aaabbb';
-    expect(validateNoRepetitiveCharacters(validPwdWithoutRepetition)).toBe(
-      true
-    );
-    expect(validateNoRepetitiveCharacters(invalidPwdWithRepetition)).toBe(
-      false
-    );
-  });
-
-  test('should check for sequential characters', () => {
-    const validPwdWithoutSequentialCharacters = 'abd';
-    const invalidPwdWithSequentialCharacters = 'abc';
-    expect(
-      validateNoSequentialCharacters(validPwdWithoutSequentialCharacters)
-    ).toBe(true);
-    expect(
-      validateNoSequentialCharacters(invalidPwdWithSequentialCharacters)
-    ).toBe(false);
   });
 });
