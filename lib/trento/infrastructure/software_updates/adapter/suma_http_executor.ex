30b3fce66a6aa36c283e4d6cabc7e411d3f3781a
Alessio Biancalana <alessio.biancalana@suse.com>
2024-05-22 09:46:40 +0200

    Add http ground work for errata details and patches for package

--- lib/trento/infrastructure/software_updates/adapter/suma_http_executor.ex
+++ lib/trento/infrastructure/software_updates/adapter/suma_http_executor.ex
@@ -35,6 +35,46 @@ defmodule Trento.Infrastructure.SoftwareUpdates.Suma.HttpExecutor do
             ) ::
               {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
 
+  @callback get_errata_details(
+              base_url :: String.t(),
+              auth :: String.t(),
+              advisory_name :: String.t(),
+              ca_cert :: String.t() | nil
+            ) ::
+              {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+
+  @callback get_affected_systems(
+              base_url :: String.t(),
+              auth :: String.t(),
+              advisory_name :: String.t(),
+              ca_cert :: String.t() | nil
+            ) ::
+              {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+
+  @callback get_cves(
+              base_url :: String.t(),
+              auth :: String.t(),
+              advisory_name :: String.t(),
+              ca_cert :: String.t() | nil
+            ) ::
+              {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+
+  @callback get_affected_packages(
+              base_url :: String.t(),
+              auth :: String.t(),
+              advisory_name :: String.t(),
+              ca_cert :: String.t() | nil
+            ) ::
+              {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+
+  @callback get_patches_for_package(
+              base_url :: String.t(),
+              auth :: String.t(),
+              package_id :: String.t(),
+              ca_cert :: String.t() | nil
+            ) ::
+              {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+
   @behaviour Trento.Infrastructure.SoftwareUpdates.Suma.HttpExecutor
 
   @impl true
@@ -55,28 +95,81 @@ defmodule Trento.Infrastructure.SoftwareUpdates.Suma.HttpExecutor do
 
   @impl true
   def get_system_id(base_url, auth, fully_qualified_domain_name, ca_cert) do
-    HTTPoison.get(
-      "#{base_url}/system/getId?name=#{fully_qualified_domain_name}",
+    HTTPoison.request(
+      :get,
+      "#{base_url}/system/getId",
       [{"Content-type", "application/json"}],
-      request_options(auth, ca_cert)
+      [params: %{name: fully_qualified_domain_name}] ++ request_options(auth, ca_cert)
     )
   end
 
   @impl true
   def get_relevant_patches(base_url, auth, system_id, ca_cert) do
-    HTTPoison.get(
-      "#{base_url}/system/getRelevantErrata?sid=#{system_id}",
+    HTTPoison.request(
+      :get,
+      "#{base_url}/system/getRelevantErrata",
       [{"Content-type", "application/json"}],
-      request_options(auth, ca_cert)
+      [params: %{sid: system_id}] ++ request_options(auth, ca_cert)
     )
   end
 
   @impl true
   def get_upgradable_packages(base_url, auth, system_id, ca_cert) do
-    HTTPoison.get(
-      "#{base_url}/system/listLatestUpgradablePackages?sid=#{system_id}",
+    HTTPoison.request(
+      :get,
+      "#{base_url}/system/listLatestUpgradablePackages",
       [{"Content-type", "application/json"}],
-      request_options(auth, ca_cert)
+      [params: %{sid: system_id}] ++ request_options(auth, ca_cert)
+    )
+  end
+
+  @impl true
+  def get_errata_details(base_url, auth, advisory_name, ca_cert) do
+    HTTPoison.request(
+      :get,
+      "#{base_url}/errata/getDetails",
+      [{"Content-type", "application/json"}],
+      [params: %{"advisoryName" => advisory_name}] ++ request_options(auth, ca_cert)
+    )
+  end
+
+  @impl true
+  def get_affected_systems(base_url, auth, advisory_name, ca_cert) do
+    HTTPoison.request(
+      :get,
+      "#{base_url}/errata/listAffectedSystems",
+      [{"Content-type", "application/json"}],
+      [params: %{"advisoryName" => advisory_name}] ++ request_options(auth, ca_cert)
+    )
+  end
+
+  @impl true
+  def get_cves(base_url, auth, advisory_name, ca_cert) do
+    HTTPoison.request(
+      :get,
+      "#{base_url}/errata/listCves",
+      [{"Content-type", "application/json"}],
+      [params: %{"advisoryName" => advisory_name}] ++ request_options(auth, ca_cert)
+    )
+  end
+
+  @impl true
+  def get_affected_packages(base_url, auth, advisory_name, ca_cert) do
+    HTTPoison.request(
+      :get,
+      "#{base_url}/errata/listPackages",
+      [{"Content-type", "application/json"}],
+      [params: %{"advisoryName" => advisory_name}] ++ request_options(auth, ca_cert)
+    )
+  end
+
+  @impl true
+  def get_patches_for_package(base_url, auth, package_id, ca_cert) do
+    HTTPoison.request(
+      :get,
+      "#{base_url}/packages/listProvidingErrata",
+      [{"Content-type", "application/json"}],
+      [params: %{pid: Integer.parse(package_id)}] ++ request_options(auth, ca_cert)
     )
   end
 
