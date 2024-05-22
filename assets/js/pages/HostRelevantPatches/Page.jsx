774392f91fbb1bea178362c2b511095379da9d94
Jan Fooken <jan.fooken@suse.com>
2024-05-22 15:50:05 +0200

    Add HostRelevantPatches page and state selectors

---
+++ assets/js/pages/HostRelevantPatches/Page.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import BackButton from '@common/BackButton';
+import PageHeader from '@common/PageHeader';
+import HostRelevantPatches from './HostRelevantPatches';
+
+import { getHost } from '@state/selectors/host';
+import { getSoftwareUpdatesPatches } from '@state/selectors/softwareUpdates';
+
+export default function Page() {
+  const { hostID } = useParams();
+
+  // TODO(janvhs): When do I need this?
+  const dispatch = useDispatch();
+  const host = useSelector(getHost(hostID));
+  const patches = useSelector((state) =>
+    getSoftwareUpdatesPatches(state, hostID)
+  );
+
+  if (!host || !patches) {
+    return <div>Retrieving data</div>;
+  }
+
+  const { hostname: hostName } = host;
+
+  // TODO(janvhs): Handle navigation
+  return (
+    <>
+      <BackButton url={`/hosts/${hostID}`}>Back to Host Details</BackButton>
+      <HostRelevantPatches patches={patches} hostName={hostName} />
+    </>
+  );
+}
