70692ba61072a5ecc38d60996cfbc26f2a3f6c7f
Alessio Biancalana <alessio.biancalana@suse.com>
2024-05-23 15:07:13 +0200

    Patches for a single package plus errata details infrastructure (#2643)
    * Add factories
    
    * Add http ground work for errata details and patches for package
    
    * Add callbacks to discovery, patches for package function to the context
    
    * Add tests
    
    * Fix missing label in tests
    
    * Change relevant_patches_hosts to relevant_patches_system_ids
    
    * LMAO logging

--- lib/trento/software_updates/discovery/gen.ex
+++ lib/trento/software_updates/discovery/gen.ex
@@ -9,10 +9,25 @@ defmodule Trento.SoftwareUpdates.Discovery.Gen do
               {:ok, pos_integer()} | {:error, any()}
 
   @callback get_relevant_patches(system_id :: pos_integer()) ::
-              {:ok, [map]} | {:error, any()}
+              {:ok, [map()]} | {:error, any()}
 
   @callback get_upgradable_packages(system_id :: pos_integer()) ::
-              {:ok, [map]} | {:error, any()}
+              {:ok, [map()]} | {:error, any()}
+
+  @callback get_patches_for_package(package_id :: String.t()) ::
+              {:ok, [map()]} | {:error, any()}
+
+  @callback get_errata_details(advisory_name :: String.t()) ::
+              {:ok, map()} | {:error, any()}
+
+  @callback get_affected_systems(advisory_name :: String.t()) ::
+              {:ok, [map()]} | {:error, any()}
+
+  @callback get_cves(advisory_name :: String.t()) ::
+              {:ok, [String.t()]} | {:error, any()}
+
+  @callback get_affected_packages(advisory_name :: String.t()) ::
+              {:ok, [map()]} | {:error, any()}
 
   @callback clear() :: :ok
 end
