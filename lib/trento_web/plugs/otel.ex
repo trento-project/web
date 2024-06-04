f8478ce80ae34e45c15088e16c936eb57f7401f2
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-06-04 09:59:09 +0200

    Some cleanup

--- lib/trento_web/plugs/otel.ex
+++ lib/trento_web/plugs/otel.ex
@@ -1,13 +1,11 @@
 defmodule TrentoWeb.Plugs.Otel do
   require OpenTelemetry.Tracer, as: Tracer
-  # alias OpenTelemetry.Ctx
 
   def init(default) do
     default
   end
 
   def call(conn, _default) do
-    _span_ctx = Tracer.start_span(:request_init)
     current_user = conn.assigns.current_user
 
     Tracer.add_event("Otel", [{"user", current_user}])
