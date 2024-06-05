09cf6aecad1a459bf2636eb9f6a913bd25fd2a75
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2024-06-05 15:19:29 +0100

    Add force ssl to demo config too

--- config/demo.exs
+++ config/demo.exs
@@ -2,6 +2,7 @@ import Config
 
 config :trento, TrentoWeb.Endpoint,
   check_origin: :conn,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
