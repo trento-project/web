43afa97ed488a575be3c985e3ec430c1dabc1f8a
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-06-06 10:37:13 +0200

    disable force ssl

--- config/demo.exs
+++ config/demo.exs
@@ -2,7 +2,7 @@ import Config
 
 config :trento, TrentoWeb.Endpoint,
   check_origin: false,
-  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
