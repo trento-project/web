c952bb92593a3c191c7ab35cf0193782414ce781
Nelson Kopliku <nelson.kopliku@suse.com>
2024-06-14 10:28:44 +0200

    Improve activity logging migration rollback

--- priv/repo/migrations/20240612081138_add_activity_log_settings.exs
+++ priv/repo/migrations/20240612081138_add_activity_log_settings.exs
@@ -6,4 +6,12 @@ defmodule Trento.Repo.Migrations.AddActivityLogSettings do
       add :activity_log_retention_time, :map
     end
   end
+
+  def down do
+    execute "DELETE from settings WHERE type = 'activity_log_settings'"
+
+    alter table(:settings) do
+      remove :activity_log_retention_time
+    end
+  end
 end
