76fb9c80975018fe4475635a9859318ddc938b1d
Jan Fooken <jan.fooken@suse.com>
2024-06-21 13:25:46 +0200

    Add simple test for the advisory details page
    This commit adds a test case for the advisory page.

---
+++ assets/js/pages/AdvisoryDetails/AdvisoryDetails.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { faker } from '@faker-js/faker';
+
+import { renderWithRouter as render } from '@lib/test-utils';
+import { advisoryErrataFactory } from '@lib/test-utils/factories';
+
+import AdvisoryDetails from './AdvisoryDetails';
+
+describe('AdvisoryDetails', () => {
+  it('displays a message, when the CVE, packages or fixes section is empty', () => {
+    const errata = advisoryErrataFactory.build();
+
+    render(
+      <AdvisoryDetails advisoryName={faker.lorem.word()} errata={errata} />
+    );
+
+    expect(screen.getAllByText('No data dvailable').length).toBe(3);
+  });
+});
