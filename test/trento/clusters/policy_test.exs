facfd830dab7f04bc8e91d078b840cda6ad41e16
arbulu89 <xarbulu@suse.com>
2024-06-27 09:39:30 +0200

    Add cluster policy tests

---
+++ test/trento/clusters/policy_test.exs
@@ -0,0 +1,28 @@
+defmodule Trento.Clusters.PolicyTest do
+  use ExUnit.Case
+
+  alias Trento.Abilities.Ability
+  alias Trento.Clusters.Policy
+  alias Trento.Clusters.Projections.ClusterReadModel
+  alias Trento.Users.User
+
+  test "should allow select_checks operations if the user has all:cluster_checks_selection ability" do
+    user = %User{abilities: [%Ability{name: "all", resource: "cluster_checks_selection"}]}
+
+    assert Policy.authorize(:select_checks, user, ClusterReadModel)
+  end
+
+  test "should disallow select_checks operations if the user does not have all:cluster_checks_selection ability" do
+    user = %User{abilities: []}
+
+    refute Policy.authorize(:select_checks, user, ClusterReadModel)
+  end
+
+  test "should allow unguarded actions" do
+    user = %User{abilities: []}
+
+    Enum.each([:list, :request_checks_execution], fn action ->
+      assert Policy.authorize(action, user, ClusterReadModel)
+    end)
+  end
+end
