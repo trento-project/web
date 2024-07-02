bc0b5a5234c91cb98bb94830b7cf7ee8505d67ca
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-07-02 18:15:45 +0200

    Implements the controller

---
+++ lib/trento_web/openapi/v1/schema/activity_log.ex
@@ -0,0 +1,34 @@
+defmodule TrentoWeb.OpenApi.V1.Schema.ActivityLog do
+  @moduledoc false
+
+  require OpenApiSpex
+  alias OpenApiSpex.Schema
+
+  OpenApiSpex.schema(%{
+    title: "ActivityLog",
+    description: "Activity Log for the current installation.",
+    type: :array,
+    items: %Schema{
+      type: :object,
+      additionalProperties: false,
+      properties: %{
+        type: %Schema{
+          type: :string,
+          description: "Type of Activity Log entry."
+        },
+        actor: %Schema{
+          type: :string,
+          description: "Actor causing an Activity Log entry. E.g. System or a specific user."
+        },
+        metadata: %Schema{
+          type: :object
+        },
+        inserted_at: %Schema{
+          type: :string,
+          description: "Timestamp upon Activity Log entry insertion."
+        }
+      },
+      required: [:type, :actor, :metadata, :inserted_at]
+    }
+  })
+end
