c72002230563cd210daaab8df27991aa2a811d10
Alessio Biancalana <alessio.biancalana@suse.com>
2024-07-08 10:51:19 +0200

    Add sequence usage to factories
    ... for upgradable packages factories and advisory factory

--- assets/js/lib/test-utils/factories/upgradablePackage.js
+++ assets/js/lib/test-utils/factories/upgradablePackage.js
@@ -5,10 +5,10 @@ import { patchForPackageFactory } from './relevantPatches';
 const releaseVersionFactory = () =>
   `${faker.number.int({ min: 100000, max: 160000 })}.${faker.system.semver()}`;
 
-export const upgradablePackageFactory = Factory.define(() => ({
+export const upgradablePackageFactory = Factory.define(({ sequence }) => ({
   from_epoch: faker.date.anytime(),
   to_release: releaseVersionFactory(),
-  name: faker.word.noun(),
+  name: `${faker.word.noun()}${sequence}`,
   from_release: releaseVersionFactory(),
   to_epoch: faker.date.anytime(),
   arch: faker.airline.flightNumber(),
