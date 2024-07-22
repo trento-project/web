eaa4ae29f47cde3ed665b3afc242ea94814623a2
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-07-22 11:01:20 +0200

    Adds FE flaky tests detection
    and some initial results

--- assets/jest.config.js
+++ assets/jest.config.js
@@ -123,7 +123,16 @@ module.exports = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    "default",
+    ["jest-junit", {
+      addFileAttribute: "true",
+      ancestorSeparator: " › ",
+      classNameTemplate: "{classname}",
+      titleTemplate: "{title}", // outputname
+      includeConsoleOutput: true
+    }]
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
