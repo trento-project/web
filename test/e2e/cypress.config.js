70aa96673b0b63339c2a29e4d591faaa9c6bd625
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-07-29 15:52:06 +0200

    test e2e cypress parallel split

--- test/e2e/cypress.config.js
+++ test/e2e/cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress');
+const cypressSplit = require('cypress-split');
 
 module.exports = defineConfig({
   viewportWidth: 1366,
@@ -20,6 +21,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      cypressSplit(on, config);
       return require('./cypress/plugins/index.js')(on, config);
     },
     testIsolation: false,
