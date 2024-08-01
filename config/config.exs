76d7678b83f834284258610b7c403ed4b82ab527
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-08-01 13:46:29 +0200

    locked users could not login through IDP

--- config/config.exs
+++ config/config.exs
@@ -84,6 +84,8 @@ config :trento, :pow,
   extensions: [PowPersistentSession],
   controller_callbacks: Pow.Extension.Phoenix.ControllerCallbacks
 
+config :trento, :pow_assent, user_identities_context: Trento.UserIdentities
+
 config :trento, :oidc, enabled: false
 
 # Agent heartbeat interval. Adding one extra second to the agent 5s interval to avoid glitches
