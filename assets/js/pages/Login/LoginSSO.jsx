77662a3fa0af402d82795d5633e80c5ee91ca539
Xabier Arbulu Insausti <xarbulu@suse.com>
2024-08-08 10:25:30 +0200

    Improve frontend OIDC callback (#2871)
    * Send oidc callback url to frontend
    
    * Get oidc callback url in frontend functions
    
    * Refactor and test oidc enrollment saga
    
    * Refactor and test oidc callback component

--- assets/js/pages/Login/LoginSSO.jsx
+++ assets/js/pages/Login/LoginSSO.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 import Button from '@common/Button';
 
-export default function LoginSSO({ singleSignOnUrl }) {
+export default function LoginSSO({ singleSignOnUrl, error }) {
   return (
-    <Button
-      onClick={() => {
-        window.location.href = singleSignOnUrl;
-      }}
-    >
-      Login with Single Sign-on
-    </Button>
+    <>
+      {error && (
+        <span className="block text-sm font-medium mb-5">
+          An error occurred while trying to Login. Please retry login again.
+          Should the error persist, contact the administrator.
+        </span>
+      )}
+      <Button
+        onClick={() => {
+          window.location.href = singleSignOnUrl;
+        }}
+      >
+        Login with Single Sign-on
+      </Button>
+    </>
   );
 }
