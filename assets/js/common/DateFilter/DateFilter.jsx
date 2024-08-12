edbb2296c704e1569273cab7b6de14f5e1709404
Emanuele De Cupis <emanuele.decupis@suse.com>
2024-08-12 15:37:48 +0200

    Change icon (#2870)

--- assets/js/common/DateFilter/DateFilter.jsx
+++ assets/js/common/DateFilter/DateFilter.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import { Transition } from '@headlessui/react';
 
 import useOnClickOutside from '@hooks/useOnClickOutside';
-import { EOS_CLOSE } from 'eos-icons-react';
+import { EOS_CLOSE, EOS_CHECK } from 'eos-icons-react';
 
 import Input from '@common/Input';
 
@@ -67,19 +67,7 @@ const getSelectedOption = (options, value) => {
 function Tick() {
   return (
     <span className="absolute inset-y-0 right-0 flex items-center pr-4">
-      <svg
-        className="h-5 w-5"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          fillRule="evenodd"
-          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-          clipRule="evenodd"
-        />
-      </svg>
+      <EOS_CHECK size="m" />
     </span>
   );
 }
