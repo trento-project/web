d8a5903a56a8a10be66d4735a628f6883d6cccd9
balanza <emanuele.decupis@suse.com>
2024-08-13 16:09:44 +0200

    refactor

--- assets/js/common/Filter/Filter.test.jsx
+++ assets/js/common/Filter/Filter.test.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import React, { act } from 'react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Filter from '.';
