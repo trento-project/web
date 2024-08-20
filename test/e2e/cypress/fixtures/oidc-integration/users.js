58f32cfeaad5571394a410974eeb2692b5eb4fa1
Xabier Arbulu Insausti <xarbulu@suse.com>
2024-08-20 14:50:29 +0200

    OIDC integration E2E  tests (#2908)
    * Add admin user to keycloak trento realm
    
    * Add OIDC integration e2e tests
    
    * Run integration test conditionally
    
    * Add github action to run integration test

---
+++ test/e2e/cypress/fixtures/oidc-integration/users.js
@@ -0,0 +1,14 @@
+export const plainUser = {
+  username: 'trentoidp',
+  password: 'password',
+  fullname: 'Trento IDP user Of Monk',
+  email: 'trentoidp@trento.suse.com',
+};
+
+export const adminUser = {
+  username: 'admin',
+  password: 'admin',
+  fullname: 'Trento Admin',
+  email: 'admin@trento.suse.com',
+  permissions: 'all:all',
+};
