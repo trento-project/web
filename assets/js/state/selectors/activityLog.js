ca1ad1790258fedbc313d8c92aedec6c78563c59
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-08-26 15:38:34 +0200

    Fixes warning
    Fix for identityFunctionCheck warning in console.

--- assets/js/state/selectors/activityLog.js
+++ assets/js/state/selectors/activityLog.js
@@ -2,6 +2,6 @@ import { createSelector } from '@reduxjs/toolkit';
 
 
 export const getAlUsers = createSelector(
-  [(state) => state.activityLog.users],
-  (users) => users
+  [(state) => state.activityLog],
+  (activityLog) => activityLog.users
 );
