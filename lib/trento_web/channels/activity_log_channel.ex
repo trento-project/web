bca30938f2fe29dcdb6cfa3cd6cc0cbdb4b6ace8
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-09-09 15:01:32 +0200

    Handles scenario with deleted users
    Collapses usernames from deleted users into a username without timestamp suffix

--- lib/trento_web/channels/activity_log_channel.ex
+++ lib/trento_web/channels/activity_log_channel.ex
@@ -31,11 +31,33 @@ defmodule TrentoWeb.ActivityLogChannel do
 
   @impl true
   def handle_info(:after_join, socket) do
-    users = ["system" | Users.list_all_usernames()]
+    all_usernames = Users.list_all_usernames()
+    collapsed_usernames = collapse_usernames(all_usernames)
+
+    users = ["system" | collapsed_usernames]
     push(socket, "al_users_pushed", %{users: users})
     Process.send_after(self(), :after_join, 60_000)
     {:noreply, socket}
   end
 
   defp allowed?(user_id, current_user_id), do: String.to_integer(user_id) == current_user_id
+
+  defp collapse_usernames(usernames) do
+    usernames
+    |> Enum.map(fn username ->
+      maybe_ts =
+        username
+        |> String.split("__")
+        |> List.last()
+
+      case DateTime.from_iso8601(maybe_ts) do
+        {:ok, _, _} ->
+          String.trim_trailing(username, "__" <> maybe_ts)
+
+        _ ->
+          username
+      end
+    end)
+    |> Enum.uniq()
+  end
 end
