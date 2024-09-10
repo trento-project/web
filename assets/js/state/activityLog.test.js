dcf0da527fae227348e2a069d622eb601e8927d5
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-09-10 13:44:52 +0200

    Adds test case for non-empty initial state

--- assets/js/state/activityLog.test.js
+++ assets/js/state/activityLog.test.js
@@ -12,4 +12,15 @@ describe('activityLog reducer', () => {
       users,
     });
   });
+  it('should set the users for activity log when setUsers is dispatched with non empty initial state', () => {
+    const { username: username1 } = userFactory.build();
+    const { username: username2 } = userFactory.build();
+    const { username: username3 } = userFactory.build();
+    const users = [username1, username2];
+    const nonEmptyInitialState = {users: [username3]};
+    const action = setUsers({ users });
+    expect(activityLogReducer(nonEmptyInitialState, action)).toEqual({
+      users
+    });
+  });
 });
