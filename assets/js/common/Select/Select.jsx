4d56d91edbbd376b80ec955c848f7baeac255ef9
balanza <emanuele.decupis@suse.com>
2024-09-26 11:58:37 +0200

    fix glitch

--- assets/js/common/Select/Select.jsx
+++ assets/js/common/Select/Select.jsx
@@ -54,7 +54,7 @@ function Select({
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-[1]">
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-40">
               {enrichedOptions.map((option) => (
                 <Listbox.Option
                   key={option.value}
