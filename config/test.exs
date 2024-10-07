e867b3896043b3149505c45d50fe40efda4ff605
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-10-07 11:33:18 +0200

    Address PR feedback

--- config/test.exs
+++ config/test.exs
@@ -103,4 +103,4 @@ config :junit_formatter,
 
 config :trento,
        :flaky_tests_detection,
-       enabled?: System.get_env("WRITE_JUNIT") == "1" || false
+       enabled?: System.get_env("WRITE_JUNIT") == "1"
