3dbff998882b65be5eac78fde44a26bf7aa8675b
Nelson Kopliku <nelson.kopliku@suse.com>
2024-10-24 15:17:32 +0200

    Remove unnecessary virtual fields from cluster

--- test/trento_web/views/v2/cluster_view_json_test.exs
+++ test/trento_web/views/v2/cluster_view_json_test.exs
@@ -2,6 +2,7 @@ defmodule TrentoWeb.V2.ClusterJSONTest do
   use TrentoWeb.ConnCase, async: true
   import Trento.Factory
 
+  require Trento.Clusters.Enums.ClusterType, as: ClusterType
   alias TrentoWeb.V2.ClusterJSON
 
   alias Trento.Clusters.Projections.ClusterReadModel
@@ -23,5 +24,15 @@ defmodule TrentoWeb.V2.ClusterJSONTest do
 
     assert %{details: %{maintenance_mode: false}} =
              ClusterJSON.cluster(%{cluster: cluster})
+  end
+
+  test "should remove cluster detail enrichment virtual field" do
+    for type <- ClusterType.values() do
+      cluster = build(:cluster, type: type)
+      assert Map.has_key?(cluster, :enriching_details)
+
+      cluster_view = ClusterJSON.cluster(%{cluster: cluster})
+      refute Map.has_key?(cluster_view, :enriching_details)
+    end
   end
 end
