c6cc2da9144f789e2229af12e22e9540e879192e
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2024-11-06 12:10:53 +0100

    Improves parser tests
    This adds expected parses for given test cases to assert against

--- test/trento/activity_logging/metadata_query_parser_test.exs
+++ test/trento/activity_logging/metadata_query_parser_test.exs
@@ -6,37 +6,38 @@ defmodule Trento.ActivityLog.MetadataQueryParserTest do
   alias Trento.ActivityLog.MetadataQueryParser
 
   @parse_succeed_cases [
-    "foo",
-    "  foo",
-    "foo  ",
-    "  foo  ",
-    "foo42",
-    "foo_bar",
-    "foo-bar",
-    "foo-bar_baz42",
-    "*foo-bar_baz42*",
-    "*foo*",
-    "*foo",
-    "foo*",
-    "foo bar",
-    "foo AND bar",
-    "foo OR bar",
-    "foo AND bar AND baz",
-    "foo AND bar OR baz",
-    "foo OR bar AND baz",
-    "foo bar OR baz",
-    "foo OR bar baz",
-    "foo bar baz",
-    "==20",
-    ">20",
-    "<20",
-    "!=20",
-    ">3 AND <5",
-    "!=5 AND !=20",
-    "==5 OR ==20",
-    "<3 OR >5",
-    "<3 >5 !=42"
+    {"foo", "(@.** == \"foo\")"},
+    {"  foo", "(@.** == \"foo\")"},
+    {"foo  ", "(@.** == \"foo\")"},
+    {"  foo  ", "(@.** == \"foo\")"},
+    {"foo42", "(@.** == \"foo42\")"},
+    {"foo_bar", "(@.** == \"foo_bar\")"},
+    {"foo-bar", "(@.** == \"foo-bar\")"},
+    {"foo-bar_baz42", "(@.** == \"foo-bar_baz42\")"},
+    {"*foo-bar_baz42*", "(@.** like_regex \"^[a-zA-Z0-9_-]*foo-bar_baz42[a-zA-Z0-9_-]*$\")"},
+    {"*foo*", "(@.** like_regex \"^[a-zA-Z0-9_-]*foo[a-zA-Z0-9_-]*$\")"},
+    {"*foo", "(@.** like_regex \"^[a-zA-Z0-9_-]*foo$\")"},
+    {"foo*", "(@.** starts with \"foo\")"},
+    {"foo bar", "((@.** == \"foo\") || (@.** == \"bar\"))"},
+    {"foo AND bar", "((@.** == \"foo\") && (@.** == \"bar\"))"},
+    {"foo OR bar", "((@.** == \"foo\") || (@.** == \"bar\"))"},
+    {"foo AND bar AND baz", "((@.** == \"foo\") && (@.** == \"bar\") && (@.** == \"baz\"))"},
+    {"foo AND bar OR baz", "((@.** == \"foo\") && (@.** == \"bar\") || (@.** == \"baz\"))"},
+    {"foo OR bar AND baz", "((@.** == \"foo\") || (@.** == \"bar\") && (@.** == \"baz\"))"},
+    {"foo bar OR baz", "((@.** == \"foo\") || (@.** == \"bar\") || (@.** == \"baz\"))"},
+    {"foo OR bar baz", "((@.** == \"foo\") || (@.** == \"bar\") || (@.** == \"baz\"))"},
+    {"foo bar baz", "((@.** == \"foo\") || (@.** == \"bar\") || (@.** == \"baz\"))"},
+    {"==20", "(@.** == 20)"},
+    {">20", "(@.** > 20)"},
+    {"<20", "(@.** < 20)"},
+    {"!=20", "(@.** != 20)"},
+    {">3 AND <5", "((@.** > 3) && (@.** < 5))"},
+    {"!=5 AND !=20", "((@.** != 5) && (@.** != 20))"},
+    {"==5 OR ==20", "((@.** == 5) || (@.** == 20))"},
+    {"<3 OR >5", "((@.** < 3) || (@.** > 5))"},
+    {"<3 >5 !=42", "((@.** < 3) || (@.** > 5) || (@.** != 42))"}
   ]
+
   @parse_fail_cases [
     "",
     "    ",
@@ -47,10 +48,10 @@ defmodule Trento.ActivityLog.MetadataQueryParserTest do
   ]
   describe "Metadata query parsing into Jsonpath expressions" do
     test "Should parse successfully." do
-      for test_case <- @parse_succeed_cases do
+      for {test_case, expected_parse} <- @parse_succeed_cases do
         assert {:ok, parsed_string} = MetadataQueryParser.parse(test_case)
 
-        assert is_binary(parsed_string)
+        assert parsed_string == expected_parse
       end
     end
 
