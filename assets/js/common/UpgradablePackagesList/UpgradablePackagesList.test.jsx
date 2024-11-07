5b3dc340446ab31544d8f182f45bb30fb1a07f5f
EMaksy <emaksymenko@suse.com>
2024-11-07 15:06:41 +0100

    Fix test

--- assets/js/common/UpgradablePackagesList/UpgradablePackagesList.test.jsx
+++ assets/js/common/UpgradablePackagesList/UpgradablePackagesList.test.jsx
@@ -7,18 +7,18 @@ import UpgradablePackagesList from './UpgradablePackagesList';
 describe('UpgradablePackagesList component', () => {
   it('should render the upgradable packages list', () => {
     const upgradablePackage = upgradablePackageFactory.build();
-    const { original_patches } = upgradablePackage;
+    const { patches } = upgradablePackage;
 
     const expectedInstalledPackage = `${upgradablePackage.name}-${upgradablePackage.from_version}-${upgradablePackage.from_release}.${upgradablePackage.arch}`;
-    upgradablePackage.installed_package = expectedInstalledPackage;
+    upgradablePackage.installedPackage = expectedInstalledPackage;
     const expectedLatestPackage = `${upgradablePackage.name}-${upgradablePackage.to_version}-${upgradablePackage.to_release}.${upgradablePackage.arch}`;
-    upgradablePackage.latest_package = expectedLatestPackage;
+    upgradablePackage.latestPackage = expectedLatestPackage;
 
     render(<UpgradablePackagesList upgradablePackages={[upgradablePackage]} />);
 
     expect(screen.getByText(expectedInstalledPackage)).toBeVisible();
     expect(screen.getByText(expectedLatestPackage)).toBeVisible();
-    original_patches.forEach(({ advisory }) => {
+    patches.forEach(({ advisory }) => {
       expect(screen.getByText(advisory)).toBeVisible();
     });
   });
