f829c6fed73abafaa99f1539c5242eed1cb3989c
Nelson Kopliku <nelson.kopliku@suse.com>
2024-11-08 10:48:42 +0100

    Add support for chained certificates (#3134)
    * Add support for chained certificates
    
    * Fix typo
    
    * Address review comments

--- lib/trento/infrastructure/software_updates/adapter/suma_http_executor.ex
+++ lib/trento/infrastructure/software_updates/adapter/suma_http_executor.ex
@@ -200,6 +200,15 @@ defmodule Trento.Infrastructure.SoftwareUpdates.Suma.HttpExecutor do
     )
   end
 
+  def get_cert_der(ca_cert) do
+    ca_cert
+    |> :public_key.pem_decode()
+    |> Enum.map(&:public_key.pem_entry_decode/1)
+    |> Enum.map(fn {asn1_type, _, _, _} = asn1_entry ->
+      :public_key.der_encode(asn1_type, asn1_entry)
+    end)
+  end
+
   defp request_options(auth, ca_cert),
     do: [hackney: [cookie: [auth]]] ++ ssl_options(ca_cert) ++ timeout_options()
 
@@ -208,21 +217,5 @@ defmodule Trento.Infrastructure.SoftwareUpdates.Suma.HttpExecutor do
   defp ssl_options(nil), do: []
 
   defp ssl_options(ca_cert),
-    do: [ssl: [verify: :verify_peer, cacerts: [get_cert_der(ca_cert)]]]
-
-  def get_cert_der(ca_cert) do
-    {cert_type, cert_entry} =
-      ca_cert
-      |> :public_key.pem_decode()
-      |> hd()
-      |> :public_key.pem_entry_decode()
-      |> split_type_and_entry()
-
-    :public_key.der_encode(cert_type, cert_entry)
-  end
-
-  def split_type_and_entry(ans1_entry) do
-    ans1_type = elem(ans1_entry, 0)
-    {ans1_type, ans1_entry}
-  end
+    do: [ssl: [verify: :verify_peer, cacerts: get_cert_der(ca_cert)]]
 end
---
+++ test/trento/infrastructure/software_updates/adapter/suma_http_executor_test.exs
@@ -0,0 +1,29 @@
+defmodule Trento.Infrastructure.SoftwareUpdates.Adapter.SumaHttpExecutorTest do
+  use ExUnit.Case
+
+  alias Trento.Infrastructure.SoftwareUpdates.Suma.HttpExecutor
+
+  def load_certificate_content(name) do
+    File.cwd!()
+    |> Path.join("/test/fixtures/cert")
+    |> Path.join(name)
+    |> File.read!()
+  end
+
+  describe "Http executor certificate handling" do
+    test "should support chained certificates" do
+      scenarios = [
+        %{cert: "one_entry_chain.pem", expected_entries: 1},
+        %{cert: "two_entries_chain.pem", expected_entries: 2},
+        %{cert: "three_entries_chain.pem", expected_entries: 3}
+      ]
+
+      for %{cert: cert, expected_entries: expected_entries} <- scenarios do
+        assert cert
+               |> load_certificate_content()
+               |> HttpExecutor.get_cert_der()
+               |> length() == expected_entries
+      end
+    end
+  end
+end
