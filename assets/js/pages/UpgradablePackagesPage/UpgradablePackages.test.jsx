86df124b540bb3e52eb90915b98db7cda6219f8d
EMaksy <emaksymenko@suse.com>
2024-11-11 12:51:27 +0100

    Remove complicated test

--- assets/js/pages/UpgradablePackagesPage/UpgradablePackages.test.jsx
+++ assets/js/pages/UpgradablePackagesPage/UpgradablePackages.test.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { noop } from 'lodash';
 import { faker } from '@faker-js/faker';
 import { upgradablePackageFactory } from '@lib/test-utils/factories/upgradablePackage';
-import { patchForPackageFactory } from '@lib/test-utils/factories/relevantPatches';
 import { renderWithRouter as render } from '@lib/test-utils';
 
 import UpgradablePackages from './UpgradablePackages';
@@ -110,87 +109,6 @@ describe('exports the packages in CSV format', () => {
       name: `${hostName}-upgradable-packages.csv`,
       size: 88,
     });
-  });
-
-  afterAll(() => {
-    delete window.URL.createObjectURL;
-    delete window.URL.revokeObjectURL;
-  });
-
-  it('sorts the packages alphabetically in the CSV', async () => {
-    const user = userEvent.setup();
-    const hostName = faker.string.uuid();
-    const firstUpgradablePackage = upgradablePackageFactory.build({
-      name: 'zlib',
-      from_version: '1.2.11',
-      from_release: '1',
-      arch: 'x86_64',
-      to_version: '3',
-      to_release: '4',
-      patches: [
-        patchForPackageFactory.build({
-          advisory: 'SUSE-15-SP4-2024-123',
-        }),
-      ],
-    });
-    const secondUpgradablePackage = upgradablePackageFactory.build({
-      name: 'elixir',
-      from_version: '1.2.11',
-      from_release: '1',
-      arch: 'x86_64',
-      to_version: '3',
-      to_release: '4',
-      patches: [
-        patchForPackageFactory.build({
-          advisory: 'SUSE-15-SP4-2024-630',
-        }),
-      ],
-    });
-
-    const upgradablePackage = [firstUpgradablePackage, secondUpgradablePackage];
-    const csvHeader = 'installed_package,latest_package,patches';
-    const firstCsvEntry = `${secondUpgradablePackage.name}-${secondUpgradablePackage.from_version}-${secondUpgradablePackage.from_release}.${secondUpgradablePackage.arch},${secondUpgradablePackage.name}-${secondUpgradablePackage.to_version}-${secondUpgradablePackage.to_release}.${secondUpgradablePackage.arch},${secondUpgradablePackage.patches[0].advisory}`;
-    const secondCsvEntry = `${firstUpgradablePackage.name}-${firstUpgradablePackage.from_version}-${firstUpgradablePackage.from_release}.${firstUpgradablePackage.arch},${firstUpgradablePackage.name}-${firstUpgradablePackage.to_version}-${firstUpgradablePackage.to_release}.${firstUpgradablePackage.arch},${firstUpgradablePackage.patches[0].advisory}`;
-
-    const patchesLoading = false;
-    render(
-      <UpgradablePackages
-        hostName={hostName}
-        upgradablePackages={upgradablePackage}
-        patchesLoading={patchesLoading}
-      />
-    );
-
-    const csvButton = screen.getByText('Download CSV');
-    user.click(csvButton);
-    expect(window.URL.createObjectURL).toHaveBeenCalledWith(new File([], ''));
-
-    const mockCreateObjectURL = jest.spyOn(window.URL, 'createObjectURL');
-    mockCreateObjectURL.mockReturnValue('mocked-url');
-    await waitFor(() => expect(mockCreateObjectURL).toHaveBeenCalled(), {
-      timeout: 1000,
-    });
-    const csvObj = mockCreateObjectURL.mock.calls[0][0];
-
-    const reader = new FileReader();
-
-    reader.readAsText(csvObj);
-    await new Promise((resolve) => {
-      reader.onloadend = resolve;
-    });
-
-    const actualCsvData = reader.result
-      .split('\n')
-      .map((line) => line.trim())
-      .join('\n');
-    const expectedCsvData = [csvHeader, firstCsvEntry, secondCsvEntry]
-      .join('\n')
-      .split('\n')
-      .map((line) => line.trim())
-      .join('\n');
-
-    expect(actualCsvData).toBe(expectedCsvData);
-    mockCreateObjectURL.mockRestore();
   });
 
   afterAll(() => {
