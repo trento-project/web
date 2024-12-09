19125606e7473b0391dc79cd41d7f68685024cbb
Nelson Kopliku <nelson.kopliku@suse.com>
2024-12-09 12:20:43 +0100

    Make sure actor is redacted in API response for users without required permissions

--- lib/trento_web/controllers/v1/activity_log_controller.ex
+++ lib/trento_web/controllers/v1/activity_log_controller.ex
@@ -3,7 +3,7 @@ defmodule TrentoWeb.V1.ActivityLogController do
   use OpenApiSpex.ControllerSpecs
 
   alias Trento.ActivityLog
-  alias Trento.Support.AbilitiesHelper
+  alias Trento.ActivityLog.Policy
   alias TrentoWeb.OpenApi.V1.Schema
 
   plug OpenApiSpex.Plug.CastAndValidate, json_render_error_v2: true
@@ -68,22 +68,15 @@ defmodule TrentoWeb.V1.ActivityLogController do
 
   def get_activity_log(conn, params) do
     user = Pow.Plug.current_user(conn)
-    include_all_logs? = include_all_logs?(user)
+    include_all_logs? = Policy.include_all_logs?(user)
 
     with {:ok, activity_log_entries, meta} <-
            ActivityLog.list_activity_log(params, include_all_logs?) do
       render(conn, :activity_log, %{
         activity_log: activity_log_entries,
-        pagination: meta
+        pagination: meta,
+        current_user: user
       })
     end
   end
-
-  defp include_all_logs?(user),
-    do:
-      AbilitiesHelper.has_global_ability?(user) or
-        AbilitiesHelper.user_has_ability?(user, %{
-          name: "all",
-          resource: "users"
-        })
 end
