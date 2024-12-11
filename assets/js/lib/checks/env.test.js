bac6a38eb5cc1fcc030cce0f939d1af0070ee3d3
EMaksy <emaksymenko@suse.com>
2024-12-11 18:31:07 +0100

    Fix and update tests

--- assets/js/lib/checks/env.test.js
+++ assets/js/lib/checks/env.test.js
@@ -1,7 +1,12 @@
 import { faker } from '@faker-js/faker';
 import { pick } from 'lodash';
 
-import { ASCS_ERS, HANA_SCALE_UP } from '@lib/model/clusters';
+import {
+  ASCS_ERS,
+  HANA_SCALE_UP,
+  COST_OPT_SCENARIO,
+  PERFORMANCE_SCENARIO,
+} from '@lib/model/clusters';
 
 import { buildEnv } from '.';
 
@@ -29,9 +34,10 @@ describe('buildEnv', () => {
     expect(env).toEqual(expectedPayload);
   });
 
-  it('should build and env for HANA clusters', () => {
+  it('should build and env for HANA sclae up performance clusters', () => {
     const payload = {
       cluster_type: HANA_SCALE_UP,
+      hana_scenario: PERFORMANCE_SCENARIO,
       provider: faker.color.rgb(),
       target_type: faker.hacker.noun(),
       ensa_version: faker.number.int(),
@@ -41,6 +47,31 @@ describe('buildEnv', () => {
 
     const expectedPayload = pick(payload, [
       'cluster_type',
+      'hana_scenario',
+      'provider',
+      'target_type',
+      'architecture_type',
+    ]);
+
+    const env = buildEnv(payload);
+
+    expect(env).toEqual(expectedPayload);
+  });
+
+  it('should build and env for HANA scale up cost opt clusters', () => {
+    const payload = {
+      cluster_type: HANA_SCALE_UP,
+      hana_scenario: COST_OPT_SCENARIO,
+      provider: faker.color.rgb(),
+      target_type: faker.hacker.noun(),
+      ensa_version: faker.number.int(),
+      filesystem_type: faker.animal.dog(),
+      architecture_type: faker.hacker.noun(),
+    };
+
+    const expectedPayload = pick(payload, [
+      'cluster_type',
+      'hana_scenario',
       'provider',
       'target_type',
       'architecture_type',
