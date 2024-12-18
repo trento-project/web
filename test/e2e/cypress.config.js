11dc3cebd8304e88a85ff13d0719a53dfc9d61c9
Vicente Ruiz <vicente.ruiz@suse.com>
2024-12-18 07:54:06 +0100

    fix cypress config, left undefined cypress split accidentally

--- test/e2e/cypress.config.js
+++ test/e2e/cypress.config.js
@@ -21,8 +21,6 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      cypressSplit(on, config);
-
       return require('./cypress/plugins/index.js')(on, config);
     },
     testIsolation: false,
