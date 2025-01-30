0ea4c9c33411555c0bb46eb9f423dfd4eba68319
Vicente Ruiz <vicente.ruiz@suse.com>
2025-01-30 13:01:11 +0100

    final touches and organize po functions

--- test/e2e/cypress/pageObject/activity-log-po.js
+++ test/e2e/cypress/pageObject/activity-log-po.js
@@ -4,6 +4,7 @@ import * as basePage from './base-po.js';
 const activityLogEndpointAlias = 'activityLogRequest';
 
 //Selectors
+
 const filteringElements = 'div[class="relative"]';
 const refreshRateFilter = 'button[class*="refresh-rate"] ';
 const metadataSearchInput = 'input[name="metadata-search"]';
@@ -32,6 +33,7 @@ const autoRefreshIntervalButton = 'button[class*="refresh-rate"]';
 const availableRefreshRates = 'button[class*="refresh-rate"] + div div';
 
 //Test data
+
 export const expectedRefreshRates = [
   'Off',
   '5s',
@@ -46,44 +48,44 @@ export const visit = (queryString = '') => {
   return basePage.visit(`/activity_log${queryString}`);
 };
 
-export const autoRefreshIntervalButtonHasTheExpectedValue = (refreshRate) => {
-  return cy.get(autoRefreshIntervalButton).should('have.text', refreshRate);
+// Network Interception
+
+export const interceptActivityLogEndpoint = () => {
+  return cy
+    .intercept({
+      url: '/api/v1/activity_log*',
+    })
+    .as(activityLogEndpointAlias);
+};
+
+export const spyActivityLogRequest = () => {
+  cy.clock();
+  return cy.intercept(
+    '/api/v1/activity_log*',
+    cy.spy().as(activityLogEndpointAlias)
+  );
+};
+
+export const waitForActivityLogRequest = () => {
+  return basePage.waitForRequest(activityLogEndpointAlias);
+};
+
+// UI Interactions
+
+export const clickFilterTypeButton = () => {
+  return cy.get(filterTypeButton).click();
 };
 
 export const clickAutoRefreshRateButton = () => {
   return cy.get(autoRefreshIntervalButton).click();
 };
 
-export const autoRefreshButtonIsEnabled = () => {
-  return cy.get(autoRefreshIntervalButton).should('be.enabled');
-};
-
-export const autoRefreshIntervalButtonIsDisabled = () => {
-  return cy.get(autoRefreshIntervalButton).should('be.disabled');
-};
-
-export const clickFilterTypeButton = () => {
-  return cy.get(filterTypeButton).click();
-};
-
-export const selectFilterTypeOption = (option) => {
-  return cy.get(`span:contains("${option}")`).click();
-};
-
 export const clickFilterNewerThanButton = () => {
   return cy.get(filterNewerThanButton).click();
 };
 
-export const typeFilterNewerThanInputField = (filterValue) => {
-  return cy.get(filterNewerThanInputField).type(filterValue);
-};
-
 export const clickFilterOlderThanButton = () => {
   return cy.get(filterOlderThanButton).click();
-};
-
-export const typeFilterOlderThanInputField = (filterValue) => {
-  return cy.get(filterOlderThanInputField).type(filterValue);
 };
 
 export const clickRefreshButton = () => {
@@ -98,17 +100,60 @@ export const clickResetFiltersButton = () => {
   return cy.get(resetFiltersButton).click();
 };
 
-export const interceptActivityLogEndpoint = () => {
-  return cy
-    .intercept({
-      url: '/api/v1/activity_log*',
-    })
-    .as(activityLogEndpointAlias);
+export const clickNextPageButton = () => {
+  return cy.get(nextPageButton).click();
 };
 
-export const activityLogEndpointIsCalledOnlyOnce = () => {
-  waitForActivityLogRequest();
-  return cy.get(`@${activityLogEndpointAlias}.all`).should('have.length', 1);
+export const clickPreviousPageButton = () => {
+  return cy.get(previousPageButton).click();
+};
+
+export const clickFirstPageButton = () => {
+  return cy.get(firstPageButton).click();
+};
+
+export const clickLastPageButton = () => {
+  return cy.get(lastPageButton).click();
+};
+
+export const typeFilterNewerThanInputField = (filterValue) => {
+  return cy.get(filterNewerThanInputField).type(filterValue);
+};
+
+export const typeFilterOlderThanInputField = (filterValue) => {
+  return cy.get(filterOlderThanInputField).type(filterValue);
+};
+
+export const selectFilterTypeOption = (option) => {
+  return cy.get(`span:contains("${option}")`).click();
+};
+
+export const typeMetadataFilter = (searchValue) => {
+  return cy.get(metadataSearchInput).type(searchValue);
+};
+
+export const selectPagination = (amountOfItems) => {
+  cy.get(selectPaginationButton).click();
+  return cy.get(`span:contains("${amountOfItems}")`).first().click();
+};
+
+export const selectRefreshRate = (refreshRate) => {
+  cy.get(autoRefreshIntervalButton).click();
+  return cy.contains(refreshRate).click();
+};
+
+// UI Validations
+
+export const autoRefreshIntervalButtonHasTheExpectedValue = (refreshRate) => {
+  return cy.get(autoRefreshIntervalButton).should('have.text', refreshRate);
+};
+
+export const autoRefreshButtonIsEnabled = () => {
+  return cy.get(autoRefreshIntervalButton).should('be.enabled');
+};
+
+export const autoRefreshIntervalButtonIsDisabled = () => {
+  return cy.get(autoRefreshIntervalButton).should('be.disabled');
 };
 
 export const filteredActionsAreTheExpectedOnes = (filteredActions) => {
@@ -117,18 +162,6 @@ export const filteredActionsAreTheExpectedOnes = (filteredActions) => {
     .eq(0)
     .find('span span')
     .should('have.text', filteredActions);
-};
-
-export const filterOlderThanHasTheExpectedValue = (filterValue) => {
-  let expectedValue;
-  isUriComponentDate(filterValue)
-    ? (expectedValue = formatEncodedDate(filterValue))
-    : (expectedValue = filterValue);
-  return cy
-    .get(filteringElements)
-    .eq(3)
-    .find('span span')
-    .should('have.text', expectedValue);
 };
 
 export const filterNewerThanHasTheExpectedValue = (filterValue) => {
@@ -140,6 +173,18 @@ export const filterNewerThanHasTheExpectedValue = (filterValue) => {
   return cy
     .get(filteringElements)
     .eq(2)
+    .find('span span')
+    .should('have.text', expectedValue);
+};
+
+export const filterOlderThanHasTheExpectedValue = (filterValue) => {
+  let expectedValue;
+  isUriComponentDate(filterValue)
+    ? (expectedValue = formatEncodedDate(filterValue))
+    : (expectedValue = filterValue);
+  return cy
+    .get(filteringElements)
+    .eq(3)
     .find('span span')
     .should('have.text', expectedValue);
 };
@@ -171,12 +216,15 @@ export const metadataSearchHasTheExpectedValue = (searchValue) => {
   return cy.get(metadataSearchInput).should('have.value', searchValue);
 };
 
-export const typeMetadataFilter = (searchValue) => {
-  return cy.get(metadataSearchInput).type(searchValue);
+export const selectPaginationButtonHasTheExpectedValue = (pagination) => {
+  return cy.get(selectPaginationButton).should('have.text', pagination);
 };
 
-export const waitForActivityLogRequest = () => {
-  return basePage.waitForRequest(activityLogEndpointAlias);
+// Response Validations
+
+export const activityLogEndpointIsCalledOnlyOnce = () => {
+  waitForActivityLogRequest();
+  return cy.get(`@${activityLogEndpointAlias}.all`).should('have.length', 1);
 };
 
 export const activityLogRequestHasExpectedStatusCode = (statusCode) => {
@@ -194,55 +242,11 @@ export const paginationPropertiesAreTheExpected = (response) => {
   expect(response.body.pagination).to.have.property('first', 20);
 };
 
-export const clickNextPageButton = () => {
-  return cy.get(nextPageButton).click();
-};
-
-export const clickPreviousPageButton = () => {
-  return cy.get(previousPageButton).click();
-};
-
-export const clickFirstPageButton = () => {
-  return cy.get(firstPageButton).click();
-};
-
-export const clickLastPageButton = () => {
-  return cy.get(lastPageButton).click();
-};
-
-export const formatEncodedDate = (encodedDate) => {
-  const decodedDate = decodeURIComponent(encodedDate);
-  const date = new Date(decodedDate);
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(date.getUTCDate()).padStart(2, '0');
-  const year = date.getUTCFullYear();
-  let hours = date.getUTCHours();
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12 || 12;
-  return `${month}/${day}/${year} ${hours}:${minutes}:${seconds} ${ampm}`;
-};
-
-const isUriComponentDate = (encodedDate) => {
-  const regex = /^\d{4}-\d{2}-\d{2}T\d{2}%3A\d{2}%3A\d{2}\.\d{3}Z$/;
-  return regex.test(encodedDate);
-};
-
 export const validateResponsePagination = (amountOfItems) => {
   return basePage
     .waitForRequest(activityLogEndpointAlias)
     .its('response.body.pagination.first')
     .should('eq', amountOfItems);
-};
-
-export const selectPagination = (amountOfItems) => {
-  cy.get(selectPaginationButton).click();
-  return cy.get(`span:contains("${amountOfItems}")`).first().click();
-};
-
-export const selectPaginationButtonHasTheExpectedValue = (pagination) => {
-  return cy.get(selectPaginationButton).should('have.text', pagination);
 };
 
 export const responseMatchesFirstPageContent = (expectedResponse) => {
@@ -268,9 +272,29 @@ export const expectedRefreshRatesAreAvailable = () => {
   return clickAutoRefreshRateButton();
 };
 
-export const selectRefreshRate = (refreshRate) => {
-  cy.get(autoRefreshIntervalButton).click();
-  return cy.contains(refreshRate).click();
+export const expectedAggregateAmountOfRequests = (amount) => {
+  return cy.get('@activityLogRequest').its('callCount').should('equal', amount);
+};
+
+//Helpers
+
+export const formatEncodedDate = (encodedDate) => {
+  const decodedDate = decodeURIComponent(encodedDate);
+  const date = new Date(decodedDate);
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const year = date.getUTCFullYear();
+  let hours = date.getUTCHours();
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12 || 12;
+  return `${month}/${day}/${year} ${hours}:${minutes}:${seconds} ${ampm}`;
+};
+
+const isUriComponentDate = (encodedDate) => {
+  const regex = /^\d{4}-\d{2}-\d{2}T\d{2}%3A\d{2}%3A\d{2}\.\d{3}Z$/;
+  return regex.test(encodedDate);
 };
 
 export const buildChangingRefreshRateScenarios = () => {
@@ -289,18 +313,6 @@ export const buildChangingRefreshRateScenarios = () => {
       expectedRefreshRate,
     };
   });
-};
-
-export const spyActivityLogRequest = () => {
-  cy.clock();
-  return cy.intercept(
-    '/api/v1/activity_log*',
-    cy.spy().as(activityLogEndpointAlias)
-  );
-};
-
-export const expectedAggregateAmountOfRequests = (amount) => {
-  return cy.get('@activityLogRequest').its('callCount').should('equal', amount);
 };
 
 export const advanceTimeBy = (timeInSeconds) => {
