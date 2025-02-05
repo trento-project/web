9dbad5f5ca3c1806264a08548d06a21593d006a3
arbulu89 <xarbulu@suse.com>
2025-02-05 15:05:20 +0100

    Add some docs about the parent fields

--- lib/trento/clusters/value_objects/cluster_resource_parent.ex
+++ lib/trento/clusters/value_objects/cluster_resource_parent.ex
@@ -1,6 +1,12 @@
 defmodule Trento.Clusters.ValueObjects.ClusterResourceParent do
   @moduledoc """
-  Represents the parent of a cluste resource
+  Represents the parent of a cluster resource
+
+  `managed` represents the maintenance state of the resource.
+  `multi_state` represents the type of the group:
+    - `true` means a Master/Slave group
+    - `false` means a Clone group
+    - `nil` means a standard Group
   """
 
   @required_fields [:id]
