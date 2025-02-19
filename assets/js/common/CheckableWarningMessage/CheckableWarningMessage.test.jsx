4985baaa4a0624d89b144c83d198fe67f1c03c63
EMaksy <emaksymenko@suse.com>
2025-02-19 14:48:41 +0100

    Add test

---
+++ assets/js/common/CheckableWarningMessage/CheckableWarningMessage.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CheckableWarningMessage from './CheckableWarningMessage';
+
+const warningMessage = 'A default warning test';
+describe('CheckableWarningMessage', () => {
+  it('should render warning text and the checkbox if hideCheckbox is false', () => {
+    const mockSetChecked = jest.fn();
+    render(
+      <CheckableWarningMessage
+        warningText={warningMessage}
+        checked={false}
+        setChecked={mockSetChecked}
+      />
+    );
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toBeInTheDocument();
+    expect(screen.getByText(warningMessage)).toBeInTheDocument();
+    const icon = screen.getByTestId('eos-svg-component');
+    expect(icon).toBeInTheDocument();
+    fireEvent.click(checkbox);
+    expect(mockSetChecked).toHaveBeenCalled();
+  });
+
+  it('should not render checkbox if hideCheckbox is true', () => {
+    render(
+      <CheckableWarningMessage
+        hideCheckbox
+        warningText={warningMessage}
+        checked={false}
+      />
+    );
+    expect(screen.queryByRole('checkbox')).not.toBeInTheDocument();
+  });
+});
