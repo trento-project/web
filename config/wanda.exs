2495713d4efac1fba7c65a23e59bf0b27ead8978
arbulu89 <xarbulu@suse.com>
2025-02-20 09:29:16 +0100

    Reorganize amqp messaging configs

--- config/wanda.exs
+++ config/wanda.exs
@@ -2,8 +2,20 @@ import Config
 
 import_config "dev.exs"
 
-config :trento, Trento.Infrastructure.Messaging.Adapter.AMQP.Publisher,
-  connection: "amqp://wanda:wanda@localhost:5674"
-
-config :trento, Trento.Infrastructure.Checks.AMQP.Consumer,
-  connection: "amqp://wanda:wanda@localhost:5674"
+config :trento, Trento.Infrastructure.Messaging.Adapter.AMQP,
+  checks: [
+    consumer: [
+      connection: "amqp://wanda:wanda@localhost:5674"
+    ],
+    publisher: [
+      connection: "amqp://wanda:wanda@localhost:5674"
+    ]
+  ],
+  operations: [
+    consumer: [
+      connection: "amqp://wanda:wanda@localhost:5674"
+    ],
+    publisher: [
+      connection: "amqp://wanda:wanda@localhost:5674"
+    ]
+  ]
