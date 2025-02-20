b2b9636c005f73bcb44e8979063f02fe5879b3ba
arbulu89 <xarbulu@suse.com>
2025-02-20 09:13:34 +0100

    Add ActionsButton component

---
+++ assets/js/common/ActionsButton/ActionsButton.jsx
@@ -0,0 +1,68 @@
+import React, { useRef, Fragment } from 'react';
+import classNames from 'classnames';
+import { some } from 'lodash';
+import {
+  Menu,
+  MenuButton,
+  MenuItems,
+  MenuItem,
+  MenuSeparator,
+} from '@headlessui/react';
+import { EOS_MORE_VERT, EOS_LOADING_ANIMATED } from 'eos-icons-react';
+
+import Button from '@common/Button';
+
+function ActionsButton({ actions }) {
+  const ref = useRef(null);
+  const someRunning = some(actions, { running: true });
+
+  return (
+    <Menu>
+      <MenuButton as={Fragment}>
+        <div className="flex" ref={ref}>
+          <Button
+            type="primary-white"
+            className="inline-block mx-0.5 border-green-500 border"
+            size="small"
+          >
+            <EOS_MORE_VERT className="inline-block fill-jungle-green-500" />{' '}
+            Actions
+          </Button>
+        </div>
+      </MenuButton>
+      <MenuItems
+        anchor={{ to: 'bottom start', gap: '5px' }}
+        as="div"
+        className="w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 px-3 py-3 focus:outline-none"
+      >
+        {actions.map(({ value, running, disabled, onClick }) => (
+          <div key={value}>
+            <MenuItem>
+              <button
+                type="button"
+                onClick={onClick}
+                className={classNames(
+                  'w-full rounded-md text-left block px-4 py-2 text-sm',
+                  {
+                    'text-gray-700 hover:bg-gray-100':
+                      !disabled && !someRunning,
+                  },
+                  { 'text-gray-400': disabled || someRunning }
+                )}
+                disabled={disabled || someRunning}
+              >
+                {running && (
+                  <EOS_LOADING_ANIMATED className="inline-block fill-jungle-green-500 pr-1" />
+                )}
+                {value}
+              </button>
+            </MenuItem>
+            <MenuSeparator className="my-1" />
+          </div>
+        ))}
+      </MenuItems>
+    </Menu>
+  );
+}
+
+export default ActionsButton;
---
+++ assets/js/common/ActionsButton/ActionsButton.test.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { act, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { map } from 'lodash';
+import ActionsButton from './ActionsButton';
+
+const mockOnClick = jest.fn();
+const testActions = [
+  {
+    value: 'Operation 1',
+    running: false,
+    disabled: false,
+    onClick: mockOnClick,
+  },
+  {
+    value: 'Operation 2',
+    running: false,
+    disabled: false,
+    onClick: mockOnClick,
+  },
+];
+
+describe('ActionsButton', () => {
+  it('should show correct actions', async () => {
+    const user = userEvent.setup();
+
+    await act(async () => {
+      render(<ActionsButton actions={testActions} />);
+    });
+
+    expect(screen.getByText('Actions')).toBeInTheDocument();
+
+    await user.click(screen.getByText('Actions'));
+
+    expect(screen.getByText('Operation 1')).toBeInTheDocument();
+    expect(screen.getByText('Operation 2')).toBeInTheDocument();
+
+    await user.click(screen.getByText('Operation 1'));
+
+    expect(mockOnClick).toBeCalled();
+  });
+
+  it('should show disabled action', async () => {
+    const user = userEvent.setup();
+    const disabledActions = map(testActions, (action, index) =>
+      index === 0 ? { ...action, disabled: true } : action
+    );
+
+    await act(async () => {
+      render(<ActionsButton actions={disabledActions} />);
+    });
+
+    await user.click(screen.getByText('Actions'));
+
+    expect(screen.getByText('Operation 1')).toBeDisabled();
+    expect(screen.getByText('Operation 2')).toBeEnabled();
+  });
+
+  it('should show running action with disabled entries', async () => {
+    const user = userEvent.setup();
+    const runningActions = map(testActions, (action, index) =>
+      index === 0 ? { ...action, running: true } : action
+    );
+
+    await act(async () => {
+      render(<ActionsButton actions={runningActions} />);
+    });
+
+    await user.click(screen.getByText('Actions'));
+
+    expect(screen.getByText('Operation 1')).toBeDisabled();
+    expect(screen.getByText('Operation 2')).toBeDisabled();
+
+    const svgEl = screen
+      .getByRole('menuitem', { name: 'Operation 1' })
+      .querySelector("[data-testid='eos-svg-component']");
+    expect(svgEl).toBeInTheDocument();
+  });
+});
