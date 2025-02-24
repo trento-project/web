3c51dcb2cfb001e50b8cea577c975d52bb5ec257
arbulu89 <xarbulu@suse.com>
2025-02-24 09:09:55 +0100

    Use common CheckableWarningMessage

--- assets/js/common/OperationModals/OperationModal.jsx
+++ assets/js/common/OperationModals/OperationModal.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { EOS_WARNING_OUTLINED } from 'eos-icons-react';
 import { noop } from 'lodash';
 
 import Modal from '@common/Modal';
 import Button from '@common/Button';
-import Input from '@common/Input';
+import CheckableWarningMessage from '@common/CheckableWarningMessage';
 
 function OperationModal({
   title,
@@ -28,23 +27,12 @@ function OperationModal({
       <p className="text-gray-500 text-sm font-normal tracking-wide pb-3">
         {description}
       </p>
-      <div className="flex items-center text-sm font-normal border border-yellow-400 bg-yellow-50 p-4 rounded-md text-yellow-600 mb-4">
-        <Input
-          type="checkbox"
-          checked={checked}
-          className="border-yellow-400"
-          onChange={onChecked}
-        />
-
-        <EOS_WARNING_OUTLINED
-          size="l"
-          className="centered fill-yellow-500 ml-4 mr-4"
-        />
-        <span className="font-semibold">
-          Trento & SUSE cannot be held liable for damages if system is unable to
-          function due applying {operationText}.
-        </span>
-      </div>
+      <CheckableWarningMessage
+        warningText={`Trento & SUSE cannot be held liable for damages if system is unable to
+          function due applying ${operationText}.`}
+        checked={checked}
+        onChecked={onChecked}
+      />
       {children}
       <div className="flex justify-start gap-2 mt-4">
         <Button
