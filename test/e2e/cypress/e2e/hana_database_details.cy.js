480981401a9868d50ee300e0ca2bf69a2c301b65
Vicente <58304150+vicenteqa@users.noreply.github.com>
2025-02-25 14:48:24 +0100

    Apply PageObject pattern to Hana DB Details E2E tests (#3330)
    * finish first group of tests
    
    * finish database overview tests
    
    * fix user with cleanup ability creation
    
    * final touches and organize code
    
    * rename function to login with abilities
    
    * improve readability check active pill test
    
    * fix on active pill test
    
    * pr changes request
    
    * first set of tests refactored
    
    * finish hana cost opt tests finished
    
    * finish angi architecture tests
    
    * finish refactor, pending organize tests
    
    * fix create user abilities
    
    * fix after tests
    
    * re-organize visit methods and before and beforeEach
    
    * re organize po functions grouped by its utility
    
    * minor fixes
    
    * wip database details tests
    
    * finish refactor, pending organize po code
    
    * organize po code

--- test/e2e/cypress/e2e/hana_database_details.cy.js
+++ test/e2e/cypress/e2e/hana_database_details.cy.js
@@ -1,144 +1,100 @@
-import { healthMap } from '../fixtures/sap-system-details/selected_system';
+import * as hanaDbDetailsPage from '../pageObject/hana-database-details-po';
 
-import {
-  selectedDatabase,
-  attachedHosts,
-} from '../fixtures/hana-database-details/selected_database';
+import { attachedHosts } from '../fixtures/hana-database-details/selected_database';
 
 context('HANA database details', () => {
   before(() => {
-    cy.preloadTestData();
-    cy.visit(`/databases/${selectedDatabase.Id}`);
-    cy.url().should('include', `/databases/${selectedDatabase.Id}`);
+    hanaDbDetailsPage.preloadTestData();
+  });
+  beforeEach(() => {
+    hanaDbDetailsPage.visitDatabase();
   });
 
   describe('HANA database details page is available', () => {
-    it(`should display the "${selectedDatabase.Sid}" database details page`, () => {
-      cy.get('h1').should('contain', 'HANA Database Details');
-      cy.get('div')
-        .contains('Name')
-        .next()
-        .should('contain', selectedDatabase.Sid);
-      cy.get('div')
-        .contains('Type')
-        .next()
-        .should('contain', selectedDatabase.Type);
+    it('should display database id in page header', () => {
+      hanaDbDetailsPage.validatePageUrl();
+    });
+
+    it('should display the expected SID in database details page', () => {
+      hanaDbDetailsPage.pageTitleIsCorrectlyDisplayed('HANA Database Details');
+      hanaDbDetailsPage.databaseHasExpectedName();
+      hanaDbDetailsPage.databaseHasExpectedType();
     });
 
     it(`should display "Not found" page when HANA database doesn't exist`, () => {
-      cy.visit(`/databases/other`, { failOnStatusCode: false });
-      cy.url().should('include', `/databases/other`);
-      cy.get('div').should('contain', 'Not Found');
+      hanaDbDetailsPage.visitNonExistentDatabase();
+      hanaDbDetailsPage.validateNonExistentDatabaseUrl();
+      hanaDbDetailsPage.pageNotFoundLabelIsDisplayed();
     });
   });
 
   describe('The database layout shows all the running instances', () => {
-    before(() => {
-      cy.visit(`/databases/${selectedDatabase.Id}`);
-      cy.url().should('include', `/databases/${selectedDatabase.Id}`);
+    beforeEach(() => {
+      hanaDbDetailsPage.visitDatabase();
     });
 
     after(() => {
-      // Restore instance health
-      cy.loadScenario('hana-database-detail-GREEN');
+      hanaDbDetailsPage.restoreDatabaseInstanceHealth();
     });
 
-    selectedDatabase.Hosts.forEach((instance, index) => {
-      it(`should show hostname "${instance.Hostname}" with the correct values`, () => {
-        cy.get('table.table-fixed')
-          .eq(0)
-          .find('tr')
-          .eq(index + 1)
-          .find('td')
-          .as('tableCell');
-        cy.get('@tableCell').eq(0).should('contain', instance.Hostname);
-        cy.get('@tableCell').eq(1).should('contain', instance.Instance);
-        instance.Features.split('|').forEach((feature) => {
-          cy.get('@tableCell').eq(2).should('contain', feature);
-        });
-        cy.get('@tableCell').eq(3).should('contain', instance.HttpPort);
-        cy.get('@tableCell').eq(4).should('contain', instance.HttpsPort);
-        cy.get('@tableCell').eq(5).should('contain', instance.StartPriority);
-        cy.get('@tableCell').eq(6).should('contain', instance.Status);
-        cy.get('@tableCell')
-          .eq(6)
-          .find('svg')
-          .should('have.class', healthMap[instance.Status]);
-      });
+    it(`should show each hostname with the expected values`, () => {
+      hanaDbDetailsPage.eachHostNameHasExpectedValues();
     });
 
-    Object.entries(healthMap).forEach(([state, health]) => {
-      it(`should show ${state} badge in instance when SAPControl-${state.toUpperCase()} state is received`, () => {
-        cy.loadScenario(`hana-database-detail-${state.toUpperCase()}`);
-        // using row 1 as the changed instance is the 3rd in order based on instance_number
-        cy.get('table.table-fixed')
-          .eq(0)
-          .find('tr')
-          .eq(1)
-          .find('td')
-          .as('tableCell');
-        cy.get('@tableCell').eq(6).should('contain', `${state}`);
-        cy.get('@tableCell').eq(6).find('svg').should('have.class', health);
-      });
+    it('should show Green badge in instance when SAPControl-GREEN state is received', () => {
+      hanaDbDetailsPage.loadScenario('hana-database-detail-GREEN');
+      hanaDbDetailsPage.hostHasStatus('Green');
+      hanaDbDetailsPage.hostHasClass('Green');
     });
-    /* This test is commented because there is not any option to remove added database instances or
-    resetting the database afterwards, and it affects the rest of the test suite.
-    it(`should show a new instance when an event with a new SAP instance is received`, () => {
-      cy.loadScenario(`hana-database-detail-NEW`);
-      cy.get('table.table-fixed').eq(0).find('tr').should('have.length', 4);
-      cy.get('table.table-fixed')
-        .eq(0)
-        .find('tr')
-        .eq(-1)
-        .find('td')
-        .as('tableCell');
-      cy.get('@tableCell').eq(0).should('contain', 'vmhdbdev02');
-      cy.get('@tableCell').eq(1).should('contain', '11');
+
+    it('should show Red badge in instance when SAPControl-RED state is received', () => {
+      hanaDbDetailsPage.loadScenario('hana-database-detail-RED');
+      hanaDbDetailsPage.hostHasStatus('Red');
+      hanaDbDetailsPage.hostHasClass('Red');
     });
-    */
+
+    it('should show Yellow badge in instance when SAPControl-YELLOW state is received', () => {
+      hanaDbDetailsPage.loadScenario('hana-database-detail-YELLOW');
+      hanaDbDetailsPage.hostHasStatus('Yellow');
+      hanaDbDetailsPage.hostHasClass('Yellow');
+    });
+
+    it('should show Gray badge in instance when SAPControl-GRAY state is received', () => {
+      hanaDbDetailsPage.loadScenario('hana-database-detail-GRAY');
+      hanaDbDetailsPage.hostHasStatus('Gray');
+      hanaDbDetailsPage.hostHasClass('Gray');
+    });
+
+    /* This test is skipped because there is not any option to remove added database instances or
+    resetting the database afterwards, and it affects the rest of the test suite.*/
+    // eslint-disable-next-line mocha/no-skipped-tests
+    it.skip(`should show a new instance when an event with a new SAP instance is received`, () => {
+      hanaDbDetailsPage.tableHasExpectedAmountOfRows(2);
+      hanaDbDetailsPage.loadNewSapInstance();
+      hanaDbDetailsPage.tableHasExpectedAmountOfRows(3);
+      hanaDbDetailsPage.newInstanceIsDisplayed();
+    });
   });
 
   describe('The hosts table shows the attached hosts to this HANA database', () => {
-    attachedHosts.forEach((host, index) => {
-      it(`should show ${host.Name} with the data`, () => {
-        cy.get('table.table-fixed')
-          .eq(1)
-          .find('tr')
-          .eq(index + 1)
-          .find('td')
-          .as('tableCell');
-        cy.get('@tableCell').eq(0).should('contain', host.Name);
-        host.Addresses.forEach((address) => {
-          cy.get('@tableCell').eq(1).should('contain', address);
-        });
-        cy.get('@tableCell').eq(2).should('contain', host.Provider);
-        cy.get('@tableCell').eq(3).should('contain', host.Cluster);
-        cy.get('@tableCell').eq(4).should('contain', host.Version);
-      });
+    it('should display each attached host expected data', () => {
+      hanaDbDetailsPage.eachAttachedHostHasExpectedValues();
+    });
 
-      it(`should have a correct link to the ${host.Name} host`, () => {
-        cy.get('table.table-fixed')
-          .eq(1)
-          .find('tr')
-          .eq(index + 1)
-          .find('td')
-          .as('tableCell');
-        cy.get('@tableCell').eq(0).find('a').click();
-        cy.location('pathname').should('eq', `/hosts/${host.AgentId}`);
-        cy.go('back');
-      });
+    it(`should have a correct link to each host`, () => {
+      hanaDbDetailsPage.eachAttachedHostHasExpectedWorkingLink();
     });
   });
 
   describe('Deregistration', () => {
     it(`should not include host ${attachedHosts[0].Name} in the list of hosts`, () => {
-      cy.deregisterHost(attachedHosts[0].AgentId);
-      cy.contains(attachedHosts[0].Name).should('not.exist');
+      hanaDbDetailsPage.deregisterFirstAttachedHost();
+      hanaDbDetailsPage.deregisteredHostIsNotDisplayed();
     });
 
     it(`should include host ${attachedHosts[0].Name} again in the list of hosts after restoring it`, () => {
-      cy.loadScenario(`host-${attachedHosts[0].Name}-restore`);
-      cy.contains(attachedHosts[0].Name).should('exist');
+      hanaDbDetailsPage.restoreFirstAttachedHost();
+      hanaDbDetailsPage.deregisteredHostIsDisplayed();
     });
   });
 });
---
+++ test/e2e/cypress/pageObject/hana-database-details-po.js
@@ -0,0 +1,229 @@
+export * from './base-po.js';
+import * as basePage from './base-po.js';
+
+// Test Data
+import {
+  selectedDatabase,
+  attachedHosts,
+} from '../fixtures/hana-database-details/selected_database';
+
+import { healthMap } from '../fixtures/sap-system-details/selected_system';
+
+const url = '/databases';
+
+// Selectors
+const databaseNameLabel =
+  'div[class*="grid-flow-row"]:contains("Name") div span';
+const databaseTypeLabel =
+  'div[class*="grid-flow-row"]:contains("Type") div span';
+const pageNotFoundLabel = 'div:contains("Not Found")';
+const attachedHostsTableRows = 'div[class="mt-16"]:contains("Layout") tbody tr';
+const newRegisteredHost = `div[class="mt-8"]:contains("Hosts") td:contains("${attachedHosts[0].Name}")`;
+const layoutTableHostNameCell = (hostName) =>
+  `div[class="mt-16"]:contains("Layout") td:contains("${hostName}")`;
+const hostsTableHostNameCell = (hostName) =>
+  `div[class="mt-8"]:contains("Hosts") td:contains("${hostName}")`;
+
+//UI Interactions
+
+export const visitDatabase = () =>
+  basePage.visit(`${url}/${selectedDatabase.Id}`);
+
+export const visitNonExistentDatabase = () =>
+  cy.visit(`${url}/other`, { failOnStatusCode: false });
+
+// Validations
+export const validatePageUrl = () =>
+  basePage.validateUrl(`${url}/${selectedDatabase.Id}`);
+
+export const validateNonExistentDatabaseUrl = () =>
+  basePage.validateUrl(`${url}/other`);
+
+export const databaseHasExpectedName = () =>
+  cy.get(databaseNameLabel).should('have.text', selectedDatabase.Sid);
+
+export const databaseHasExpectedType = () =>
+  cy.get(databaseTypeLabel).should('have.text', selectedDatabase.Type);
+
+export const pageNotFoundLabelIsDisplayed = () =>
+  cy.get(pageNotFoundLabel).should('be.visible');
+
+const hostNameHasExpectedInstanceNumber = (hostName) => {
+  const instanceNumber = getHostAttribute(hostName, 'Instance');
+  const hostNameCellSelector = layoutTableHostNameCell(hostName);
+  cy.get(hostNameCellSelector).next().should('have.text', instanceNumber);
+};
+
+const hostNameHasExpectedFeatures = (hostName) => {
+  const features = getHostAttribute(hostName, 'Features');
+  const formattedFeatures = features.replace(/\|/g, '');
+  const hostNameCellSelector = layoutTableHostNameCell(hostName);
+  cy.get(hostNameCellSelector)
+    .nextAll()
+    .eq(1)
+    .should('have.text', formattedFeatures);
+};
+
+const hostHasExpectedHttpPort = (hostName) => {
+  const httpPort = getHostAttribute(hostName, 'HttpPort');
+  const hostNameCellSelector = layoutTableHostNameCell(hostName);
+  cy.get(hostNameCellSelector).nextAll().eq(2).should('have.text', httpPort);
+};
+
+const hostHasExpectedHttpsPort = (hostName) => {
+  const httpsPort = getHostAttribute(hostName, 'HttpsPort');
+  const hostNameCellSelector = layoutTableHostNameCell(hostName);
+  cy.get(hostNameCellSelector).nextAll().eq(3).should('have.text', httpsPort);
+};
+
+const hostHasExpectedStartPriority = (hostName) => {
+  const startPriority = getHostAttribute(hostName, 'StartPriority');
+  const hostNameCellSelector = layoutTableHostNameCell(hostName);
+  cy.get(hostNameCellSelector)
+    .nextAll()
+    .eq(4)
+    .should('have.text', startPriority);
+};
+
+const hostStatusHasExpectedClass = (hostName) => {
+  const status = getHostAttribute(hostName, 'Status');
+  validateHostClass(hostName, status);
+};
+
+const validateHostClass = (hostName, status) => {
+  const hostNameCellSelector = layoutTableHostNameCell(hostName);
+  cy.get(hostNameCellSelector)
+    .nextAll()
+    .eq(5)
+    .find('svg')
+    .should('have.class', healthMap[status]);
+};
+
+const validateHostStatus = (hostName, status) => {
+  const hostNameCellSelector = layoutTableHostNameCell(hostName);
+  cy.get(hostNameCellSelector)
+    .nextAll()
+    .eq(5)
+    .should('have.text', `SAPControl: ${status}`);
+};
+
+const hostHasExpectedStatus = (hostName) => {
+  const status = getHostAttribute(hostName, 'Status');
+  validateHostStatus(hostName, status);
+};
+
+export const hostHasStatus = (status) =>
+  validateHostStatus(selectedDatabase.Hosts[0].Hostname, status);
+
+export const hostHasClass = (status) =>
+  validateHostClass(selectedDatabase.Hosts[0].Hostname, status);
+
+export const eachHostNameHasExpectedValues = () => {
+  selectedDatabase.Hosts.forEach((host) => {
+    const hostName = host.Hostname;
+    hostNameHasExpectedInstanceNumber(hostName);
+    hostNameHasExpectedFeatures(hostName);
+    hostHasExpectedHttpPort(hostName);
+    hostHasExpectedHttpsPort(hostName);
+    hostHasExpectedStartPriority(hostName);
+    hostHasExpectedStatus(hostName);
+    hostStatusHasExpectedClass(hostName);
+  });
+};
+
+const getHostAttribute = (hostname, attribute) => {
+  const host = selectedDatabase.Hosts.find((h) => h.Hostname === hostname);
+  return host ? host[attribute] : undefined;
+};
+
+const getAttachedHostAttribute = (hostname, attribute) => {
+  const host = attachedHosts.find((h) => h.Name === hostname);
+  return host ? host[attribute] : undefined;
+};
+
+const hostHostHasExpectedAddresses = (hostName) => {
+  const hostNameCellSelector = hostsTableHostNameCell(hostName);
+  const expectedAddresses = getAttachedHostAttribute(
+    hostName,
+    'Addresses'
+  ).join('');
+  cy.get(hostNameCellSelector).next().should('have.text', expectedAddresses);
+};
+
+const hostHasExpectedProvider = (hostName) => {
+  const hostNameCellSelector = hostsTableHostNameCell(hostName);
+  const expectedProviderValue = getAttachedHostAttribute(hostName, 'Provider');
+  cy.get(hostNameCellSelector)
+    .nextAll()
+    .eq(1)
+    .should('have.text', expectedProviderValue);
+};
+
+const hostHasExpectedClusterValue = (hostName) => {
+  const hostNameCellSelector = hostsTableHostNameCell(hostName);
+  const expectedCluster = getAttachedHostAttribute(hostName, 'Cluster');
+  cy.get(hostNameCellSelector)
+    .nextAll()
+    .eq(2)
+    .should('contain', expectedCluster);
+};
+
+const hostHasExpectedVersion = (hostName) => {
+  const hostNameCellSelector = hostsTableHostNameCell(hostName);
+  const expectedVersion = getAttachedHostAttribute(hostName, 'Version');
+  cy.get(hostNameCellSelector)
+    .nextAll()
+    .eq(3)
+    .should('have.text', expectedVersion);
+};
+
+const hostHasExpectedWorkingLink = (host) => {
+  const hostNameCellSelector = hostsTableHostNameCell(host.Name);
+  const hostNameSelector = `${hostNameCellSelector} a`;
+  const expectedHref = `/hosts/${host.AgentId}`;
+  cy.get(hostNameSelector).should('have.attr', 'href', expectedHref);
+  cy.get(hostNameSelector).click();
+  basePage.validateUrl(expectedHref);
+  cy.go('back');
+};
+
+export const eachAttachedHostHasExpectedValues = () => {
+  attachedHosts.forEach((host) => {
+    hostHostHasExpectedAddresses(host.Name);
+    hostHasExpectedProvider(host.Name);
+    hostHasExpectedClusterValue(host.Name);
+    hostHasExpectedVersion(host.Name);
+  });
+};
+
+export const eachAttachedHostHasExpectedWorkingLink = () =>
+  attachedHosts.forEach((host) => hostHasExpectedWorkingLink(host));
+
+export const newInstanceIsDisplayed = () => {
+  const newInstanceSelector = `div[class="mt-16"]:contains("Layout") td:contains("${selectedDatabase.Hosts[0].Hostname}")`;
+  cy.get(newInstanceSelector).eq(1).should('be.visible');
+  cy.get(`${newInstanceSelector} + td`).eq(1).should('have.text', 11);
+};
+
+export const tableHasExpectedAmountOfRows = (expectedAmountOfRows) =>
+  cy.get(attachedHostsTableRows).should('have.length', expectedAmountOfRows);
+
+export const deregisteredHostIsNotDisplayed = () =>
+  cy.get(newRegisteredHost).should('not.exist');
+
+export const deregisteredHostIsDisplayed = () =>
+  cy.get(newRegisteredHost).should('be.visible');
+
+// API
+
+export const loadNewSapInstance = () =>
+  basePage.loadScenario(`hana-database-detail-NEW`);
+
+export const deregisterFirstAttachedHost = () =>
+  basePage.apiDeregisterHost(attachedHosts[0].AgentId);
+
+export const restoreFirstAttachedHost = () =>
+  basePage.loadScenario(`host-${attachedHosts[0].Name}-restore`);
+
+export const restoreDatabaseInstanceHealth = () =>
+  basePage.loadScenario('hana-database-detail-GREEN');
