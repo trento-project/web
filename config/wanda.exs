276a387819205a6fe48d8cac6f7ee1353fff9019
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2025-03-03 17:00:21 +0100

    Addressing review feedback

--- config/wanda.exs
+++ config/wanda.exs
@@ -2,20 +2,27 @@ import Config
 
 import_config "dev.exs"
 
+amqp_connection =
+  if System.get_env("USE_LOCAL_RABBIT_TLS") do
+    "amqps://wanda:wanda@localhost:5676?certfile=container_fixtures/rabbitmq/certs/client_web.trento.local_certificate.pem&keyfile=container_fixtures/rabbitmq/certs/client_web.trento.local_key.pem&verify=verify_peer&cacertfile=container_fixtures/rabbitmq/certs/ca_certificate.pem"
+  else
+    "amqp://wanda:wanda@localhost:5674"
+  end
+
 config :trento, Trento.Infrastructure.Messaging.Adapter.AMQP,
   checks: [
     consumer: [
-      connection: "amqp://wanda:wanda@localhost:5674"
+      connection: amqp_connection
     ],
     publisher: [
-      connection: "amqp://wanda:wanda@localhost:5674"
+      connection: amqp_connection
     ]
   ],
   operations: [
     consumer: [
-      connection: "amqp://wanda:wanda@localhost:5674"
+      connection: amqp_connection
     ],
     publisher: [
-      connection: "amqp://wanda:wanda@localhost:5674"
+      connection: amqp_connection
     ]
   ]
