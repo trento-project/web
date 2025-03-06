f750761c96d2a5178e0de9ce270bb318d12a266f
Nelson Kopliku <nelson.kopliku@suse.com>
2025-03-06 11:23:27 +0100

    Center severity icon in table cell (#3363)

--- assets/js/common/ActivityLogOverview/ActivityLogOverview.jsx
+++ assets/js/common/ActivityLogOverview/ActivityLogOverview.jsx
@@ -24,10 +24,14 @@ import {
 import ActivityLogDetailModal from '@common/ActivityLogDetailsModal';
 
 export const logLevelToIcon = {
-  [LEVEL_DEBUG]: <EOS_BUG_REPORT_OUTLINED className="w-auto" />,
-  [LEVEL_INFO]: <EOS_INFO_OUTLINED className="w-auto" />,
-  [LEVEL_WARNING]: <EOS_WARNING_OUTLINED className="fill-yellow-500 w-auto" />,
-  [LEVEL_CRITICAL]: <EOS_ERROR_OUTLINED className="fill-red-500 w-auto" />,
+  [LEVEL_DEBUG]: <EOS_BUG_REPORT_OUTLINED className="w-auto mx-auto" />,
+  [LEVEL_INFO]: <EOS_INFO_OUTLINED className="w-auto mx-auto" />,
+  [LEVEL_WARNING]: (
+    <EOS_WARNING_OUTLINED className="fill-yellow-500 w-auto mx-auto" />
+  ),
+  [LEVEL_CRITICAL]: (
+    <EOS_ERROR_OUTLINED className="fill-red-500 w-auto mx-auto" />
+  ),
 };
 
 export const toRenderedEntry = (entry) => ({
