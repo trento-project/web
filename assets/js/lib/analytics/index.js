879bc9949b1123f00f85ee13aaabb871c1b783ae
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2025-03-06 13:15:00 +0200

    Enables checking of Analytics optin from db

--- assets/js/lib/analytics/index.js
+++ assets/js/lib/analytics/index.js
@@ -1,17 +1,29 @@
 import posthog from 'posthog-js';
 
 import { getFromConfig } from '@lib/config/config';
+import { getSettings } from '@lib/api/analyticsSettings';
 
 const analyticsEnabled = getFromConfig('analyticsEnabled');
 const analyticsKey = getFromConfig('analyticsKey');
 const analyticsUrl = getFromConfig('analyticsUrl');
 const installationID = getFromConfig('installationID');
 
+// Check if Analytics is enabled
 if (analyticsEnabled) {
-  posthog.init(analyticsKey, {
-    api_host: analyticsUrl,
-    capture_pageview: false,
-  });
+  try {
+    //Fetch Analytics settings from the API
+    getSettings().then(({ data }) => {
+      if (data.analytics_optin) {
+        //Load the Analytics library
+        posthog.init(analyticsKey, {
+          api_host: analyticsUrl,
+          capture_pageview: false,
+        });
+      }
+    });
+  } catch (error) {
+    console.log('Error fetching analytics settings', error);
+  }
 }
 
 export const capture = (event, payload) => {
