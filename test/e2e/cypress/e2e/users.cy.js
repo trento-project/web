5dbdfae19879117938ec5d730374abe8af21731e
Vicente <vicente.ruiz@suse.com>
2025-03-08 12:29:02 +0100

    add 2 more waits

--- test/e2e/cypress/e2e/users.cy.js
+++ test/e2e/cypress/e2e/users.cy.js
@@ -275,6 +275,7 @@ describe('Users', () => {
 
       cy.wait('@totpEnrollment');
       usersPage.typeUserTotpCode().then((totpSecret) => {
+        cy.wait(1000);
         usersPage.clickVerifyTotpButton();
         cy.wait(1000);
         cy.wait('@profile');
@@ -293,6 +294,7 @@ describe('Users', () => {
         loginPage.invalidCredentialsErrorIsDisplayed();
         loginPage.waitForNewTotpCodeAndTypeIt(totpSecret);
         cy.intercept('api/v1/hosts').as('hosts');
+        cy.wait(1000);
         loginPage.clickSubmitLoginButton();
         cy.wait('@hosts');
         dashboardPage.dashboardPageIsDisplayed();
