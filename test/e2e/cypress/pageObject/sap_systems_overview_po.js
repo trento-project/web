829da99b7a3d3cb19a392a09d1feee2ac64b606d
Vicente Ruiz <vicente.ruiz@suse.com>
2025-03-19 17:04:22 +0100

    start sap systems overview refactor

---
+++ test/e2e/cypress/pageObject/sap_systems_overview_po.js
@@ -0,0 +1,45 @@
+export * from './base_po.js';
+import * as basePage from './base_po.js';
+
+// Test data
+const url = '/sap_systems';
+
+import {
+  availableSAPSystems,
+  availableJavaSystem,
+  isHanaInstace,
+  isHanaPrimary,
+  isHanaSecondary,
+  healthMap,
+} from '../fixtures/sap-systems-overview/available_sap_systems';
+// Selectors
+
+// UI Interactions
+export const visit = () => basePage.visit(url);
+
+// UI Validations
+
+export const validateUrl = () => basePage.validateUrl(url);
+
+export const expectedSidsAreDisplayed = () => {
+  availableSAPSystems.forEach(({ sid: sid }) => {
+    cy.get(`td:contains('${sid}')`).should('be.visible');
+  });
+};
+
+export const eachSystemHasItsExpectedWorkingLink = () => {
+  availableSAPSystems.forEach(({ sid: sid, id: id }) => {
+    cy.get(`td:contains("${sid}")`).click();
+    basePage.validateUrl(`${url}/${id}`);
+    cy.go('back');
+  });
+};
+
+export const eachSystemHasExpectedHealth = () => {
+  availableSAPSystems.forEach(({ health: health }, index) => {
+    const healthClass = healthMap[health];
+    const healthCellSelector = `tbody tr:eq(${index}) td:eq(0) svg`;
+    cy.get(healthCellSelector).should('have.class', healthClass);
+  });
+};
+// API
