e4a7c53c5ca27e250f60e8705b4e23c600d42629
Vicente Ruiz <vicente.ruiz@suse.com>
2025-04-01 09:30:26 +0200

    fix to get input value

--- test/e2e/cypress/pageObject/sso_integration_po.js
+++ test/e2e/cypress/pageObject/sso_integration_po.js
@@ -22,10 +22,10 @@ const adminUser = {
 
 // Selectors
 
-const fullNameInputField = 'label:contains("Full Name") + div';
-const emailAddressInputField = 'label:contains("Email Address") + div';
-const usernameInputField = 'label:contains("Username") + div';
-const permissionsInputField = 'label:contains("Permissions") + div';
+const fullNameInputField = 'label:contains("Full Name") + div input';
+const emailAddressInputField = 'label:contains("Email Address") + div input';
+const usernameInputField = 'label:contains("Username") + div input';
+const permissionsInputField = 'label:contains("Permissions") + div input';
 const usernameMenu = `span:contains("${plainUser.username}")`;
 const usersListPlainUser = `a:contains("${plainUser.username}")`;
 const usersListAdminUser = `a:contains("${adminUser.username}")`;
