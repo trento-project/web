0d87d3f0642a4a8d8419e41154825cf212c68eb3
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2025-04-10 15:15:09 +0200

    Adds functionality to analytics index file

--- assets/js/lib/analytics/index.js
+++ assets/js/lib/analytics/index.js
@@ -1,4 +1,5 @@
 import posthog from 'posthog-js';
+import { v5 as uuidv5 } from 'uuid';
 
 import { getFromConfig } from '@lib/config/config';
 
@@ -10,13 +11,43 @@ const installationID = getFromConfig('installationID');
 if (analyticsEnabledConfig) {
   posthog.init(analyticsKey, {
     api_host: analyticsUrl,
+    opt_out_capturing_by_default: true,
     capture_pageview: false,
   });
 }
 
-export const capture = (event, payload) => {
+export const getAnalyticsEnabledConfig = () => analyticsEnabledConfig;
+
+export const identify = (analyticsEnabled, userID, username) => {
+  if (!analyticsEnabled) {
+    return;
+  }
+
+  const distinctUserID = uuidv5(userID.toString(), installationID);
+  posthog.identify(distinctUserID, {
+    username,
+    installationID,
+  });
+};
+
+export const optinCapturing = (analyticsEnabled) => {
+  if (analyticsEnabled && posthog.has_opted_out_capturing()) {
+    posthog.opt_in_capturing();
+  } else {
+    posthog.opt_out_capturing();
+  }
+};
+
+export const capture = (analyticsEnabled, event, payload) => {
   if (!analyticsEnabled) {
     return;
   }
   posthog.capture(event, { ...payload, installationID });
+};
+
+export const reset = () => {
+  if (!analyticsEnabledConfig) {
+    return;
+  }
+  posthog.reset();
 };
