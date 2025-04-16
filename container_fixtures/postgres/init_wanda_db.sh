d4806d684528eda0a1248d1a92ec226eac5e4f98
EMaksy <emaksymenko@suse.com>
2025-04-16 13:24:35 +0200

    Use docker profiles

--- container_fixtures/postgres/init_wanda_db.sh
+++ container_fixtures/postgres/init_wanda_db.sh
@@ -3,9 +3,4 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE DATABASE wanda_db;
-  CREATE ROLE wanda_user WITH LOGIN PASSWORD 'wanda_pass';
-  GRANT ALL PRIVILEGES ON DATABASE wanda_db TO wanda_user;
-  
-  \c wanda_db
-  GRANT USAGE, CREATE ON SCHEMA public TO wanda_user;
 EOSQL
