2e04a17c763d474d4531728c9acf91f2eaf29dce
Kristiyan Kanchev <kristiyan.kanchev@suse.com>
2025-04-24 11:39:04 +0300

    Separated abliities migration from the other one

--- priv/repo/migrations/20250304172641_add_alerting_settings.exs
+++ priv/repo/migrations/20250304172641_add_alerting_settings.exs
@@ -1,12 +1,7 @@
 defmodule Trento.Repo.Migrations.AddAlertingSettings do
   use Ecto.Migration
 
-  def up do
-    execute """
-    INSERT INTO abilities(name, resource, label, inserted_at, updated_at)
-    VALUES ('all', 'alerting_settings', 'Permits all operations on Alerting Settings', NOW(), NOW())
-    """
-
+  def change do
     alter table(:settings) do
       add :alerting_enabled, :boolean
       add :alerting_sender_email, :string
@@ -16,22 +11,5 @@ defmodule Trento.Repo.Migrations.AddAlertingSettings do
       add :alerting_smtp_username, :string
       add :alerting_smtp_password, :binary
     end
-  end
-
-  def down do
-    alter table(:settings) do
-      remove :alerting_enabled
-      remove :alerting_sender_email
-      remove :alerting_recipient_email
-      remove :alerting_smtp_server
-      remove :alerting_smtp_port
-      remove :alerting_smtp_username
-      remove :alerting_smtp_password
-    end
-
-    execute """
-    DELETE FROM abilities
-    WHERE name = 'all' AND resource = 'alerting_settings'
-    """
   end
 end
