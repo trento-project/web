debf754da188f91a737d13839186a02c53327639
arbulu89 <xarbulu@suse.com>
2025-05-05 09:15:10 +0200

    Expose cluster resource parent in api

--- lib/trento_web/controllers/v2/cluster_json.ex
+++ lib/trento_web/controllers/v2/cluster_json.ex
@@ -9,7 +9,6 @@ defmodule TrentoWeb.V2.ClusterJSON do
     |> Map.delete(:deregistered_at)
     |> Map.delete(:hosts)
     |> Map.delete(:__meta__)
-    |> adapt_details()
     |> adapt_sids()
   end
 
@@ -27,58 +26,6 @@ defmodule TrentoWeb.V2.ClusterJSON do
 
   def cluster_health_changed(%{cluster: %{id: id, name: name, health: health}}),
     do: %{cluster_id: id, name: name, health: health}
-
-  defp adapt_details(
-         %{
-           details: %{nodes: nodes, stopped_resources: stopped_resources} = details
-         } =
-           cluster
-       ) do
-    adapted_nodes = Enum.map(nodes, &adapt_node/1)
-    adapted_stopped_resources = adapt_resources(stopped_resources)
-
-    adapted_details =
-      details
-      |> Map.put(:nodes, adapted_nodes)
-      |> Map.put(:stopped_resources, adapted_stopped_resources)
-
-    %{cluster | details: adapted_details}
-  end
-
-  defp adapt_details(
-         %{
-           details: %{sap_systems: nodes, stopped_resources: stopped_resources} = details
-         } =
-           cluster
-       ) do
-    adapted_sap_systems = Enum.map(nodes, &adapt_sap_system/1)
-    adapted_stopped_resources = adapt_resources(stopped_resources)
-
-    adapted_details =
-      details
-      |> Map.put(:sap_systems, adapted_sap_systems)
-      |> Map.put(:stopped_resources, adapted_stopped_resources)
-
-    %{cluster | details: adapted_details}
-  end
-
-  defp adapt_details(cluster), do: cluster
-
-  defp adapt_sap_system(%{nodes: nodes} = sap_system) do
-    adapted_nodes = Enum.map(nodes, &adapt_node/1)
-
-    Map.put(sap_system, :nodes, adapted_nodes)
-  end
-
-  defp adapt_node(%{resources: resources} = node) do
-    adapted_resources = adapt_resources(resources)
-
-    Map.put(node, :resources, adapted_resources)
-  end
-
-  defp adapt_resources(resources) do
-    Enum.map(resources, &Map.drop(&1, [:parent]))
-  end
 
   defp adapt_sids(%{sap_instances: sap_instances} = cluster) do
     adapted_sap_instances =
