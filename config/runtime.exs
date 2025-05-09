e489933b8c67f5f5b0e69466d89f2e30188301fd
Eugen Maksymenko <emaksymenko@suse.com>
2025-05-09 12:33:19 +0200

    Bugfix for web as it fails to start when no IPV6 is available (#3490)
    * Add check if ipv6 missing
    
    * Add fix for missing ipv6

--- config/runtime.exs
+++ config/runtime.exs
@@ -69,13 +69,22 @@ if config_env() in [:prod, :demo] do
       For example: yourdomain.example.com
       """
 
+  # Returns an IP address to bind the HTTP server, based on IPv6 support.
+  # This prevents startup failures on systems without IPv6 where the app would crash
+  # Falls back to IPv4 `{0, 0, 0, 0}` if IPv6 is not available.
+  ip =
+    case :inet.getaddr(~c"localhost", :inet6) do
+      {:ok, _addr} -> {0, 0, 0, 0, 0, 0, 0, 0}
+      {:error, _} -> {0, 0, 0, 0}
+    end
+
   config :trento, TrentoWeb.Endpoint,
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: ip,
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     check_origin: true,
