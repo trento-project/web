7872d049afd4888a79ad52089b1df2675825717c
Nelson Kopliku <nelson.kopliku@suse.com>
2025-05-12 11:14:11 +0200

    Improve operations mapping (#3499)
    * Add a HostOperations enum
    
    * Remove unnecessary newlines
    
    * Be stricter with operations in host policy and processor callback

--- lib/trento/infrastructure/operations/operations.ex
+++ lib/trento/infrastructure/operations/operations.ex
@@ -46,8 +46,10 @@ defmodule Trento.Infrastructure.Operations do
   end
 
   def map_operation_type("saptuneapplysolution@v1"), do: :saptune_solution_apply
-
   def map_operation_type("saptunechangesolution@v1"), do: :saptune_solution_change
-
   def map_operation_type(_), do: :unknown
+
+  def map_operation(:saptune_solution_apply), do: "saptuneapplysolution@v1"
+  def map_operation(:saptune_solution_change), do: "saptunechangesolution@v1"
+  def map_operation(_), do: :unknown
 end
