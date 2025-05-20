0647a46cd2ea1976b96737db8025dee110ff14ba
Kristiyan Kanchev <kristiyan.kanchev@suse.com>
2025-05-20 12:04:23 +0300

    Re-factored the layout of AlertingSettingsConfig

--- assets/js/common/AlertingSettingsConfig/AlertingSettingsConfig.jsx
+++ assets/js/common/AlertingSettingsConfig/AlertingSettingsConfig.jsx
@@ -23,6 +23,47 @@ export default function AlertingSettingsConfig({
   userAbilities = [],
   onEditClick = noop,
 }) {
+  const alertingFields = [
+    {
+      label: 'SMTP Server',
+      value: smtpServer,
+      ariaLabel: 'smtp-server',
+      addClasses: 'truncate pr-12',
+    },
+    {
+      label: 'SMTP Port',
+      value: smtpPort,
+      ariaLabel: 'smtp-port',
+    },
+    {
+      label: 'SMTP User',
+      value: smtpUsername,
+      ariaLabel: 'smtp-username',
+      addClasses: 'truncate pr-12',
+    },
+    {
+      label: 'Password',
+      value: '•••••',
+      ariaLabel: 'smtp-password',
+    },
+    {
+      label: 'Alerting Sender',
+      value: senderEmail,
+      ariaLabel: 'alerting-sender',
+      addClasses: 'truncate pr-12',
+    },
+    {
+      label: 'Alerting Recipient',
+      value: recipientEmail,
+      ariaLabel: 'alerting-recipient',
+    },
+    {
+      label: 'Send Email Alerts',
+      value: alertingEnabled ? 'Enabled' : 'Disabled',
+      ariaLabel: 'alerting-enabled',
+    },
+  ];
+
   return (
     <div className="container max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 bg-white dark:bg-gray-800 rounded-lg">
       <div>
@@ -56,60 +97,31 @@ export default function AlertingSettingsConfig({
         Settings used to send Trento system e-mails.
       </p>
 
-      <div className="grid grid-cols-6 mt-5 items-center">
-        <div className="font-bold mb-3">SMTP Server</div>
-        <div
-          aria-label="smtp-server"
-          className="col-span-2 text-gray-500 mb-3 truncate pr-12"
-        >
-          {smtpServer}
-        </div>
-
-        <div className="font-bold mb-3">SMTP Port</div>
-        <div aria-label="smtp-port" className="col-span-2 text-gray-500 mb-3">
-          {smtpPort}
-        </div>
-
-        <div className="font-bold mb-3">SMTP User</div>
-        <div
-          aria-label="smtp-username"
-          className="col-span-2 text-gray-500 mb-3 pr-12"
-        >
-          {smtpUsername}
-        </div>
-
-        <div className="font-bold mb-3">Password</div>
-        <div
-          aria-label="smtp-password"
-          className="col-span-2 text-gray-500 mb-3"
-        >
-          •••••
-        </div>
-
-        <div className="font-bold mb-3">Alerting Sender</div>
-        <div
-          aria-label="alerting-sender"
-          className="col-span-2 text-gray-500 mb-3 truncate pr-12"
-        >
-          {senderEmail}
-        </div>
-
-        <div className="font-bold mb-3">Alerting Recipient</div>
-        <div
-          aria-label="alerting-recipient"
-          className="col-span-2 text-gray-500 mb-3"
-        >
-          {recipientEmail}
-        </div>
-
-        <div className="font-bold mb-3">Send Email Alerts</div>
-        <div
-          aria-label="alerting-enabled"
-          className="col-span-2 text-gray-500 mb-3 truncate pr-12"
-        >
-          {alertingEnabled ? 'Enabled' : 'Disabled'}
-        </div>
+      <div className="grid grid-cols-6 gap-y-3 mt-5 items-center">
+        {alertingFields.map(({ label, value, ariaLabel, addClasses }) => (
+          <AlertingSettingsRow
+            key={ariaLabel}
+            label={label}
+            value={value}
+            ariaLabel={ariaLabel}
+            addClasses={addClasses}
+          />
+        ))}
       </div>
     </div>
+  );
+}
+
+function AlertingSettingsRow({ label, value, ariaLabel, addClasses = '' }) {
+  return (
+    <>
+      <div className="font-bold">{label}</div>
+      <div
+        aria-label={ariaLabel}
+        className={`col-span-2 text-gray-500 ${addClasses}`}
+      >
+        {value}
+      </div>
+    </>
   );
 }
