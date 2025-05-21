37341b8dd9997706edced9a68c2d2ea52af47245
Kristiyan Kanchev <kristiyan.kanchev@suse.com>
2025-05-21 14:46:11 +0300

    Alerting Config -- better testing for user abilities

--- assets/js/common/AlertingSettingsConfig/AlertingSettingsConfig.test.jsx
+++ assets/js/common/AlertingSettingsConfig/AlertingSettingsConfig.test.jsx
@@ -58,40 +58,60 @@ describe('AlertingSettingsConfig', () => {
     );
   });
 
-  it('forbids editing when called with default arguments', async () => {
-    const onEditClick = jest.fn();
-    const user = userEvent.setup();
+  it.each`
+    case                        | abilities
+    ${'not set'}                | ${undefined}
+    ${'empty list'}             | ${[]}
+    ${'non-relevant abilities'} | ${[{ name: 'all', resource: 'some_resource' }]}
+  `(
+    'forbids editing when user has not sufficient abilities ($case)',
+    async ({ abilities }) => {
+      const onEditClick = jest.fn();
+      const user = userEvent.setup();
 
-    render(<AlertingSettingsConfig onEditClick={onEditClick} />);
+      render(
+        <AlertingSettingsConfig
+          onEditClick={onEditClick}
+          userAbilities={abilities}
+        />
+      );
 
-    const editButton = screen.getByLabelText('alerting-edit-button');
-    expect(editButton).toBeDisabled();
+      const editButton = screen.getByLabelText('alerting-edit-button');
+      expect(editButton).toBeDisabled();
 
-    await user.click(editButton);
-    expect(onEditClick).not.toHaveBeenCalled();
+      await user.click(editButton);
+      expect(onEditClick).not.toHaveBeenCalled();
 
-    await user.hover(editButton);
-    expect(
-      screen.queryByText('You are not authorized for this action')
-    ).toBeVisible();
-  });
+      await user.hover(editButton);
+      expect(
+        screen.queryByText('You are not authorized for this action')
+      ).toBeVisible();
+    }
+  );
 
-  it('allows editing when user has sufficient abilities and calls correct handler', async () => {
-    const onEditClick = jest.fn();
-    const user = userEvent.setup();
+  it.each`
+    case                       | abilities
+    ${'admin '}                | ${adminUser}
+    ${'all:alerting_settings'} | ${[{ name: 'all', resource: 'alerting_settings' }]}
+  `(
+    'allows editing when user has sufficient abilities ($case) and calls correct handler',
+    async ({ abilities }) => {
+      const onEditClick = jest.fn();
+      const user = userEvent.setup();
 
-    render(
-      <AlertingSettingsConfig
-        onEditClick={onEditClick}
-        userAbilities={adminUser}
-      />
-    );
+      render(
+        <AlertingSettingsConfig
+          onEditClick={onEditClick}
+          userAbilities={abilities}
+        />
+      );
 
-    expect(screen.getByLabelText('alerting-edit-button')).toBeEnabled();
+      expect(screen.getByLabelText('alerting-edit-button')).toBeEnabled();
 
-    await user.click(screen.getByLabelText('alerting-edit-button'));
-    expect(onEditClick).toHaveBeenCalledTimes(1);
-  });
+      await user.click(screen.getByLabelText('alerting-edit-button'));
+      expect(onEditClick).toHaveBeenCalledTimes(1);
+    }
+  );
 
   it('disables editing when settings are enforced from application env', async () => {
     const onEditClick = jest.fn();
