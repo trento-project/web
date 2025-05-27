505bdb91f30bb59348636b6025e61525252a0879
arbulu89 <xarbulu@suse.com>
2025-05-27 10:35:42 +0200

    Add new entry to activity log usage

--- lib/trento/activity_logging/parser/metadata_enricher.ex
+++ lib/trento/activity_logging/parser/metadata_enricher.ex
@@ -9,6 +9,7 @@ defmodule Trento.ActivityLog.Logger.Parser.MetadataEnricher do
 
   alias Trento.Users.User
 
+  require Trento.Operations.Enums.ClusterOperations, as: ClusterOperations
   require Trento.Operations.Enums.HostOperations, as: HostOperations
 
   @spec enrich(activity :: ActivityCatalog.activity_type(), metadata :: map()) ::
@@ -92,6 +93,13 @@ defmodule Trento.ActivityLog.Logger.Parser.MetadataEnricher do
 
   defp detect_enrichment(:host, {_, %{resource_id: id, operation: operation}})
        when operation in HostOperations.values(),
+       do: {:ok, id}
+
+  defp detect_enrichment(
+         :cluster,
+         {_, %{resource_id: id, operation: operation}}
+       )
+       when operation in ClusterOperations.values(),
        do: {:ok, id}
 
   defp detect_enrichment(
