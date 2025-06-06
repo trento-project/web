3325417d66b8d1399b2cc848b2dc66bd2b1d7f12
Jan Fooken <jan.fooken@suse.com>
2025-06-06 12:51:00 +0200

    Update API Schema To Include the Host Architecture

--- lib/trento_web/openapi/v1/schema/host.ex
+++ lib/trento_web/openapi/v1/schema/host.ex
@@ -5,6 +5,7 @@ defmodule TrentoWeb.OpenApi.V1.Schema.Host do
   alias OpenApiSpex.Schema
 
   alias TrentoWeb.OpenApi.V1.Schema.{
+    Architecture,
     Provider,
     ResourceHealth,
     SaptuneStatus,
@@ -71,6 +72,7 @@ defmodule TrentoWeb.OpenApi.V1.Schema.Host do
             type: :string,
             description: "Version of the agent installed on the host"
           },
+          arch: Architecture,
           health: ResourceHealth,
           cluster_id: %Schema{
             type: :string,
