c1d478224f20982f8cd741c347cb9814d9132cb5
arbulu89 <xarbulu@suse.com>
2025-06-17 14:27:12 +0200

    Improve resource managed tests

--- test/trento/operations/database_instance_policy_test.exs
+++ test/trento/operations/database_instance_policy_test.exs
@@ -129,7 +129,7 @@ defmodule Trento.Operations.DatabaseInstancePolicyTest do
 
     [%{sid: sid}] =
       clustered_sap_instances =
-      build_list(1, :clustered_sap_instance)
+      build_list(1, :clustered_sap_instance, instance_number: "00")
 
     cluster =
       build(:cluster,
@@ -152,17 +152,39 @@ defmodule Trento.Operations.DatabaseInstancePolicyTest do
              DatabaseInstancePolicy.authorize_operation(:maintenance, instance, %{})
   end
 
-  test "should authorize operation if HANA resources are not managed" do
+  test "should authorize operation if HANA resources are unmanaged" do
+    cluster_name = Faker.StarWars.character()
+    resource_id = Faker.UUID.v4()
+
     scenarios = [
-      %{cluster_resource_type: "ocf::suse:SAPHana"},
-      %{cluster_resource_type: "ocf::suse:SAPHanaController"}
+      %{cluster_resource_type: "ocf::suse:SAPHana", managed: false, result: :ok},
+      %{cluster_resource_type: "ocf::suse:SAPHanaController", managed: false, result: :ok},
+      %{
+        cluster_resource_type: "ocf::suse:SAPHana",
+        managed: true,
+        result:
+          {:error,
+           [
+             "Cluster #{cluster_name} or resource #{resource_id} operating this host are not in maintenance mode"
+           ]}
+      },
+      %{
+        cluster_resource_type: "ocf::suse:SAPHanaController",
+        managed: true,
+        result:
+          {:error,
+           [
+             "Cluster #{cluster_name} or resource #{resource_id} operating this host are not in maintenance mode"
+           ]}
+      }
     ]
 
-    for %{cluster_resource_type: cluster_resource_type} <- scenarios do
-      parent = build(:cluster_resource_parent, managed: false)
+    for %{cluster_resource_type: cluster_resource_type, managed: managed, result: result} <-
+          scenarios do
+      parent = build(:cluster_resource_parent, id: resource_id, managed: managed)
 
       cluster_resource =
-        build(:cluster_resource, type: cluster_resource_type, managed: true, parent: parent)
+        build(:cluster_resource, type: cluster_resource_type, parent: parent)
 
       [%{sid: sid, instance_number: instance_number}] =
         clustered_sap_instances =
@@ -170,7 +192,13 @@ defmodule Trento.Operations.DatabaseInstancePolicyTest do
 
       nodes = build_list(1, :hana_cluster_node, resources: [cluster_resource])
       cluster_details = build(:hana_cluster_details, maintenance_mode: false, nodes: nodes)
-      cluster = build(:cluster, sap_instances: clustered_sap_instances, details: cluster_details)
+
+      cluster =
+        build(:cluster,
+          name: cluster_name,
+          sap_instances: clustered_sap_instances,
+          details: cluster_details
+        )
 
       host = build(:host, cluster: cluster)
 
@@ -182,7 +210,7 @@ defmodule Trento.Operations.DatabaseInstancePolicyTest do
           instance_number: instance_number
         )
 
-      assert :ok == DatabaseInstancePolicy.authorize_operation(:maintenance, instance, %{})
+      assert result == DatabaseInstancePolicy.authorize_operation(:maintenance, instance, %{})
     end
   end
 end
