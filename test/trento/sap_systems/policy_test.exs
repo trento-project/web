d2ae5e8263c26e43b293575eb2c29ddd7073896f
arbulu89 <xarbulu@suse.com>
2025-06-18 08:01:42 +0200

    Add sap instance start/stop user policies

--- test/trento/sap_systems/policy_test.exs
+++ test/trento/sap_systems/policy_test.exs
@@ -24,6 +24,48 @@ defmodule Trento.SapSystems.PolicyTest do
     refute Policy.authorize(:delete_application_instance, user, SapSystemReadModel)
   end
 
+  describe "request_instance_operation" do
+    operations = [
+      %{
+        operation: "sap_instance_start",
+        ability: "start"
+      },
+      %{
+        operation: "sap_instance_stop",
+        ability: "stop"
+      }
+    ]
+
+    for %{operation: operation, ability: ability} <- operations do
+      @operation operation
+      @ability ability
+
+      test "should allow #{operation} operation if the user has #{ability}:application_instance ability" do
+        user = %User{abilities: [%Ability{name: @ability, resource: "application_instance"}]}
+
+        assert Policy.authorize(:request_instance_operation, user, %{
+                 operation: @operation
+               })
+      end
+
+      test "should allow #{operation} operation if the user has all:all ability" do
+        user = %User{abilities: [%Ability{name: "all", resource: "all"}]}
+
+        assert Policy.authorize(:request_instance_operation, user, %{
+                 operation: @operation
+               })
+      end
+
+      test "should disallow #{operation} operation if the user does not have #{ability}:application_instance ability" do
+        user = %User{abilities: [%Ability{name: "all", resource: "other_resource"}]}
+
+        refute Policy.authorize(:request_instance_operation, user, %{
+                 operation: @operation
+               })
+      end
+    end
+  end
+
   test "should allow unguarded actions" do
     user = %User{abilities: []}
 
