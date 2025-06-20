4c349f9fc01e8a1928d5b006bb4338b1888be089
Nelson Kopliku <nelson.kopliku@suse.com>
2025-06-20 12:26:47 +0200

    Map pacemaker operations in ui

--- assets/js/lib/operations/index.js
+++ assets/js/lib/operations/index.js
@@ -5,12 +5,15 @@ import { SAPTUNE_SOLUTION_OPERATION_FORBIDDEN_MSG } from './ForbiddenMessages';
 export const HOST_OPERATION = 'host';
 export const CLUSTER_OPERATION = 'cluster';
 export const SAP_INSTANCE_OPERATION = 'sap_instance';
+export const CLUSTER_HOST_OPERATION = 'cluster_host';
 
 export const SAPTUNE_SOLUTION_APPLY = 'saptune_solution_apply';
 export const SAPTUNE_SOLUTION_CHANGE = 'saptune_solution_change';
 export const CLUSTER_MAINTENANCE_CHANGE = 'cluster_maintenance_change';
 export const SAP_INSTANCE_START = 'sap_instance_start';
 export const SAP_INSTANCE_STOP = 'sap_instance_stop';
+export const PACEMAKER_ENABLE = 'pacemaker_enable';
+export const PACEMAKER_DISABLE = 'pacemaker_disable';
 
 const OPERATION_LABELS = {
   [SAPTUNE_SOLUTION_APPLY]: 'Apply Saptune solution',
@@ -18,6 +21,8 @@ const OPERATION_LABELS = {
   [CLUSTER_MAINTENANCE_CHANGE]: 'Cluster maintenance change',
   [SAP_INSTANCE_START]: 'SAP instance start',
   [SAP_INSTANCE_STOP]: 'SAP instance stop',
+  [PACEMAKER_ENABLE]: 'Enable Pacemaker',
+  [PACEMAKER_DISABLE]: 'Disable Pacemaker',
 };
 
 const OPERATION_INTERNAL_NAMES = {
@@ -26,6 +31,8 @@ const OPERATION_INTERNAL_NAMES = {
   'clustermaintenancechange@v1': CLUSTER_MAINTENANCE_CHANGE,
   'sapinstancestart@v1': SAP_INSTANCE_START,
   'sapinstancestop@v1': SAP_INSTANCE_STOP,
+  'pacemakerenable@v1': PACEMAKER_ENABLE,
+  'pacemakerdisable@v1': PACEMAKER_DISABLE,
 };
 
 const OPERATION_RESOURCE_TYPES = {
@@ -34,6 +41,8 @@ const OPERATION_RESOURCE_TYPES = {
   [CLUSTER_MAINTENANCE_CHANGE]: CLUSTER_OPERATION,
   [SAP_INSTANCE_START]: SAP_INSTANCE_OPERATION,
   [SAP_INSTANCE_STOP]: SAP_INSTANCE_OPERATION,
+  [PACEMAKER_ENABLE]: CLUSTER_HOST_OPERATION,
+  [PACEMAKER_DISABLE]: CLUSTER_HOST_OPERATION,
 };
 
 const OPERATION_FORBIDDEN_MESSAGES = {
