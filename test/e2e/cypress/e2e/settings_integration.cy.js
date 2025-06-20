938d130d34152d130157d545f9deb9988a6e204d
Kristiyan Kanchev <kristiyan.kanchev@suse.com>
2025-06-20 20:32:11 +0300

    Split alerting db test into their own filie

---
+++ test/e2e/cypress/e2e/settings_integration.cy.js
@@ -0,0 +1,77 @@
+import * as settingsPage from '../pageObject/settings_po';
+
+describe('Settings from DB', () => {
+  before(function () {
+    if (!Cypress.env('ALERTING_DB_TESTS')) {
+      this.skip();
+    }
+  });
+
+  beforeEach(() => {
+    settingsPage.resetAlertingSettingsDB();
+    settingsPage.visit();
+    settingsPage.waitForRequest('alertingSettingsEndpoint');
+  });
+
+  describe('when not previously set', () => {
+    it('should be displayed with their placeholder values', () => {
+      settingsPage.alertingConfigDisplaysPlaceholderValues();
+    });
+
+    it('should show empty fields when edit modal is opened', () => {
+      settingsPage.clickAlertingEditButton();
+      settingsPage.alertingEditFormDisplaysEmptyFields();
+    });
+
+    it('should be created successfully', () => {
+      settingsPage.clickAlertingEditButton();
+      settingsPage.enterAlertingInitialSettings();
+      settingsPage.alertingConfigDisplaysInitialValues();
+    });
+  });
+
+  describe('when previously set', () => {
+    beforeEach(() => {
+      settingsPage.saveInitialAlertingSettings();
+      settingsPage.refresh();
+      settingsPage.waitForRequest('alertingSettingsEndpoint');
+    });
+
+    it('should display their current values', () => {
+      settingsPage.alertingConfigDisplaysInitialValues();
+    });
+
+    it('should show current values when edit modal is opened', () => {
+      settingsPage.clickAlertingEditButton();
+      settingsPage.alertingEditFormDisplaysInitialSettings();
+    });
+
+    it('should be updated successfully without password', () => {
+      settingsPage.clickAlertingEditButton();
+      settingsPage.enterAlertingUpdateSettingsWithoutPassword();
+      settingsPage.alertingConfigDisplaysUpdateValues();
+    });
+
+    it('should update successfully with password', () => {
+      settingsPage.clickAlertingEditButton();
+      settingsPage.enterAlertingUpdateSettingsWithPassword();
+      settingsPage.alertingConfigDisplaysUpdateValues();
+    });
+  });
+
+  describe('validation', () => {
+    settingsPage.alertingErrorScenarios.forEach(
+      ({
+        name,
+        valueConf: { values, removePasswordProtection },
+        errorConf,
+      }) => {
+        it(`should fail when ${name}`, () => {
+          settingsPage.clickAlertingEditButton();
+          settingsPage.enterAlertingSettings(values, removePasswordProtection);
+          settingsPage.showExpectedErrors(errorConf);
+        });
+      }
+    );
+  });
+});
