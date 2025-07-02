69025864d25d9c8029568e33085bd8eaad9076fa
EMaksy <emaksymenko@suse.com>
2025-07-02 17:02:52 +0200

    Add encoder/decoder

--- assets/setupTests.js
+++ assets/setupTests.js
@@ -3,3 +3,8 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
   unobserve: jest.fn(),
   disconnect: jest.fn(),
 }));
+
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
