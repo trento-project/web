132e0ae66f4b1b47ca7433a58db8ead6b6dd129a
Kristiyan Kanchev <kristiyan.kanchev@suse.com>
2025-07-02 17:37:11 +0300

    Added test for unexpectedly failed discovery event

--- lib/trento/discovery.ex
+++ lib/trento/discovery.ex
@@ -44,7 +44,7 @@ defmodule Trento.Discovery do
     end
   rescue
     error ->
-      IO.inspect(error, label: "Error handling discovery event")
+      Logger.error("Error handling discovery event: #{inspect(error)}")
       store_discarded_discovery_event(event, inspect(error))
       {:error, :discovery_exception}
   end
