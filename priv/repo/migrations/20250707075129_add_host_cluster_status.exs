3f6deec9431cec384a1845ebe7f7e3e63a68abc5
balanza <emanuele.decupis@suse.com>
2025-07-07 12:52:31 +0200

    add cluster status field

---
+++ priv/repo/migrations/20250707075129_add_host_cluster_status.exs
@@ -0,0 +1,16 @@
+defmodule Trento.Repo.Migrations.AddHostClusterStatus do
+  use Ecto.Migration
+
+  def change do
+
+    alter table(:hosts) do
+      add :cluster_status, :string
+    end
+
+    execute(
+      "UPDATE hosts SET cluster_status = 'online' WHERE cluster_status IS NOT NULL",
+      "UPDATE hosts SET cluster_status = NULL"
+    )
+
+  end
+end
