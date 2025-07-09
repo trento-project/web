7c6fcc3f7cf27e3824954bd884e3f46c049ca882
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2025-07-09 14:39:22 +0200

    Adds environment to PageView event

--- assets/js/lib/analytics/pageview.js
+++ assets/js/lib/analytics/pageview.js
@@ -7,13 +7,19 @@ import { getUserProfile } from '@state/selectors/user';
 export default function PostHogPageView() {
   const location = useLocation();
   const { analytics_enabled } = useSelector(getUserProfile);
-  const webVersion = document.querySelector('#trento').getAttribute('data-ph-capture-attribute-webversion');
+  const webversion = document
+    .querySelector('#trento')
+    .getAttribute('data-ph-capture-attribute-webversion');
+  const environment = document
+    .querySelector('#trento')
+    .getAttribute('data-ph-capture-attribute-environment');
 
   // Track pageviews
   useEffect(() => {
     capture(analytics_enabled, '$pageview', {
       $current_url: window.location.href,
-      webversion: webVersion
+      webversion,
+      environment,
     });
   }, [location]);
 
