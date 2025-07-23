39a6ee1a19c34aeaf43c77778dda90617a91da87
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2025-07-23 10:52:32 +0200

    Adds webversion to pageview events

--- assets/js/lib/analytics/pageview.js
+++ assets/js/lib/analytics/pageview.js
@@ -1,17 +1,20 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { capture } from '@lib/analytics';
+import { getFromConfig } from '@lib/config';
 import { useSelector } from 'react-redux';
 import { getUserProfile } from '@state/selectors/user';
 
 export default function PostHogPageView() {
   const location = useLocation();
   const { analytics_enabled } = useSelector(getUserProfile);
+  const webversion = getFromConfig('webversion');
 
   // Track pageviews
   useEffect(() => {
     capture(analytics_enabled, '$pageview', {
       $current_url: window.location.href,
+      webversion,
     });
   }, [location]);
 
