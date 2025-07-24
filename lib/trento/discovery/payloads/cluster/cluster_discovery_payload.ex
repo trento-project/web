bc8e6e06a3423ee8a149809f5cde976180029547
arbulu89 <xarbulu@suse.com>
2025-07-24 10:23:26 +0200

    Use caster crm and cib to parse cluster type

--- lib/trento/discovery/payloads/cluster/cluster_discovery_payload.ex
+++ lib/trento/discovery/payloads/cluster/cluster_discovery_payload.ex
@@ -50,27 +50,25 @@ defmodule Trento.Discovery.Payloads.Cluster.ClusterDiscoveryPayload do
   defp offline?(_), do: false
 
   defp changeset_online(cluster, attrs) do
-    glob_topology = parse_hana_glob_topology(attrs)
+    casted_cluster =
+      cluster
+      |> cast(attrs, fields())
+      |> cast_embed(:cib, required: true)
+      |> cast_embed(:sbd)
+      |> cast_embed(:crmmon, required: true)
+      |> put_change(:cluster_host_status, ClusterHostStatus.online())
 
-    hana_type = parse_hana_cluster_type(attrs)
-    ascs_ers_type = parse_ascs_ers_cluster_type(attrs)
+    casted_cib = get_embed(casted_cluster, :cib, :struct)
+    casted_crmon = get_embed(casted_cluster, :crmmon, :struct)
 
-    enriched_attributes =
-      enrich_cluster_and_hana_architecture_types(
-        attrs,
-        hana_type,
-        ascs_ers_type,
-        glob_topology
-      )
+    glob_topology = parse_hana_glob_topology(casted_cib)
+    hana_type = parse_hana_cluster_type(casted_crmon)
+    ascs_ers_type = parse_ascs_ers_cluster_type(casted_crmon)
 
-    cluster
-    |> cast(enriched_attributes, fields())
-    |> cast_embed(:cib, required: true)
-    |> cast_embed(:sbd)
-    |> cast_embed(:crmmon, required: true)
+    casted_cluster
+    |> put_change_cluster_and_hana_architecture_types(hana_type, ascs_ers_type, glob_topology)
     |> validate_required_fields(@required_fields_online)
-    |> maybe_validate_required_fields(enriched_attributes)
-    |> put_change(:cluster_host_status, ClusterHostStatus.online())
+    |> maybe_validate_required_fields()
   end
 
   defp changeset_offline(cluster, attrs) do
@@ -81,12 +79,10 @@ defmodule Trento.Discovery.Payloads.Cluster.ClusterDiscoveryPayload do
   end
 
   defp parse_hana_glob_topology(%{
-         "cib" => %{
-           "configuration" => %{"crm_config" => %{"cluster_properties" => cluster_properties}}
-         }
+         configuration: %{crm_config: %{cluster_properties: cluster_properties}}
        }) do
     Enum.find_value(cluster_properties, nil, fn
-      %{"name" => name, "value" => value} ->
+      %{name: name, value: value} ->
         if String.ends_with?(name, "_glob_topology"), do: value
 
       _ ->
@@ -96,38 +92,37 @@ defmodule Trento.Discovery.Payloads.Cluster.ClusterDiscoveryPayload do
 
   # The cluster manages a HANA system when it manages a SAPHanaTopology resource
   # and other SAPHana for Scale up or SAPHanaController for Scale Out.
-  defp parse_hana_cluster_type(%{"crmmon" => %{"clones" => clones}}) when not is_nil(clones) do
+  defp parse_hana_cluster_type(%{clones: []}), do: ClusterType.unknown()
+
+  defp parse_hana_cluster_type(%{clones: clones}) do
     all_clone_resources =
       Enum.flat_map(clones, fn
-        %{"resources" => nil} -> []
-        %{"resources" => resources} -> resources
+        %{resources: resources} -> resources
       end)
 
     has_sap_hana_topology =
-      Enum.any?(all_clone_resources, fn %{"agent" => agent} ->
+      Enum.any?(all_clone_resources, fn %{agent: agent} ->
         agent == "ocf::suse:SAPHanaTopology"
       end)
 
     has_sap_hana =
-      Enum.any?(all_clone_resources, fn %{"agent" => agent} -> agent == "ocf::suse:SAPHana" end)
+      Enum.any?(all_clone_resources, fn %{agent: agent} -> agent == "ocf::suse:SAPHana" end)
 
     has_sap_hana_controller =
-      Enum.any?(all_clone_resources, fn %{"agent" => agent} ->
+      Enum.any?(all_clone_resources, fn %{agent: agent} ->
         agent == "ocf::suse:SAPHanaController"
       end)
 
     do_detect_cluster_type(has_sap_hana_topology, has_sap_hana, has_sap_hana_controller)
   end
 
-  defp parse_hana_cluster_type(_), do: ClusterType.unknown()
-
   # The cluster manages a ASCS/ERS system when it manages an even number of
   # SAPInstance resources that are not managing HANA (HDB) databases.
-  defp parse_ascs_ers_cluster_type(%{"crmmon" => %{"groups" => groups}})
+  defp parse_ascs_ers_cluster_type(%{groups: groups})
        when not is_nil(groups) do
     sap_instance_count =
-      Enum.count(groups, fn %{"resources" => resources} ->
-        Enum.any?(resources, fn %{"id" => id, "agent" => agent} ->
+      Enum.count(groups, fn %{resources: resources} ->
+        Enum.any?(resources, fn %{id: id, agent: agent} ->
           agent == "ocf::heartbeat:SAPInstance" && not String.contains?(id, "HDB")
         end)
       end)
@@ -146,59 +141,73 @@ defmodule Trento.Discovery.Payloads.Cluster.ClusterDiscoveryPayload do
 
   defp do_detect_cluster_type(_), do: ClusterType.unknown()
 
-  defp enrich_cluster_and_hana_architecture_types(
-         attrs,
+  defp put_change_cluster_and_hana_architecture_types(
+         changeset,
          _,
          ClusterType.unknown(),
          "ScaleUp"
        ) do
-    attrs
-    |> Map.put("cluster_type", ClusterType.hana_scale_up())
-    |> Map.put("hana_architecture_type", HanaArchitectureType.angi())
+    changeset
+    |> put_change(:cluster_type, ClusterType.hana_scale_up())
+    |> put_change(:hana_architecture_type, HanaArchitectureType.angi())
   end
 
-  defp enrich_cluster_and_hana_architecture_types(
-         attrs,
+  defp put_change_cluster_and_hana_architecture_types(
+         changeset,
          _,
          ClusterType.unknown(),
          "ScaleOut"
        ) do
-    attrs
-    |> Map.put("cluster_type", ClusterType.hana_scale_out())
-    |> Map.put("hana_architecture_type", HanaArchitectureType.angi())
+    changeset
+    |> put_change(:cluster_type, ClusterType.hana_scale_out())
+    |> put_change(:hana_architecture_type, HanaArchitectureType.angi())
   end
 
-  defp enrich_cluster_and_hana_architecture_types(attrs, hana_type, ClusterType.unknown(), _)
+  defp put_change_cluster_and_hana_architecture_types(
+         changeset,
+         hana_type,
+         ClusterType.unknown(),
+         _
+       )
        when hana_type in [ClusterType.hana_scale_up(), ClusterType.hana_scale_out()] do
-    attrs
-    |> Map.put("cluster_type", hana_type)
-    |> Map.put("hana_architecture_type", HanaArchitectureType.classic())
+    changeset
+    |> put_change(:cluster_type, hana_type)
+    |> put_change(:hana_architecture_type, HanaArchitectureType.classic())
   end
 
-  defp enrich_cluster_and_hana_architecture_types(
-         attrs,
+  defp put_change_cluster_and_hana_architecture_types(
+         changeset,
          ClusterType.unknown(),
          ClusterType.ascs_ers(),
          _
        ) do
-    Map.put(attrs, "cluster_type", ClusterType.ascs_ers())
+    put_change(changeset, :cluster_type, ClusterType.ascs_ers())
   end
 
-  defp enrich_cluster_and_hana_architecture_types(attrs, hana_type, ClusterType.ascs_ers(), _)
+  defp put_change_cluster_and_hana_architecture_types(
+         changeset,
+         hana_type,
+         ClusterType.ascs_ers(),
+         _
+       )
        when hana_type in [ClusterType.hana_scale_up(), ClusterType.hana_scale_out()] do
-    Map.put(attrs, "cluster_type", ClusterType.hana_ascs_ers())
+    put_change(changeset, :cluster_type, ClusterType.hana_ascs_ers())
   end
 
-  defp enrich_cluster_and_hana_architecture_types(attrs, _, _, _) do
-    Map.put(attrs, "cluster_type", ClusterType.unknown())
+  defp put_change_cluster_and_hana_architecture_types(changeset, _, _, _) do
+    put_change(changeset, :cluster_type, ClusterType.unknown())
   end
 
-  defp maybe_validate_required_fields(cluster, %{"cluster_type" => ClusterType.hana_scale_up()}),
-    do: validate_required(cluster, @required_fields_hana)
+  defp maybe_validate_required_fields(
+         %{changes: %{cluster_type: ClusterType.hana_scale_up()}} = cluster
+       ),
+       do: validate_required(cluster, @required_fields_hana)
 
-  defp maybe_validate_required_fields(cluster, %{"cluster_type" => ClusterType.hana_scale_out()}),
-    do: validate_required(cluster, @required_fields_hana)
+  defp maybe_validate_required_fields(
+         %{changes: %{cluster_type: ClusterType.hana_scale_out()}} = cluster
+       ),
+       do: validate_required(cluster, @required_fields_hana)
 
-  defp maybe_validate_required_fields(cluster, _),
+  defp maybe_validate_required_fields(cluster),
     do: cluster
 end
