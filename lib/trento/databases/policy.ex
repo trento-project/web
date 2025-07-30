49f7ecda24e7a27e986ff42ae00f646aa21d7e3f
arbulu89 <xarbulu@suse.com>
2025-07-30 16:15:55 +0200

    Add new abilities policy code

--- lib/trento/databases/policy.ex
+++ lib/trento/databases/policy.ex
@@ -13,8 +13,20 @@ defmodule Trento.Databases.Policy do
   def authorize(:delete_database_instance, %User{} = user, DatabaseReadModel),
     do: has_global_ability?(user) or has_cleanup_ability?(user)
 
+  def authorize(:request_operation, %User{} = user, %{operation: "database_start"}),
+    do: has_global_ability?(user) or has_database_start_ability?(user)
+
+  def authorize(:request_operation, %User{} = user, %{operation: "database_stop"}),
+    do: has_global_ability?(user) or has_database_stop_ability?(user)
+
   def authorize(_, _, _), do: true
 
   defp has_cleanup_ability?(user),
     do: user_has_ability?(user, %{name: "cleanup", resource: "database_instance"})
+
+  defp has_database_start_ability?(user),
+    do: user_has_ability?(user, %{name: "start", resource: "database"})
+
+  defp has_database_stop_ability?(user),
+    do: user_has_ability?(user, %{name: "stop", resource: "database"})
 end
