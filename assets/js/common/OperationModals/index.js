12f0f1cb8d8f69a6f2c223de6e578dbd95b337bc
arbulu89 <xarbulu@suse.com>
2025-08-04 10:08:34 +0200

    Export new operation modal

--- assets/js/common/OperationModals/index.js
+++ assets/js/common/OperationModals/index.js
@@ -1,9 +1,11 @@
 import SaptuneSolutionOperationModal from './SaptuneSolutionOperationModal';
 import OperationForbiddenModal from './OperationForbiddenModal';
 import SimpleAcceptanceOperationModal from './SimpleAcceptanceOperationModal';
+import SapStartStopOperationModal from './SapStartStopOperationModal';
 
 export {
   SaptuneSolutionOperationModal,
   SimpleAcceptanceOperationModal,
+  SapStartStopOperationModal,
   OperationForbiddenModal,
 };
