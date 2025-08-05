63f7781b392d7616b5b75e7673eab644243dfa76
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2025-08-05 16:13:28 +0200

    Adds AnalyticsEulaModal to HomeHealthSummaryPage

--- assets/js/pages/HealthSummary/HomeHealthSummaryPage.jsx
+++ assets/js/pages/HealthSummary/HomeHealthSummaryPage.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+
+import { isAdmin } from '@lib/model/users';
+import { editUserProfile } from '@lib/api/users';
+import { getUserProfile } from '@state/selectors/user';
 import HomeHealthSummary from './HomeHealthSummary';
+import AnalyticsEulaModal from '../AnalyticsEulaModal';
 
 export function HomeHealthSummaryPage() {
   const { loading, sapSystemsHealth } = useSelector(
     (state) => state.sapSystemsHealthSummary
   );
 
+  const { analytics_eula_accepted, username } = useSelector(getUserProfile);
+  const [analyticsEulaModalOpen, setAnalyticsEulaModalOpen] = useState(
+    !analytics_eula_accepted
+  );
+  const [checked, setChecked] = useState(false);
+
+  const user = { username };
+  const isDefaultAdmin = isAdmin(user);
+
   return (
-    <HomeHealthSummary sapSystemsHealth={sapSystemsHealth} loading={loading} />
+    <div>
+      <AnalyticsEulaModal
+        isOpen={!isDefaultAdmin && analyticsEulaModalOpen}
+        checked={checked}
+        onChecked={() => setChecked((prev) => !prev)}
+        onEnable={() => {
+          setAnalyticsEulaModalOpen(false);
+          editUserProfile({
+            analytics_enabled: true,
+            analytics_eula_accepted: true,
+          });
+        }}
+        onCancel={() => {
+          setAnalyticsEulaModalOpen(false);
+          if (checked) {
+            editUserProfile({
+              analytics_eula_accepted: checked,
+            });
+          }
+        }}
+      />
+      <HomeHealthSummary
+        sapSystemsHealth={sapSystemsHealth}
+        loading={loading}
+      />
+    </div>
   );
 }
