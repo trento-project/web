2bd109376851b1f4d18c6083fdaffc7e1a8b5b74
Jurgen Goldschmidt <jurgen.goldschmidt@suse.com>
2025-08-13 14:38:38 +0200

    Adds Analytics Eula and Modal

---
+++ assets/js/pages/AnalyticsEula/AnalyticsEulaModal.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Modal from '@common/Modal';
+import Button from '@common/Button';
+import Input from '@common/Input';
+
+function AnalyticsEulaModal({
+  isOpen = false,
+  checked = false,
+  onChecked,
+  onEnable,
+  onCancel,
+}) {
+  return (
+    <Modal
+      className="!w-1/2"
+      title="Collection of Anonymous Metrics"
+      open={isOpen}
+      onClose={() => null}
+    >
+      Allow the collection of{' '}
+      <a
+        className="text-jungle-green-500 hover:opacity-75"
+        href="https://trento-project.io/docs"
+        target="_blank"
+        rel="noreferrer"
+      >
+        anonymous metrics
+      </a>{' '}
+      to help improve Trento. This can also be managed in your Profile.
+      <div>
+        <Input
+          className="inline-block py-4 pr-2"
+          type="checkbox"
+          checked={checked}
+          onChange={onChecked}
+        />
+        Never show this message again.
+      </div>
+      <div className="flex flex-row space-x-2">
+        <Button type="default-fit" onClick={onEnable}>
+          Enable Analytics Collection
+        </Button>
+        <Button type="primary-white-fit" onClick={onCancel}>
+          Continue without Analytics
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default AnalyticsEulaModal;
