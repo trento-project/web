8295f69734e970678cbe6603497bbcb89fc2ef29
Antonio Gamez Diaz <antonio.gamez@suse.com>
2025-08-22 15:11:06 +0200

    Remove conflicting changes
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- lib/trento_web/controllers/v1/tags_controller.ex
+++ lib/trento_web/controllers/v1/tags_controller.ex
@@ -20,16 +20,16 @@ defmodule TrentoWeb.V1.TagsController do
   action_fallback TrentoWeb.FallbackController
   plug OpenApiSpex.Plug.CastAndValidate, json_render_error_v2: true
 
-  operation :add_tag_to_host,
-    summary: "Add tag to host.",
+  operation :add_tag,
+    summary: "Add tag.",
     tags: ["Tags"],
     description:
-      "Adds a new tag to the specified host, supporting resource categorization and management for infrastructure operations.",
+      "Adds a new tag to the specified resource, supporting resource categorization and management for infrastructure operations.",
     parameters: [
       id: [
         in: :path,
         description:
-          "Unique identifier of the host to which the tag will be added. This value must be a valid UUID string.",
+          "Unique identifier of the resource to which the tag will be added. This value must be a valid UUID string.",
         required: true,
         schema: %OpenApiSpex.Schema{
           type: :string,
@@ -39,7 +39,7 @@ defmodule TrentoWeb.V1.TagsController do
       ]
     ],
     request_body:
-      {"Request containing tag value to be added to the specified host for resource categorization and management.",
+      {"Request containing tag value to be added to the specified resource for resource categorization and management.",
        "application/json",
        %OpenApiSpex.Schema{
          type: :object,
@@ -49,133 +49,7 @@ defmodule TrentoWeb.V1.TagsController do
        }},
     responses: [
       created:
-        {"Tag has been successfully added to the specified host, supporting resource categorization and management.",
-         "application/json",
-         %OpenApiSpex.Schema{
-           type: :object,
-           properties: %{},
-           example: %{}
-         }},
-      bad_request: Schema.BadRequest.response(),
-      unprocessable_entity: UnprocessableEntity.response()
-    ]
-
-  operation :add_tag_to_cluster,
-    summary: "Add tag to cluster.",
-    tags: ["Tags"],
-    description:
-      "Adds a new tag to the specified cluster, supporting resource categorization and management for infrastructure operations.",
-    parameters: [
-      id: [
-        in: :path,
-        description:
-          "Unique identifier of the cluster to which the tag will be added. This value must be a valid UUID string.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          format: :uuid,
-          example: "c1a2b3c4-d5e6-7890-abcd-ef1234567890"
-        }
-      ]
-    ],
-    request_body:
-      {"Request containing tag value to be added to the specified cluster for resource categorization and management.",
-       "application/json",
-       %OpenApiSpex.Schema{
-         type: :object,
-         example: %{
-           value: "production"
-         },
-         properties: %{
-           value: %OpenApiSpex.Schema{
-             type: :string,
-             example: "production"
-           }
-         }
-       }},
-    responses: [
-      created:
-        {"Tag has been successfully added to the specified cluster, supporting resource categorization and management.",
-         "application/json",
-         %OpenApiSpex.Schema{
-           type: :object,
-           properties: %{},
-           example: %{}
-         }},
-      bad_request: Schema.BadRequest.response(),
-      unprocessable_entity: UnprocessableEntity.response()
-    ]
-
-  operation :add_tag_to_sap_system,
-    summary: "Add tag to SAP system.",
-    tags: ["Tags"],
-    description:
-      "Adds a new tag to the specified SAP system, supporting resource categorization and management for infrastructure operations.",
-    parameters: [
-      id: [
-        in: :path,
-        description:
-          "Unique identifier of the SAP system to which the tag will be added. This value must be a valid UUID string.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          format: :uuid,
-          example: "d59523fc-0497-4b1e-9fdd-14aa7cda77f1"
-        }
-      ]
-    ],
-    request_body:
-      {"Request containing tag value to be added to the specified SAP system for resource categorization and management.",
-       "application/json",
-       %OpenApiSpex.Schema{
-         type: :object,
-         properties: %{
-           value: %OpenApiSpex.Schema{type: :string, example: "production"}
-         }
-       }},
-    responses: [
-      created:
-        {"Tag has been successfully added to the specified SAP system, supporting resource categorization and management.",
-         "application/json",
-         %OpenApiSpex.Schema{
-           type: :object,
-           properties: %{},
-           example: %{}
-         }},
-      bad_request: Schema.BadRequest.response(),
-      unprocessable_entity: UnprocessableEntity.response()
-    ]
-
-  operation :add_tag_to_database,
-    summary: "Add tag to database.",
-    tags: ["Tags"],
-    description:
-      "Adds a new tag to the specified database, supporting resource categorization and management for infrastructure operations.",
-    parameters: [
-      id: [
-        in: :path,
-        description:
-          "Unique identifier of the database to which the tag will be added. This value must be a valid UUID string.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          format: :uuid,
-          example: "d1a2b3c4-d5e6-7890-abcd-ef1234567890"
-        }
-      ]
-    ],
-    request_body:
-      {"Request containing tag value to be added to the specified database for resource categorization and management.",
-       "application/json",
-       %OpenApiSpex.Schema{
-         type: :object,
-         properties: %{
-           value: %OpenApiSpex.Schema{type: :string, example: "production"}
-         }
-       }},
-    responses: [
-      created:
-        {"Tag has been successfully added to the specified database, supporting resource categorization and management.",
+        {"Tag has been successfully added to the specified resource, supporting resource categorization and management.",
          "application/json",
          %OpenApiSpex.Schema{
            type: :object,
@@ -203,21 +77,16 @@ defmodule TrentoWeb.V1.TagsController do
     end
   end
 
-  def add_tag_to_host(conn, params), do: add_tag(conn, params)
-  def add_tag_to_cluster(conn, params), do: add_tag(conn, params)
-  def add_tag_to_sap_system(conn, params), do: add_tag(conn, params)
-  def add_tag_to_database(conn, params), do: add_tag(conn, params)
-
-  operation :remove_tag_from_host,
-    summary: "Remove tag from host.",
+  operation :remove_tag,
+    summary: "Remove tag from resource.",
     tags: ["Tags"],
     description:
-      "Removes a tag from the specified host, supporting resource management and cleanup for infrastructure operations.",
+      "Removes a tag from the specified resource, supporting resource management and cleanup for infrastructure operations.",
     parameters: [
       id: [
         in: :path,
         description:
-          "Unique identifier of the host from which the tag will be removed. This value must be a valid UUID string.",
+          "Unique identifier of the resource from which the tag will be removed. This value must be a valid UUID string.",
         required: true,
         schema: %OpenApiSpex.Schema{
           type: :string,
@@ -227,7 +96,7 @@ defmodule TrentoWeb.V1.TagsController do
       ],
       value: [
         in: :path,
-        description: "The value of the tag to be removed from the host.",
+        description: "The value of the tag to be removed from the resource.",
         required: true,
         schema: %OpenApiSpex.Schema{
           type: :string,
@@ -237,120 +106,12 @@ defmodule TrentoWeb.V1.TagsController do
     ],
     responses: [
       no_content:
-        {"The tag has been removed from the host.", "application/json",
+        {"The tag has been removed from the resource.", "application/json",
          %OpenApiSpex.Schema{
            type: :object,
            properties: %{},
            example: %{}
          }},
-      bad_request: Schema.BadRequest.response(),
-      unprocessable_entity: UnprocessableEntity.response(),
-      not_found: NotFound.response()
-    ]
-
-  operation :remove_tag_from_cluster,
-    summary: "Remove tag from cluster.",
-    tags: ["Tags"],
-    description:
-      "Removes a tag from the specified cluster, supporting resource management and cleanup for infrastructure operations.",
-    parameters: [
-      id: [
-        in: :path,
-        description:
-          "Unique identifier of the cluster from which the tag will be removed. This value must be a valid UUID string.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          format: :uuid,
-          example: "c1a2b3c4-d5e6-7890-abcd-ef1234567890"
-        }
-      ],
-      value: [
-        in: :path,
-        description: "The value of the tag to be removed from the cluster.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          example: "production"
-        }
-      ]
-    ],
-    responses: [
-      no_content:
-        {"The tag has been removed from the cluster.", "application/json",
-         %OpenApiSpex.Schema{
-           type: :object,
-           properties: %{},
-           example: %{}
-         }},
-      bad_request: Schema.BadRequest.response(),
-      unprocessable_entity: UnprocessableEntity.response(),
-      not_found: NotFound.response()
-    ]
-
-  operation :remove_tag_from_sap_system,
-    summary: "Remove tag from SAP system.",
-    tags: ["Tags"],
-    description:
-      "Removes a tag from the specified SAP system, supporting resource management and cleanup for infrastructure operations.",
-    parameters: [
-      id: [
-        in: :path,
-        description:
-          "Unique identifier of the SAP system from which the tag will be removed. This value must be a valid UUID string.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          format: :uuid,
-          example: "d59523fc-0497-4b1e-9fdd-14aa7cda77f1"
-        }
-      ],
-      value: [
-        in: :path,
-        description: "The value of the tag to be removed from the SAP system.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          example: "production"
-        }
-      ]
-    ],
-    responses: [
-      no_content: "The tag has been removed from the SAP system.",
-      bad_request: Schema.BadRequest.response(),
-      unprocessable_entity: UnprocessableEntity.response(),
-      not_found: NotFound.response()
-    ]
-
-  operation :remove_tag_from_database,
-    summary: "Remove tag from database.",
-    tags: ["Tags"],
-    description:
-      "Removes a tag from the specified database, supporting resource management and cleanup for infrastructure operations.",
-    parameters: [
-      id: [
-        in: :path,
-        description:
-          "Unique identifier of the database from which the tag will be removed. This value must be a valid UUID string.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          format: :uuid,
-          example: "d1a2b3c4-d5e6-7890-abcd-ef1234567890"
-        }
-      ],
-      value: [
-        in: :path,
-        description: "The value of the tag to be removed from the database.",
-        required: true,
-        schema: %OpenApiSpex.Schema{
-          type: :string,
-          example: "production"
-        }
-      ]
-    ],
-    responses: [
-      no_content: "The tag has been removed from the database.",
       bad_request: Schema.BadRequest.response(),
       unprocessable_entity: UnprocessableEntity.response(),
       not_found: NotFound.response()
@@ -364,11 +125,6 @@ defmodule TrentoWeb.V1.TagsController do
       send_resp(conn, :no_content, "")
     end
   end
-
-  def remove_tag_from_host(conn, params), do: remove_tag(conn, params)
-  def remove_tag_from_cluster(conn, params), do: remove_tag(conn, params)
-  def remove_tag_from_sap_system(conn, params), do: remove_tag(conn, params)
-  def remove_tag_from_database(conn, params), do: remove_tag(conn, params)
 
   def get_policy_resource(%{
         assigns: %{
