e09e889c141f659c03957861fcfeadb05af4a569
Antonio Gamez Diaz <antonio.gamez@suse.com>
2025-08-22 14:21:58 +0200

    Fix duplicated operation name
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- lib/trento_web/router.ex
+++ lib/trento_web/router.ex
@@ -201,7 +201,7 @@ defmodule TrentoWeb.Router do
           get "/", SettingsController, :get_suse_manager_settings
           post "/", SettingsController, :save_suse_manager_settings
           patch "/", SettingsController, :update_suse_manager_settings
-          put "/", SettingsController, :update_suse_manager_settings
+          put "/", SettingsController, :put_suse_manager_settings
           delete "/", SettingsController, :delete_suse_manager_settings
           post "/test", SettingsController, :test_suse_manager_settings
         end
