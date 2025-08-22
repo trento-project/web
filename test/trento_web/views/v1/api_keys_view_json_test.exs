050ec347d5a179be6a5a4dc6fbcb9c6b26c778c7
Nelson Kopliku <nelson.kopliku@suse.com>
2025-08-22 14:40:15 +0200

    Add api keys retrieval controller action

--- test/trento_web/views/v1/api_keys_view_json_test.exs
+++ test/trento_web/views/v1/api_keys_view_json_test.exs
@@ -6,6 +6,50 @@ defmodule TrentoWeb.V1.ApiKeysViewJSONTest do
 
   import Trento.Factory
 
+  describe "rendering a list of keys" do
+    test "should render an empty list of api keys" do
+      assert [] == ApiKeysJSON.api_keys(%{api_keys: []})
+    end
+
+    test "should render a list of api keys" do
+      name1 = Faker.Lorem.word()
+      created_at1 = Faker.DateTime.backward(1)
+      expire_at1 = nil
+
+      name2 = Faker.StarWars.character()
+      created_at2 = Faker.DateTime.backward(2)
+      expire_at2 = Faker.DateTime.forward(3)
+
+      name3 = Faker.StarWars.character()
+      created_at3 = Faker.DateTime.backward(3)
+      expire_at3 = Faker.DateTime.forward(4)
+
+      api_keys = [
+        build(:api_key, name: name1, created_at: created_at1, expire_at: expire_at1),
+        build(:api_key, name: name2, created_at: created_at2, expire_at: expire_at2),
+        build(:api_key, name: name3, created_at: created_at3, expire_at: expire_at3)
+      ]
+
+      assert [
+               %{
+                 name: name1,
+                 created_at: created_at1,
+                 expire_at: expire_at1
+               },
+               %{
+                 name: name2,
+                 created_at: created_at2,
+                 expire_at: expire_at2
+               },
+               %{
+                 name: name3,
+                 created_at: created_at3,
+                 expire_at: expire_at3
+               }
+             ] == ApiKeysJSON.api_keys(%{api_keys: api_keys})
+    end
+  end
+
   describe "rendering a newly created key" do
     scenarios = [
       %{
