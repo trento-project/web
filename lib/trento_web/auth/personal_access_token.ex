a2ef1d33cb3be47880a5e2fbeb11d74deb27c2f5
Nelson Kopliku <nelson.kopliku@suse.com>
2025-09-15 16:30:19 +0200

    Add a PersonalAccessToken with a different aud claim

---
+++ lib/trento_web/auth/personal_access_token.ex
@@ -0,0 +1,19 @@
+defmodule TrentoWeb.Auth.PersonalAccessToken do
+  @moduledoc """
+  PersonalAccessToken represents a users' generated token.
+  """
+  use Joken.Config, default_signer: :access_token_signer
+
+  alias TrentoWeb.Auth.ApiKey
+
+  @iss Application.compile_env!(:trento, :jwt_authentication)[:issuer]
+
+  @impl Joken.Config
+  def token_config, do: default_claims(iss: @iss, aud: "trento_pat")
+
+  def generate!(claims, created_at, expire_at) do
+    claims
+    |> ApiKey.prepare_claims(created_at, expire_at)
+    |> generate_and_sign!()
+  end
+end
