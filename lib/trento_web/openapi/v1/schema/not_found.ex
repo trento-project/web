3cc62a2cc0bded38c3f52ff500af4df2cccf3c23
Antonio Gámez, PhD <antonio.gamez@suse.com>
2025-09-16 10:58:19 +0200

    [TRNT-3845] Run linters on the API spec and fix the errors (general info) (#3742)
    * Add extra info fields to the API spec
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Improve API spec description field
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Add descriptions for all the tags
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Improve http statuses schemas
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * [TRNT-3845] Run linters on the API spec and fix the errors (schemas) (#3743)
    
    * Improve schemas for API docs
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Revert some "type: object" conflicting changes
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    ---------
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * [TRNT-3845] Run linters on the API spec and fix the errors (controllers, non-breaking changes) (#3757)
    
    * Perform non breaking changes in API docs
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Add missing changes
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Rename type to schema
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Revert "Rename type to schema"
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Extract credential schemas
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Rename old references to "SUSE Multi-Linux Manager"
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Nelson Kopliku <nelson.kopliku@suse.com>
    
    * Add missing rename
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Group credentials schemas into TrentoWeb.OpenApi.V1.Schema.Auth
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Remove "instance" word
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Add missing parameters (#3760)
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * [TRNT-3845] Run linters on the API spec and fix the errors (controllers: improve schema) (#3761)
    
    * Improve credentials schemas
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Fix alias
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Fix format issue
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    ---------
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    * Rename type to schema (#3758)
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    
    ---------
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    Co-authored-by: Nelson Kopliku <nelson.kopliku@suse.com>
    
    ---------
    
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>
    Co-authored-by: Nelson Kopliku <nelson.kopliku@suse.com>

--- lib/trento_web/openapi/v1/schema/not_found.ex
+++ lib/trento_web/openapi/v1/schema/not_found.ex
@@ -1,6 +1,6 @@
 defmodule TrentoWeb.OpenApi.V1.Schema.NotFound do
   @moduledoc """
-  404 - Not Found
+  404 - Not Found.
   """
   require OpenApiSpex
 
@@ -10,16 +10,42 @@ defmodule TrentoWeb.OpenApi.V1.Schema.NotFound do
   OpenApiSpex.schema(
     %{
       title: "NotFound",
+      description:
+        "Represents an error response for a resource not found, providing details about the reason the requested resource could not be located.",
       type: :object,
       additionalProperties: false,
+      example: %{
+        errors: [
+          %{
+            detail: "The requested resource cannot be found.",
+            title: "Not Found"
+          }
+        ]
+      },
       properties: %{
         errors: %Schema{
           type: :array,
+          example: [
+            %{
+              detail: "The requested resource cannot be found.",
+              title: "Not Found"
+            }
+          ],
           items: %Schema{
             type: :object,
             properties: %{
-              detail: %Schema{type: :string, example: "The requested resource cannot be found."},
-              title: %Schema{type: :string, example: "Not Found"}
+              detail: %Schema{
+                type: :string,
+                description:
+                  "Provides a detailed explanation of why the requested resource could not be found, supporting troubleshooting and resolution.",
+                example: "The requested resource cannot be found."
+              },
+              title: %Schema{
+                type: :string,
+                description:
+                  "A short summary indicating the not found status, used for quick identification and error handling.",
+                example: "Not Found"
+              }
             }
           }
         }
@@ -30,7 +56,7 @@ defmodule TrentoWeb.OpenApi.V1.Schema.NotFound do
 
   def response do
     Operation.response(
-      "Not Found",
+      "A detailed error response indicating that the requested resource could not be found, including information about the reason for the missing resource.",
       "application/json",
       __MODULE__
     )
