f13a2998864d0c9dfcc34b58db1603b91d5e3ea6
vicente <vicente.ruiz@suse.com>
2025-09-22 10:30:12 +0200

    fix how spec is set in integratio job

--- test/e2e/cypress/e2e/alerting.cy.js
+++ test/e2e/cypress/e2e/alerting.cy.js
@@ -2,15 +2,34 @@ import * as settingsPage from '../pageObject/settings_po';
 
 context('Settings page', () => {
   beforeEach(() => {
+    settingsPage.preloadTestData();
     settingsPage.visit();
     settingsPage.waitForRequest('settingsEndpoint');
   });
 
-  describe('Alerting Settings enforced from env', () => {
+  describe('Send/Receive Emails when alerting settings are setup from env', () => {
     before(function () {
       if (!Cypress.env('ALERTING_DB_TESTS')) {
         this.skip();
       }
+    });
+
+    it('should display empty values', () => {
+      cy.get('body').should('be.visible');
+    });
+
+    it('should have an edit button enabled', () => {
+      cy.get('body').should('be.visible');
+    });
+  });
+
+  describe('Send/Receive Emails when alerting settings are manually setup', () => {
+    before(function () {
+      settingsPage.getAlertingSettings().then((resp) => {
+        if (!resp.body.enforced_from_env) {
+          this.skip();
+        }
+      });
     });
 
     it('should display empty values', () => {
