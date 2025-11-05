e43c5180a8d3cd8af56b1bad181a2fd00d53bc25
arbulu89 <xarbulu@suse.com>
2025-11-05 16:14:15 +0100

    Enable analytics GTM usage

--- assets/js/lib/analytics/index.js
+++ assets/js/lib/analytics/index.js
@@ -1,24 +1,59 @@
 import posthog from 'posthog-js';
 import { v5 as uuidv5 } from 'uuid';
+import { get, has, noop } from 'lodash';
 
+import { logError } from '@lib/log';
 import { getFromConfig } from '@lib/config/config';
 
+const DEFAULT_OPTS = {
+  api_host: 'https://eu.posthog.com',
+  opt_out_capturing_by_default: true,
+  capture_pageview: false,
+  disable_persistence: true,
+};
+
 const analyticsEnabledConfig = getFromConfig('analyticsEnabled');
-const analyticsKey = getFromConfig('analyticsKey');
-const analyticsUrl = getFromConfig('analyticsUrl');
 const installationID = getFromConfig('installationID');
 
-if (analyticsEnabledConfig) {
-  posthog.init(analyticsKey, {
-    api_host: analyticsUrl,
-    opt_out_capturing_by_default: true,
-    capture_pageview: false,
-    disable_persistence: true,
-  });
-}
+// window.posthogConfig is loaded from GTM snippet
+const getGtmConfig = () => window.posthogConfig;
 
 // TODO: Remove this when this feature is ready for production
 export const getAnalyticsEnabledConfig = () => analyticsEnabledConfig;
+
+export const isLoaded = () => posthog.__loaded;
+
+// initialize posthog waiting until the required data is loaded from GTM
+export const init = (loadedFunc = noop) => {
+  if (!analyticsEnabledConfig) {
+    return;
+  }
+
+  if (isLoaded()) {
+    return;
+  }
+
+  const gtmConfig = getGtmConfig();
+
+  if (gtmConfig) {
+    if (!has(gtmConfig, 'apiKey')) {
+      logError('cannot load apiKey value from GTM');
+      return;
+    }
+
+    const analyticsKey = get(gtmConfig, 'apiKey');
+    const opts = { ...DEFAULT_OPTS, ...get(gtmConfig, 'config', {}) };
+
+    posthog.init(analyticsKey, {
+      ...opts,
+      loaded: (_posthog) => {
+        loadedFunc();
+      },
+    });
+  } else {
+    setTimeout(init, 100);
+  }
+};
 
 export const identify = (analyticsEnabled, userID) => {
   if (!analyticsEnabled) {
@@ -53,5 +88,6 @@ export const reset = () => {
   if (!analyticsEnabledConfig) {
     return;
   }
+
   posthog.reset();
 };
