1b80ecd54306dc1d6ff0fabf81481b34fbc712a7
Antonio Gamez Diaz <antonio.gamez@suse.com>
2026-01-12 20:53:55 +0100

    Fix race condition
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- test/e2e/cypress/e2e/saptune_details.cy.js
+++ test/e2e/cypress/e2e/saptune_details.cy.js
@@ -3,21 +3,23 @@ import * as saptuneDetailsPage from '../pageObject/saptune_details_po';
 describe('Saptune Details page', () => {
   before(() => {
     saptuneDetailsPage.preloadTestData();
-    saptuneDetailsPage.visit();
   });
 
   it('should render saptune details not found if saptune version is unsupported', () => {
     saptuneDetailsPage.loadSaptuneUnsupportedScenario();
+    saptuneDetailsPage.visit();
     saptuneDetailsPage.saptuneNotFoundLabelIsDisplayed();
   });
 
   it('should render details not found if saptune is not installed', () => {
     saptuneDetailsPage.loadSaptuneUninstalledScenario();
+    saptuneDetailsPage.visit();
     saptuneDetailsPage.saptuneNotFoundLabelIsDisplayed();
   });
 
   it('should render versions list view when saptune is not tuning correctly', () => {
     saptuneDetailsPage.loadSaptuneNotTunedScenario();
+    saptuneDetailsPage.visit();
     saptuneDetailsPage.hasExpectedPackageVersion();
     saptuneDetailsPage.hasExpectedConfiguredVersion();
     saptuneDetailsPage.hasExpectedTuning();
@@ -25,6 +27,7 @@ describe('Saptune Details page', () => {
 
   it('should render each part of saptune services status with correct content', () => {
     saptuneDetailsPage.loadSaptunePassingScenario();
+    saptuneDetailsPage.visit();
     saptuneDetailsPage.hasExpectedServiceStatus();
     saptuneDetailsPage.hasExpectedSapConf();
     saptuneDetailsPage.hasExpectedTunedService();
@@ -32,6 +35,7 @@ describe('Saptune Details page', () => {
 
   it('should render correctly a tuned system', () => {
     saptuneDetailsPage.loadSaptuneCompliantScenario();
+    saptuneDetailsPage.visit();
     saptuneDetailsPage.hasExpectedEnabledSolution();
     saptuneDetailsPage.hasExpectedAppliedSolution();
     saptuneDetailsPage.hasExpectedEnabledNotes();
