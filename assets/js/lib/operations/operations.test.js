9b3b5b9e3da33ecc42037f6e6c0bdd400d41a3a4
arbulu89 <xarbulu@suse.com>
2026-01-19 09:11:23 +0100

    Add new operation constants

--- assets/js/lib/operations/operations.test.js
+++ assets/js/lib/operations/operations.test.js
@@ -16,6 +16,7 @@ describe('operations', () => {
     ${'saptune_solution_apply'}     | ${'Apply Saptune solution'}
     ${'saptune_solution_change'}    | ${'Change Saptune solution'}
     ${'cluster_maintenance_change'} | ${'Cluster maintenance change'}
+    ${'cluster_resource_refresh'}   | ${'Refresh cluster resources'}
     ${'sap_instance_start'}         | ${'SAP instance start'}
     ${'sap_instance_stop'}          | ${'SAP instance stop'}
     ${'sap_system_start'}           | ${'SAP system start'}
@@ -47,6 +48,7 @@ describe('operations', () => {
     ${'cluster_maintenance_change'} | ${'Maintenance change'}
     ${'cluster_host_start'}         | ${'Start cluster host'}
     ${'cluster_host_stop'}          | ${'Stop cluster host'}
+    ${'cluster_resource_refresh'}   | ${'Refresh resources'}
     ${'reboot'}                     | ${'Reboot host'}
   `(`should return the operation $operation title`, ({ operation, title }) => {
     expect(getOperationTitle(operation)).toBe(title);
@@ -58,6 +60,7 @@ describe('operations', () => {
     ${'saptuneapplysolution@v1'}     | ${'saptune_solution_apply'}
     ${'saptunechangesolution@v1'}    | ${'saptune_solution_change'}
     ${'clustermaintenancechange@v1'} | ${'cluster_maintenance_change'}
+    ${'clusterresourcerefresh@v1'}   | ${'cluster_resource_refresh'}
     ${'sapinstancestart@v1'}         | ${'sap_instance_start'}
     ${'sapinstancestop@v1'}          | ${'sap_instance_stop'}
     ${'sapsystemstart@v1'}           | ${'sap_system_start'}
@@ -82,6 +85,7 @@ describe('operations', () => {
     ${'saptune_solution_apply'}     | ${'host'}
     ${'saptune_solution_change'}    | ${'host'}
     ${'cluster_maintenance_change'} | ${'cluster'}
+    ${'cluster_resource_refresh'}   | ${'cluster'}
     ${'sap_instance_start'}         | ${'application_instance'}
     ${'sap_instance_stop'}          | ${'application_instance'}
     ${'sap_system_start'}           | ${'sap_system'}
