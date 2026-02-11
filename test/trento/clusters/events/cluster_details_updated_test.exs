1cfef5277ddfcab751d2bccfbbd651ede4339912
arbulu89 <xarbulu@suse.com>
2026-02-11 16:08:02 +0100

    Add cluster state in events

---
+++ test/trento/clusters/events/cluster_details_updated_test.exs
@@ -0,0 +1,17 @@
+defmodule Trento.Cluster.Events.ClusterDetailsUpdatedTest do
+  use Trento.AggregateCase, aggregate: Trento.Hosts.Host, async: true
+
+  alias Trento.Clusters.Events.ClusterDetailsUpdated
+
+  describe "ClusterDetailsUpdated event upcasting" do
+    test "should upcast ClusterDetailsUpdated event properly from version 1" do
+      assert %ClusterDetailsUpdated{
+               version: 2,
+               state: "unknown"
+             } =
+               %{}
+               |> ClusterDetailsUpdated.upcast(%{})
+               |> ClusterDetailsUpdated.new!()
+    end
+  end
+end
