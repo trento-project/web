093d2aba277da10fd8b3344a98021e26408800d1
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2026-02-16 16:03:11 +0100

    Fix warning

--- lib/trento_web/gettext.ex
+++ lib/trento_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule TrentoWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :trento
+  use Gettext.Backend, otp_app: :trento
 end
