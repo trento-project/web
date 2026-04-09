731691ba39ab7c0100edbe886c3d7f63ce99f529
Antonio Gamez Diaz <antonio.gamez@suse.com>
2026-04-09 17:56:35 +0200

    Add SearchableSelect component
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

---
+++ assets/js/common/SearchableSelect/SearchableSelect.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import classNames from 'classnames';
+import MultiSelect from '@common/MultiSelect';
+
+const defaultFilterOption = (option, rawInput) => {
+  if (!rawInput) return true;
+  const input = rawInput.toLowerCase();
+  return (
+    option.data.searchLabel?.toLowerCase().includes(input) ||
+    option.data.label.toLowerCase().includes(input)
+  );
+};
+
+function SearchableSelect({
+  value,
+  options,
+  onChange,
+  disabled = false,
+  className = '',
+  isClearable = false,
+  placeholder,
+  noOptionsMessage,
+  filterOption = defaultFilterOption,
+}) {
+  const selectedOption = options.find((opt) => opt.value === value);
+
+  return (
+    <MultiSelect
+      options={options}
+      value={selectedOption}
+      isMulti={false}
+      onChange={(option) => onChange(option?.value)}
+      disabled={disabled}
+      isClearable={isClearable}
+      isSearchable
+      className={classNames('text-sm', className)}
+      placeholder={placeholder}
+      noOptionsMessage={noOptionsMessage}
+      filterOption={filterOption}
+    />
+  );
+}
+
+export default SearchableSelect;
