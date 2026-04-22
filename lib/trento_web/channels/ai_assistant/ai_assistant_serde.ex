3c6f2d98256254625fc65757b7d638b868151aa1
Gagandeep Bhatia <gagandeep.bhatia@suse.com>
2026-04-22 10:44:36 +0200

    Implements ag-ui event envelope (#4214)
    ...support by introducing serde capabilities

--- lib/trento_web/channels/ai_assistant/ai_assistant_serde.ex
+++ lib/trento_web/channels/ai_assistant/ai_assistant_serde.ex
@@ -3,3 +3,20 @@ defimpl Jason.Encoder, for: AgenticRuntime.Conversations.DisplayMessage do
     Jason.Encode.map(value.content, opts)
   end
 end
+
+defimpl Jason.Encoder, for: AgUi.Core.Events.RunStarted do
+  def encode(value, _opts) do
+    AgUi.Encoder.EventEncoder.encode(value)
+  end
+end
+
+defimpl Jason.Encoder, for: AgUi.Core.Events.RunFinished do
+  def encode(value, _opts) do
+    AgUi.Encoder.EventEncoder.encode(value)
+  end
+end
+defimpl Jason.Encoder, for: AgUi.Core.Events.TextMessageContent do
+  def encode(value, _opts) do
+    AgUi.Encoder.EventEncoder.encode(value)
+  end
+end
