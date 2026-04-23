c16c0c3011f9235f3b2f6c13c15134afd799b04c
arbulu89 <xarbulu@suse.com>
2026-04-23 11:28:16 +0200

    Update SapSystemLink to use constants for type

--- assets/js/common/SapSystemLink/SapSystemLink.test.jsx
+++ assets/js/common/SapSystemLink/SapSystemLink.test.jsx
@@ -4,14 +4,15 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { renderWithRouter } from '@lib/test-utils';
 import { sapSystemFactory } from '@lib/test-utils/factories';
+import { APPLICATION_TYPE, DATABASE_TYPE } from '@lib/model/sapSystems';
 import SapSystemLink from './SapSystemLink';
 
 describe('SapSystemLink', () => {
-  it('renders Link when sapSystemId and systemType are provided', () => {
+  it('renders database link when sapSystemId and systemType are provided', () => {
     const { id, sid } = sapSystemFactory.build();
 
     renderWithRouter(
-      <SapSystemLink systemType="databases" sapSystemId={id}>
+      <SapSystemLink systemType={DATABASE_TYPE} sapSystemId={id}>
         {sid}
       </SapSystemLink>
     );
@@ -22,18 +23,51 @@ describe('SapSystemLink', () => {
     expect(sapSystemLinkElement).toHaveAttribute('href', `/databases/${id}`);
   });
 
-  it('renders tooltip when sapSystemId or systemType are not provided', async () => {
-    const user = userEvent.setup();
-    const { sid } = sapSystemFactory.build();
+  it('renders SAP system link when sapSystemId and systemType are provided', () => {
+    const { id, sid } = sapSystemFactory.build();
 
-    render(<SapSystemLink>{sid}</SapSystemLink>);
+    renderWithRouter(
+      <SapSystemLink systemType={APPLICATION_TYPE} sapSystemId={id}>
+        {sid}
+      </SapSystemLink>
+    );
 
-    const sidElement = screen.getByText(sid);
+    const sapSystemLinkElement = screen.getByRole('link', { sid });
 
-    await act(async () => user.hover(sidElement));
-
-    expect(
-      screen.getByText('System currently not registered.')
-    ).toBeInTheDocument();
+    expect(sapSystemLinkElement).toBeInTheDocument();
+    expect(sapSystemLinkElement).toHaveAttribute('href', `/sap_systems/${id}`);
   });
+
+  it.each([
+    {
+      systemType: APPLICATION_TYPE,
+      expectedTooltip: 'SAP system currently not registered',
+    },
+    {
+      systemType: DATABASE_TYPE,
+      expectedTooltip: 'HANA database currently not registered',
+    },
+    {
+      systemType: 'unknown',
+      expectedTooltip: 'System currently not registered',
+    },
+    {
+      systemType: undefined,
+      expectedTooltip: 'System currently not registered',
+    },
+  ])(
+    `renders $systemType tooltip when sapSystemId or systemType are not provided`,
+    async ({ systemType, expectedTooltip }) => {
+      const user = userEvent.setup();
+      const { sid } = sapSystemFactory.build();
+
+      render(<SapSystemLink systemType={systemType}>{sid}</SapSystemLink>);
+
+      const sidElement = screen.getByText(sid);
+
+      await act(async () => user.hover(sidElement));
+
+      expect(screen.getByText(expectedTooltip)).toBeInTheDocument();
+    }
+  );
 });
