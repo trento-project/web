ce142ccbaf2ff104c3c7745c537294445347d10f
vicente <vicente.ruiz@suse.com>
2026-04-23 20:47:10 +0200

    fix trigger email

--- test/e2e/cypress/pageObject/alerting_po.js
+++ test/e2e/cypress/pageObject/alerting_po.js
@@ -40,7 +40,7 @@ export const emailIsReceived = (type) =>
     .then((result) => cy.wrap(result.length).should('equal', 1));
 
 export const triggerHostAlertingEmail = () => {
-  cy.task('startAgentHeartbeat', ['9cd46919-5f19-59aa-993e-cf3736c71053']);
+  basePage.startAgentsHeartbeat(['9cd46919-5f19-59aa-993e-cf3736c71053']);
   return basePage.stopAgentsHeartbeat();
 };
 
