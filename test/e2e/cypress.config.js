a24f54f4f3ca36ff37fdacbe035b17cb698fba4b
vicente <vicente.ruiz@suse.com>
2026-04-27 08:47:20 +0200

    remove logic for commands timeout

--- test/e2e/cypress.config.js
+++ test/e2e/cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress');
 
 const DEMO = 'demo';
 const DEV = 'dev';
-const PROD = 'prod';
 
 const calculateWandaUrl = (config) => {
   if (config.env.wandaUrl) return config.env.wandaUrl;
@@ -11,25 +10,10 @@ const calculateWandaUrl = (config) => {
     : `${config.baseUrl}/wanda`;
 };
 
-const calculateTimeouts = (config) => {
-  if (config.env.web_mode === PROD) {
-    return {
-      defaultCommandTimeout: 100000,
-      requestTimeout: 80000,
-      responseTimeout: 80000,
-    };
-  }
-
-  return {
-    defaultCommandTimeout: 10000,
-    requestTimeout: 5000,
-    responseTimeout: 30000,
-  };
-};
-
 module.exports = defineConfig({
   viewportWidth: 1366,
   viewportHeight: 768,
+  defaultCommandTimeout: 10000,
   env: {
     heartbeat_interval: 5000,
     project_root: '../..',
@@ -44,10 +28,6 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     async setupNodeEvents(on, config) {
-      const timeouts = calculateTimeouts(config);
-      config.defaultCommandTimeout = timeouts.defaultCommandTimeout;
-      config.requestTimeout = timeouts.requestTimeout;
-      config.responseTimeout = timeouts.responseTimeout;
       config.env.wandaUrl = calculateWandaUrl(config);
       return require('./cypress/plugins/index.js')(on, config);
     },
