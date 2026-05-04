512f2465f635aed4e10abe1edf693a8b437518d6
Xabier Arbulu Insausti <xarbulu@suse.com>
2026-05-04 11:18:17 +0200

    Fix polyfilled warning in frontend UT (#4256)
    * Add jsdom-testing-mocks dev dependency
    
    * Mock animations in test setup

--- assets/jest.config.js
+++ assets/jest.config.js
@@ -171,10 +171,10 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['./setupTests.js'],
+  // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['./setupTests.js'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
