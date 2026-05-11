111c88dde132d180cb90a09af620b291768077ed
Nelson Kopliku <nelson.kopliku@suse.com>
2026-05-11 18:00:05 +0200

    re-wire assistant channel

--- lib/trento_web/channels/ai_assistant/ai_assistant_serde.ex
+++ lib/trento_web/channels/ai_assistant/ai_assistant_serde.ex
@@ -1,5 +1,26 @@
+# SPDX-FileCopyrightText: SUSE LLC
+# SPDX-License-Identifier: Apache-2.0
+
 defimpl Jason.Encoder, for: AgenticRuntime.Conversations.DisplayMessage do
   def encode(value, opts) do
     Jason.Encode.map(value.content, opts)
+  end
+end
+
+defimpl Jason.Encoder, for: AgUi.Core.Events.RunStarted do
+  def encode(value, _opts) do
+    AgUi.Encoder.EventEncoder.encode(value)
+  end
+end
+
+defimpl Jason.Encoder, for: AgUi.Core.Events.RunFinished do
+  def encode(value, _opts) do
+    AgUi.Encoder.EventEncoder.encode(value)
+  end
+end
+
+defimpl Jason.Encoder, for: AgUi.Core.Events.TextMessageContent do
+  def encode(value, _opts) do
+    AgUi.Encoder.EventEncoder.encode(value)
   end
 end
