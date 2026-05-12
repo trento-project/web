c769a4522c5a3a154157a39a5b803c32eb906fd2
arbulu89 <xarbulu@suse.com>
2026-05-12 15:01:31 +0200

    Use static image as doughnut

--- assets/js/common/FilesystemCharts/SwapUsageChart.jsx
+++ assets/js/common/FilesystemCharts/SwapUsageChart.jsx
@@ -14,6 +14,7 @@ import {
 } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import { formatBytes } from '@lib/charts';
+import SwapNotConfigured from '@static/empty-doughnut-chart.svg';
 
 ChartJS.register(CategoryScale, ArcElement, Title, Tooltip, Legend);
 
@@ -23,34 +24,21 @@ const availableColor = 'rgba(178, 186, 186, 0.7)';
 function SwapUsageChart({ availBytes, usedBytes, totalBytes, className }) {
   const isSwapConfigured = Boolean(totalBytes);
 
-  const swapData = isSwapConfigured
-    ? {
-        labels: ['Used', 'Available'],
-        datasets: [
-          {
-            data: [usedBytes, availBytes],
-            backgroundColor: [usedColor, availableColor],
-            borderColor: [usedColor, availableColor],
-            borderWidth: 1,
-          },
-        ],
-      }
-    : {
-        labels: [],
-        datasets: [
-          {
-            data: [1],
-            backgroundColor: [availableColor],
-            borderColor: [availableColor],
-            borderWidth: 0,
-          },
-        ],
-      };
+  const swapData = {
+    labels: ['Used', 'Available'],
+    datasets: [
+      {
+        data: [usedBytes, availBytes],
+        backgroundColor: [usedColor, availableColor],
+        borderColor: [usedColor, availableColor],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   const swapChartOptions = {
     responsive: true,
     maintainAspectRatio: false,
-    ...(!isSwapConfigured && { events: [] }),
     plugins: {
       legend: {
         position: 'bottom',
@@ -76,20 +64,29 @@ function SwapUsageChart({ availBytes, usedBytes, totalBytes, className }) {
       )}
     >
       <h2 className="font-bold text-center text-xl">Swap Utilization</h2>
-      <div className="flex items-center justify-center pt-6 h-[300px]">
-        <div className="h-[92%]">
-          <Doughnut data={swapData} options={swapChartOptions} />
-        </div>
-        {isSwapConfigured && (
+      {isSwapConfigured ? (
+        <div className="flex items-center justify-center pt-6 h-[300px]">
+          <div className="h-[92%]">
+            <Doughnut data={swapData} options={swapChartOptions} />
+          </div>
+
           <div className="ml-4 align-self-center">
             <p className="text-3xl font-bold">{formatBytes(totalBytes)}</p>
           </div>
-        )}
-      </div>
-      {!isSwapConfigured && (
-        <div className="flex items-center justify-center pt-3 mb-3">
-          <p className="text-2xl font-bold">Swap Not Configured</p>
         </div>
+      ) : (
+        <>
+          <div className="flex items-center justify-center pt-6 h-[300px]">
+            <img
+              className="h-[92%] inline-block align-middle"
+              alt="swap-not-configured"
+              src={SwapNotConfigured}
+            />
+          </div>
+          <div className="flex items-center justify-center pt-3 mb-3">
+            <p className="text-2xl font-bold">Swap Not Configured</p>
+          </div>
+        </>
       )}
     </div>
   );
