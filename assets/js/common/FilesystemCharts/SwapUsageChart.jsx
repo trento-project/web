089586b024e5905ea033ee44ed4a2a21eb898ffe
Xabier Arbulu Insausti <xarbulu@suse.com>
2026-05-13 08:55:28 +0200

    Display empty swap chart when data is not available (#4289)
    * Display empty swap chart when data is not available
    
    * Use static image as doughnut

--- assets/js/common/FilesystemCharts/SwapUsageChart.jsx
+++ assets/js/common/FilesystemCharts/SwapUsageChart.jsx
@@ -14,17 +14,23 @@ import {
 } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import { formatBytes } from '@lib/charts';
+import SwapNotConfigured from '@static/empty-doughnut-chart.svg';
 
 ChartJS.register(CategoryScale, ArcElement, Title, Tooltip, Legend);
 
+const usedColor = 'rgba(88, 207, 155, 1)';
+const availableColor = 'rgba(178, 186, 186, 0.7)';
+
 function SwapUsageChart({ availBytes, usedBytes, totalBytes, className }) {
+  const isSwapConfigured = Boolean(totalBytes);
+
   const swapData = {
     labels: ['Used', 'Available'],
     datasets: [
       {
         data: [usedBytes, availBytes],
-        backgroundColor: ['rgba(88, 207, 155, 1)', 'rgba(178, 186, 186, 0.7)'],
-        borderColor: ['rgba(88, 207, 155, 1)', 'rgba(178, 186, 186, 0.7)'],
+        backgroundColor: [usedColor, availableColor],
+        borderColor: [usedColor, availableColor],
         borderWidth: 1,
       },
     ],
@@ -50,8 +56,6 @@ function SwapUsageChart({ availBytes, usedBytes, totalBytes, className }) {
     },
   };
 
-  const renderedTotal = totalBytes ? formatBytes(totalBytes) : 'N/A';
-
   return (
     <div
       className={classNames(
@@ -60,14 +64,30 @@ function SwapUsageChart({ availBytes, usedBytes, totalBytes, className }) {
       )}
     >
       <h2 className="font-bold text-center text-xl">Swap Utilization</h2>
-      <div className="flex items-center justify-center pt-6 h-[300px]">
-        <div className="h-[92%]">
-          <Doughnut data={swapData} options={swapChartOptions} />
+      {isSwapConfigured ? (
+        <div className="flex items-center justify-center pt-6 h-[300px]">
+          <div className="h-[92%]">
+            <Doughnut data={swapData} options={swapChartOptions} />
+          </div>
+
+          <div className="ml-4 align-self-center">
+            <p className="text-3xl font-bold">{formatBytes(totalBytes)}</p>
+          </div>
         </div>
-        <div className="ml-4 align-self-center">
-          <p className="text-3xl font-bold">{renderedTotal}</p>
-        </div>
-      </div>
+      ) : (
+        <>
+          <div className="flex items-center justify-center pt-6 h-[300px]">
+            <img
+              className="h-[92%] inline-block align-middle"
+              alt="swap-not-configured"
+              src={SwapNotConfigured}
+            />
+          </div>
+          <div className="flex items-center justify-center pt-3 mb-3">
+            <p className="text-2xl font-bold">Swap Not Configured</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
