cc2ae42b2eb3a73f51fe518e26a941df16f92920
Nelson Kopliku <nelson.kopliku@suse.com>
2026-05-15 14:08:19 +0200

    cleanup serialization

--- lib/trento_web/channels/ai_assistant/ai_assistant_serde.ex
+++ lib/trento_web/channels/ai_assistant/ai_assistant_serde.ex
@@ -1,26 +1,8 @@
 # SPDX-FileCopyrightText: SUSE LLC
 # SPDX-License-Identifier: Apache-2.0
 
-defimpl Jason.Encoder, for: AgenticRuntime.Conversations.DisplayMessage do
-  def encode(value, opts) do
-    Jason.Encode.map(value.content, opts)
-  end
-end
-
-# defimpl Jason.Encoder, for: AgUi.Core.Events.RunStarted do
-#   def encode(value, _opts) do
-#     AgUi.Encoder.EventEncoder.encode(value)
-#   end
-# end
-
-# defimpl Jason.Encoder, for: AgUi.Core.Events.RunFinished do
-#   def encode(value, _opts) do
-#     AgUi.Encoder.EventEncoder.encode(value)
-#   end
-# end
-
-# defimpl Jason.Encoder, for: AgUi.Core.Events.TextMessageContent do
-#   def encode(value, _opts) do
-#     AgUi.Encoder.EventEncoder.encode(value)
+# defimpl Jason.Encoder, for: AgenticRuntime.Conversations.DisplayMessage do
+#   def encode(value, opts) do
+#     Jason.Encode.map(value.content, opts)
 #   end
 # end
