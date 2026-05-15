aa3ed1e247dd17d1be9e3bb547ac5741136055ed
Nelson Kopliku <nelson.kopliku@suse.com>
2026-05-15 15:38:00 +0200

    move scope, remove serde

--- lib/trento_web/channels/ai_assistant/ai_assistant_serde.ex
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: SUSE LLC
-# SPDX-License-Identifier: Apache-2.0
-
-# defimpl Jason.Encoder, for: AgenticRuntime.Conversations.DisplayMessage do
-#   def encode(value, opts) do
-#     Jason.Encode.map(value.content, opts)
-#   end
-# end
