62dacf85a230a0fdf361d6cea9cfc6974e93bb13
vicente <vicente.ruiz@suse.com>
2026-05-19 12:01:48 +0200

    use expose properly in workflows

--- test/e2e/cypress.config.js
+++ test/e2e/cypress.config.js
@@ -6,38 +6,6 @@ const { defineConfig } = require('cypress');
 const DEMO = 'demo';
 const DEV = 'dev';
 
-// Whitelist of environment variables allowed to override 'expose' configuration
-const PUBLIC_CONFIG_KEYS = [
-  'ALERTING_DB_TESTS',
-  'ALERTING_TESTS',
-  'REAL_CLUSTER_TESTS',
-  'SSO_INTEGRATION_TESTS',
-  'SSO_TYPE',
-  'idp_url',
-  'photofinish_binary',
-  'project_root',
-  'wandaUrl',
-  'wanda_mode',
-  'web_mode',
-];
-
-// Filters Cypress.env to only include allowed keys from the whitelist
-const getPublicEnvOverrides = (env = {}) => {
-  const overrides = {};
-
-  PUBLIC_CONFIG_KEYS.forEach((key) => {
-    if (env[key] !== undefined) overrides[key] = env[key];
-  });
-
-  return overrides;
-};
-
-// Merges default 'expose' config with environment overrides (overrides take precedence)
-const exposePublicConfig = (config) => ({
-  ...config.expose,
-  ...getPublicEnvOverrides(config.env),
-});
-
 const calculateWandaUrl = (config) => {
   const { wandaUrl, wanda_mode: wandaMode } = config.expose;
 
@@ -70,7 +38,6 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     async setupNodeEvents(on, config) {
-      config.expose = exposePublicConfig(config);
       config.expose.wandaUrl = calculateWandaUrl(config);
       return require('./cypress/plugins/index.js')(on, config);
     },
