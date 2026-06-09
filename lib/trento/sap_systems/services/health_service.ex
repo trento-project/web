eac8cac47be7c03f1b0a7d2d62d93b4d9f5a6dec
arbulu89 <xarbulu@suse.com>
2026-06-09 09:58:55 +0200

    Add new sap systems health service

---
+++ lib/trento/sap_systems/services/health_service.ex
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: SUSE LLC
+# SPDX-License-Identifier: Apache-2.0
+
+defmodule Trento.SapSystems.Services.HealthService do
+  @moduledoc """
+  Provides SAP system/Database instances health related functions.
+
+  Some of the functions are used to handle old SAP/Database instances "health" field,
+  now renamed and updated to "status" field.
+  """
+
+  require Trento.Enums.Health, as: Health
+  require Trento.SapSystems.Enums.Status, as: Status
+
+  @spec derive_health_from_status(Status.t()) :: Health.t()
+  def derive_health_from_status(Status.green()), do: Health.passing()
+  def derive_health_from_status(Status.yellow()), do: Health.warning()
+  def derive_health_from_status(Status.red()), do: Health.critical()
+  def derive_health_from_status(Status.gray()), do: Health.unknown()
+
+  # Legacy "health" field handling functions
+
+  @doc """
+  Converts legacy instance health into new status.
+  To be used in events upcast function.
+  """
+  @spec health_to_status(String.t()) :: Status.t()
+  def health_to_status("passing"), do: Status.green()
+  def health_to_status("warning"), do: Status.yellow()
+  def health_to_status("critical"), do: Status.red()
+  def health_to_status("unknown"), do: Status.gray()
+
+  @spec add_deprecated_health(map()) :: map()
+  def add_deprecated_health(%{status: Status.green()} = instance),
+    do: Map.put(instance, :health, Health.passing())
+
+  def add_deprecated_health(%{status: Status.yellow()} = instance),
+    do: Map.put(instance, :health, Health.warning())
+
+  def add_deprecated_health(%{status: Status.red()} = instance),
+    do: Map.put(instance, :health, Health.critical())
+
+  def add_deprecated_health(%{status: Status.gray()} = instance),
+    do: Map.put(instance, :health, Health.unknown())
+end
