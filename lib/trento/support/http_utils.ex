ab38e3c672fcbc02dc8314f0ff993287bab124a4
Nelson Kopliku <nelson.kopliku@suse.com>
2026-06-11 11:46:09 +0200

    Hndle nil ports

--- lib/trento/support/http_utils.ex
+++ lib/trento/support/http_utils.ex
@@ -42,6 +42,7 @@ defmodule Trento.Support.HttpUtils do
 
   def request_origin(_), do: nil
 
+  defp detect_port_part({_scheme, nil}), do: ""
   defp detect_port_part({scheme, 80}) when scheme in [:http, "http"], do: ""
   defp detect_port_part({scheme, 443}) when scheme in [:https, "https"], do: ""
   defp detect_port_part({_, port}), do: ":#{port}"
