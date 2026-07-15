0f1afd87a44ee16c1a5fcdd43ae692dc0c1daea9
Emanuele De Cupis <emanuele.decupis@suse.com>
2026-07-15 09:24:57 +0200

    Relax gcp image metadata requirement (#4489)
    * Adjust gcp landscape photofinish scenarios
    
    * Add gcp-landscape scenarios to demo scenarios
    
    * gracefully handle missing `image` from gcp metadata in cloud discovery
    
    * Improve test descriptions
    
    * Make GcpProviderDataV1.image nullable in openapi
    
    Co-authored-by: Nelson Kopliku <nelson.kopliku@suse.com>

--- test/trento_web/views/v1/host_view_json_test.exs
+++ test/trento_web/views/v1/host_view_json_test.exs
@@ -45,4 +45,50 @@ defmodule TrentoWeb.V1.HostJSONTest do
 
     assert Enum.all?(ignored_properties, &(!Map.has_key?(rendered_map, &1)))
   end
+
+  test "should handle gcp host metadata" do
+    gcp_metadata = %{
+      "disk_number" => 4,
+      "image" => "sles-15-sp1-sap-byos-v20220126",
+      "instance_name" => "vmhana01",
+      "machine_type" => "n1-highmem-8",
+      "network" => "network",
+      "project_id" => "123456",
+      "zone" => "europe-west1-b"
+    }
+
+    hosts = [
+      build(:host,
+        sles_subscriptions: build_list(1, :sles_subscription),
+        provider: :gcp,
+        provider_data: gcp_metadata
+      ),
+      build(:host,
+        sles_subscriptions: build_list(1, :sles_subscription),
+        provider: :gcp,
+        provider_data: nil
+      ),
+      build(:host,
+        sles_subscriptions: build_list(1, :sles_subscription),
+        provider: :gcp,
+        provider_data: Map.put(gcp_metadata, "image", nil)
+      )
+    ]
+
+    assert [
+             %{
+               provider_data: %{
+                 "image" => "sles-15-sp1-sap-byos-v20220126"
+               }
+             },
+             %{
+               provider_data: nil
+             },
+             %{
+               provider_data: %{
+                 "image" => nil
+               }
+             }
+           ] = HostJSON.hosts(%{hosts: hosts})
+  end
 end
