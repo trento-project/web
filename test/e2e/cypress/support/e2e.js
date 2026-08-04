6e10fdd7db280e7bd8100d5920d86530535ba67f
Vicente <58304150+vicenteqa@users.noreply.github.com>
2026-08-04 08:05:31 +0200

    Fixes on test suite when targeting a remote instance (#4537)
    * reset checks & run only if env is demo
    
    * remove logic to deregister prod host & remove ignore prometheus requests exceptions on sles16 targets

--- test/e2e/cypress/support/e2e.js
+++ test/e2e/cypress/support/e2e.js
@@ -17,10 +17,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import {
-  apiLoginAndCreateSession,
-  apiDeregisterProdHost,
-} from '../pageObject/base_po';
+import { apiLoginAndCreateSession } from '../pageObject/base_po';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
@@ -29,15 +26,5 @@ import {
 // eslint-disable-next-line mocha/no-top-level-hooks
 before(() => {
   Cypress.session.clearAllSavedSessions();
-  if (!Cypress.expose('SSO_INTEGRATION_TESTS')) {
-    apiLoginAndCreateSession();
-  }
-
-  // This is required to not break cypress tests when running against a prod instance (which installs a real agent)
-  if (Cypress.config().baseUrl.includes('target')) apiDeregisterProdHost();
-});
-
-// This is needed because requests that depend on Prometheus in a real environment return a 500 in SLES16, this can be removed once TRNT-4344 is done.
-Cypress.on('uncaught:exception', () => {
-  if (Cypress.config().baseUrl.includes('target16sp0')) return false;
+  if (!Cypress.expose('SSO_INTEGRATION_TESTS')) apiLoginAndCreateSession();
 });
