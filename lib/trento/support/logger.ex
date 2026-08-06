eb5ebc9eeb55887c3825ddf5ea4c33b9db20d9e7
arbulu89 <xarbulu@suse.com>
2026-08-06 12:17:27 +0200

    Add logging parse_log_level helper function

---
+++ lib/trento/support/logger.ex
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: SUSE LLC
+# SPDX-License-Identifier: Apache-2.0
+
+defmodule Trento.Support.Logger do
+  @moduledoc """
+  Logging related helper functions
+  """
+
+  @doc """
+  Parses a string into a `t:Logger.level/0`.
+  """
+  @spec parse_log_level(term()) :: {:ok, Logger.level()} | {:error, :invalid_level}
+  def parse_log_level(level_str) when is_binary(level_str) do
+    level_atom = String.to_existing_atom(level_str)
+
+    if level_atom in Logger.levels() do
+      {:ok, level_atom}
+    else
+      {:error, :invalid_level}
+    end
+  rescue
+    ArgumentError -> {:error, :invalid_level}
+  end
+
+  def parse_log_level(_), do: {:error, :invalid_level}
+end
---
+++ test/trento/support/logger_test.exs
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: SUSE LLC
+# SPDX-License-Identifier: Apache-2.0
+
+defmodule Trento.Support.LoggerTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  alias Trento.Support.Logger
+
+  describe "parse_log_level/1" do
+    for level <- Elixir.Logger.levels() do
+      @level level
+
+      test "should parse the #{level} level" do
+        assert {:ok, @level} == Logger.parse_log_level(Atom.to_string(@level))
+      end
+    end
+
+    scenarios = [
+      %{name: "unknown level", value: "verbose"},
+      %{name: "existing atom which is not a level", value: "trento"},
+      %{name: "empty string", value: ""},
+      %{name: "nil", value: nil},
+      %{name: "atom", value: :info},
+      %{name: "integer", value: 1}
+    ]
+
+    for %{name: name} = scenario <- scenarios do
+      @scenario scenario
+
+      test "should not parse an invalid level - #{name}" do
+        %{value: value} = @scenario
+
+        assert {:error, :invalid_level} == Logger.parse_log_level(value)
+      end
+    end
+  end
+end
