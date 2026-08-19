c133681a8991e5c25a63b09040cbeac7adea3156
Kristiyan Kanchev <kristiyan.kanchev@suse.com>
2026-08-19 17:10:24 +0300

    Renamed to tsconfig.json for better vite support
    This removes unmaintained transitive dep

--- assets/vite.config.js
+++ assets/vite.config.js
@@ -2,15 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    // We use the `vite-tsconfig-paths` plugin because the
-    // `resolve/tsconfigPaths: true` builtin option doesn't work with
-    // `jsconfig.json` file. It can potentially be replaced it with
-    // `tsconfig.json` with `allowJs: true` option, but we should
-    // ensure that all LSP clients can work properly with that setup.
-    tsconfigPaths(),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
