6173e085889c5136bbb855d0bf6a502587f58808
trentobot <trentobot@suse.com>
2026-09-01 11:12:09 +0300

    Add runtime LOG_LEVEL option to change logging level (#4584) (#4690)
    * Add logging parse_log_level helper function
    
    * Get LOG_LEVEL env in runtime execution
    
    * Set our demo and pr env deployments with debug log level
    
    * Move helper functions to Config module
    
    * Add new env variable in systemd example file
    
    * Make code elixir 1.15 compatible
    
    * Use to_atom instead of to_existing_atom
    
    * Remove not needed prod log level
    
    (cherry picked from commit 0b3970ea3535f38efc5ed1ba24ffe7bd570ca579)
    
    Co-authored-by: Xabier Arbulu Insausti <xarbulu@suse.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -15,6 +15,18 @@ if config_env() in [:prod, :demo] do
   config :trento,
     admin_user: admin_user
 
+  log_levels = ~w(debug info warning error)
+  log_level = System.get_env("LOG_LEVEL", "info")
+
+  if log_level not in log_levels do
+    raise """
+    environment variable LOG_LEVEL is invalid.
+    Valid values are: #{Enum.join(log_levels, ", ")}
+    """
+  end
+
+  config :logger, level: String.to_atom(log_level)
+
   database_url =
     System.get_env("DATABASE_URL") ||
       raise """
