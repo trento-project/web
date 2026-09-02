4b0d5033b1abacf92ade147b455a2c148c6fea0c
Kristiyan Kanchev <kristiyan.kanchev@suse.com>
2026-09-02 12:15:14 +0300

    Remove ranch override dependency (#4703)
    (cherry picked from commit 6072fc5873355a05701b792d0d485f20d52f0266)

--- mix.exs
+++ mix.exs
@@ -78,8 +78,6 @@ defmodule Trento.MixProject do
       {:floki, ">= 0.36.2", only: :test},
       {:gettext, "~> 0.26"},
       {:gen_smtp, "~> 1.2.0"},
-      # see: https://github.com/pma/amqp/issues/231#issuecomment-2445049446
-      {:ranch, "~> 1.8.0", override: true},
       {:gen_rmq, github: "trento-project/trnt_gen_rmq", ref: "v5.0.1"},
       {:httpoison, "== 2.3.0"},
       {:jason, "~> 1.2"},
