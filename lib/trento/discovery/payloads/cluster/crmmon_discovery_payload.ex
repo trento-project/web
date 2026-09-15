0c93f8a2b5652d1ee6dde9c5103f261406cea24e
Antonio Gamez Diaz <antonio.gamez@suse.com>
2026-09-15 18:47:54 +0200

    Improve mandatory fields logic
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- lib/trento/discovery/payloads/cluster/crmmon_discovery_payload.ex
+++ lib/trento/discovery/payloads/cluster/crmmon_discovery_payload.ex
@@ -91,6 +91,15 @@ defmodule Trento.Discovery.Payloads.Cluster.CrmmonDiscoveryPayload do
       |> cast(attrs, fields())
       |> cast_embed(:node, with: &resource_node_changeset/2)
       |> validate_required_fields(@required_fields)
+      |> validate_orphaned_or_removed()
+    end
+
+    defp validate_orphaned_or_removed(changeset) do
+      if is_nil(get_field(changeset, :orphaned)) and is_nil(get_field(changeset, :removed)) do
+        add_error(changeset, :removed, "either orphaned or removed must be present")
+      else
+        changeset
+      end
     end
 
     defp resource_node_changeset(resource_node, attrs) do
