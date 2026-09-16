f1e67d15436ff3d81bfec2a1be2ab015bf23c537
arbulu89 <xarbulu@suse.com>
2026-09-16 10:39:22 +0200

    Remove checks filter storage from redux

--- assets/js/state/selectors/checksResultsFilters.js
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: SUSE LLC
-// SPDX-License-Identifier: Apache-2.0
-
-import { get } from 'lodash';
-
-export const getSelectedFilters =
-  (resourceID) =>
-  ({ checksResultsFilters }) =>
-    get(checksResultsFilters, resourceID, []);
